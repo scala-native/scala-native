0258ad6d8529e8b2be5173ac4cdda9f8450995f1
Denys Shabalin <denys.shabalin@epfl.ch>
2015-10-01 17:54:12 +0200

    Test case for overoptimistic dead code elimination

--- sandbox/Test.scala
+++ sandbox/Test.scala
@@ -1,7 +1,18 @@
 package test
 
-object Foo
+abstract class Abstract {
+  def foo: Int
+}
 
-class Bar {
-  def bar = Foo
+class Concrete extends Abstract {
+  def foo = 2
+}
+
+object Module {
+  private[this] var x: Abstract = _
+  def main = {
+    val x: Abstract = new Concrete
+    this.x = null
+    x.foo
+  }
 }
