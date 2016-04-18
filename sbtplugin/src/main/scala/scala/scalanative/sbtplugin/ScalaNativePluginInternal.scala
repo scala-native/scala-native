33fc2e8fb9f1e41a797e153aa31d6f596a565751
Denys Shabalin <denys.shabalin@epfl.ch>
2016-04-18 11:35:32 +0200

    Remove dotty support for now
    We don't have enough time to polish both before ScalaDays,
    and considering that dotty support depends on scalac support
    (as dotty can't compile std lib on its own yet) we need to
    finish scalac frontend first.
    
    Latest dotty wip is available at topic/dotty-support branch.

--- sbtplugin/src/main/scala/scala/scalanative/sbtplugin/ScalaNativePluginInternal.scala
+++ sbtplugin/src/main/scala/scala/scalanative/sbtplugin/ScalaNativePluginInternal.scala
@@ -12,104 +12,6 @@ object ScalaNativePluginInternal {
   private def cpToString(cp: Seq[File]): String =
     cpToStrings(cp).mkString(java.io.File.pathSeparator)
 
-  lazy val compileWithDottySettings =
-    inConfig(Compile)(inTask(compile)(Defaults.runnerTask) ++ Seq(
-      fork in compile := true,
-
-      scalacOptions ++= Seq("-nir", "-language:Scala2"),
-
-      compile := {
-        val inputs = (compileInputs in compile).value
-        import inputs.config._
-
-        val s = streams.value
-        val logger = s.log
-        val cacheDir = s.cacheDirectory
-
-        // Discover classpaths
-
-        val compilerClasspath = classpath.filter { entry =>
-          val path = entry.getAbsolutePath
-
-          path.contains("scala-compiler") ||
-          path.contains("org.scala-lang") ||
-          path.contains("org.scala-native")
-        }
-        val applicationClasspath = classpath
-
-        // List all my dependencies (recompile if any of these changes)
-
-        val allMyDependencies = classpath filterNot (_ == classesDirectory) flatMap { cpFile =>
-          if (cpFile.isDirectory) (cpFile ** "*.class").get
-          else Seq(cpFile)
-        }
-
-        // Compile
-
-        val cachedCompile = FileFunction.cached(cacheDir / "compile",
-            FilesInfo.lastModified, FilesInfo.exists) { dependencies =>
-
-          logger.info(
-              "Compiling %d Scala sources to %s..." format (
-              sources.size, classesDirectory))
-
-          if (classesDirectory.exists)
-            IO.delete(classesDirectory)
-          IO.createDirectory(classesDirectory)
-
-          val sourcesArgs = sources.map(_.getAbsolutePath()).toList
-
-          /* run.run() below in doCompile() will emit a call to its
-           * logger.info("Running dotty.tools.dotc.Main [...]")
-           * which we do not want to see. We use this patched logger to
-           * filter out that particular message.
-           */
-          val patchedLogger = new Logger {
-            def log(level: Level.Value, message: => String) = {
-              val msg = message
-              if (level != Level.Info ||
-                  !msg.startsWith("Running dotty.tools.dotc.Main"))
-                logger.log(level, msg)
-            }
-            def success(message: => String) = logger.success(message)
-            def trace(t: => Throwable) = logger.trace(t)
-          }
-
-          def doCompile(sourcesArgs: List[String]): Unit = {
-            val run = (runner in compile).value
-            val args =
-                options ++:
-                ("-classpath" :: cpToString(applicationClasspath) ::
-                "-d" :: classesDirectory.getAbsolutePath() ::
-                sourcesArgs)
-            run.run("dotty.tools.dotc.Main", compilerClasspath,
-                args, patchedLogger) foreach sys.error
-          }
-
-          // Work around the Windows limitation on command line length.
-          val isWindows =
-            System.getProperty("os.name").toLowerCase().indexOf("win") >= 0
-          if ((fork in compile).value && isWindows &&
-              (sourcesArgs.map(_.length).sum > 1536)) {
-            IO.withTemporaryFile("sourcesargs", ".txt") { sourceListFile =>
-              IO.writeLines(sourceListFile, sourcesArgs)
-              doCompile(List("@"+sourceListFile.getAbsolutePath()))
-            }
-          } else {
-            doCompile(sourcesArgs)
-          }
-
-          // Output is all files in classesDirectory
-          (classesDirectory ** AllPassFilter).get.toSet
-        }
-
-        cachedCompile((sources ++ allMyDependencies).toSet)
-
-        // We do not have dependency analysis when compiling externally
-        sbt.inc.Analysis.Empty
-      }
-    ))
-
   /** Compiles application nir to llvm ir. */
   private def compileNir(opts: NativeOpts): Unit = {
     IO.createDirectory(file(opts.outpath).getParentFile)
@@ -172,10 +74,4 @@ object ScalaNativePluginInternal {
       libraryDependencies += "org.scala-native" %% "nrt" % nir.Versions.current
     )
   }
-
-  lazy val dottyProjectSettings =
-    compileWithDottySettings ++ commonProjectSettings ++ Seq(
-      scalaVersion := "2.11.5",
-      libraryDependencies += "org.scala-lang" %% "dotty" % "0.1-SNAPSHOT" changing()
-    )
 }
