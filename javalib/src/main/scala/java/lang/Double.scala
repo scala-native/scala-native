f3c0757bda6f87871847ea2a8a9e29acd1eadd6f
Denys Shabalin <denys.shabalin@epfl.ch>
2016-04-21 10:35:38 +0200

    java.lang.{Float, Double} stubs

---
+++ javalib/src/main/scala/java/lang/Double.scala
@@ -0,0 +1,77 @@
+package java.lang
+
+final class Double(val doubleValue: scala.Double)
+    extends Number with Comparable[Double] {
+  def this(s: String) = this(Double.parseDouble(s))
+
+  @inline override def byteValue(): scala.Byte = doubleValue.toByte
+  @inline override def shortValue(): scala.Short = doubleValue.toShort
+  @inline def intValue(): scala.Int = doubleValue.toInt
+  @inline def longValue(): scala.Long = doubleValue.toLong
+  @inline def floatValue(): scala.Float = doubleValue.toFloat
+
+  override def equals(that: Any): scala.Boolean = that match {
+    case that: Double =>
+      val a = doubleValue
+      val b = that.doubleValue
+      (a == b) || (Double.isNaN(a) && Double.isNaN(b))
+
+    case _ =>
+      false
+  }
+
+  @inline override def hashCode(): Int =
+    Double.hashCode(doubleValue)
+
+  @inline override def compareTo(that: Double): Int =
+    Double.compare(doubleValue, that.doubleValue)
+
+  @inline override def toString(): String =
+    Double.toString(doubleValue)
+
+  @inline def isNaN(): scala.Boolean =
+    Double.isNaN(doubleValue)
+
+  @inline def isInfinite(): scala.Boolean =
+    Double.isInfinite(doubleValue)
+
+}
+
+object Double {
+  final val TYPE = classOf[scala.Double]
+  final val POSITIVE_INFINITY = 1.0 / 0.0
+  final val NEGATIVE_INFINITY = 1.0 / -0.0
+  final val NaN = 0.0 / 0.0
+  final val MAX_VALUE = scala.Double.MaxValue
+  final val MIN_VALUE = scala.Double.MinPositiveValue
+  final val MAX_EXPONENT = 1023
+  final val MIN_EXPONENT = -1022
+  final val SIZE = 64
+
+  @inline def valueOf(doubleValue: scala.Double): Double =
+    new Double(doubleValue)
+
+  @inline def valueOf(s: String): Double = valueOf(parseDouble(s))
+
+  def parseDouble(s: String): scala.Double = ???
+
+  @inline def toString(d: scala.Double): String =
+    "" + d
+
+  def compare(a: scala.Double, b: scala.Double): scala.Int = ???
+
+  @inline def isNaN(v: scala.Double): scala.Boolean =
+    v != v
+
+  @inline def isInfinite(v: scala.Double): scala.Boolean =
+    v == POSITIVE_INFINITY || v == NEGATIVE_INFINITY
+
+  @inline def longBitsToDouble(bits: scala.Long): scala.Double =
+    ???
+
+  @inline def doubleToLongBits(value: scala.Double): scala.Long =
+    ???
+
+  @inline def hashCode(value: scala.Double): scala.Int =
+    ???
+}
---
+++ javalib/src/main/scala/java/lang/Float.scala
@@ -0,0 +1,72 @@
+package java.lang
+
+final class Float(val floatValue: scala.Float)
+    extends Number with Comparable[Float] {
+  def this(s: String) = this(Float.parseFloat(s))
+
+  @inline override def byteValue(): scala.Byte = floatValue.toByte
+  @inline override def shortValue(): scala.Short = floatValue.toShort
+  @inline def intValue(): scala.Int = floatValue.toInt
+  @inline def longValue(): scala.Long = floatValue.toLong
+  @inline def doubleValue(): scala.Double = floatValue.toDouble
+
+  override def equals(that: Any): scala.Boolean = that match {
+    case that: Float =>
+      val a = floatValue
+      val b = that.floatValue
+      (a == b) || (Float.isNaN(a) && Float.isNaN(b))
+
+    case _ =>
+      false
+  }
+
+  @inline override def hashCode(): Int =
+    Float.hashCode(floatValue)
+
+  @inline override def compareTo(that: Float): Int =
+    Float.compare(floatValue, that.floatValue)
+
+  @inline override def toString(): String =
+    Float.toString(floatValue)
+
+  @inline def isNaN(): scala.Boolean =
+    Float.isNaN(floatValue)
+
+  @inline def isInfinite(): scala.Boolean =
+    Float.isInfinite(floatValue)
+
+}
+
+object Float {
+  final val TYPE = classOf[scala.Float]
+  final val POSITIVE_INFINITY = 1.0f / 0.0f
+  final val NEGATIVE_INFINITY = 1.0f / -0.0f
+  final val NaN = 0.0f / 0.0f
+  final val MAX_VALUE = scala.Float.MaxValue
+  final val MIN_VALUE = scala.Float.MinPositiveValue
+  final val MAX_EXPONENT = 127
+  final val MIN_EXPONENT = -126
+  final val SIZE = 32
+
+  @inline def valueOf(floatValue: scala.Float): Float = new Float(floatValue)
+
+  @inline def valueOf(s: String): Float = valueOf(parseFloat(s))
+
+  @inline def parseFloat(s: String): scala.Float = ???
+
+  @inline def toString(f: scala.Float): String = ???
+
+  @inline def compare(a: scala.Float, b: scala.Float): scala.Int = ???
+
+  @inline def isNaN(v: scala.Float): scala.Boolean =
+    v != v
+
+  @inline def isInfinite(v: scala.Float): scala.Boolean =
+    v == POSITIVE_INFINITY || v == NEGATIVE_INFINITY
+
+  @inline def intBitsToFloat(bits: scala.Int): scala.Float = ???
+
+  @inline def floatToIntBits(value: scala.Float): scala.Int = ???
+
+  @inline def hashCode(value: scala.Float): scala.Int = ???
+}
