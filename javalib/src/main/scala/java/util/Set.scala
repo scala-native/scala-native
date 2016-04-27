1b2715df00e8839379efb83bfe3529e627638ad7
Denys Shabalin <denys.shabalin@epfl.ch>
2016-04-27 10:25:28 +0200

    Better j.u.Set stub

--- javalib/src/main/scala/java/util/Set.scala
+++ javalib/src/main/scala/java/util/Set.scala
@@ -1,3 +1,21 @@
 package java.util
 
-trait Set[E] extends Collection[E]
+trait Set[E] extends Collection[E] {
+  def size(): scala.Int
+  def add(obj: E): scala.Boolean
+  def contains(obj: Any): scala.Boolean
+  def isEmpty(): scala.Boolean
+  def iterator(): Iterator[E]
+
+  //TODO:
+  //def addAll(coll: Collection[_ <: E]): scala.Boolean
+  //def clear(): Unit
+  //def hashCode(): scala.Int
+  //def removeAll(coll: Collection[_]): scala.Boolean
+  //def retainAll(coll: Collection[_]): scala.Boolean
+  //def toArray(): Array[Any]
+  //def toArray[T](array: Array[T]): Array[T]
+  //def contains(coll: Collection[_]): scala.Boolean
+  //def equals(obj: Any): scala.Boolean
+  //def remove(obj: Any): scala.Boolean
+}
