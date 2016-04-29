72d73805b81af1fe083b37858910cf1d6f70a645
Denys Shabalin <denys.shabalin@epfl.ch>
2016-04-29 16:31:12 +0200

    ML-style api for pointers
    * `!ptr` to dereference
    * `ptr := value` to store a value
    * `ptr(offset)` to compute a derived pointer with given offset
    * `ptr.field` to compute a derived pointer to given field

--- nativelib/src/main/scala/scala/scalanative/native/Ptr.scala
+++ nativelib/src/main/scala/scala/scalanative/native/Ptr.scala
@@ -3,7 +3,19 @@ package native
 
 import runtime.undefined
 
-final class Ptr[T] private () {
-  def apply(): T = undefined
-  def update(value: T): Unit = undefined
+final class Ptr[+T] private () extends Dynamic {
+  /** Dereference a pointer. */
+  def unary_! : T = undefined
+
+  /** Store a value to the address pointed at by a pointer. */
+  def := (value: T) = undefined
+
+  /** Compute a derived pointer with given offset. */
+  def apply(offset: Int): T = undefined
+
+  /** Compute a derived pointer to given field. */
+  def selectDynamic(field: String): Ptr[Any] = undefined
+
+  /** Compute a derived pointer to given field plus offset. */
+  def applyDynamic(field: String)(offset: Int): Ptr[Any] = undefined
 }
