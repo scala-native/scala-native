e3f555e6e0834a6a5f3c3a46f4d081005aefe548
Francois Bertrand <francois.bertrand@navigomining.com>
2016-06-12 16:30:04 -0700

    Call GC_malloc_atomic to allocate memory for primitive arrays as they don't store pointers
    - modified:   javalib/src/main/scala/java/lang/Math.scala
    - modified:   nativelib/src/main/scala/scala/scalanative/runtime/Arrays.scala
    - modified:   nativelib/src/main/scala/scala/scalanative/runtime/Arrays.scala.gyb
    - modified:   nativelib/src/main/scala/scala/scalanative/runtime/GC.scala
    - renamed:    nativelib/src/main/scala/scala/scalanative/runtime/Math.scala -> nativelib/src/main/scala/scala/scalanative/runtime/Intrinsics.scala
    - modified:   nativelib/src/main/scala/scala/scalanative/runtime/package.scala

--- nativelib/src/main/scala/scala/scalanative/runtime/GC.scala
+++ nativelib/src/main/scala/scala/scalanative/runtime/GC.scala
@@ -3,11 +3,18 @@ package runtime
 
 import native._
 
+/**
+ * The Boehm GC conservative garbage collector
+ *
+ * @see [[http://hboehm.info/gc/gcinterface.html C Interface]]
+ */
 @link("gc")
 @extern
 object GC {
   @name("GC_malloc")
   def malloc(size: CSize): Ptr[_] = extern
+  @name("GC_malloc_atomic")
+  def malloc_atomic(size: CSize): Ptr[_] = extern
   @name("GC_init")
   def init(): Unit = extern
 }
