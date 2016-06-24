cc5ac537282a550ff3e73d467af15f492923f0dc
Denys Shabalin <denys.shabalin@epfl.ch>
2016-06-24 17:10:22 +0200

    Complete implementation of java.lang.Math
    1. Use LLVM intrinsics whenever possible;
    2. Use libc implementation if no corresponding intrinsic available;
    3. Do our own implementaion if none of the above apply.
    
    Address pr feedback

--- javalib/src/main/scala/java/lang/Math.scala
+++ javalib/src/main/scala/java/lang/Math.scala
@@ -1,284 +1,84 @@
 package java.lang
 
 import scalanative.runtime.Intrinsics._
+import scalanative.native.{math => cmath}
 
 object Math {
+  private lazy val rand = new java.util.Random
+
   final val E  = 2.718281828459045
   final val PI = 3.141592653589793
 
-  @inline def abs(a: scala.Int): scala.Int       = if (a < 0) -a else a
-  @inline def abs(a: scala.Long): scala.Long     = if (a < 0) -a else a
-  @inline def abs(a: scala.Float): scala.Float   = `llvm.fabs.f32`(a)
-  @inline def abs(a: scala.Double): scala.Double = `llvm.fabs.f64`(a)
+  @inline def abs(a: scala.Double): scala.Double =
+    `llvm.fabs.f64`(a)
 
-  @inline def max(a: scala.Int, b: scala.Int): scala.Int = if (a > b) a else b
-  @inline def max(a: scala.Long, b: scala.Long): scala.Long =
-    if (a > b) a else b
-  @inline def max(a: scala.Float, b: scala.Float): scala.Float =
-    `llvm.maxnum.f32`(a, b)
-  @inline def max(a: scala.Double, b: scala.Double): scala.Double =
-    `llvm.maxnum.f64`(a, b)
+  @inline def abs(a: scala.Float): scala.Float =
+    `llvm.fabs.f32`(a)
 
-  @inline def min(a: scala.Int, b: scala.Int): scala.Int = if (a < b) a else b
-  @inline def min(a: scala.Long, b: scala.Long): scala.Long =
-    if (a < b) a else b
-  @inline def min(a: scala.Float, b: scala.Float): scala.Float =
-    `llvm.minnum.f32`(a, b)
-  @inline def min(a: scala.Double, b: scala.Double): scala.Double =
-    `llvm.maxnum.f64`(a, b)
+  @inline def abs(a: scala.Int): scala.Int =
+    if (a < 0) -a else a
 
-  @inline def ceil(a: scala.Double): scala.Double  = `llvm.ceil.f64`(a)
-  @inline def floor(a: scala.Double): scala.Double = `llvm.floor.f64`(a)
-  @inline def rint(a: scala.Double): scala.Double  = `llvm.rint.f64`(a)
+  @inline def abs(a: scala.Long): scala.Long =
+    if (a < 0) -a else a
 
-  @inline def round(a: scala.Float): scala.Int   = `llvm.round.f32`(a).toInt
-  @inline def round(a: scala.Double): scala.Long = `llvm.round.f64`(a).toLong
+  @inline def acos(a: scala.Double): scala.Double =
+    cmath.acos(a)
 
-  @inline def sqrt(a: scala.Double): scala.Double = `llvm.sqrt.f64`(a)
-  @inline def pow(a: scala.Double, b: scala.Double): scala.Double =
-    `llvm.pow.f64`(a, b)
-
-  @inline def exp(a: scala.Double): scala.Double   = `llvm.exp.f64`(a)
-  @inline def log(a: scala.Double): scala.Double   = `llvm.log.f64`(a)
-  @inline def log10(a: scala.Double): scala.Double = `llvm.log10.f64`(a)
-  @inline def log1p(a: scala.Double): scala.Double = log(a + 1)
-
-  @inline def sin(a: scala.Double): scala.Double                    = `llvm.sin.f64`(a)
-  @inline def cos(a: scala.Double): scala.Double                    = `llvm.cos.f64`(a)
-  @inline def tan(a: scala.Double): scala.Double                    = ???
-  @inline def asin(a: scala.Double): scala.Double                   = ???
-  @inline def acos(a: scala.Double): scala.Double                   = ???
-  @inline def atan(a: scala.Double): scala.Double                   = ???
-  @inline def atan2(y: scala.Double, x: scala.Double): scala.Double = ???
-
-  @inline def random(): scala.Double = ???
-
-  @inline def toDegrees(a: scala.Double): scala.Double = a * 180.0 / PI
-  @inline def toRadians(a: scala.Double): scala.Double = a / 180.0 * PI
-
-  @inline def signum(a: scala.Double): scala.Double = {
-    if (a > 0) 1.0
-    else if (a < 0) -1.0
-    else a
+  @inline def addExact(a: scala.Int, b: scala.Int): scala.Int = {
+    val overflow = `llvm.sadd.with.overflow.i32`(a, b)
+    if (overflow.flag) throw new ArithmeticException("Integer overflow")
+    else overflow.value
   }
 
-  @inline def signum(a: scala.Float): scala.Float = {
-    if (a > 0) 1.0f
-    else if (a < 0) -1.0f
-    else a
+  @inline def addExact(a: scala.Long, b: scala.Long): scala.Long = {
+    val overflow = `llvm.sadd.with.overflow.i64`(a, b)
+    if (overflow.flag) throw new ArithmeticException("Long overflow")
+    else overflow.value
   }
 
-  def cbrt(a: scala.Double): scala.Double = ???
+  @inline def asin(a: scala.Double): scala.Double =
+    cmath.asin(a)
 
-  def nextUp(a: scala.Double): scala.Double = {
-    // js implementation of nextUp https://gist.github.com/Yaffle/4654250
-    import scala.Double._
-    if (a != a || a == PositiveInfinity)
-      a
-    else if (a == NegativeInfinity)
-      MinValue
-    else if (a == MaxValue)
-      PositiveInfinity
-    else if (a == 0)
-      MinPositiveValue
-    else {
-      def iter(
-          x: scala.Double, xi: scala.Double, n: scala.Double): scala.Double = {
-        if (Math.abs(xi - x) >= 1E-16) {
-          val c0 = (xi + x) / 2
-          val c =
-            if (c0 == NegativeInfinity || c0 == PositiveInfinity)
-              x + (xi - x) / 2
-            else
-              c0
-          if (n == c) xi
-          else if (a < c) iter(x = x, xi = c, n = c)
-          else iter(x = c, xi = xi, n = c)
-        } else xi
-      }
-      val d  = Math.max(Math.abs(a) * 2E-16, MinPositiveValue)
-      val ad = a + d
-      val xi0 =
-        if (ad == PositiveInfinity) MaxValue
-        else ad
-      iter(x = a, xi = xi0, n = a)
-    }
-  }
+  @inline def atan(a: scala.Double): scala.Double =
+    cmath.atan(a)
 
-  def nextAfter(a: scala.Double, b: scala.Double): scala.Double = {
-    if (b < a)
-      -nextUp(-a)
-    else if (a < b)
-      nextUp(a)
-    else if (a != a || b != b)
-      scala.Double.NaN
-    else
-      b
-  }
+  @inline def atan2(y: scala.Double, x: scala.Double): scala.Double =
+    cmath.atan2(y, x)
 
-  def ulp(a: scala.Double): scala.Double = {
-    if (abs(a) == scala.Double.PositiveInfinity)
-      scala.Double.PositiveInfinity
-    else if (abs(a) == scala.Double.MaxValue)
-      pow(2, 971)
-    else
-      nextAfter(abs(a), scala.Double.MaxValue) - a
-  }
+  @inline def cbrt(a: scala.Double): scala.Double =
+    cmath.cbrt(a)
 
-  def hypot(a: scala.Double, b: scala.Double): scala.Double = {
-    // http://en.wikipedia.org/wiki/Hypot#Implementation
-    if (abs(a) == scala.Double.PositiveInfinity ||
-        abs(b) == scala.Double.PositiveInfinity)
-      scala.Double.PositiveInfinity
-    else if (a.isNaN || b.isNaN)
-      scala.Double.NaN
-    else if (a == 0 && b == 0)
-      0.0
-    else {
-      //To Avoid Overflow and UnderFlow
-      // calculate |x| * sqrt(1 - (y/x)^2) instead of sqrt(x^2 + y^2)
-      val x = abs(a)
-      val y = abs(b)
-      val m = max(x, y)
-      val t = min(x, y) / m
-      m * sqrt(1 + t * t)
-    }
-  }
+  @inline def ceil(a: scala.Double): scala.Double =
+    `llvm.ceil.f64`(a)
 
-  def expm1(a: scala.Double): scala.Double = {
-    // https://github.com/ghewgill/picomath/blob/master/javascript/expm1.js
-    if (a == 0 || a.isNaN)
-      a
-    // Power Series http://en.wikipedia.org/wiki/Power_series
-    // for small values of a, exp(a) = 1 + a + (a*a)/2
-    else if (abs(a) < 1E-5)
-      a + 0.5 * a * a
-    else
-      exp(a) - 1.0
-  }
+  @inline
+  def copySign(magnitude: scala.Double, sign: scala.Double): scala.Double =
+    `llvm.copysign.f64`(magnitude, sign)
 
-  def sinh(a: scala.Double): scala.Double = {
-    if (a.isNaN || a == 0.0 || abs(a) == scala.Double.PositiveInfinity)
-      a
-    else
-      (exp(a) - exp(-a)) / 2.0
-  }
+  @inline
+  def copySign(magnitude: scala.Float, sign: scala.Float): scala.Float =
+    `llvm.copysign.f32`(magnitude, sign)
 
-  def cosh(a: scala.Double): scala.Double = {
-    if (a.isNaN)
-      a
-    else if (a == 0.0)
-      1.0
-    else if (abs(a) == scala.Double.PositiveInfinity)
-      scala.Double.PositiveInfinity
-    else
-      (exp(a) + exp(-a)) / 2.0
-  }
+  @inline def cos(a: scala.Double): scala.Double =
+    `llvm.cos.f64`(a)
 
-  def tanh(a: scala.Double): scala.Double = {
-    if (a.isNaN || a == 0.0)
-      a
-    else if (abs(a) == scala.Double.PositiveInfinity)
-      signum(a)
-    else {
-      // sinh(a) / cosh(a) =
-      // 1 - 2 * (exp(-a)/ (exp(-a) + exp (a)))
-      val expma = exp(-a)
-      if (expma == scala.Double.PositiveInfinity) //Infinity / Infinity
-        -1.0
-      else {
-        val expa = exp(a)
-        val ret  = expma / (expa + expma)
-        1.0 - (2.0 * ret)
-      }
-    }
-  }
+  @inline def cosh(a: scala.Double): scala.Double =
+    cmath.cosh(a)
 
-  def addExact(a: scala.Int, b: scala.Int): scala.Int = {
-    val res       = a + b
-    val resSgnBit = res < 0
-    if (resSgnBit == (a < 0) || resSgnBit == (b < 0)) res
-    else throw new ArithmeticException("Integer overflow")
-  }
+  @inline def decrementExact(a: scala.Int): scala.Int =
+    subtractExact(a, 1)
 
-  def addExact(a: scala.Long, b: scala.Long): scala.Long = {
-    val res       = a + b
-    val resSgnBit = res < 0
-    if (resSgnBit == (a < 0) || resSgnBit == (b < 0)) res
-    else throw new ArithmeticException("Long overflow")
-  }
+  @inline def decrementExact(a: scala.Long): scala.Long =
+    subtractExact(a, 1L)
 
-  def subtractExact(a: scala.Int, b: scala.Int): scala.Int = {
-    val res       = a - b
-    val resSgnBit = res < 0
-    if (resSgnBit == (a < 0) || resSgnBit == (b > 0)) res
-    else throw new ArithmeticException("Integer overflow")
-  }
+  @inline def exp(a: scala.Double): scala.Double =
+    `llvm.exp.f64`(a)
 
-  def subtractExact(a: scala.Long, b: scala.Long): scala.Long = {
-    val res       = a - b
-    val resSgnBit = res < 0
-    if (resSgnBit == (a < 0) || resSgnBit == (b > 0)) res
-    else throw new ArithmeticException("Long overflow")
-  }
+  @inline def expm1(a: scala.Double): scala.Double =
+    cmath.expm1(a)
 
-  def multiplyExact(a: scala.Int, b: scala.Int): scala.Int = {
-    val overflow = {
-      if (b > 0)
-        a > Integer.MAX_VALUE / b || a < Integer.MIN_VALUE / b
-      else if (b < -1)
-        a > Integer.MIN_VALUE / b || a < Integer.MAX_VALUE / b
-      else if (b == -1)
-        a == Integer.MIN_VALUE
-      else
-        false
-    }
-    if (!overflow) a * b
-    else throw new ArithmeticException("Integer overflow")
-  }
-
-  def multiplyExact(a: scala.Long, b: scala.Long): scala.Long = {
-    val overflow = {
-      if (b > 0)
-        a > Long.MAX_VALUE / b || a < Long.MIN_VALUE / b
-      else if (b < -1)
-        a > Long.MIN_VALUE / b || a < Long.MAX_VALUE / b
-      else if (b == -1)
-        a == Long.MIN_VALUE
-      else
-        false
-    }
-    if (!overflow) a * b
-    else throw new ArithmeticException("Long overflow")
-  }
-
-  def incrementExact(a: scala.Int): scala.Int =
-    if (a != Integer.MAX_VALUE) a + 1
-    else throw new ArithmeticException("Integer overflow")
-
-  def incrementExact(a: scala.Long): scala.Long =
-    if (a != Long.MAX_VALUE) a + 1
-    else throw new ArithmeticException("Long overflow")
-
-  def decrementExact(a: scala.Int): scala.Int =
-    if (a != Integer.MIN_VALUE) a - 1
-    else throw new ArithmeticException("Integer overflow")
-
-  def decrementExact(a: scala.Long): scala.Long =
-    if (a != Long.MIN_VALUE) a - 1
-    else throw new ArithmeticException("Long overflow")
-
-  def negateExact(a: scala.Int): scala.Int =
-    if (a != Integer.MIN_VALUE) -a
-    else throw new ArithmeticException("Integer overflow")
-
-  def negateExact(a: scala.Long): scala.Long =
-    if (a != Long.MIN_VALUE) -a
-    else throw new ArithmeticException("Long overflow")
-
-  def toIntExact(a: scala.Long): scala.Int =
-    if (a >= Integer.MIN_VALUE && a <= Integer.MAX_VALUE) a.toInt
-    else throw new ArithmeticException("Integer overflow")
+  @inline def floor(a: scala.Double): scala.Double =
+    `llvm.floor.f64`(a)
 
   def floorDiv(a: scala.Int, b: scala.Int): scala.Int = {
     val quot = a / b
@@ -304,21 +104,216 @@ object Math {
     else rem + b
   }
 
-  def copySign(magnitude: scala.Float, sign: scala.Float): scala.Float =
-    `llvm.copysign.f32`(magnitude, sign)
+  @inline def getExponent(a: scala.Float): scala.Int =
+    cmath.ilogbf(a)
 
-  def copySign(magnitude: scala.Double, sign: scala.Double): scala.Double =
-    `llvm.copysign.f64`(magnitude, sign)
+  @inline def getExponent(a: scala.Double): scala.Long =
+    cmath.ilogb(a)
 
-  // TODO
-  // def IEEEremainder(f1: scala.Double, f2: scala.Double): Double
-  // def ulp(a: scala.Float): scala.Float
-  // def getExponent(a: scala.Float): scala.Int
-  // def getExponent(a: scala.Double): scala.Int
-  // def nextAfter(a: scala.Float, b: scala.Double): scala.Float
-  // def nextUp(a: scala.Float): scala.Float
-  // def nextDown(a: scala.Double): scala.Double
-  // def nextDown(a: scala.Float): scala.Float
-  // def scalb(a: scala.Double, scalaFactor: scala.Int): scala.Double
-  // def scalb(a: scala.Float, scalaFactor: scala.Int): scala.Float
+  @inline def hypot(a: scala.Double, b: scala.Double): scala.Double =
+    cmath.hypot(a, b)
+
+  @inline def IEEEremainder(f1: scala.Double, f2: scala.Double): Double =
+    cmath.remainder(f1, f2)
+
+  @inline def incrementExact(a: scala.Int): scala.Int =
+    addExact(a, 1)
+
+  @inline def incrementExact(a: scala.Long): scala.Long =
+    addExact(a, 1L)
+
+  @inline def log(a: scala.Double): scala.Double =
+    `llvm.log.f64`(a)
+
+  @inline def log10(a: scala.Double): scala.Double =
+    `llvm.log10.f64`(a)
+
+  @inline def log1p(a: scala.Double): scala.Double =
+    cmath.log1p(a)
+
+  @inline def max(a: scala.Double, b: scala.Double): scala.Double =
+    `llvm.maxnum.f64`(a, b)
+
+  @inline def max(a: scala.Float, b: scala.Float): scala.Float =
+    `llvm.maxnum.f32`(a, b)
+
+  @inline def max(a: scala.Int, b: scala.Int): scala.Int =
+    if (a > b) a else b
+
+  @inline def max(a: scala.Long, b: scala.Long): scala.Long =
+    if (a > b) a else b
+
+  @inline def min(a: scala.Double, b: scala.Double): scala.Double =
+    `llvm.maxnum.f64`(a, b)
+
+  @inline def min(a: scala.Float, b: scala.Float): scala.Float =
+    `llvm.minnum.f32`(a, b)
+
+  @inline def min(a: scala.Int, b: scala.Int): scala.Int =
+    if (a < b) a else b
+
+  @inline def min(a: scala.Long, b: scala.Long): scala.Long =
+    if (a < b) a else b
+
+  @inline def multiplyExact(a: scala.Int, b: scala.Int): scala.Int = {
+    val overflow = `llvm.smul.with.overflow.i32`(a, b)
+    if (overflow.flag) throw new ArithmeticException("Integer overflow")
+    else overflow.value
+  }
+
+  @inline def multiplyExact(a: scala.Long, b: scala.Long): scala.Long = {
+    val overflow = `llvm.smul.with.overflow.i64`(a, b)
+    if (overflow.flag) throw new ArithmeticException("Long overflow")
+    else overflow.value
+  }
+
+  @inline def negateExact(a: scala.Int): scala.Int =
+    subtractExact(0, a)
+
+  @inline def negateExact(a: scala.Long): scala.Long =
+    subtractExact(0, a)
+
+  def nextAfter(a: scala.Float, b: scala.Double): scala.Float = {
+    val aabs = abs(a.toDouble)
+    val babs = abs(b)
+
+    if (Float.isNaN(a) || Double.isNaN(b)) {
+      Float.NaN
+    } else if (aabs == 0f && babs == 0d) {
+      b.toFloat
+    } else if (aabs == Float.MIN_VALUE && babs < aabs) {
+      copySign(0, a)
+    } else if (Float.isInfinite(a) && babs < aabs) {
+      copySign(Float.MAX_VALUE, a)
+    } else if (aabs == Float.MAX_VALUE && babs > aabs) {
+      copySign(Float.POSITIVE_INFINITY, a)
+    } else if (b > a) {
+      cmath.nextafterf(a, Float.POSITIVE_INFINITY)
+    } else {
+      cmath.nextafterf(a, Float.NEGATIVE_INFINITY)
+    }
+  }
+
+  def nextAfter(a: scala.Double, b: scala.Double): scala.Double = {
+    val aabs = abs(a)
+    val babs = abs(b)
+
+    if (Double.isNaN(a) || Double.isNaN(b)) {
+      Double.NaN
+    } else if (aabs == 0f && babs == 0d) {
+      b
+    } else if (aabs == Double.MIN_VALUE && babs < aabs) {
+      copySign(0, a)
+    } else if (Double.isInfinite(a) && babs < aabs) {
+      copySign(Double.MAX_VALUE, a)
+    } else if (aabs == Double.MAX_VALUE && babs > aabs) {
+      copySign(Double.POSITIVE_INFINITY, a)
+    } else {
+      cmath.nextafter(a, b)
+    }
+  }
+
+  def nextDown(a: scala.Float): scala.Float =
+    nextAfter(a, Double.NEGATIVE_INFINITY)
+
+  def nextDown(a: scala.Double): scala.Double =
+    nextAfter(a, Double.NEGATIVE_INFINITY)
+
+  def nextUp(a: scala.Float): scala.Float =
+    nextAfter(a, Double.POSITIVE_INFINITY)
+
+  def nextUp(a: scala.Double): scala.Double =
+    nextAfter(a, Double.POSITIVE_INFINITY)
+
+  @inline def pow(a: scala.Double, b: scala.Double): scala.Double =
+    `llvm.pow.f64`(a, b)
+
+  @inline def random(): scala.Double =
+    rand.nextDouble()
+
+  @inline def rint(a: scala.Double): scala.Double =
+    `llvm.rint.f64`(a)
+
+  @inline def round(a: scala.Float): scala.Int =
+    `llvm.round.f32`(a).toInt
+
+  @inline def round(a: scala.Double): scala.Long =
+    `llvm.round.f64`(a).toLong
+
+  @inline def scalb(a: scala.Float, scaleFactor: scala.Int): scala.Float =
+    cmath.scalbnf(a, scaleFactor)
+
+  @inline def scalb(a: scala.Double, scaleFactor: scala.Int): scala.Double =
+    cmath.scalbn(a, scaleFactor)
+
+  @inline def signum(a: scala.Float): scala.Float = {
+    if (a > 0) 1.0f
+    else if (a < 0) -1.0f
+    else a
+  }
+
+  @inline def signum(a: scala.Double): scala.Double = {
+    if (a > 0) 1.0
+    else if (a < 0) -1.0
+    else a
+  }
+
+  @inline def sin(a: scala.Double): scala.Double =
+    `llvm.sin.f64`(a)
+
+  @inline def sinh(a: scala.Double): scala.Double =
+    cmath.sinh(a)
+
+  @inline def sqrt(a: scala.Double): scala.Double =
+    `llvm.sqrt.f64`(a)
+
+  @inline def subtractExact(a: scala.Int, b: scala.Int): scala.Int = {
+    val overflow = `llvm.ssub.with.overflow.i32`(a, b)
+    if (overflow.flag) throw new ArithmeticException("Integer overflow")
+    else overflow.value
+  }
+
+  @inline def subtractExact(a: scala.Long, b: scala.Long): scala.Long = {
+    val overflow = `llvm.ssub.with.overflow.i64`(a, b)
+    if (overflow.flag) throw new ArithmeticException("Long overflow")
+    else overflow.value
+  }
+
+  @inline def tan(a: scala.Double): scala.Double =
+    cmath.tan(a)
+
+  @inline def tanh(a: scala.Double): scala.Double =
+    cmath.tanh(a)
+
+  @inline def toDegrees(a: scala.Double): scala.Double = a * 180.0 / PI
+
+  def toIntExact(a: scala.Long): scala.Int =
+    if (a >= Integer.MIN_VALUE && a <= Integer.MAX_VALUE) a.toInt
+    else throw new ArithmeticException("Integer overflow")
+
+  @inline def toRadians(a: scala.Double): scala.Double = a / 180.0 * PI
+
+  def ulp(a: scala.Float): scala.Float = {
+    if (Float.isNaN(a)) {
+      Float.NaN
+    } else if (Float.isInfinite(a)) {
+      Float.POSITIVE_INFINITY
+    } else if (abs(a) == Float.MAX_VALUE) {
+      2.028241E31F // pow(2, 104).toFloat
+    } else {
+      val f = abs(a)
+      cmath.nextafterf(f, Float.MAX_VALUE) - f
+    }
+  }
+
+  def ulp(a: scala.Double): scala.Double = {
+    if (Double.isInfinite(a)) {
+      scala.Double.PositiveInfinity
+    } else if (a == scala.Double.MaxValue || a == -Double.MAX_VALUE) {
+      1.9958403095347198E292D // pow(2, 971)
+    } else {
+      val d = abs(a)
+      cmath.nextafter(d, scala.Double.MaxValue) - d
+    }
+  }
 }
