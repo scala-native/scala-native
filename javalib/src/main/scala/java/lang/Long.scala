26e78fbe30d69358a6a9563062389330e26429dc
Denys Shabalin <denys.shabalin@epfl.ch>
2016-07-11 15:26:58 +0200

    Nearly complete implementation of java.lang.Long

--- javalib/src/main/scala/java/lang/Long.scala
+++ javalib/src/main/scala/java/lang/Long.scala
@@ -1,11 +1,11 @@
 package java.lang
 
-import scalanative.runtime.{divULong, remULong, undefined, Intrinsics}
+import scalanative.runtime.{select, divULong, remULong, undefined, Intrinsics}
 
 final class Long(val longValue: scala.Long)
     extends Number
     with Comparable[Long] {
-  def this(s: String) =
+  @inline def this(s: String) =
     this(Long.parseLong(s))
 
   @inline override def byteValue(): scala.Byte =
@@ -48,43 +48,8 @@ object Long {
   final val SIZE      = 64
   final val BYTES     = 8
 
-  @inline private def parse(s: String,
-                            _offset: Int,
-                            radix: Int,
-                            negative: scala.Boolean): scala.Long = {
-    val max = MIN_VALUE / radix
-    var result = 0
-    var offset = _offset
-    val length = s.length()
-    while (offset < length) {
-      offset += 1
-      val digit = Character.digit(s.charAt(offset), radix)
-      if (digit == -1) throw new NumberFormatException(s)
-      if (max > result) throw new NumberFormatException(s)
-      val next = result * radix - digit
-      if (next > result) throw new NumberFormatException(s)
-      result = next
-    }
-
-    if (!negative) {
-      result = -result
-      if (result < 0) {
-        throw new NumberFormatException(s)
-      }
-    }
-
-    result
-  }
-
-  @inline def bitCount(l: scala.Long): scala.Int = {
-    var k = (l & 0x5555555555555555L) + ((l >> 1) & 0x5555555555555555L)
-    k = (k & 0x3333333333333333L) + ((k >> 2) & 0x3333333333333333L)
-    var i = ((k >>> 32) + k).toInt
-    i = (i & 0x0F0F0F0F) + ((i >> 4) & 0x0F0F0F0F)
-    i = (i & 0x00FF00FF) + ((i >> 8) & 0x00FF00FF)
-    i = (i & 0x0000FFFF) + ((i >> 16) & 0x0000FFFF)
-    i
-  }
+  @inline def bitCount(l: scala.Long): scala.Int =
+    Intrinsics.`llvm.ctpop.i64`(l).toInt
 
   @inline def compare(x: scala.Long, y: scala.Long): scala.Int =
     if (x == y) 0
@@ -92,7 +57,7 @@ object Long {
     else 1
 
   @inline def compareUnsigned(x: scala.Long, y: scala.Long): scala.Int =
-    undefined
+    compare(x ^ scala.Long.MinValue, y ^ scala.Long.MinValue)
 
   def decode(nm: String): Long = {
     val length = nm.length()
@@ -145,36 +110,28 @@ object Long {
   def divideUnsigned(dividend: scala.Long, divisor: scala.Long): scala.Long =
     divULong(dividend, divisor)
 
-  def getLong(nm: String): Long =
-    if (nm == null || nm.length() == 0) {
-      null
-    } else {
-      val prop = System.getProperty(nm)
-      if (prop == null) {
-        null
-      } else {
-        try {
-          decode(prop)
-        } catch {
-          case e: NumberFormatException =>
-            null
-        }
-      }
-    }
+  @inline def getLong(nm: String): Long =
+    getLong(nm, null)
 
-  def getLong(nm: String, v: scala.Long): Long =
+  @inline def getLong(nm: String, v: scala.Long): Long = {
+    val result = getLong(nm, null)
+    if (result == null) new Long(v)
+    else result
+  }
+
+  def getLong(nm: String, v: Long): Long =
     if (nm == null || nm.length() == 0) {
-      valueOf(v)
+      v
     } else {
       val prop = System.getProperty(nm)
       if (prop == null) {
-        valueOf(v)
+        v
       } else {
         try {
           decode(prop)
         } catch {
           case e: NumberFormatException =>
-            valueOf(v)
+            v
         }
       }
     }
@@ -182,19 +139,10 @@ object Long {
   @inline def hashCode(value: scala.Long): Int =
     value.toInt ^ (value >>> 32).toInt
 
-  def highestOneBit(i: scala.Long): scala.Long = {
-    // ToDo reimplement
-    var lng = i
-    lng |= (lng >> 1)
-    lng |= (lng >> 2)
-    lng |= (lng >> 4)
-    lng |= (lng >> 8)
-    lng |= (lng >> 16)
-    lng |= (lng >> 32)
-    lng & ~(lng >>> 1)
-  }
+  @inline def highestOneBit(i: scala.Long): scala.Long =
+    select(i == 0L, 0L, (1 << 63) >>> numberOfLeadingZeros(i))
 
-  def lowestOneBit(i: scala.Long): scala.Long =
+  @inline def lowestOneBit(i: scala.Long): scala.Long =
     i & (-i)
 
   @inline def max(a: scala.Long, b: scala.Long): scala.Long =
@@ -209,6 +157,9 @@ object Long {
   @inline def numberOfTrailingZeros(l: scala.Long): Int =
     Intrinsics.`llvm.cttz.i64`(l, iszeroundef = false).toInt
 
+  @inline def parseLong(s: String): scala.Long =
+    parseLong(s, 10)
+
   @inline def parseLong(s: String, radix: Int): scala.Long = {
     val length   = s.length()
     val negative = s.charAt(0) == '-'
@@ -221,33 +172,52 @@ object Long {
     parse(s, 1, radix, negative)
   }
 
-  @inline def parseLong(s: String): scala.Long =
-    parseLong(s, 10)
+  private def parse(s: String,
+                    _offset: Int,
+                    radix: Int,
+                    negative: scala.Boolean): scala.Long = {
+    val max = MIN_VALUE / radix
+    var result = 0
+    var offset = _offset
+    val length = s.length()
+    while (offset < length) {
+      val digit = Character.digit(s.charAt(offset), radix)
+      offset += 1
+      if (digit == -1) throw new NumberFormatException(s)
+      if (max > result) throw new NumberFormatException(s)
+      val next = result * radix - digit
+      if (next > result) throw new NumberFormatException(s)
+      result = next
+    }
 
-  @inline def parseUnsignedLong(s: String): scala.Long =
-    parseUnsignedLong(s, 10)
+    if (!negative) {
+      result = -result
+      if (result < 0) {
+        throw new NumberFormatException(s)
+      }
+    }
 
-  @inline def parseUnsignedLong(s: String, radix: Int): scala.Long =
-    undefined
+    result
+  }
 
   @inline
   def remainderUnsigned(dividend: scala.Long,
                         divisor: scala.Long): scala.Long =
     remULong(dividend, divisor)
 
-  def reverse(l: scala.Long): scala.Long =
+  @inline def reverse(l: scala.Long): scala.Long =
     Intrinsics.`llvm.bitreverse.i64`(l)
 
-  def reverseBytes(l: scala.Long): scala.Long =
+  @inline def reverseBytes(l: scala.Long): scala.Long =
     Intrinsics.`llvm.bswap.i64`(l)
 
-  def rotateLeft(i: scala.Long, distance: scala.Int): scala.Long =
+  @inline def rotateLeft(i: scala.Long, distance: scala.Int): scala.Long =
     (i << distance) | (i >>> -distance)
 
-  def rotateRight(i: scala.Long, distance: scala.Int): scala.Long =
+  @inline def rotateRight(i: scala.Long, distance: scala.Int): scala.Long =
     (i >>> distance) | (i << -distance)
 
-  def signum(i: scala.Long): scala.Int =
+  @inline def signum(i: scala.Long): scala.Int =
     if (i == 0) 0
     else if (i < 0) -1
     else 1
@@ -259,12 +229,12 @@ object Long {
     var count =
       if (l == 0L) 1
       else 64 - numberOfLeadingZeros(l)
-    var j = l
     val buffer = new Array[Char](count)
+    var k = l
     do {
       count -= 1
-      buffer(count) = ((j & 1) + '0').toChar
-      j >>= 1
+      buffer(count) = ((k & 1) + '0').toChar
+      k >>= 1
     } while (count > 0)
 
     new String(buffer)
@@ -273,11 +243,11 @@ object Long {
   def toHexString(l: scala.Long): String = {
     var count =
       if (l == 0L) 1
-      else ((64 + 3) - numberOfLeadingZeros(l)) >> 2
-    var j = l
+      else ((64 - numberOfLeadingZeros(l)) + 3) / 4
     val buffer = new Array[Char](count)
+    var k = l
     do {
-      var t = (j & 15).toInt
+      var t = (k & 15).toInt
       if (t > 9) {
         t = t - 10 + 'a'
       } else {
@@ -285,7 +255,7 @@ object Long {
       }
       count -= 1
       buffer(count) = t.toChar
-      j >>= 4
+      k >>= 4
     } while (count > 0)
 
     new String(buffer)
@@ -294,13 +264,13 @@ object Long {
   def toOctalString(l: scala.Long): String = {
     var count =
       if (l == 0L) 1
-      else ((64 + 2) - numberOfLeadingZeros(l)) / 3
-    var j = l
+      else ((64 - numberOfLeadingZeros(l)) + 2) / 3
     val buffer = new Array[Char](count)
+    var k = l
     do {
       count -= 1
-      buffer(count) = ((j & 7) + '0').toChar
-      j >>>= 3
+      buffer(count) = ((k & 7) + '0').toChar
+      k >>>= 3
     } while (count > 0)
 
     new String(buffer)
@@ -317,10 +287,15 @@ object Long {
         if (_radix < Character.MIN_RADIX || _radix > Character.MAX_RADIX) 10
         else _radix
       val negative = _l < 0
-      var (count, j) =
-        if (negative) (2, _l)
-        else (1, -_l)
+      var count = 2
+      var j     = _l
+      if (!negative) {
+        count = 1
+        j = -_l
+      }
+
       var l = _l
+      l /= radix
       while (l != 0) {
         count += 1
         l = l / radix
@@ -347,12 +322,6 @@ object Long {
     }
   }
 
-  @inline def toUnsignedString(l: scala.Long, radix: Int): String =
-    undefined
-
-  @inline def toUnsignedString(l: scala.Long): String =
-    toUnsignedString(l, 10)
-
   @inline def valueOf(longValue: scala.Long): Long =
     new Long(longValue)
 
@@ -361,4 +330,10 @@ object Long {
 
   @inline def valueOf(s: String, radix: Int): Long =
     valueOf(parseLong(s, radix))
+
+  // TODO:
+  // def parseUnsignedLong(s: String): scala.Long = parseUnsignedLong(s, 10)
+  // def parseUnsignedLong(s: String, radix: Int): scala.Long = ???
+  // def toUnsignedString(l: scala.Long): String = toUnsignedString(l, 10)
+  // def toUnsignedString(l: scala.Long, radix: Int): String = ???
 }
