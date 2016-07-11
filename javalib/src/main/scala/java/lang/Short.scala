8d97a1d631785f36be569d8fef55946e7ca3093d
Denys Shabalin <denys.shabalin@epfl.ch>
2016-07-11 13:42:16 +0200

    Complete implementation of java.lang.Short

--- javalib/src/main/scala/java/lang/Short.scala
+++ javalib/src/main/scala/java/lang/Short.scala
@@ -1,11 +1,11 @@
 package java.lang
 
-import scalanative.runtime.undefined
+import scalanative.runtime.{shortToUInt, shortToULong, Intrinsics}
 
 final class Short(override val shortValue: scala.Short)
     extends Number
     with Comparable[Short] {
-  def this(s: String) =
+  @inline def this(s: String) =
     this(Short.parseShort(s))
 
   @inline override def byteValue(): scala.Byte =
@@ -42,8 +42,9 @@ final class Short(override val shortValue: scala.Short)
 }
 
 object Short {
-  final val TYPE = classOf[scala.Short]
-  final val SIZE = 16
+  final val TYPE  = classOf[scala.Short]
+  final val SIZE  = 16
+  final val BYTES = 2
 
   /* MIN_VALUE and MAX_VALUE should be 'final val's. But it is impossible to
    * write a proper Short literal in Scala, that would both considered a Short
@@ -53,13 +54,13 @@ object Short {
    * defs. Source-compatibility is not an issue because user code is compiled
    * against the JDK .class files anyway.
    */
-  def MIN_VALUE: scala.Short = -32768
-  def MAX_VALUE: scala.Short = 32767
+  @inline def MIN_VALUE: scala.Short = -32768
+  @inline def MAX_VALUE: scala.Short = 32767
 
   @inline def compare(x: scala.Short, y: scala.Short): scala.Int =
     x - y
 
-  def decode(nm: String): Short = {
+  @inline def decode(nm: String): Short = {
     val i = Integer.decode(nm).intValue
     val r = i.toShort
     if (r == i)
@@ -83,16 +84,16 @@ object Short {
   }
 
   @inline def reverseBytes(i: scala.Short): scala.Short =
-    (((i >>> 8) & 0xff) + ((i & 0xff) << 8)).toShort
+    Intrinsics.`llvm.bswap.i16`(i)
 
   @inline def toString(s: scala.Short): String =
     Integer.toString(s)
 
-  def toUnsignedInt(x: scala.Short): scala.Int =
-    undefined
+  @inline def toUnsignedInt(x: scala.Short): scala.Int =
+    shortToUInt(x)
 
-  def toUnsignedLong(x: scala.Short): scala.Long =
-    undefined
+  @inline def toUnsignedLong(x: scala.Short): scala.Long =
+    shortToULong(x)
 
   @inline def valueOf(shortValue: scala.Short): Short =
     new Short(shortValue)
