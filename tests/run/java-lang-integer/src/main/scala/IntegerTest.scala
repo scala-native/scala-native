b76e9c9d516297989c1da68cf13bf344581de2f8
Martin Duhem <martin.duhem@gmail.com>
2016-08-24 21:41:26 +0200

    Put `assert`s in test `run/java-lang-integer`
    The scripted tests won't fail unless the execution is actually stopped
    by a failed assertion.
    
    Also, fix a typo in the test.

--- tests/run/java-lang-integer/src/main/scala/IntegerTest.scala
+++ tests/run/java-lang-integer/src/main/scala/IntegerTest.scala
@@ -7,12 +7,12 @@ object IntegerTest {
 
   def testIntegerToString() = {
     // issue #270
-    println(Integer.toString(1).equals("1"))
-    println(Integer.toString(-1).equals("1"))
-    println(Integer.toString(123).equals("123"))
-    println(Integer.toString(-123).equals("-123"))
-    println(Integer.toString(1234).equals("1234"))
-    println(Integer.toString(-1234).equals("-1234"))
+    assert(Integer.toString(1).equals("1"))
+    assert(Integer.toString(-1).equals("-1"))
+    assert(Integer.toString(123).equals("123"))
+    assert(Integer.toString(-123).equals("-123"))
+    assert(Integer.toString(1234).equals("1234"))
+    assert(Integer.toString(-1234).equals("-1234"))
   }
 }
 
