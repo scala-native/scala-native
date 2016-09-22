f715bacaa2dffe540cd67df310161ccf962f482a
Denys Shabalin <denys.shabalin@epfl.ch>
2016-09-22 14:40:35 +0200

    Add sidebar logo

--- docs/conf.py
+++ docs/conf.py
@@ -142,7 +142,7 @@ html_theme = 'alabaster'
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #
-# html_logo = None
+html_logo = '_static/logo.png'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
