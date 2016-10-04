49ac609eb40028de481d3480ceff7b28907c40bd
Martin Duhem <martin.duhem@gmail.com>
2016-10-04 14:27:58 +0200

    Add EmptyPass, options in Ctx
    This commit introduces a new type of pass, EmptyPass, which does
    nothing. This pass can be used to replace a pass that has been disabled
    (eg. `MainInjection` when `nativeSharedLibrary` is true).
    
    Also, the `Ctx` gained an `options: Opts` field that contains the
    options specific to scala-native. They make it easier to export
    configuration to specific phases.

--- tools/src/main/scala/scala/scalanative/compiler/Ctx.scala
+++ tools/src/main/scala/scala/scalanative/compiler/Ctx.scala
@@ -4,4 +4,5 @@ package compiler
 /** Context that pass companion can uses to instantiate passes. */
 final case class Ctx(fresh: nir.Fresh,
                      entry: nir.Global,
-                     top: analysis.ClassHierarchy.Top)
+                     top: analysis.ClassHierarchy.Top,
+                     options: Opts)
