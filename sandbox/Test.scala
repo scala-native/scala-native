4812341f44f3935a9572ed14af22dd13cb8d063c
Kamil Tomala <kamil.tomala@gmail.com>
2016-10-10 17:15:12 +0200

    Fix c-style varargs accepting only single parameter

--- sandbox/Test.scala
+++ sandbox/Test.scala
@@ -1,5 +1,9 @@
+import scalanative.native._, stdlib._, stdio._
+
 object Test {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    val W = 800
+    val H = 600
+    fprintf(stdout, c"P3\n%d %d\n%d\n", W, H, 255)
   }
 }
