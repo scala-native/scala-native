7d18c1c96fe33bce7b884d9a73f6f79bf1838994
Denys Shabalin <denys.shabalin@epfl.ch>
2016-11-08 13:39:08 +0100

    Test suite name must end with "Suite"

--- unit-tests/src/main/scala/scala/scalanative/issues/_260Suite.scala
+++ unit-tests/src/main/scala/scala/scalanative/issues/_260Suite.scala
@@ -1,7 +1,7 @@
 package scala.scalanative.issues
 
-object _260 extends tests.Suite {
-  test("https://github.com/scala-native/scala-native/issues/260") {
+object _260Suite extends tests.Suite {
+  test("#260") {
     def getStr(): String = {
       val bytes = Array('h'.toByte, 'o'.toByte, 'l'.toByte, 'a'.toByte)
       new String(bytes)
