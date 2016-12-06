fb2cd2f6391e827383ece9505d11139ae397aec4
Martin Duhem <martin.duhem@gmail.com>
2016-12-06 15:09:42 +0100

    Extract logic to display benchmark results
    This will allow us to define different formats to display the results of
    the benchmarks.

--- benchmarks/src/main/scala/benchmarks/Benchmark.scala
+++ benchmarks/src/main/scala/benchmarks/Benchmark.scala
@@ -5,57 +5,13 @@ sealed abstract class BenchmarkResult(val name: String, val success: Boolean)
 case class BenchmarkCompleted(override val name: String,
                               timesNs: Seq[Long],
                               override val success: Boolean)
-    extends BenchmarkResult(name, success) {
-
-  private def format(n: Double, decimals: Int = 3): String = {
-    val s = n.toString
-    s.substring(0, s.indexOf('.') + decimals + 1)
-  }
-
-  private def percentile[T](n: Int, sortedData: Seq[T]): T = {
-    val index = Math.ceil(n * sortedData.length / 100.0).toInt - 1
-    sortedData(index)
-  }
-
-  private def average(data: Seq[Double]): Double = {
-    val count = data.length.toDouble
-    data.foldLeft(0.0) { _ + _ / count }
-  }
-
-  override def toString: String = {
-    val timesMs    = timesNs map (_ / 1e6)
-    val sortedMs   = timesMs.sorted
-    val minMs      = timesMs.min
-    val maxMs      = timesMs.max
-    val avgMs      = average(timesMs)
-    val medianMs   = percentile(50, sortedMs)
-    val p95Ms      = percentile(95, sortedMs)
-    val p05Ms      = percentile(5, sortedMs)
-    val iterations = timesNs.length
-    val stddev =
-      Math.sqrt(timesMs.map(t => Math.pow(t - avgMs, 2) / iterations).sum)
-    val avgBetweenP05AndP95 =
-      average(timesMs filter (t => t >= p05Ms && t <= p95Ms))
-
-    (if (success) "  [ok] " else "  [fail] ") + name +
-      s": $iterations iterations, min ${format(minMs)}ms, max ${format(maxMs)}ms," +
-      s" avg ${format(avgMs)}ms, median ${format(medianMs)}, p05 ${format(p05Ms)}ms," +
-      s" p95 ${format(p95Ms)}ms, avg in [p05, p95] ${format(avgBetweenP05AndP95)}ms"
-  }
-}
+    extends BenchmarkResult(name, success)
 
 case class BenchmarkFailed(override val name: String, cause: Throwable)
-    extends BenchmarkResult(name, false) {
-  override def toString: String =
-    s"""  [fail] $name threw an exception:
-       |$cause""".stripMargin
-}
+    extends BenchmarkResult(name, false)
 
 case class BenchmarkDisabled(override val name: String)
-    extends BenchmarkResult(name, true) {
-  override def toString: String =
-    s""" [???] $name is disabled."""
-}
+    extends BenchmarkResult(name, true)
 
 abstract class Benchmark[T] {
   def run(): T
--- benchmarks/src/main/scala/benchmarks/Main.scala
+++ benchmarks/src/main/scala/benchmarks/Main.scala
@@ -6,12 +6,13 @@ object Main {
   def main(args: Array[String]): Unit = {
     val benchmarks = Discover.discovered
 
-    val success =
-      benchmarks.forall { bench =>
-        val result = bench.loop(bench.iterations)
-        println(result)
-        result.success
-      }
+    val format = args.lift(0).map(Format(_)).getOrElse(TextFormat)
+    val results = benchmarks.map { bench =>
+      bench.loop(bench.iterations)
+    }
+    val success = results.forall(_.success)
+
+    println(format.show(results))
 
     if (success) exit(0) else exit(1)
   }
