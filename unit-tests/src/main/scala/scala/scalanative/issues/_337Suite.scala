fea5f7349e9908651b82023445cddb7714ee9e6b
Denys Shabalin <denys.shabalin@epfl.ch>
2016-12-13 11:33:04 +0100

    Merge issue suites into one
    Fixes #424

--- unit-tests/src/main/scala/scala/scalanative/issues/_337Suite.scala
+++
@@ -1,9 +0,0 @@
-package scala.scalanative.issues
-
-object _337Suite extends tests.Suite {
-  test("#337") {
-    case class TestObj(value: Int)
-    val obj = TestObj(10)
-    assert(obj.value == 10)
-  }
-}
