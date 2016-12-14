7d6550d1344f6a02d557d00068cfb5a1609322fc
Ruben Berenguel Montoro <ruben@mostlymaths.net>
2016-12-14 20:06:46 +0000

    Removed the sandbox, whoops. Also, scalafmt seems to work once every 3 times I run it

--- sandbox/Test.scala
+++ sandbox/Test.scala
@@ -1,4 +1,13 @@
+<<<<<<< HEAD
 object Test {
   def main(args: Array[String]): Unit =
     println("Hello, world!")
+=======
+import scalanative.native._, stdlib._, stdio._
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    println('foo)
+  }
+>>>>>>> Removed the sandbox, whoops. Also, scalafmt seems to work once every 3 times I run it
 }
