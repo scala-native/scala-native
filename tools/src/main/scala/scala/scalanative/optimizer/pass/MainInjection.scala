1ca459bae6c673c6af706d6ec22fc9bc61021a6a
Denys Shabalin <denys.shabalin@epfl.ch>
2017-01-05 14:39:35 +0100

    Implement backtraces using libunwind
    Fixes #158

--- tools/src/main/scala/scala/scalanative/optimizer/pass/MainInjection.scala
+++ tools/src/main/scala/scala/scalanative/optimizer/pass/MainInjection.scala
@@ -20,24 +20,32 @@ class MainInjection(entry: Global)(implicit fresh: Fresh) extends Pass {
         Global.Member(entry, "main_class.ssnr.ObjectArray_unit")
       val entryMain = Val.Global(entryMainName, Type.Ptr)
 
+      val start, succ, fail = fresh()
+
       val argc   = Val.Local(fresh(), Type.I32)
       val argv   = Val.Local(fresh(), Type.Ptr)
       val module = Val.Local(fresh(), Type.Module(entry.top))
       val rt     = Val.Local(fresh(), Rt)
       val arr    = Val.Local(fresh(), ObjectArray)
+      val exc    = Val.Local(fresh(), nir.Rt.Object)
 
       defns :+ Defn.Define(
         Attrs.None,
         MainName,
         MainSig,
         Seq(
-          Inst.Label(fresh(), Seq(argc, argv)),
+          Inst.Label(start, Seq(argc, argv)),
+          Inst.Try(Next.Succ(succ), Next.Fail(fail)),
+          Inst.Label(succ, Seq()),
           Inst.Let(Op.Call(InitSig, Init, Seq())),
           Inst.Let(rt.name, Op.Module(Rt.name)),
           Inst.Let(arr.name, Op.Call(RtInitSig, RtInit, Seq(rt, argc, argv))),
           Inst.Let(module.name, Op.Module(entry.top)),
           Inst.Let(Op.Call(entryMainTy, entryMain, Seq(module, arr))),
-          Inst.Ret(Val.I32(0))
+          Inst.Ret(Val.I32(0)),
+          Inst.Label(fail, Seq(exc)),
+          Inst.Let(Op.Call(PrintStackTraceSig, PrintStackTrace, Seq(exc))),
+          Inst.Ret(Val.I32(1))
         )
       )
   }
@@ -57,12 +65,22 @@ object MainInjection extends PassCompanion {
   val MainName = Global.Top("main")
   val MainSig  = Type.Function(Seq(Arg(Type.I32), Arg(Type.Ptr)), Type.I32)
 
+  val ThrowableName = Global.Top("java.lang.Throwable")
+  val Throwable     = Type.Class(ThrowableName)
+
+  val PrintStackTraceSig =
+    Type.Function(Seq(Arg(Throwable)), Type.Unit)
+  val PrintStackTraceName =
+    Global.Member(ThrowableName, "printStackTrace_unit")
+  val PrintStackTrace =
+    Val.Global(PrintStackTraceName, Type.Ptr)
+
   val InitSig  = Type.Function(Seq(), Type.Unit)
   val Init     = Val.Global(Global.Top("scalanative_init"), Type.Ptr)
   val InitDecl = Defn.Declare(Attrs.None, Init.name, InitSig)
 
   override val depends =
-    Seq(ObjectArray.name, Rt.name, RtInit.name)
+    Seq(ObjectArray.name, Rt.name, RtInit.name, PrintStackTraceName)
 
   override val injects =
     Seq(InitDecl)
---
+++ unit-tests/src/main/scala/java/lang/ExceptionSuite.scala
@@ -0,0 +1,45 @@
+package java.lang
+
+class DummyNoStackTraceException extends scala.util.control.NoStackTrace
+
+object ExceptionSuite extends tests.Suite {
+  test("printStackTrace") {
+    val sw = new java.io.StringWriter
+    val pw = new java.io.PrintWriter(sw)
+    (new Exception).printStackTrace(pw)
+    val expected = Seq(
+      "java.lang.Exception",
+      "\tat java.lang.Throwable::fillInStackTrace_class.java.lang.Throwable",
+      "\tat java.lang.Throwable::init_class.java.lang.String_class.java.lang.Throwable",
+      "\tat java.lang.Exception::init_class.java.lang.String_class.java.lang.Throwable",
+      "\tat java.lang.Exception::init",
+      "\tat java.lang.ExceptionSuite$$anonfun$1::apply$mcV$sp_unit",
+      "\tat tests.Suite$$anonfun$test$1::apply$mcZ$sp_bool",
+      "\tat tests.Suite$$anonfun$run$1::apply_class.tests.Test_unit",
+      "\tat tests.Suite$$anonfun$run$1::apply_class.java.lang.Object_class.java.lang.Object",
+      "\tat scala.collection.mutable.UnrolledBuffer$Unrolled::foreach_trait.scala.Function1_unit",
+      "\tat scala.collection.mutable.UnrolledBuffer::foreach_trait.scala.Function1_unit",
+      "\tat tests.Suite::run_bool",
+      "\tat tests.Main$$anonfun$main$1::apply_class.tests.Suite_unit",
+      "\tat tests.Main$$anonfun$main$1::apply_class.java.lang.Object_class.java.lang.Object",
+      "\tat scala.collection.immutable.List::foreach_trait.scala.Function1_unit",
+      "\tat tests.Main$::main_class.ssnr.ObjectArray_unit",
+      "\tat main"
+    ).mkString("\n")
+    // It's startsWith and not == as there could be additional stack
+    // activations before main in the stack trace. For example on linux
+    // there would be additional __libc_start_main and _start frames.
+    assert(sw.toString.startsWith(expected))
+  }
+
+  test("printStackTrace <no stack trace available>") {
+    val sw = new java.io.StringWriter
+    val pw = new java.io.PrintWriter(sw)
+    (new DummyNoStackTraceException).printStackTrace(pw)
+    val expected = Seq(
+      "java.lang.DummyNoStackTraceException",
+      "\t<no stack trace available>"
+    ).mkString("\n")
+    assert(sw.toString.startsWith(expected))
+  }
+}
