857105cd1a90c4fed7026126cb43c7e8a364a887
Denys Shabalin <denys.shabalin@epfl.ch>
2017-01-10 14:34:56 +0100

    Restore sandbox/Test.scala

--- sandbox/Test.scala
+++ sandbox/Test.scala
@@ -1,13 +1,4 @@
 object Test {
-
-  def newBuf: java.lang.StringBuffer =
-    new java.lang.StringBuffer
-
-  def initBuf(str: String): java.lang.StringBuffer =
-    new java.lang.StringBuffer(str)
-
-  def main(args: Array[String]): Unit = {
-    val rhs = initBuf("abef").insert(2, initBuf("abcde"), 2, 4).toString
-    assert("abcdef" == rhs, rhs)
-  }
+  def main(args: Array[String]): Unit =
+    println("Hello, world!")
 }
