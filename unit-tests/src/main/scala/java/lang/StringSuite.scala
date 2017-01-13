e41fe9820db51896fca84a71a9b7af3202212fd6
Denys Shabalin <den.shabalin@gmail.com>
2017-01-13 19:21:39 +0100

    Minor improvements to java.lang.String (#488)
    * Add support for naive java.lang.String::intern
    * Better tests for java.lang.String::{indexOf, lastIndexOf}

--- unit-tests/src/main/scala/java/lang/StringSuite.scala
+++ unit-tests/src/main/scala/java/lang/StringSuite.scala
@@ -1,7 +1,7 @@
 package java.lang
 
 object StringSuite extends tests.Suite {
-  test("concat primitive") {
+  test("+") {
     assert("big 5" == "big " + 5.toByte)
     assert("big 5" == "big " + 5.toShort)
     assert("big 5" == "big " + 5)
@@ -10,56 +10,45 @@ object StringSuite extends tests.Suite {
     assert("5 big" == 5.toShort + " big")
     assert("5 big" == 5 + " big")
     assert("5 big" == 5L + " big")
-  }
-
-  test("concat string") {
     assert("foo" == "foo" + "")
     assert("foo" == "" + "foo")
     assert("foobar" == "foo" + "bar")
     assert("foobarbaz" == "foo" + "bar" + "baz")
   }
 
-  test("String.indexOf") {
-    assert("test".indexOf("e", 100) == -1) // indexOf outside the range
-    assert("".indexOf("e", 0) == -1)       // empty
-    assert("test".indexOf("e", 0) == 1)    // pos1
-  }
-
-  test("String.compareTo") {
+  test("compareTo") {
     assert("test".compareTo("utest") < 0)
     assert("test".compareTo("test") == 0)
     assert("test".compareTo("stest") > 0)
     assert("test".compareTo("tess") > 0)
   }
 
-  //need toUpperCase and toLowerCase from Charset to be implemented for those tests to work.
-  /*test("String.compareToIgnoreCase") {
+  testFails("compareToIgnoreCase", issue = 380) {
     assert("test".compareToIgnoreCase("Utest") < 0)
     assert("test".compareToIgnoreCase("Test") == 0)
     assert("Test".compareToIgnoreCase("stest") > 0)
     assert("tesT".compareToIgnoreCase("teSs") > 0)
-  }*/
+  }
 
-  //need toUpperCase and toLowerCase from Charset to be implemented for those tests to work.
-  /*test("String.equalsIgnoreCase") {
+  testFails("equalsIgnoreCase", issue = 380) {
     assert("test".equalsIgnoreCase("TEST"))
     assert("TEst".equalsIgnoreCase("teST"))
     assert(!("SEst".equalsIgnoreCase("TEss")))
-  }*/
+  }
 
-  test("String.regionMatches") {
+  test("regionMatches") {
     assert("This is a test".regionMatches(10, "test", 0, 4))
     assert(!("This is a test".regionMatches(10, "TEST", 0, 4)))
     assert("This is a test".regionMatches(0, "This", 0, 4))
   }
 
-  test("String.replace") {
+  test("replace") {
     assert("test".replace('t', 'p') equals "pesp")
     assert("Test".replace('t', 'p') equals "Tesp")
     assert("Test".replace('T', 'p') equals "pest")
   }
 
-  test("String.getBytes") {
+  test("getBytes") {
     val b = new Array[scala.Byte](4)
     "This is a test".getBytes(10, 14, b, 0)
     assert(new String(b) equals "test")
@@ -68,5 +57,42 @@ object StringSuite extends tests.Suite {
   test("literals have consistent hash code implementation") {
     assert(
       "foobar".hashCode == new String(Array('f', 'o', 'o', 'b', 'a', 'r')).hashCode)
+  }
+
+  testFails("intern", issue = 486) {
+    val chars = Array('f', 'o', 'o', 'b', 'a', 'r')
+    val s1    = new String(chars)
+    val s2    = new String(chars)
+    assert(s1.intern eq s2.intern)
+  }
+
+  test("indexOf") {
+    assert("afoobar".indexOf("a") == 0)
+    assert("afoobar".indexOf(97) == 0)
+    assert("afoobar".indexOf("a", 1) == 5)
+    assert("afoobar".indexOf(97, 1) == 5)
+    assert("".indexOf("a") == -1)
+    assert("".indexOf(97) == -1)
+    assert("".indexOf("a", 4) == -1)
+    assert("".indexOf(97, 4) == -1)
+    assert("fubår".indexOf("a") == -1)
+    assert("fubår".indexOf(97) == -1)
+    assert("fubår".indexOf("a", 4) == -1)
+    assert("fubår".indexOf(97, 4) == -1)
+  }
+
+  test("lastIndexOf") {
+    assert("afoobar".lastIndexOf("a") == 5)
+    assert("afoobar".lastIndexOf(97) == 5)
+    assert("afoobar".lastIndexOf("a", 4) == 0)
+    assert("afoobar".lastIndexOf(97, 4) == 0)
+    assert("".lastIndexOf("a") == -1)
+    assert("".lastIndexOf(97) == -1)
+    assert("".lastIndexOf("a", 4) == -1)
+    assert("".lastIndexOf(97, 4) == -1)
+    assert("fubår".lastIndexOf("a") == -1)
+    assert("fubår".lastIndexOf(97) == -1)
+    assert("fubår".lastIndexOf("a", 4) == -1)
+    assert("fubår".lastIndexOf(97, 4) == -1)
   }
 }
