ce215cad49c7569a0d672658cfbc271f4a9fa184
Denys Shabalin <denys.shabalin@epfl.ch>
2017-03-14 10:26:42 +0100

    Hide "Show Source" from the docs website

--- docs/conf.py
+++ docs/conf.py
@@ -195,7 +195,7 @@ html_static_path = ['_static']
 
 # If true, links to the reST sources are added to the pages.
 #
-# html_show_sourcelink = True
+html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 #
