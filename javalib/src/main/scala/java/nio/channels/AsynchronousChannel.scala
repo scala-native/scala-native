4ceb31cbe58c425e5077b3baa333916b2a4f1178
Martin Duhem <martin.duhem@gmail.com>
2017-03-17 08:58:11 +0100

    Scaffolding for AsynchronousChannel

---
+++ javalib/src/main/scala/java/nio/channels/AsynchronousChannel.scala
@@ -0,0 +1,3 @@
+package java.nio.channels
+
+trait AsynchronousChannel extends Channel
---
+++ javalib/src/main/scala/java/nio/channels/AsynchronousFileChannel.scala
@@ -0,0 +1,36 @@
+package java.nio.channels
+
+import java.util.concurrent.{ExecutorService, Future}
+import java.nio.ByteBuffer
+import java.nio.file.{OpenOption, Path}
+import java.nio.file.attribute.FileAttribute
+
+
+abstract class AsynchronousFileChannel extends AsynchronousChannel {
+  def size(): Long
+  def truncate(size: Long): AsynchronousFileChannel
+  def force(metaData: Boolean): Unit
+  def lock[A](position: Long, size: Long, shared: Boolean, attachment: A, handler: CompletionHandler[FileLock, _ >: A]): Unit
+  final def lock[A](attachment: A, handler: CompletionHandler[FileLock, _ >: A]): Unit =
+    lock(0L, Long.MaxValue, false, attachment, handler)
+  def lock(position: Long, size: Long, shared: Boolean): Future[FileLock]
+  final def lock(): Future[FileLock] =
+    lock(0L, Long.MaxValue, false)
+  def tryLock(position: Long, size: Long, shared: Boolean): FileLock
+  final def tryLock(): FileLock =
+    tryLock(0L, Long.MaxValue, false)
+  def read[A](dst: ByteBuffer, position: Long, attachment: A, handler: CompletionHandler[Integer, _ >: A]): Unit
+  def read(dst: ByteBuffer, position: Long): Future[Integer]
+  def write[A](src: ByteBuffer, position: Long, attachment: A, handler: CompletionHandler[Integer, _ >: A]): Unit
+  def write(src: ByteBuffer, position: Long)
+}
+
+object AsynchronousFileChannel {
+  // TODO:
+  def open(file: Path, options: Set[_ <: OpenOption], executor: ExecutorService, attrs: FileAttribute[_]*): AsynchronousFileChannel =
+    ???
+
+  // TODO:
+  def open(file: Path, options: OpenOption*): AsynchronousFileChannel =
+    ???
+}
