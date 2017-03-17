2763b5afd71f74e39adf76643452761b3ad6aeb5
Martin Duhem <martin.duhem@gmail.com>
2017-03-17 09:46:21 +0100

    Add `FileSystem`

---
+++ javalib/src/main/scala/java/nio/file/FileSystem.scala
@@ -0,0 +1,22 @@
+package java.nio.file
+
+import java.io.Closeable
+import java.lang.Iterable
+import java.nio.file.attribute.UserPrincipalLookupService
+import java.nio.file.spi.FileSystemProvider
+import java.util.Set
+
+abstract class FileSystem extends Closeable {
+  def close(): Unit
+  def getFileStores(): Iterable[FileStore]
+  def getPath(first: String, more: String*): Path
+  def getPathMatcher(syntaxAndPattern: String): PathMatcher
+  def getRootDirectories(): Iterable[Path]
+  def getSeparator(): String
+  def getUserPrincipalLookupService(): UserPrincipalLookupService
+  def isOpen(): Boolean
+  def isReadOnly(): Boolean
+  def newWatchService(): WatchService
+  def provider(): FileSystemProvider
+  def supportedFileAttributeViews(): Set[String]
+}
