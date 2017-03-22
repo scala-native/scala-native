0e71f0e38a40e9e25b9ee08886cd38001c7aa8fb
Martin Duhem <martin.duhem@gmail.com>
2017-03-22 15:27:50 +0100

    Test for `Path.subpath`

--- unit-tests/src/main/scala/java/nio/file/PathSuite.scala
+++ unit-tests/src/main/scala/java/nio/file/PathSuite.scala
@@ -53,4 +53,24 @@ object PathSuite extends tests.Suite {
     assert(Paths.get("foo/bar").getFileName.toString == "bar")
     assert(Paths.get("/foo/bar").getFileName.toString == "bar")
   }
+
+  test("Path.subpath") {
+    assert(Paths.get("").subpath(0, 1).toString == "")
+    assertThrows[IllegalArgumentException] {
+      Paths.get("").subpath(0, 2)
+    }
+
+    assert(Paths.get("foo/bar/baz").subpath(0, 1).toString == "foo")
+    assert(Paths.get("foo/bar/baz").subpath(0, 2).toString == "foo/bar")
+    assert(Paths.get("foo/bar/baz").subpath(0, 3).toString == "foo/bar/baz")
+    assert(Paths.get("foo/bar/baz").subpath(1, 3).toString == "bar/baz")
+    assert(Paths.get("foo/bar/baz").subpath(2, 3).toString == "baz")
+
+    assert(Paths.get("/foo/bar/baz").subpath(0, 1).toString == "foo")
+    assert(Paths.get("/foo/bar/baz").subpath(0, 2).toString == "foo/bar")
+    assert(Paths.get("/foo/bar/baz").subpath(0, 3).toString == "foo/bar/baz")
+    assert(Paths.get("/foo/bar/baz").subpath(1, 3).toString == "bar/baz")
+    assert(Paths.get("/foo/bar/baz").subpath(2, 3).toString == "baz")
+  }
+
 }
