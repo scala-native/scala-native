54b226ba19999e9d26f03aba22c075b531a1faf3
Martin Duhem <martin.duhem@gmail.com>
2017-04-03 17:27:20 +0200

    Add channel exceptions

---
+++ javalib/src/main/scala/java/nio/channels/NonReadableChannelException.scala
@@ -0,0 +1,5 @@
+package java.nio.channels
+
+class NonReadableChannelException extends Exception {
+  // TODO: Check its conformant; I don't have the doc atm...
+}
---
+++ javalib/src/main/scala/java/nio/channels/NonWritableChannelException.scala
@@ -0,0 +1,5 @@
+package java.nio.channels
+
+class NonWritableChannelException extends Exception {
+  // TODO: Check its conformant; I don't have the doc atm...
+}
