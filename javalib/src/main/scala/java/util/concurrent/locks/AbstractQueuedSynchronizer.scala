570069d08c438e425720f2bb38070aa9e798b0f5
Guillaume Massé <masgui@gmail.com>
2017-04-19 16:21:36 -0400

    Stub for java.util.concurrent classes needed for utest (#435)

---
+++ javalib/src/main/scala/java/util/concurrent/locks/AbstractQueuedSynchronizer.scala
@@ -0,0 +1,57 @@
+package java.util
+package concurrent
+package locks
+
+abstract class AbstractQueuedSynchronizer extends AbstractOwnableSynchronizer {
+  def acquire(arg: Int): Unit
+  def acquireInterruptibly(arg: Int): Unit
+  def acquireShared(arg: Int): Unit
+  def acquireSharedInterruptibly(arg: Int): Unit
+
+  def getFirstQueuedThread(): Thread
+
+  def getExclusiveQueuedThreads(): Collection[Thread]
+  def getQueuedThreads(): Collection[Thread]
+  def getSharedQueuedThreads(): Collection[Thread]
+  def getWaitingThreads(condition: ConditionObject): Collection[Thread]
+
+  def getQueueLength(): Int
+  def getWaitQueueLength(condition: ConditionObject): Int
+
+  def hasContended(): Boolean
+  def hasQueuedPredecessors(): Boolean
+  def hasQueuedThreads(): Boolean
+  def hasWaiters(condition: ConditionObject): Boolean
+  def isQueued(thread: Thread): Boolean
+  def owns(condition: ConditionObject): Boolean
+  def release(arg: Int): Boolean
+  def releaseShared(arg: Int): Boolean
+  def tryAcquireNanos(arg: Int, nanosTimeout: Long): Boolean
+  def tryAcquireSharedNanos(arg: Int, nanosTimeout: Long): Boolean
+
+  protected def getState(): Int
+  protected def compareAndSetState(expect: Int, update: Int): Boolean
+  protected def isHeldExclusively(): Boolean
+  protected def setState(newState: Int): Void
+  protected def tryAcquire(arg: Int): Boolean
+  protected def tryAcquireShared(arg: Int): Int
+  protected def tryRelease(arg: Int): Boolean
+  protected def tryReleaseShared(arg: Int): Boolean
+
+  override def toString: String = ???
+
+  class ConditionObject {
+    def await(): Unit                = ???
+    def awaitUninterruptibly(): Unit = ???
+    def signal(): Unit               = ???
+    def signalAll(): Unit            = ???
+
+    def awaitNanos(nanosTimeout: Long): Long       = ???
+    def await(time: Long, unit: TimeUnit): Boolean = ???
+    def awaitUntil(deadline: Date): Boolean        = ???
+
+    protected def getWaitingThreads(): Collection[Thread] = ???
+    protected def getWaitQueueLength(): Int               = ???
+    protected def hasWaiters(): Boolean                   = ???
+  }
+}
