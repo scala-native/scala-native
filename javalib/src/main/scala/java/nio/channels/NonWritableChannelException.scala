13f513689955c162709c91249a78e738b4fc7e28
Martin Duhem <martin.duhem@gmail.com>
2017-05-02 07:57:54 +0200

    Fix NonWritableChannelException

--- javalib/src/main/scala/java/nio/channels/NonWritableChannelException.scala
+++ javalib/src/main/scala/java/nio/channels/NonWritableChannelException.scala
@@ -1,5 +1,3 @@
 package java.nio.channels
 
-class NonWritableChannelException extends Exception {
-  // TODO: Check its conformant; I don't have the doc atm...
-}
+class NonWritableChannelException extends IllegalStateException
