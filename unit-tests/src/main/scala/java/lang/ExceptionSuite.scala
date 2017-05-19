eea39b2b7e0073b026f22422ec28c54e169b180f
Martin Duhem <martin.duhem@gmail.com>
2017-05-19 11:50:54 +0200

    Make `StackTraceElement` closer to Java's impl

--- unit-tests/src/main/scala/java/lang/ExceptionSuite.scala
+++ unit-tests/src/main/scala/java/lang/ExceptionSuite.scala
@@ -9,7 +9,9 @@ object ExceptionSuite extends tests.Suite {
     (new Exception).printStackTrace(pw)
     val trace = sw.toString
     assert(trace.startsWith("java.lang.Exception"))
-    assert(trace.contains("\tat tests.Main$.main(Unknown Source)"))
+    assert(
+      trace.contains(
+        "\tat tests.Main$.main(Unknown Source)"))
     assert(trace.contains("\tat <none>.main(Unknown Source)"))
   }
 
