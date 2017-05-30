8023dafe2622e56683e40a61395b216cb5a8990c
Remi Coudert <remi.coudert@epfl.ch>
2017-05-30 16:14:13 +0200

    std atomics

--- nativelib/src/main/scala/scala/scalanative/runtime/Atomic.scala
+++ nativelib/src/main/scala/scala/scalanative/runtime/Atomic.scala
@@ -1,86 +1,169 @@
 package scala.scalanative
 package runtime
 
-import scala.scalanative.native.{CBool, CChar, CDouble, CFloat, CInt, CLong, CShort, CSize, CUnsignedChar, CUnsignedInt, CUnsignedLong, CUnsignedShort, Ptr, extern}
+import scala.scalanative.native.{
+  CBool,
+  CChar,
+  CInt,
+  CLong,
+  CShort,
+  CSize,
+  CUnsignedChar,
+  CUnsignedInt,
+  CUnsignedLong,
+  CUnsignedShort,
+  Ptr,
+  extern
+}
+
+// see http://en.cppreference.com/w/cpp/atomic
 
 @extern
 object Atomic {
 
   // Compare and Swap
-  def compare_and_swap_strong_int(value: Ptr[CInt], expected: CInt, desired: CInt): CBool = extern
+  def compare_and_swap_strong_int(value: Ptr[CInt],
+                                  expected: Ptr[CInt],
+                                  desired: CInt): CBool = extern
 
-  /*def compare_and_swap_bool(value: Ptr[CBool], expected: CBool, desired: CBool): CBool = extern
+  def compare_and_swap_weak_int(value: Ptr[CInt],
+                                expected: Ptr[CInt],
+                                desired: CInt): CBool = extern
 
-  def compare_and_swap_short(value: Ptr[CShort], expected: CShort, desired: CShort): CBool = extern
+  def compare_and_swap_strong_bool(value: Ptr[CBool],
+                                   expected: CBool,
+                                   desired: CBool): CBool = extern
 
-  def compare_and_swap_long(value: Ptr[CLong], expected: CLong, desired: CLong): CBool = extern
+  def compare_and_swap_weak_bool(value: Ptr[CBool],
+                                 expected: CBool,
+                                 desired: CBool): CBool = extern
 
-  def compare_and_swap_char(value: Ptr[CChar], expected: CChar, desired: CChar): CBool = extern
+  def compare_and_swap_strong_short(value: Ptr[CShort],
+                                    expected: CShort,
+                                    desired: CShort): CBool = extern
 
-  def compare_and_swap_byte(value: Ptr[Byte], expected: Byte, desired: Byte): CBool = extern
+  def compare_and_swap_weak_short(value: Ptr[CShort],
+                                  expected: CShort,
+                                  desired: CShort): CBool = extern
 
-  def compare_and_swap_uint(value: Ptr[CUnsignedInt], expected: CUnsignedInt, desired: CUnsignedInt): CBool = extern
+  def compare_and_swap_strong_long(value: Ptr[CLong],
+                                   expected: CLong,
+                                   desired: CLong): CBool = extern
 
-  def compare_and_swap_ulong(value: Ptr[CUnsignedLong], expected: CUnsignedLong, desired: CUnsignedLong): CBool = extern
+  def compare_and_swap_weak_long(value: Ptr[CLong],
+                                 expected: CLong,
+                                 desired: CLong): CBool = extern
 
-  def compare_and_swap_ushort(value: Ptr[CUnsignedShort], expected: CUnsignedShort, desired: CUnsignedShort): CBool = extern
+  def compare_and_swap_strong_char(value: Ptr[CChar],
+                                   expected: CChar,
+                                   desired: CChar): CBool = extern
 
-  def compare_and_swap_uchar(value: Ptr[CUnsignedChar], expected: CUnsignedChar, desired: CUnsignedChar): CBool = extern
+  def compare_and_swap_weak_char(value: Ptr[CChar],
+                                 expected: CChar,
+                                 desired: CChar): CBool = extern
 
-  def compare_and_swap_csize(value: Ptr[CSize], expected: CSize, desired: CSize): CBool = extern
+  def compare_and_swap_strong_byte(value: Ptr[Byte],
+                                   expected: Byte,
+                                   desired: Byte): CBool = extern
+
+  def compare_and_swap_weak_byte(value: Ptr[Byte],
+                                 expected: Byte,
+                                 desired: Byte): CBool = extern
+
+  def compare_and_swap_stronguint(value: Ptr[CUnsignedInt],
+                                  expected: CUnsignedInt,
+                                  desired: CUnsignedInt): CBool = extern
+
+  def compare_and_swap_weak_uint(value: Ptr[CUnsignedInt],
+                                 expected: CUnsignedInt,
+                                 desired: CUnsignedInt): CBool = extern
+
+  def compare_and_swap_strong_ulong(value: Ptr[CUnsignedLong],
+                                    expected: CUnsignedLong,
+                                    desired: CUnsignedLong): CBool = extern
+
+  def compare_and_swap_weak_ulong(value: Ptr[CUnsignedLong],
+                                  expected: CUnsignedLong,
+                                  desired: CUnsignedLong): CBool = extern
+
+  def compare_and_swap_strong_ushort(value: Ptr[CUnsignedShort],
+                                     expected: CUnsignedShort,
+                                     desired: CUnsignedShort): CBool = extern
+
+  def compare_and_swap_weak_ushort(value: Ptr[CUnsignedShort],
+                                   expected: CUnsignedShort,
+                                   desired: CUnsignedShort): CBool = extern
+
+  def compare_and_swap_strong_uchar(value: Ptr[CUnsignedChar],
+                                    expected: CUnsignedChar,
+                                    desired: CUnsignedChar): CBool = extern
+
+  def compare_and_swap_weak_uchar(value: Ptr[CUnsignedChar],
+                                  expected: CUnsignedChar,
+                                  desired: CUnsignedChar): CBool = extern
+
+  def compare_and_swap_strong_csize(value: Ptr[CSize],
+                                    expected: CSize,
+                                    desired: CSize): CBool = extern
+
+  def compare_and_swap_weak_csize(value: Ptr[CSize],
+                                  expected: CSize,
+                                  desired: CSize): CBool = extern
 
   // Add and Sub
-  def add_and_fetch_int(ptr: Ptr[CInt], value: CInt): CInt = extern
+  def atomic_add_int(ptr: Ptr[CInt], value: CInt): CInt = extern
 
-  def add_and_fetch_bool(ptr: Ptr[CBool], value: CBool): CBool = extern
+  def atomic_add_long(ptr: Ptr[CLong], value: CLong): CLong = extern
 
-  def add_and_fetch_long(ptr: Ptr[CLong], value: CLong): CLong = extern
+  def atomic_add_short(ptr: Ptr[CShort], value: CShort): CShort = extern
 
-  def add_and_fetch_short(ptr: Ptr[CShort], value: CShort): CShort = extern
+  def atomic_add_char(ptr: Ptr[CChar], value: CChar): CChar = extern
 
-  def add_and_fetch_char(ptr: Ptr[CChar], value: CChar): CChar = extern
+  def atomic_add_byte(ptr: Ptr[Byte], value: Byte): Byte = extern
 
-  def add_and_fetch_byte(ptr: Ptr[Byte], value: Byte): Byte = extern
+  def atomic_add_uint(ptr: Ptr[CUnsignedInt],
+                      value: CUnsignedInt): CUnsignedInt = extern
 
-  def add_and_fetch_uint(ptr: Ptr[CUnsignedInt], value: CUnsignedInt): CUnsignedInt = extern
+  def atomic_add_ulong(ptr: Ptr[CUnsignedLong],
+                       value: CUnsignedLong): CUnsignedLong = extern
 
-  def add_and_fetch_ulong(ptr: Ptr[CUnsignedLong], value: CUnsignedLong): CUnsignedLong = extern
+  def atomic_add_ushort(ptr: Ptr[CUnsignedShort],
+                        value: CUnsignedShort): CUnsignedShort = extern
 
-  def add_and_fetch_ushort(ptr: Ptr[CUnsignedShort], value: CUnsignedShort): CUnsignedShort = extern
+  def atomic_add_uchar(ptr: Ptr[CUnsignedChar],
+                       value: CUnsignedChar): CUnsignedChar = extern
 
-  def add_and_fetch_uchar(ptr: Ptr[CUnsignedChar], value: CUnsignedChar): CUnsignedChar = extern
+  def atomic_add_csize(ptr: Ptr[CSize], value: CSize): CSize = extern
 
-  def add_and_fetch_csize(ptr: Ptr[CSize], value: CSize): CSize = extern
+  def atomic_sub_int(ptr: Ptr[CInt], value: CInt): CInt = extern
 
-  def sub_and_fetch_int(ptr: Ptr[CInt], value: CInt): CInt = extern
+  def atomic_sub_long(ptr: Ptr[CLong], value: CLong): CLong = extern
 
-  def sub_and_fetch_bool(ptr: Ptr[CBool], value: CBool): CBool = extern
+  def atomic_sub_short(ptr: Ptr[CShort], value: CShort): CShort = extern
 
-  def sub_and_fetch_long(ptr: Ptr[CLong], value: CLong): CLong = extern
+  def atomic_sub_char(ptr: Ptr[CChar], value: CChar): CChar = extern
 
-  def sub_and_fetch_short(ptr: Ptr[CShort], value: CShort): CShort = extern
+  def atomic_sub_byte(ptr: Ptr[Byte], value: Byte): Byte = extern
 
-  def sub_and_fetch_char(ptr: Ptr[CChar], value: CChar): CChar = extern
+  def atomic_sub_uint(ptr: Ptr[CUnsignedInt],
+                      value: CUnsignedInt): CUnsignedInt = extern
 
-  def sub_and_fetch_byte(ptr: Ptr[Byte], value: Byte): Byte = extern
+  def atomic_sub_ulong(ptr: Ptr[CUnsignedLong],
+                       value: CUnsignedLong): CUnsignedLong = extern
 
-  def sub_and_fetch_uint(ptr: Ptr[CUnsignedInt], value: CUnsignedInt): CUnsignedInt = extern
+  def atomic_sub_ushort(ptr: Ptr[CUnsignedShort],
+                        value: CUnsignedShort): CUnsignedShort = extern
 
-  def sub_and_fetch_ulong(ptr: Ptr[CUnsignedLong], value: CUnsignedLong): CUnsignedLong = extern
+  def atomic_sub_uchar(ptr: Ptr[CUnsignedChar],
+                       value: CUnsignedChar): CUnsignedChar = extern
 
-  def sub_and_fetch_ushort(ptr: Ptr[CUnsignedShort], value: CUnsignedShort): CUnsignedShort = extern
-
-  def sub_and_fetch_uchar(ptr: Ptr[CUnsignedChar], value: CUnsignedChar): CUnsignedChar = extern
-
-  def sub_and_fetch_csize(ptr: Ptr[CSize], value: CSize): CSize = extern
+  def atomic_sub_csize(ptr: Ptr[CSize], value: CSize): CSize = extern
 
   // Boolean operations
-  def or_and_fetch_bool(ptr: Ptr[CBool], value: CBool): CBool = extern
+  def atomic_or(ptr: Ptr[CInt], value: CInt): CInt = extern
 
-  def and_and_fetch_bool(ptr: Ptr[CBool], value: CBool): CBool = extern
+  def atomic_and(ptr: Ptr[CInt], value: CInt): CInt = extern
 
-  def xor_and_fetch_bool(ptr: Ptr[CBool], value: CBool): CBool = extern
-
-  def nand_and_fetch_bool(ptr: Ptr[CBool], value: CBool): CBool = extern*/
+  def atomic_xor(ptr: Ptr[CInt], value: CInt): CInt = extern
 
 }
--- unit-tests/src/main/scala/scala/scalanative/native/AtomicSuite.scala
+++ unit-tests/src/main/scala/scala/scalanative/native/AtomicSuite.scala
@@ -14,64 +14,66 @@ object AtomicSuite extends tests.Suite {
     !b = 2
     !c = 1
 
-    assertNot(compare_and_swap_strong_int(a, !b, 3))
+    assertNot(compare_and_swap_strong_int(a, b, 3))
 
-    assert(compare_and_swap_strong_int(c, !a, 3))
+    assert(compare_and_swap_strong_int(c, a, 3))
 
     assert(!c == 3)
   }
-/*
-  test("add/sub and fetch long") {
+
+  test("atomic add/sub on long") {
     val a = stackalloc[CLong]
     val b = stackalloc[CLong]
 
     !a = 1
     !b = 2
 
-    assert(add_and_fetch_long(a, 1) == 2)
-    assert(sub_and_fetch_long(b, 1) == 1)
+    assert(atomic_add_long(a, 1) == 2)
+    assert(atomic_sub_long(b, 1) == 1)
   }
 
-  test("add/sub and fetch short") {
+  test("atomic add/sub on short") {
     val a = stackalloc[CShort]
     val b = stackalloc[CShort]
 
     !a = 1.toShort
     !b = 2.toShort
 
-    assert(add_and_fetch_short(a, 1) == 2)
-    assert(sub_and_fetch_short(b, 1) == 1)
+    assert(atomic_add_short(a, 1) == 2)
+    assert(atomic_sub_short(b, 1) == 1)
   }
 
-  test("add/sub and fetch csize") {
+  test("atomic add/sub on csize") {
     val a = stackalloc[CSize]
     val b = stackalloc[CSize]
 
     !a = 1
     !b = 2
 
-    assert(add_and_fetch_csize(a, 1) == 2.toShort)
-    assert(sub_and_fetch_csize(b, 1) == 1.toShort)
+    assert(atomic_add_csize(a, 1) == 2.toShort)
+    assert(atomic_sub_csize(b, 1) == 1.toShort)
   }
 
-  test("add/sub and fetch char") {
+  test("atomic add/sub on char") {
     val a = stackalloc[CChar]
     val b = stackalloc[CChar]
 
     !a = 'a'
     !b = 'b'
 
-    assert(add_and_fetch_char(a, 1) == 'b')
-    assert(sub_and_fetch_char(b, 1) == 'a')
+    assert(atomic_add_char(a, 1) == 'b')
+    assert(atomic_sub_char(b, 1) == 'a')
   }
 
-  test("or/nand and fetch on booleans") {
-    val b = stackalloc[CBool]
+  test("atomic or/and on boolean") {
+    val a = stackalloc[CInt]
+    val b = stackalloc[CInt]
 
-    !b = false
+    !a = 1
+    !b = 0
 
-    assert(or_and_fetch_bool(b, true) == true)
-    assert(nand_and_fetch_bool(b, true) == false)
-  }*/
+    assert(atomic_and(a, 0) == 0)
+    assert(atomic_or(b, 1) == 1)
+  }
 
 }
