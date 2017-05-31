39a3710adb17acd175fb2904e0b24b3bc67e638e
Greg Oledzki <goledzki@sumologic.com>
2017-05-31 21:50:55 +0200

    scalafmt

--- unit-tests/src/main/scala/java/io/FileReaderSuite.scala
+++ unit-tests/src/main/scala/java/io/FileReaderSuite.scala
@@ -4,7 +4,8 @@ import scala.util.Try
 
 object FileReaderSuite extends tests.Suite {
 
-  test("throws FileNotFoundException when creating new FileReader with non-existing file path") {
+  test(
+    "throws FileNotFoundException when creating new FileReader with non-existing file path") {
     assertThrows[FileNotFoundException] {
       new FileReader("/the/path/does/not/exist/for/sure")
     }
