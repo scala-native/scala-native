aaea64b5b5c883e2d64a19909e67b99190342484
Grzegorz Olędzki <grzegon@poczta.onet.pl>
2017-06-03 22:14:42 +0200

    scalafmt

--- unit-tests/src/main/scala/java/io/FileWriterSuite.scala
+++ unit-tests/src/main/scala/java/io/FileWriterSuite.scala
@@ -4,7 +4,8 @@ import scala.util.Try
 
 object FileWriterSuite extends tests.Suite {
 
-  test("throws FileNotFoundException when writing to a directory (or non-existing path)") {
+  test(
+    "throws FileNotFoundException when writing to a directory (or non-existing path)") {
     assertThrows[FileNotFoundException] {
       new FileWriter("/etc")
     }
