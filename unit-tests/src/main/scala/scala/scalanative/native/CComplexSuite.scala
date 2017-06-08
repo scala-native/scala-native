9e169a8536710345d06c28b99298d416032acbae
ekrich <ekrichardson@gmail.com>
2017-06-08 16:18:40 -0400

    Finish the tests

--- unit-tests/src/main/scala/scala/scalanative/native/CComplexSuite.scala
+++ unit-tests/src/main/scala/scala/scalanative/native/CComplexSuite.scala
@@ -3,9 +3,12 @@ package scala.scalanative.native
 import complex._
 import complexOps._
 
+import java.{lang => jl}
+
 // Reference
 // http://en.cppreference.com/w/c/numeric/complex
-// TODO: commented out print statements need tests
+// Ran the complex functions in C to get the results
+// in hex which are used here for the tests.
 object CComplexSuite extends tests.Suite {
   // helpers to see results
   def printD(str: String, cp: Ptr[CDoubleComplex]): Unit =
@@ -13,11 +16,12 @@ object CComplexSuite extends tests.Suite {
   def printF(str: String, cp: Ptr[CFloatComplex]): Unit =
     println(s"$str -> ${cp.re} ${cp.im}")
 
-  //assert(equalsEpsilon(cabsf(tf),  Math.sqrt(2.0).toFloat, eps)) // example
-  def equalsEpsilon(a: Float, b: Float, epsilon: Float): Boolean =
-    Math.abs(a - b) <= epsilon
-  def equalsEpsilon(a: Double, b: Double, epsilon: Double): Boolean =
-    Math.abs(a - b) <= epsilon
+  def toFloat(i: Int): Float = jl.Float.intBitsToFloat(i)
+  def toDouble(hex: String): Double = {
+    import java.math.BigInteger
+    val l = new BigInteger(hex, 16).longValue
+    jl.Double.longBitsToDouble(l)
+  }
 
   def assertEqualsComplexF(left: Ptr[CFloatComplex],
                            right: Ptr[CFloatComplex]): Unit =
@@ -32,32 +36,60 @@ object CComplexSuite extends tests.Suite {
   test("float complex") {
     val real = 1.0f
     val imag = 1.0f
-    val ulp  = Math.ulp(1.0f)
-    val eps  = Math.nextAfter(ulp / 2, 1.0f)
 
     val tf  = stackalloc[CFloatComplex].init(real, imag)
     val buf = stackalloc[CFloatComplex]
 
-//    printF("cacosf", cacosf(tf, buf))
-//    printF("casinf", casinf(tf, buf))
-//    printF("catanf", catanf(tf, buf))
-//    printF("ccosf", ccosf(tf, buf))
-//    printF("csinf", csinf(tf, buf))
-//    printF("ctanf", ctanf(tf, buf))
-//    printF("cacoshf", cacoshf(tf, buf))
-//    printF("casinhf", casinhf(tf, buf))
-//    printF("catanhf", catanhf(tf, buf))
-//    printF("ccoshf", ccoshf(tf, buf))
-//    printF("csinhf", csinhf(tf, buf))
-//    printF("ctanhf", ctanhf(tf, buf))
-//    printF("cexpf", cexpf(tf, buf))
-//    printF("clogf", clogf(tf, buf))
+    assertEqualsComplexF(
+      cacosf(tf, buf),
+      stackalloc[CFloatComplex].init(toFloat(0x3f67910a), toFloat(0xbf87d7dc)))
+    assertEqualsComplexF(
+      casinf(tf, buf),
+      stackalloc[CFloatComplex].init(toFloat(0x3f2a8eab), toFloat(0x3f87d7dc)))
+    assertEqualsComplexF(
+      catanf(tf, buf),
+      stackalloc[CFloatComplex].init(toFloat(0x3f823454), toFloat(0x3ece0210)))
+    assertEqualsComplexF(
+      ccosf(tf, buf),
+      stackalloc[CFloatComplex].init(toFloat(0x3f556f55), toFloat(0xbf7d2866)))
+    assertEqualsComplexF(
+      csinf(tf, buf),
+      stackalloc[CFloatComplex].init(toFloat(0x3fa633dc), toFloat(0x3f228cff)))
+    assertEqualsComplexF(
+      ctanf(tf, buf),
+      stackalloc[CFloatComplex].init(toFloat(0x3e8b2327), toFloat(0x3f8abe00)))
 
+    assertEqualsComplexF(
+      cacoshf(tf, buf),
+      stackalloc[CFloatComplex].init(toFloat(0x3f87d7dc), toFloat(0x3f67910a)))
+    assertEqualsComplexF(
+      casinhf(tf, buf),
+      stackalloc[CFloatComplex].init(toFloat(0x3f87d7dc), toFloat(0x3f2a8eab)))
+    assertEqualsComplexF(
+      catanhf(tf, buf),
+      stackalloc[CFloatComplex].init(toFloat(0x3ece0210), toFloat(0x3f823454)))
+    assertEqualsComplexF(
+      ccoshf(tf, buf),
+      stackalloc[CFloatComplex].init(toFloat(0x3f556f55), toFloat(0x3f7d2866)))
+    assertEqualsComplexF(
+      csinhf(tf, buf),
+      stackalloc[CFloatComplex].init(toFloat(0x3f228cff), toFloat(0x3fa633dc)))
+    assertEqualsComplexF(
+      ctanhf(tf, buf),
+      stackalloc[CFloatComplex].init(toFloat(0x3f8abe00), toFloat(0x3e8b2327)))
+    assertEqualsComplexF(
+      cexpf(tf, buf),
+      stackalloc[CFloatComplex].init(toFloat(0x3fbbfe2a), toFloat(0x40126407)))
+    assertEqualsComplexF(
+      clogf(tf, buf),
+      stackalloc[CFloatComplex].init(toFloat(0x3eb17218), toFloat(0x3f490fdb)))
     assertEquals(cabsf(tf), sqrt2.toFloat)
-
-//    printF("cpowf", cpowf(tf, tf, buf))
-//    printF("csqrtf", csqrtf(tf, buf))
-
+    assertEqualsComplexF(
+      cpowf(tf, tf, buf),
+      stackalloc[CFloatComplex].init(toFloat(0x3e8c441e), toFloat(0x3f156d6a)))
+    assertEqualsComplexF(
+      csqrtf(tf, buf),
+      stackalloc[CFloatComplex].init(toFloat(0x3f8ca1af), toFloat(0x3ee90189)))
     assertEquals(cargf(tf), qtrPI.toFloat)
     assertEquals(cimagf(tf), imag)
     assertEqualsComplexF(conjf(tf, buf),
@@ -69,32 +101,79 @@ object CComplexSuite extends tests.Suite {
   test("double complex") {
     val real = 1.0
     val imag = 1.0
-    val ulp  = Math.ulp(1.0)
-    val eps  = Math.nextAfter(ulp / 2, 1.0)
 
     val td  = stackalloc[CDoubleComplex].init(real, imag)
     val buf = stackalloc[CDoubleComplex]
 
-//    printD("cacos", cacos(td, buf))
-//    printD("casin", casin(td, buf))
-//    printD("catan", catan(td, buf))
-//    printD("ccos", ccos(td, buf))
-//    printD("csin", csin(td, buf))
-//    printD("ctan", ctan(td, buf))
-//    printD("cacosh", cacosh(td, buf))
-//    printD("casinh", casinh(td, buf))
-//    printD("catanh", catanh(td, buf))
-//    printD("ccosh", ccosh(td, buf))
-//    printD("csinh", csinh(td, buf))
-//    printD("ctanh", ctanh(td, buf))
-//    printD("cexp", cexp(td, buf))
-//    printD("clog", clog(td, buf))
-
+    assertEqualsComplexD(
+      cacos(td, buf),
+      stackalloc[CDoubleComplex].init(toDouble("3fecf2214ccccd44"),
+                                      toDouble("bff0fafb8f2f147f")))
+    assertEqualsComplexD(
+      cacos(td, buf),
+      stackalloc[CDoubleComplex].init(toDouble("3fecf2214ccccd44"),
+                                      toDouble("bff0fafb8f2f147f")))
+    assertEqualsComplexD(
+      casin(td, buf),
+      stackalloc[CDoubleComplex].init(toDouble("3fe551d55bbb8ced"),
+                                      toDouble("3ff0fafb8f2f147f")))
+    assertEqualsComplexD(
+      ctan(td, buf),
+      stackalloc[CDoubleComplex].init(toDouble("3fd16464f4a33f88"),
+                                      toDouble("3ff157bffca4a8bc")))
+    assertEqualsComplexD(
+      ccos(td, buf),
+      stackalloc[CDoubleComplex].init(toDouble("3feaadea96f4359b"),
+                                      toDouble("bfefa50ccd2ae8f3")))
+    assertEqualsComplexD(
+      csin(td, buf),
+      stackalloc[CDoubleComplex].init(toDouble("3ff4c67b74f6cc4f"),
+                                      toDouble("3fe4519fd8047f92")))
+    assertEqualsComplexD(
+      ctan(td, buf),
+      stackalloc[CDoubleComplex].init(toDouble("3fd16464f4a33f88"),
+                                      toDouble("3ff157bffca4a8bc")))
+    assertEqualsComplexD(
+      cacosh(td, buf),
+      stackalloc[CDoubleComplex].init(toDouble("3ff0fafb8f2f147f"),
+                                      toDouble("3fecf2214ccccd44")))
+    assertEqualsComplexD(
+      casinh(td, buf),
+      stackalloc[CDoubleComplex].init(toDouble("3ff0fafb8f2f147f"),
+                                      toDouble("3fe551d55bbb8ced")))
+    assertEqualsComplexD(
+      catanh(td, buf),
+      stackalloc[CDoubleComplex].init(toDouble("3fd9c041f7ed8d33"),
+                                      toDouble("3ff0468a8ace4df6")))
+    assertEqualsComplexD(
+      ccosh(td, buf),
+      stackalloc[CDoubleComplex].init(toDouble("3feaadea96f4359b"),
+                                      toDouble("3fefa50ccd2ae8f3")))
+    assertEqualsComplexD(
+      csinh(td, buf),
+      stackalloc[CDoubleComplex].init(toDouble("3fe4519fd8047f92"),
+                                      toDouble("3ff4c67b74f6cc4f")))
+    assertEqualsComplexD(
+      ctanh(td, buf),
+      stackalloc[CDoubleComplex].init(toDouble("3ff157bffca4a8bc"),
+                                      toDouble("3fd16464f4a33f88")))
+    assertEqualsComplexD(
+      cexp(td, buf),
+      stackalloc[CDoubleComplex].init(toDouble("3ff77fc5377c5a96"),
+                                      toDouble("40024c80edc62064")))
+    assertEqualsComplexD(
+      clog(td, buf),
+      stackalloc[CDoubleComplex].init(toDouble("3fd62e42fefa39ef"),
+                                      toDouble("3fe921fb54442d18")))
     assertEquals(cabs(td), sqrt2)
-
-//    printD("cpow", cpow(td, td, buf))
-//    printD("csqrt", csqrt(td, buf))
-
+    assertEqualsComplexD(
+      cpow(td, td, buf),
+      stackalloc[CDoubleComplex].init(toDouble("3fd18884016cf327"),
+                                      toDouble("3fe2adad36b098aa")))
+    assertEqualsComplexD(
+      csqrt(td, buf),
+      stackalloc[CDoubleComplex].init(toDouble("3ff19435caffa9f9"),
+                                      toDouble("3fdd203138f6c828")))
     assertEquals(carg(td), qtrPI)
     assertEquals(cimag(td), imag)
     assertEqualsComplexD(conj(td, buf),
