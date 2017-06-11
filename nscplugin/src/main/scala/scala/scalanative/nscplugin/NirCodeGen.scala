af1facc35638d2bcb7273be4041a78965666f8b9
Denys Shabalin <den.shabalin@gmail.com>
2017-06-11 19:35:52 +0200

    Fix #809 (#811)
    Another method that has non-throwing semantics on the JVM for nulls.

--- nscplugin/src/main/scala/scala/scalanative/nscplugin/NirCodeGen.scala
+++ nscplugin/src/main/scala/scala/scalanative/nscplugin/NirCodeGen.scala
@@ -1449,9 +1449,17 @@ abstract class NirCodeGen
     }
 
     def genHashCode(argp: Tree, focus: Focus) = {
-      val meth = NObjectHashCodeMethod
-      val arg  = boxValue(argp.tpe, genExpr(argp, focus))
-      genMethodCall(meth, statically = false, arg.value, Seq(), arg)
+      val arg    = boxValue(argp.tpe, genExpr(argp, focus))
+      val isnull = arg withOp Op.Comp(Comp.Ieq, Rt.Object, arg.value, Val.Null)
+      val cond   = ValTree(isnull.value)
+      val thenp  = ContTree { focus =>
+        focus withValue Val.Int(0)
+      }
+      val elsep  = ContTree { focus =>
+        val meth = NObjectHashCodeMethod
+        genMethodCall(meth, statically = false, arg.value, Seq(), focus)
+      }
+      genIf(Type.Int, cond, thenp, elsep, isnull)
     }
 
     def genArrayOp(app: Apply, code: Int, focus: Focus): Focus = {
@@ -1462,12 +1470,15 @@ abstract class NirCodeGen
       val array = genExpr(arrayp, focus)
       def elemcode = genArrayCode(arrayp.tpe)
       val method =
-        if (code == ARRAY_CLONE) RuntimeArrayCloneMethod(elemcode)
-        else if (scalaPrimitives.isArrayGet(code))
+        if (code == ARRAY_CLONE) {
+          RuntimeArrayCloneMethod(elemcode)
+        } else if (scalaPrimitives.isArrayGet(code)) {
           RuntimeArrayApplyMethod(elemcode)
-        else if (scalaPrimitives.isArraySet(code))
+        } else if (scalaPrimitives.isArraySet(code)) {
           RuntimeArrayUpdateMethod(elemcode)
-        else RuntimeArrayLengthMethod(elemcode)
+        } else {
+          RuntimeArrayLengthMethod(elemcode)
+        }
 
       genMethodCall(method, statically = true, array.value, argsp, array)
     }
