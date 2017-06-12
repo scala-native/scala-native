cfe20d2105113ce1124f8e9474d7d597c52bfd60
Martin Duhem <martin.duhem@gmail.com>
2017-06-12 14:08:31 +0200

    Deprecate `JSExportDescendent{Classes,Objects}`

--- test-interface/src/main/scala/scala/scalajs/js/annotation/JSExportDescendentObjects.scala
+++ test-interface/src/main/scala/scala/scalajs/js/annotation/JSExportDescendentObjects.scala
@@ -1,5 +1,8 @@
 package scala.scalajs.js.annotation
 
+@deprecated(
+  "0.3.0",
+  "Internal implementation detail from Scala.js, will be removed in the future.")
 class JSExportDescendentObjects(ignoreInvalidDescendants: Boolean)
     extends scala.annotation.StaticAnnotation {
   def this() = this(false)
