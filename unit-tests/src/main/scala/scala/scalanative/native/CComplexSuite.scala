02a828b51cbb8236d37d8b5bed17123824744358
ekrich <ekrichardson@gmail.com>
2017-06-12 11:54:01 -0400

    Refactor test suite to use Zones

--- unit-tests/src/main/scala/scala/scalanative/native/CComplexSuite.scala
+++ unit-tests/src/main/scala/scala/scalanative/native/CComplexSuite.scala
@@ -20,272 +20,330 @@ object CComplexSuite extends tests.Suite {
   // float complex helper fcns
   def printF(str: String, cp: Ptr[CFloatComplex]): Unit =
     println(s"$str -> ${cp.re} ${cp.im}")
+
   def toFloat(i: Int): Float = jl.Float.intBitsToFloat(i)
+
   def isAlmostEqual(act: Float, exp: Float): Boolean = {
     val diff    = Math.abs(act - exp)
     val epsilon = Math.max(Math.ulp(act), Math.ulp(exp))
     diff <= epsilon
   }
+
   def assertEqualsComplexF(act: Ptr[CFloatComplex],
                            exp: Ptr[CFloatComplex]): Unit =
     assert(isAlmostEqual(act.re, exp.re) && isAlmostEqual(act.im, exp.im))
-  // complex data for float
-  val tf = stdlib
-    .malloc(sizeof[CFloatComplex])
-    .cast[Ptr[CFloatComplex]]
-    .init(real.toFloat, imag.toFloat)
-  val buff = stdlib.malloc(sizeof[CFloatComplex]).cast[Ptr[CFloatComplex]]
+
+  def tf(implicit z: Zone) = res(real.toFloat, imag.toFloat)
+
+  def buff(implicit z: Zone) = alloc[CFloatComplex]
+
+  def res(real: Float, imag: Float)(implicit z: Zone) =
+    alloc[CFloatComplex].init(real.toFloat, imag.toFloat)
 
   test("cacosf") {
-    assertEqualsComplexF(
-      cacosf(tf, buff),
-      stackalloc[CFloatComplex].init(toFloat(0x3f67910a), toFloat(0xbf87d7dc)))
+    Zone { implicit z =>
+      assertEqualsComplexF(cacosf(tf, buff),
+                           res(toFloat(0x3f67910a), toFloat(0xbf87d7dc)))
+    }
   }
   test("casinf") {
-    assertEqualsComplexF(
-      casinf(tf, buff),
-      stackalloc[CFloatComplex].init(toFloat(0x3f2a8eab), toFloat(0x3f87d7dc)))
+    Zone { implicit z =>
+      assertEqualsComplexF(casinf(tf, buff),
+                           res(toFloat(0x3f2a8eab), toFloat(0x3f87d7dc)))
+    }
   }
   test("catanf") {
-    assertEqualsComplexF(
-      catanf(tf, buff),
-      stackalloc[CFloatComplex].init(toFloat(0x3f823454), toFloat(0x3ece0210)))
+    Zone { implicit z =>
+      assertEqualsComplexF(catanf(tf, buff),
+                           res(toFloat(0x3f823454), toFloat(0x3ece0210)))
+    }
   }
 //  test("ccosf") {
-//    assertEqualsComplexF(
-//      ccosf(tf, buff),
-//      stackalloc[CFloatComplex].init(toFloat(0x3f556f55), toFloat(0xbf7d2866)))
+//    Zone { implicit z =>
+//      assertEqualsComplexF(
+//        ccosf(tf, buff),
+//        res(toFloat(0x3f556f55), toFloat(0xbf7d2866)))
+//    }
 //  }
   test("csinf") {
-    assertEqualsComplexF(
-      csinf(tf, buff),
-      stackalloc[CFloatComplex].init(toFloat(0x3fa633dc), toFloat(0x3f228cff)))
+    Zone { implicit z =>
+      assertEqualsComplexF(csinf(tf, buff),
+                           res(toFloat(0x3fa633dc), toFloat(0x3f228cff)))
+    }
   }
   test("ctanf") {
-    assertEqualsComplexF(
-      ctanf(tf, buff),
-      stackalloc[CFloatComplex].init(toFloat(0x3e8b2327), toFloat(0x3f8abe00)))
+    Zone { implicit z =>
+      assertEqualsComplexF(ctanf(tf, buff),
+                           res(toFloat(0x3e8b2327), toFloat(0x3f8abe00)))
+    }
 
   }
   test("cacoshf") {
-    assertEqualsComplexF(
-      cacoshf(tf, buff),
-      stackalloc[CFloatComplex].init(toFloat(0x3f87d7dc), toFloat(0x3f67910a)))
+    Zone { implicit z =>
+      assertEqualsComplexF(cacoshf(tf, buff),
+                           res(toFloat(0x3f87d7dc), toFloat(0x3f67910a)))
+    }
   }
   test("casinhf") {
-    assertEqualsComplexF(
-      casinhf(tf, buff),
-      stackalloc[CFloatComplex].init(toFloat(0x3f87d7dc), toFloat(0x3f2a8eab)))
+    Zone { implicit z =>
+      assertEqualsComplexF(casinhf(tf, buff),
+                           res(toFloat(0x3f87d7dc), toFloat(0x3f2a8eab)))
+    }
   }
   test("catanhf") {
-    assertEqualsComplexF(
-      catanhf(tf, buff),
-      stackalloc[CFloatComplex].init(toFloat(0x3ece0210), toFloat(0x3f823454)))
+    Zone { implicit z =>
+      assertEqualsComplexF(catanhf(tf, buff),
+                           res(toFloat(0x3ece0210), toFloat(0x3f823454)))
+    }
   }
 //  test("ccoshf") {
-//    assertEqualsComplexF(
-//      ccoshf(tf, buff),
-//      stackalloc[CFloatComplex].init(toFloat(0x3f556f55), toFloat(0x3f7d2866)))
+//    Zone { implicit z =>
+//      assertEqualsComplexF(
+//        ccoshf(tf, buff),
+//        res(toFloat(0x3f556f55), toFloat(0x3f7d2866)))
+//    }
 //  }
   test("csinhf") {
-    assertEqualsComplexF(
-      csinhf(tf, buff),
-      stackalloc[CFloatComplex].init(toFloat(0x3f228cff), toFloat(0x3fa633dc)))
+    Zone { implicit z =>
+      assertEqualsComplexF(csinhf(tf, buff),
+                           res(toFloat(0x3f228cff), toFloat(0x3fa633dc)))
+    }
   }
   test("ctanhf") {
-    assertEqualsComplexF(
-      ctanhf(tf, buff),
-      stackalloc[CFloatComplex].init(toFloat(0x3f8abe00), toFloat(0x3e8b2327)))
+    Zone { implicit z =>
+      assertEqualsComplexF(ctanhf(tf, buff),
+                           res(toFloat(0x3f8abe00), toFloat(0x3e8b2327)))
+    }
   }
   test("cexpf") {
-    assertEqualsComplexF(
-      cexpf(tf, buff),
-      stackalloc[CFloatComplex].init(toFloat(0x3fbbfe2a), toFloat(0x40126407)))
+    Zone { implicit z =>
+      assertEqualsComplexF(cexpf(tf, buff),
+                           res(toFloat(0x3fbbfe2a), toFloat(0x40126407)))
+    }
   }
   test("clogf") {
-    assertEqualsComplexF(
-      clogf(tf, buff),
-      stackalloc[CFloatComplex].init(toFloat(0x3eb17218), toFloat(0x3f490fdb)))
+    Zone { implicit z =>
+      assertEqualsComplexF(clogf(tf, buff),
+                           res(toFloat(0x3eb17218), toFloat(0x3f490fdb)))
+    }
   }
   test("cabsf") {
-    assertEquals(cabsf(tf), sqrt2.toFloat)
+    Zone { implicit z =>
+      assertEquals(cabsf(tf), sqrt2.toFloat)
+    }
   }
   test("cpowf") {
-    assertEqualsComplexF(
-      cpowf(tf, tf, buff),
-      stackalloc[CFloatComplex].init(toFloat(0x3e8c441e), toFloat(0x3f156d6a)))
+    Zone { implicit z =>
+      assertEqualsComplexF(cpowf(tf, tf, buff),
+                           res(toFloat(0x3e8c441e), toFloat(0x3f156d6a)))
+    }
   }
   test("csqrtf") {
-    assertEqualsComplexF(
-      csqrtf(tf, buff),
-      stackalloc[CFloatComplex].init(toFloat(0x3f8ca1af), toFloat(0x3ee90189)))
+    Zone { implicit z =>
+      assertEqualsComplexF(csqrtf(tf, buff),
+                           res(toFloat(0x3f8ca1af), toFloat(0x3ee90189)))
+    }
   }
   test("cargf") {
-    assertEquals(cargf(tf), qtrPI.toFloat)
+    Zone { implicit z =>
+      assertEquals(cargf(tf), qtrPI.toFloat)
+    }
   }
   test("cimagf") {
-    assertEquals(cimagf(tf), imag)
+    Zone { implicit z =>
+      assertEquals(cimagf(tf), imag)
+    }
   }
   test("conjf") {
-    assertEqualsComplexF(
-      conjf(tf, buff),
-      stackalloc[CFloatComplex].init(real.toFloat, -imag.toFloat))
+    Zone { implicit z =>
+      assertEqualsComplexF(conjf(tf, buff), res(real.toFloat, -imag.toFloat))
+    }
   }
   test("cprojf") {
-    assertEqualsComplexF(cprojf(tf, buff), tf)
+    Zone { implicit z =>
+      assertEqualsComplexF(cprojf(tf, buff), tf)
+    }
   }
   test("crealf") {
-    assertEquals(crealf(tf), real)
+    Zone { implicit z =>
+      assertEquals(crealf(tf), real)
+    }
   }
 
   // double complex helper fcns
   def printD(str: String, cp: Ptr[CDoubleComplex]): Unit =
     println(s"$str -> ${cp.re} ${cp.im}")
+
   def toDouble(hex: String): Double = {
     import java.math.BigInteger
     val l = new BigInteger(hex, 16).longValue
     jl.Double.longBitsToDouble(l)
   }
+
   def isAlmostEqual(act: Double, exp: Double): Boolean = {
     val diff    = Math.abs(act - exp)
     val epsilon = Math.max(Math.ulp(act), Math.ulp(exp))
     diff <= epsilon
   }
+
   def assertEqualsComplexD(act: Ptr[CDoubleComplex],
                            exp: Ptr[CDoubleComplex]): Unit =
     assert(isAlmostEqual(act.re, exp.re) && isAlmostEqual(act.im, exp.im))
 
-  // complex data for double
-  val td = stdlib
-    .malloc(sizeof[CDoubleComplex])
-    .cast[Ptr[CDoubleComplex]]
-    .init(real, imag)
-  val buf = stdlib.malloc(sizeof[CDoubleComplex]).cast[Ptr[CDoubleComplex]]
+  def td(implicit z: Zone) = res(real, imag)
+
+  def buf(implicit z: Zone) = alloc[CDoubleComplex]
+
+  def res(real: Double, imag: Double)(implicit z: Zone) =
+    alloc[CDoubleComplex].init(real, imag)
 
   test("cacos") {
-    assertEqualsComplexD(
-      cacos(td, buf),
-      stackalloc[CDoubleComplex].init(toDouble("3fecf2214ccccd44"),
-                                      toDouble("bff0fafb8f2f147f")))
+    Zone { implicit z =>
+      assertEqualsComplexD(cacos(td, buf),
+                           res(toDouble("3fecf2214ccccd44"),
+                               toDouble("bff0fafb8f2f147f")))
+    }
   }
   test("cacos") {
-    assertEqualsComplexD(
-      cacos(td, buf),
-      stackalloc[CDoubleComplex].init(toDouble("3fecf2214ccccd44"),
-                                      toDouble("bff0fafb8f2f147f")))
+    Zone { implicit z =>
+      assertEqualsComplexD(cacos(td, buf),
+                           res(toDouble("3fecf2214ccccd44"),
+                               toDouble("bff0fafb8f2f147f")))
+    }
   }
   test("casin") {
-    assertEqualsComplexD(
-      casin(td, buf),
-      stackalloc[CDoubleComplex].init(toDouble("3fe551d55bbb8ced"),
-                                      toDouble("3ff0fafb8f2f147f")))
+    Zone { implicit z =>
+      assertEqualsComplexD(casin(td, buf),
+                           res(toDouble("3fe551d55bbb8ced"),
+                               toDouble("3ff0fafb8f2f147f")))
+    }
   }
   test("ctan") {
-    assertEqualsComplexD(
-      ctan(td, buf),
-      stackalloc[CDoubleComplex].init(toDouble("3fd16464f4a33f88"),
-                                      toDouble("3ff157bffca4a8bc")))
+    Zone { implicit z =>
+      assertEqualsComplexD(ctan(td, buf),
+                           res(toDouble("3fd16464f4a33f88"),
+                               toDouble("3ff157bffca4a8bc")))
+    }
   }
   test("ccos") {
-    assertEqualsComplexD(
-      ccos(td, buf),
-      stackalloc[CDoubleComplex].init(toDouble("3feaadea96f4359b"),
-                                      toDouble("bfefa50ccd2ae8f3")))
+    Zone { implicit z =>
+      assertEqualsComplexD(ccos(td, buf),
+                           res(toDouble("3feaadea96f4359b"),
+                               toDouble("bfefa50ccd2ae8f3")))
+    }
   }
   test("csin") {
-    assertEqualsComplexD(
-      csin(td, buf),
-      stackalloc[CDoubleComplex].init(toDouble("3ff4c67b74f6cc4f"),
-                                      toDouble("3fe4519fd8047f92")))
+    Zone { implicit z =>
+      assertEqualsComplexD(csin(td, buf),
+                           res(toDouble("3ff4c67b74f6cc4f"),
+                               toDouble("3fe4519fd8047f92")))
+    }
   }
   test("ctan") {
-    assertEqualsComplexD(
-      ctan(td, buf),
-      stackalloc[CDoubleComplex].init(toDouble("3fd16464f4a33f88"),
-                                      toDouble("3ff157bffca4a8bc")))
+    Zone { implicit z =>
+      assertEqualsComplexD(ctan(td, buf),
+                           res(toDouble("3fd16464f4a33f88"),
+                               toDouble("3ff157bffca4a8bc")))
+    }
   }
   test("cacosh") {
-    assertEqualsComplexD(
-      cacosh(td, buf),
-      stackalloc[CDoubleComplex].init(toDouble("3ff0fafb8f2f147f"),
-                                      toDouble("3fecf2214ccccd44")))
+    Zone { implicit z =>
+      assertEqualsComplexD(cacosh(td, buf),
+                           res(toDouble("3ff0fafb8f2f147f"),
+                               toDouble("3fecf2214ccccd44")))
+    }
   }
   test("casinh") {
-    assertEqualsComplexD(
-      casinh(td, buf),
-      stackalloc[CDoubleComplex].init(toDouble("3ff0fafb8f2f147f"),
-                                      toDouble("3fe551d55bbb8ced")))
+    Zone { implicit z =>
+      assertEqualsComplexD(casinh(td, buf),
+                           res(toDouble("3ff0fafb8f2f147f"),
+                               toDouble("3fe551d55bbb8ced")))
+    }
   }
   test("catanh") {
-    assertEqualsComplexD(
-      catanh(td, buf),
-      stackalloc[CDoubleComplex].init(toDouble("3fd9c041f7ed8d33"),
-                                      toDouble("3ff0468a8ace4df6")))
+    Zone { implicit z =>
+      assertEqualsComplexD(catanh(td, buf),
+                           res(toDouble("3fd9c041f7ed8d33"),
+                               toDouble("3ff0468a8ace4df6")))
+    }
   }
   test("ccosh") {
-    assertEqualsComplexD(
-      ccosh(td, buf),
-      stackalloc[CDoubleComplex].init(toDouble("3feaadea96f4359b"),
-                                      toDouble("3fefa50ccd2ae8f3")))
+    Zone { implicit z =>
+      assertEqualsComplexD(ccosh(td, buf),
+                           res(toDouble("3feaadea96f4359b"),
+                               toDouble("3fefa50ccd2ae8f3")))
+    }
   }
   test("csinh") {
-    assertEqualsComplexD(
-      csinh(td, buf),
-      stackalloc[CDoubleComplex].init(toDouble("3fe4519fd8047f92"),
-                                      toDouble("3ff4c67b74f6cc4f")))
+    Zone { implicit z =>
+      assertEqualsComplexD(csinh(td, buf),
+                           res(toDouble("3fe4519fd8047f92"),
+                               toDouble("3ff4c67b74f6cc4f")))
+    }
   }
   test("ctanh") {
-    assertEqualsComplexD(
-      ctanh(td, buf),
-      stackalloc[CDoubleComplex].init(toDouble("3ff157bffca4a8bc"),
-                                      toDouble("3fd16464f4a33f88")))
+    Zone { implicit z =>
+      assertEqualsComplexD(ctanh(td, buf),
+                           res(toDouble("3ff157bffca4a8bc"),
+                               toDouble("3fd16464f4a33f88")))
+    }
   }
   test("cexp") {
-    assertEqualsComplexD(
-      cexp(td, buf),
-      stackalloc[CDoubleComplex].init(toDouble("3ff77fc5377c5a96"),
-                                      toDouble("40024c80edc62064")))
+    Zone { implicit z =>
+      assertEqualsComplexD(cexp(td, buf),
+                           res(toDouble("3ff77fc5377c5a96"),
+                               toDouble("40024c80edc62064")))
+    }
   }
   test("clog") {
-    assertEqualsComplexD(
-      clog(td, buf),
-      stackalloc[CDoubleComplex].init(toDouble("3fd62e42fefa39ef"),
-                                      toDouble("3fe921fb54442d18")))
+    Zone { implicit z =>
+      assertEqualsComplexD(clog(td, buf),
+                           res(toDouble("3fd62e42fefa39ef"),
+                               toDouble("3fe921fb54442d18")))
+    }
   }
   test("cabs") {
-    assertEquals(cabs(td), sqrt2)
+    Zone { implicit z =>
+      assertEquals(cabs(td), sqrt2)
+    }
   }
   test("cpow") {
-    assertEqualsComplexD(
-      cpow(td, td, buf),
-      stackalloc[CDoubleComplex].init(toDouble("3fd18884016cf327"),
-                                      toDouble("3fe2adad36b098aa")))
+    Zone { implicit z =>
+      assertEqualsComplexD(cpow(td, td, buf),
+                           res(toDouble("3fd18884016cf327"),
+                               toDouble("3fe2adad36b098aa")))
+    }
   }
   test("csqrt") {
-    assertEqualsComplexD(
-      csqrt(td, buf),
-      stackalloc[CDoubleComplex].init(toDouble("3ff19435caffa9f9"),
-                                      toDouble("3fdd203138f6c828")))
+    Zone { implicit z =>
+      assertEqualsComplexD(csqrt(td, buf),
+                           res(toDouble("3ff19435caffa9f9"),
+                               toDouble("3fdd203138f6c828")))
+    }
   }
   test("carg") {
-    assertEquals(carg(td), qtrPI)
+    Zone { implicit z =>
+      assertEquals(carg(td), qtrPI)
+    }
   }
   test("cimag") {
-    assertEquals(cimag(td), imag)
+    Zone { implicit z =>
+      assertEquals(cimag(td), imag)
+    }
   }
   test("conj") {
-    assertEqualsComplexD(conj(td, buf),
-                         stackalloc[CDoubleComplex].init(real, -imag))
+    Zone { implicit z =>
+      assertEqualsComplexD(conj(td, buf), res(real, -imag))
+    }
   }
   test("cproj") {
-    assertEqualsComplexD(cproj(td, buf), td)
+    Zone { implicit z =>
+      assertEqualsComplexD(cproj(td, buf), td)
+    }
   }
   test("creal") {
-    assertEquals(creal(td), real)
+    Zone { implicit z =>
+      assertEquals(creal(td), real)
+    }
   }
-  // Can't seem to free here - need after test hook
-//  stdlib.free(tf.cast[Ptr[Byte]])
-//  stdlib.free(buff.cast[Ptr[Byte]])
-//  stdlib.free(td.cast[Ptr[Byte]])
-//  stdlib.free(buf.cast[Ptr[Byte]])
 }
