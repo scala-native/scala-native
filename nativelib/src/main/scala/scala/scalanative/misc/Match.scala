71e36603a96578fc0994f130ff014c6626d56add
MuxaHuk <muxanick@gmail.com>
2017-06-13 19:16:17 +0100

    misc regex

---
+++ nativelib/src/main/scala/scala/scalanative/misc/Match.scala
@@ -0,0 +1,219 @@
+package scala.scalanative.misc
+package regex
+
+import scala.scalanative.native._
+import scalanative.runtime.struct
+
+sealed trait Match {
+    def groupCount: Int
+    def format(fmt: String, flags: Match.FormatFlags = Match.default): String
+    def groupRange(index: Int): Match.Range
+    def groupString(index: Int): String
+    def tokenRange: Match.Range
+    def tokenString: String
+    def prefix: String
+    def suffix: String
+    def iteratorNext:Boolean
+    def tokenIteratorNext:Boolean
+    // compound methods
+    def group(index: Int): Match.Group
+    def token: Match.Group
+    def result: Match.MatchResult
+}
+
+object Match {
+    // Flags
+    type Flags = CUnsignedInt
+    type FormatFlags = CUnsignedInt
+
+    // Note: [first, last) refers to the character sequence being matched.
+    val default:Flags    = 0x0000.toUInt
+    // The first character in [first,last) will be treated as if it is not at the beginning of a line (i.e. ^ will not match [first,first)
+    val not_bol:Flags    = 0x0001.toUInt
+    // The last character in [first,last) will be treated as if it is not at the end of a line (i.e. $ will not match [last,last)
+    val not_eol:Flags    = 0x0002.toUInt
+    // "\b" will not match [first,first)
+    val not_bow:Flags    = 0x0004.toUInt
+    // "\b" will not match [last,last)
+    val not_eow:Flags    = 0x0008.toUInt
+    // If more than one match is possible, then any match is an acceptable result
+    val any:Flags        = 0x0010.toUInt
+    // Do not match empty sequences
+    val not_null:Flags   = 0x0020.toUInt
+    // Only match a sub-sequence that begins at first
+    val continuous:Flags = 0x0040.toUInt
+    // --first is a valid iterator position. When set, causes match_not_bol and match_not_bow to be ignored    
+    val prev_avail:Flags = 0x0100.toUInt
+
+    // Don't keep copy of input data where possible, user garantees non-moving memory (faster processing)
+    val dont_keep_text_copy:Flags = 0x8000.toUInt
+    
+    // Use ECMAScript rules to construct strings in replaceAll (http://ecma-international.org/ecma-262/5.1/#sec-15.5.4.11)
+    val format_default:FormatFlags = 0x0000.toUInt
+    // Use POSIX sed utility rules in replaceAll (http://pubs.opengroup.org/onlinepubs/9699919799/utilities/sed.html#tag_20_116_13_03)
+    val format_sed:FormatFlags = 0x0400.toUInt
+    // Do not copy un-matched strings to the output in replaceAll
+    val format_no_copy:FormatFlags = 0x0800.toUInt
+    // Only replace the first match in replaceAll
+    val format_first_only:FormatFlags = 0x1000.toUInt
+
+    private[regex] def apply(matchResult: CRegex.CRegexMatchStruct)(implicit in: Scope): Match = {
+        val r = new MatchImpl(matchResult)
+        in.acquire(r)
+    }
+
+    def compareArrays(a: Array[MatchResult], b: Array[MatchResult]): Boolean = {
+        if (a.length == b.length)
+        {
+            (0 until a.length).find(i => a(i) != b(i)).isEmpty
+        }
+        else false
+    }
+
+    def compareArrays(a: Array[Group], b: Array[Group]): Boolean = {
+        if (a.length == b.length)
+        {
+            (0 until a.length).find(i => a(i) != b(i)).isEmpty
+        }
+        else false
+    }
+
+    final case class Range(val position: Int, val length: Int) {
+        override def toString: String = s"(pos: $position, len: $length)"
+
+        def ==(other:Range): Boolean = {
+            position == other.position && length == other.length
+        }
+
+        def !=(other:Range): Boolean = {
+            !(this == other)
+        }
+    }
+
+    final case class Group(string: String, range: Range) {
+        override def toString: String = s"('$string', $range)"
+
+        def ==(other:Group): Boolean = {
+            string == other.string && range == other.range
+        }
+
+        def !=(other:Group): Boolean = {
+            !(this == other)
+        }
+    }
+    final case class MatchResult(prefix: String, groups: Array[Group], suffix: String) {
+        override def toString: String = {
+            val g = groups.mkString("[", ", ", "]")
+            s"{'$prefix', $g, '$suffix'}"
+        }
+        def ==(other:MatchResult): Boolean = {
+            prefix == other.prefix && suffix == other.suffix && compareArrays(groups, other.groups)
+        }
+
+        def !=(other:MatchResult): Boolean = {
+            !(this == other)
+        }
+    }
+
+    // Private section below
+
+    private[regex] class MatchImpl(var matchResult: CRegex.CRegexMatchStruct)(implicit in: Scope) extends Match with Resource {
+
+        def groupCount: Int = {
+            CRegex.getMatchSubmatchCount(matchResult)
+        }
+
+        def groupRange(index: Int): Range = {
+            if (0<=index && index < groupCount)
+            {
+                val result = stackalloc[CInt](2)
+                CRegex.getMatchSubmatchRange(matchResult, index, result)
+                new Range(result(0), result(1))
+            }
+            else throw new RegexException("Group index out of bound")
+        }
+
+        def groupString(index: Int): String = Zone { implicit z =>
+            if (0<=index && index < groupCount)
+            {
+                val result = stackalloc[CString]
+                val result_size = stackalloc[CInt]
+                CRegex.getMatchSubmatchString(matchResult, index, result, result_size)
+                if (!result_size>0) fromCString(!result) else ""
+            }
+            else throw new RegexException("Group index out of bound")
+        }
+
+        def prefix: String = Zone { implicit z =>
+            val result = stackalloc[CString]
+            val result_size = stackalloc[CInt]
+            CRegex.getMatchSubmatchString(matchResult, -1, result, result_size)
+            if (!result_size>0) fromCString(!result) else ""
+        }
+
+        def suffix: String = Zone { implicit z =>
+            val result = stackalloc[CString]
+            val result_size = stackalloc[CInt]
+            CRegex.getMatchSubmatchString(matchResult, groupCount, result, result_size)
+            if (!result_size>0) fromCString(!result) else ""
+        }
+
+        def tokenRange: Range = {
+            val result = stackalloc[CInt](2)
+            CRegex.getMatchTokenRange(matchResult, result)
+            new Range(result(0), result(1))
+        }
+
+        def tokenString: String = Zone { implicit z =>
+            val result = stackalloc[CString]
+            val result_size = stackalloc[CInt]
+            CRegex.getMatchTokenString(matchResult, result, result_size)
+            if (!result_size>0) fromCString(!result) else ""
+        }
+
+        def iteratorNext:Boolean = {
+            matchResult = CRegex.matchIteratorNext(matchResult)
+            if (matchResult != null) true else false
+        }
+        def tokenIteratorNext:Boolean = {
+            matchResult = CRegex.matchTokenIteratorNext(matchResult)
+            if (matchResult != null) true else false
+        }
+
+        def format(fmt: String, flags: Match.FormatFlags = Match.format_default): String = Zone {
+        implicit z =>
+            val result = stackalloc[CString]
+            val result_size = stackalloc[CInt]
+            CRegex.matchFormat(matchResult, toCString(fmt), flags, result, result_size)
+            if (!result_size>0) fromCString(!result) else ""
+        }
+
+        def group(index: Int): Match.Group = {
+            new Group(groupString(index), groupRange(index))
+        }
+        def token: Match.Group = {
+            new Group(tokenString, tokenRange)
+        }
+        def result: Match.MatchResult = {
+        import scala.annotation.tailrec
+            def fill(matchResult: Match):Match.MatchResult = {
+                @tailrec def makeGroups(matchResult: Match, i: Int = 0, result: Array[Match.Group] = Array.empty[Match.Group]): Array[Match.Group] = {
+                    if (i < matchResult.groupCount)
+                    {
+                        makeGroups(matchResult, i + 1, result :+ new Match.Group(matchResult.groupString(i), matchResult.groupRange(i)))
+                    }
+                    else {
+                        result
+                    }
+                }
+                new Match.MatchResult(matchResult.prefix, makeGroups(matchResult), matchResult.suffix)
+            }
+            fill(this)
+        }
+
+        def close(): Unit = {
+            CRegex.deleteMatchResult(matchResult)
+            matchResult = null
+        }
+    }
+}
---
+++ nativelib/src/main/scala/scala/scalanative/misc/Regex.scala
@@ -0,0 +1,346 @@
+package scala.scalanative.misc
+package regex
+
+import scala.scalanative.native._
+
+sealed trait Regex {
+    def search(text: String, flags: Match.Flags = Match.default): Boolean
+    def searchWithResult(text: String, matchFlags: Match.Flags = Match.default)(implicit in: Scope): Match
+    def matchAll(text: String, matchFlags: Match.Flags = Match.default): Boolean
+    def matchAllWithResult(text: String, matchFlags: Match.Flags = Match.default)(implicit in: Scope): Match
+    def iteratorFirst(text: String, matchFlags: Match.Flags = Match.default)(implicit in: Scope): Match
+    def tokenIteratorFirst(text: String, tokens:Array[Int], matchFlags: Match.Flags = Match.default)(implicit in: Scope): Match
+    def replaceAll(text: String, format: String, formatFlags: Match.FormatFlags = Match.format_default, matchFlags: Match.Flags = Match.default): String
+    def replaceFirst(text: String, format: String, formatFlags: Match.FormatFlags = Match.format_default, matchFlags: Match.Flags = Match.default): String
+    def split(text: String, matchFlags: Match.Flags = Match.default)(implicit in: Scope): Array[String]
+    def iterator(text: String, matchFlags: Match.Flags = Match.default)(implicit in: Scope): Array[Match.MatchResult]
+    def tokenIterator(text: String, tokens:Array[Int], matchFlags: Match.Flags = Match.default)(implicit in: Scope): Array[Match.Group]
+}
+
+object Regex {
+    // Flags
+    type Flags = CUnsignedInt
+
+    // default
+    val default:Flags = 0x0000.toUInt
+
+    // "Ignore case": Character matching should be performed without regard to case.
+    val icase:Flags = 0x0100.toUInt
+
+    // "No substitutions": When performing matches, all marked sub-expressions (expr) are treated as non-marking sub-expressions (?:expr). No matches are stored in the supplied RegexMatch structure and markCount() is zero
+    val nosubs:Flags = 0x0200.toUInt
+
+    // "Optimize for multiple uses": Instructs the regular expression engine to make matching faster, with the potential cost of making construction slower. For example, this might mean converting a non-deterministic FSA to a deterministic FSA.
+    val optimize:Flags = 0x0400.toUInt
+
+    // "Locale sensetive": Character ranges of the form "[a-b]" will be locale sensitive.
+    val collate:Flags = 0x0800.toUInt
+
+    // "No unicode": Ignore UTF-8 (faster processing)
+    val no_unicode:Flags = 0x8000.toUInt
+
+    // Syntax
+    sealed trait Syntax {
+        def f:Flags = ???
+    }
+
+    // Similar to JavaScript (default)
+    // (differences from JavaScript: http://en.cppreference.com/w/cpp/regex/ecmascript)
+    final object ECMAScript extends Syntax {override val f:Flags = 0x01.toUInt}
+
+    // Similar to POSIX BRE
+    final object Basic extends Syntax {override val f:Flags = 0x02.toUInt}
+
+    // Similar to POSIX ERE
+    final object Extended extends Syntax {override val f:Flags = 0x04.toUInt}
+
+    // Same as Extended, with the addition of supporting common escapes for non-printable characters.
+    final object Awk extends Syntax {override val f:Flags = 0x08.toUInt}
+
+    // Same as Basic, with the addition of treating line feeds as alternation operators.
+    final object Grep extends Syntax {override val f:Flags = 0x10.toUInt}
+
+    // Same as Extended, with the addition of treating line feeds as alternation operators.
+    final object Egrep extends Syntax {override val f:Flags = 0x20.toUInt}
+
+    // default locale
+    val locDefault:String = "en-US"
+    
+    def apply(pattern:String, flags: Flags = default, syntax: Syntax = ECMAScript, locale: String = null)(implicit in: Scope): Regex = Zone {
+    implicit z =>
+            val cre:CRegex.CRegexStruct = createRegexInternal(pattern, flags, syntax, locale)
+            val r = new RegexImpl(pattern, cre)
+            in.acquire(r)
+    }
+
+    def search(pattern: String, text: String, flags: Flags = default, matchFlags: Match.Flags = Match.default): Boolean = Zone {
+    implicit z =>
+        val cre = createRegexInternal(pattern, flags, ECMAScript, null)
+        val result = CRegex.search(cre, toCString(text), matchFlags)
+        CRegex.delete(cre)
+        result
+    }
+
+    def searchWithResult(pattern: String, text: String, flags: Flags = default, matchFlags: Match.Flags = Match.default)(implicit in: Scope): Match = Zone {
+    implicit z =>
+        val cre = createRegexInternal(pattern, flags, ECMAScript, null)
+        val result = CRegex.searchWithResult(cre, toCString(text), matchFlags, null)
+        val matchResult = Match(result)        
+        CRegex.delete(cre)
+        matchResult
+    }
+
+    def matchAll(pattern: String, text: String, flags: Flags = default, matchFlags: Match.Flags = Match.default): Boolean = Zone {
+    implicit z =>
+        val cre = createRegexInternal(pattern, flags, ECMAScript, null)
+        val result = CRegex.matchAll(cre, toCString(text), matchFlags)
+        CRegex.delete(cre)
+        result
+    }
+
+    def matchAllWithResult(pattern: String, text: String, flags: Flags = default, matchFlags: Match.Flags = Match.default)(implicit in: Scope): Match = Zone {
+    implicit z =>
+        val cre = createRegexInternal(pattern, flags, ECMAScript, null)
+        val result = CRegex.matchAllWithResult(cre, toCString(text), matchFlags, null)
+        val matchResult = Match(result)        
+        CRegex.delete(cre)
+        matchResult
+    }
+
+    def iteratorFirst(pattern: String, text: String, flags: Flags = default, matchFlags: Match.Flags = Match.default)(implicit in: Scope): Match = Zone {
+    implicit z =>
+        val cre = createRegexInternal(pattern, flags, ECMAScript, null)
+        val result = CRegex.matchIteratorFirst(cre, toCString(text), matchFlags, null)
+        val matchResult = Match(result)        
+        CRegex.delete(cre)
+        matchResult
+    }
+
+    def tokenIteratorFirst(pattern: String, text: String, tokens:Array[Int], flags: Flags = default, matchFlags: Match.Flags = Match.default)(implicit in: Scope): Match = Zone {
+    implicit z =>
+        val ctokens = stackalloc[CInt](100)
+        var idx = tokens.length
+        (0 until idx).foreach( i => {
+            ctokens(i) = tokens(i)
+        })
+        val cre = createRegexInternal(pattern, flags, ECMAScript, null)
+        val result = CRegex.matchTokenIteratorFirst(cre, toCString(text), idx, ctokens, matchFlags, null)
+        val matchResult = Match(result)        
+        CRegex.delete(cre)
+        matchResult
+    }
+
+    def replaceAll(pattern: String, text: String, format: String, formatFlags: Match.FormatFlags = Match.format_default, flags: Flags = default, matchFlags: Match.Flags = Match.default): String = Zone {
+    implicit z =>
+        val result = stackalloc[CString]
+        val result_size = stackalloc[CInt]        
+        val cre = createRegexInternal(pattern, flags, ECMAScript, null)
+        val success = CRegex.replaceAll(cre, toCString(text), toCString(format), matchFlags | formatFlags, result, result_size)
+        val resultString = if (!result_size>0 && success) fromCString(!result) else text
+        CRegex.delete(cre)
+        resultString
+    }
+
+    def replaceFirst(pattern: String, text: String, format: String, formatFlags: Match.FormatFlags = Match.format_default, flags: Flags = default, matchFlags: Match.Flags = Match.default): String = {
+        replaceAll(pattern, text, format, formatFlags | Match.format_first_only, flags, matchFlags)
+    }
+
+    def split(pattern: String, text: String, flags: Flags = default, matchFlags: Match.Flags = Match.default)(implicit in: Scope): Array[String] = Scope {
+    implicit in =>
+        import scala.annotation.tailrec
+
+        @tailrec def innerLoop(matchResult: Match, result: Array[String] = Array.empty[String]): Array[String] = {
+            if (matchResult.tokenIteratorNext) {
+                innerLoop(matchResult, result :+ matchResult.tokenString)
+            }
+            else {
+                result
+            }
+        }
+        val matchResult = tokenIteratorFirst(pattern, text, Array(-1), flags, matchFlags)
+        innerLoop(matchResult)
+    }
+
+    def iterator(pattern: String, text: String, flags: Flags = default, matchFlags: Match.Flags = Match.default)(implicit in: Scope): Array[Match.MatchResult] = Scope {
+    implicit in =>
+        import scala.annotation.tailrec
+
+        def fill(matchResult: Match):Match.MatchResult = {
+            @tailrec def makeGroups(matchResult: Match, i: Int = 0, result: Array[Match.Group] = Array.empty[Match.Group]): Array[Match.Group] = {
+                if (i < matchResult.groupCount)
+                {
+                    makeGroups(matchResult, i + 1, result :+ new Match.Group(matchResult.groupString(i), matchResult.groupRange(i)))
+                }
+                else {
+                    result
+                }
+            }
+            new Match.MatchResult(matchResult.prefix, makeGroups(matchResult), matchResult.suffix)
+        }
+
+        @tailrec def innerLoop(matchResult: Match, result: Array[Match.MatchResult] = Array.empty[Match.MatchResult]): Array[Match.MatchResult] = {
+            if (matchResult.iteratorNext) {
+                innerLoop(matchResult, result :+ fill(matchResult))
+            }
+            else {
+                result
+            }
+        }
+        val matchResult = iteratorFirst(pattern, text, flags, matchFlags)
+        innerLoop(matchResult)
+    }
+
+    def tokenIterator(pattern: String, text: String, tokens:Array[Int], flags: Flags = default, matchFlags: Match.Flags = Match.default)(implicit in: Scope): Array[Match.Group] = Scope {
+    implicit in =>
+        import scala.annotation.tailrec
+
+        @tailrec def innerLoop(matchResult: Match, result: Array[Match.Group] = Array.empty[Match.Group]): Array[Match.Group] = {
+            if (matchResult.tokenIteratorNext) {
+                innerLoop(matchResult, result :+ new Match.Group(matchResult.tokenString, matchResult.tokenRange))
+            }
+            else {
+                result
+            }
+        }
+        val matchResult = tokenIteratorFirst(pattern, text, tokens, flags, matchFlags)
+        innerLoop(matchResult)
+    }
+
+    // private section
+
+    private[regex] class RegexImpl(val pattern: String, var cre:CRegex.CRegexStruct)(implicit in: Scope) extends Regex with Resource {
+
+        def search(text: String, matchFlags: Match.Flags = Match.default): Boolean = Zone {
+        implicit z =>
+            CRegex.search(cre, toCString(text), matchFlags)
+        }
+
+        def searchWithResult(text: String, matchFlags: Match.Flags = Match.default)(implicit in: Scope): Match = Zone {
+        implicit z =>
+            val result = CRegex.searchWithResult(cre, toCString(text), matchFlags, null)
+            val matchResult = Match(result)        
+            matchResult
+        }
+
+        def matchAll(text: String, matchFlags: Match.Flags = Match.default): Boolean = Zone {
+        implicit z =>
+            val result = CRegex.matchAll(cre, toCString(text), matchFlags)
+            result
+        }
+
+        def matchAllWithResult(text: String, matchFlags: Match.Flags = Match.default)(implicit in: Scope): Match = Zone {
+        implicit z =>
+            val result = CRegex.matchAllWithResult(cre, toCString(text), matchFlags, null)
+            val matchResult = Match(result)        
+            matchResult
+        }
+
+        def iteratorFirst(text: String, matchFlags: Match.Flags = Match.default)(implicit in: Scope): Match = Zone {
+        implicit z =>
+            val result = CRegex.matchIteratorFirst(cre, toCString(text), matchFlags, null)
+            val matchResult = Match(result)        
+            matchResult
+        }
+
+        def tokenIteratorFirst(text: String, tokens:Array[Int], matchFlags: Match.Flags = Match.default)(implicit in: Scope): Match = Zone {
+        implicit z =>
+            val ctokens = stackalloc[CInt](100)
+            var idx = tokens.length
+            (0 until idx).foreach( i => {
+                ctokens(i) = tokens(i)
+            })
+            val result = CRegex.matchTokenIteratorFirst(cre, toCString(text), idx, ctokens, matchFlags, null)
+            val matchResult = Match(result)        
+            matchResult
+        }
+
+        def replaceAll(text: String, format: String, formatFlags: Match.FormatFlags = Match.format_default, matchFlags: Match.Flags = Match.default): String = Zone {
+        implicit z =>
+            val result = stackalloc[CString]
+            val result_size = stackalloc[CInt]        
+            val success = CRegex.replaceAll(cre, toCString(text), toCString(format), matchFlags, result, result_size)
+            val resultString = if (!result_size>0 && success) fromCString(!result) else text
+            resultString
+        }
+
+        def replaceFirst(text: String, format: String, formatFlags: Match.FormatFlags = Match.format_default, matchFlags: Match.Flags = Match.default): String = {
+            replaceAll(text, format, formatFlags | Match.format_first_only, matchFlags)
+        }
+
+        def split(text: String, matchFlags: Match.Flags = Match.default)(implicit in: Scope): Array[String] = Scope {
+        implicit in =>
+            import scala.annotation.tailrec
+
+            @tailrec def innerLoop(matchResult: Match, result: Array[String] = Array.empty[String]): Array[String] = {
+                if (matchResult.tokenIteratorNext) {
+                    innerLoop(matchResult, result :+ matchResult.tokenString)
+                }
+                else {
+                    result
+                }
+            }
+            val matchResult = tokenIteratorFirst(text, Array(-1), matchFlags)
+            innerLoop(matchResult)
+        }
+
+        def iterator(text: String, matchFlags: Match.Flags = Match.default)(implicit in: Scope): Array[Match.MatchResult] = Scope {
+        implicit in =>
+            import scala.annotation.tailrec
+
+            def fill(matchResult: Match):Match.MatchResult = {
+                @tailrec def makeGroups(matchResult: Match, i: Int = 0, result: Array[Match.Group] = Array.empty[Match.Group]): Array[Match.Group] = {
+                    if (i < matchResult.groupCount)
+                    {
+                        makeGroups(matchResult, i + 1, result :+ new Match.Group(matchResult.groupString(i), matchResult.groupRange(i)))
+                    }
+                    else {
+                        result
+                    }
+                }
+                new Match.MatchResult(matchResult.prefix, makeGroups(matchResult), matchResult.suffix)
+            }
+
+            @tailrec def innerLoop(matchResult: Match, result: Array[Match.MatchResult] = Array.empty[Match.MatchResult]): Array[Match.MatchResult] = {
+                if (matchResult.iteratorNext) {
+                    innerLoop(matchResult, result :+ fill(matchResult))
+                }
+                else {
+                    result
+                }
+            }
+            val matchResult = iteratorFirst(text, matchFlags)
+            innerLoop(matchResult)
+        }
+
+        def tokenIterator(text: String, tokens:Array[Int], matchFlags: Match.Flags = Match.default)(implicit in: Scope): Array[Match.Group] = Scope {
+        implicit in =>
+            import scala.annotation.tailrec
+
+            @tailrec def innerLoop(matchResult: Match, result: Array[Match.Group] = Array.empty[Match.Group]): Array[Match.Group] = {
+                if (matchResult.tokenIteratorNext) {
+                    innerLoop(matchResult, result :+ new Match.Group(matchResult.tokenString, matchResult.tokenRange))
+                }
+                else {
+                    result
+                }
+            }
+            val matchResult = tokenIteratorFirst(text, tokens, matchFlags)
+            innerLoop(matchResult)
+        }
+
+        def close(): Unit = {
+            CRegex.delete(cre)
+            cre = null
+        }
+    }
+
+    private[regex] def createRegexInternal(pattern:String, flags: Flags, syntax: Syntax, locale: String)(implicit z: Zone): CRegex.CRegexStruct = {
+        val error = stackalloc[CChar](1024).cast[CString]
+        val cre = CRegex.create(toCString(pattern), flags | syntax.f, if (locale != null) toCString(locale) else null, error, 1024)
+        if (error(0) != 0)
+        {
+            throw new RegexException(fromCString(error))
+        }
+        cre
+    }
+}
