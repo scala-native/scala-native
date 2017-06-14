f479e22d3be268e91e549813520ec745f6b18a52
Denys Shabalin <denys.shabalin@epfl.ch>
2017-06-14 16:58:47 +0200

    Rename FileDescriptor.{open => openReadOnly}
    And remove unnecessary import in the corresponding test.

--- javalib/src/main/scala/java/io/FileDescriptor.scala
+++ javalib/src/main/scala/java/io/FileDescriptor.scala
@@ -27,12 +27,11 @@ final class FileDescriptor private[io] (private[io] val fd: Int,
 }
 
 object FileDescriptor {
-
   val in: FileDescriptor  = new FileDescriptor(unistd.STDIN_FILENO)
   val out: FileDescriptor = new FileDescriptor(unistd.STDOUT_FILENO)
   val err: FileDescriptor = new FileDescriptor(unistd.STDERR_FILENO)
 
-  private[io] def open(file: File): FileDescriptor =
+  private[io] def openReadOnly(file: File): FileDescriptor =
     Zone { implicit z =>
       val fd = fcntl.open(toCString(file.getPath), fcntl.O_RDONLY)
       if (fd == -1) {
@@ -40,5 +39,4 @@ object FileDescriptor {
       }
       new FileDescriptor(fd, true)
     }
-
 }
