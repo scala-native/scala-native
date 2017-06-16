b28dd77f5aa0b10475479cfa867fd84b9f85cfc0
ekrich <ekrichardson@gmail.com>
2017-06-16 09:41:22 -0400

    Rebase, move test for new test setup, and update test for Linux

--- unit-tests/src/test/scala/scala/scalanative/native/CComplexSuite.scala
+++ unit-tests/src/test/scala/scala/scalanative/native/CComplexSuite.scala
@@ -5,12 +5,27 @@ import complexOps._
 
 import java.{lang => jl}
 
-// Reference
-// http://en.cppreference.com/w/c/numeric/complex
-// Ran the complex functions in C to get the results
-// in hex which are used here for the tests.
-// TODO: 2 float tests fail on Linux so they are omitted for now
+/** Reference
+ * http://en.cppreference.com/w/c/numeric/complex
+ *
+ * The complex functions were run in C on macOS to get the results
+ * in hex which are used here for the tests. The C code is below.
+ *
+ * Since the code is run on macOS to get results and we need to
+ * test on other platforms we use ULP (Units of least precision)
+ * which is the spacing between floating point numbers to
+ * implement an "almost equals" function. We calculate epsilon,
+ * our error value, based on ULP. Currently the complex
+ * double functions use 1 times ulp and the float functions use
+ * 2 times ulp for the tests to pass. Practically this has the
+ * effect of making the epsilon slightly larger for Float.
+ * Either way epsilon is a very small number.
+ *
+ * Example printed in hex: 0x1.0p-23 (2*ulp) 0x1.0p-24 (ulp)
+ */
 object CComplexSuite extends tests.Suite {
+  // change for debug
+  val debug = false
   // shared values for special calculations
   val qtrPI = Math.PI / 4
   val sqrt2 = Math.sqrt(2)
@@ -25,7 +40,11 @@ object CComplexSuite extends tests.Suite {
 
   def isAlmostEqual(act: Float, exp: Float): Boolean = {
     val diff    = Math.abs(act - exp)
-    val epsilon = Math.max(Math.ulp(act), Math.ulp(exp))
+    val epsilon = 2 * Math.max(Math.ulp(act), Math.ulp(exp))
+    if (debug) {
+      println(
+        s"diff: ${jl.Float.toHexString(diff)} eps: ${jl.Float.toHexString(epsilon)}")
+    }
     diff <= epsilon
   }
 
@@ -58,13 +77,12 @@ object CComplexSuite extends tests.Suite {
                            res(toFloat(0x3f823454), toFloat(0x3ece0210)))
     }
   }
-//  test("ccosf") {
-//    Zone { implicit z =>
-//      assertEqualsComplexF(
-//        ccosf(tf, buff),
-//        res(toFloat(0x3f556f55), toFloat(0xbf7d2866)))
-//    }
-//  }
+  test("ccosf") {
+    Zone { implicit z =>
+      assertEqualsComplexF(ccosf(tf, buff),
+                           res(toFloat(0x3f556f55), toFloat(0xbf7d2866)))
+    }
+  }
   test("csinf") {
     Zone { implicit z =>
       assertEqualsComplexF(csinf(tf, buff),
@@ -96,13 +114,12 @@ object CComplexSuite extends tests.Suite {
                            res(toFloat(0x3ece0210), toFloat(0x3f823454)))
     }
   }
-//  test("ccoshf") {
-//    Zone { implicit z =>
-//      assertEqualsComplexF(
-//        ccoshf(tf, buff),
-//        res(toFloat(0x3f556f55), toFloat(0x3f7d2866)))
-//    }
-//  }
+  test("ccoshf") {
+    Zone { implicit z =>
+      assertEqualsComplexF(ccoshf(tf, buff),
+                           res(toFloat(0x3f556f55), toFloat(0x3f7d2866)))
+    }
+  }
   test("csinhf") {
     Zone { implicit z =>
       assertEqualsComplexF(csinhf(tf, buff),
@@ -183,6 +200,10 @@ object CComplexSuite extends tests.Suite {
   def isAlmostEqual(act: Double, exp: Double): Boolean = {
     val diff    = Math.abs(act - exp)
     val epsilon = Math.max(Math.ulp(act), Math.ulp(exp))
+    if (debug) {
+      println(
+        s"diff: ${jl.Double.toHexString(diff)} eps: ${jl.Double.toHexString(epsilon)}")
+    }
     diff <= epsilon
   }
 
@@ -347,3 +368,94 @@ object CComplexSuite extends tests.Suite {
     }
   }
 }
+
+//// C code to run and generate complex values
+//#include <stdio.h>
+//#include <complex.h>
+//
+//// 1 for view output, 0 for test output
+//int view = 1;
+//
+//// recommend char[255]
+//const char* toStrFloat(char* msg, float complex x, char* str) {
+//    float re = crealf(x);
+//    float im = cimagf(x);
+//    if (view) {
+//        sprintf(str, "%-7s -> %f, %f", msg, re, im); // dec
+//    } else {
+//        sprintf(str, "%-7s -> toFloat(0x%08x), toFloat(0x%08x)",
+//                msg, *(unsigned int*)&re, *(unsigned int*)&im); //hex
+//    }
+//    return str;
+//}
+//
+//const char* toStrDouble(char* msg, double complex x, char* str) {
+//    double re = creal(x);
+//    double im = cimag(x);
+//    if (view) {
+//        sprintf(str, "%-7s -> %f, %f", msg, re, im); // dec
+//    } else {
+//        sprintf(str, "%-7s -> toDouble(\"%lx\"), toDouble(\"%lx\")",
+//                msg, *(unsigned long*)&re, *(unsigned long*)&im); // hex
+//    }
+//    return str;
+//}
+//
+//char buf[255];
+//
+//void printF(char* msg, float complex f) {
+//    printf("%s\n", toStrFloat(msg, f, buf));
+//}
+//
+//void printD(char* msg, double complex d) {
+//    printf("%s\n", toStrDouble(msg, d, buf));
+//}
+//
+//int main(int argc, const char * argv[]) {
+//    printf("Running complex functions...\n");
+//
+//    float complex f1 = 1.0 + 1.0 * I;
+//
+//    printf("\nRunning float...\n");
+//    printF("f1", f1);
+//    printF("cacosf", cacosf(f1));
+//    printF("casinf", casinf(f1));
+//    printF("catanf", catanf(f1));
+//    printF("ccosf", ccosf(f1));
+//    printF("csinf", csinf(f1));
+//    printF("ctanf", ctanf(f1));
+//    printF("cacoshf", cacoshf(f1));
+//    printF("casinhf", casinhf(f1));
+//    printF("catanhf", catanhf(f1));
+//    printF("ccoshf", ccoshf(f1));
+//    printF("csinhf", csinhf(f1));
+//    printF("ctanhf", ctanhf(f1));
+//    printF("cexpf", cexpf(f1));
+//    printF("clogf", clogf(f1));
+//    printF("cpowf", cpowf(f1, f1));
+//    printF("csqrtf", csqrtf(f1));
+//
+//    double complex d1 = 1.0 + 1.0 * I;
+//
+//    printf("\nRunning double...\n");
+//    printD("d1", d1);
+//    printD("cacos", cacos(d1));
+//    printD("casin", casin(d1));
+//    printD("catan", catan(d1));
+//    printD("ccos", ccos(d1));
+//    printD("csin", csin(d1));
+//    printD("ctan", ctan(d1));
+//    printD("cacosh", cacosh(d1));
+//    printD("casinh", casinh(d1));
+//    printD("catanh", catanh(d1));
+//    printD("ccosh", ccosh(d1));
+//    printD("csinh", csinh(d1));
+//    printD("ctanh", ctanh(d1));
+//    printD("cexp", cexp(d1));
+//    printD("clog", clog(d1));
+//    printD("cpow", cpow(d1, d1));
+//    printD("csqrt", csqrt(d1));
+//    printf("Done.");
+//
+//    return 0;
+//}
