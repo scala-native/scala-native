27c5956140472822c452b5eb34389eaadd824f5a
Allan Renucci <allan.renucci@gmail.com>
2017-06-24 20:26:06 +0200

    Fix #848
    Delegate `FileWriter` implementation to `OutputStreamWriter`.

--- unit-tests/src/test/scala/java/io/FileWriterSuite.scala
+++ unit-tests/src/test/scala/java/io/FileWriterSuite.scala
@@ -1,13 +1,20 @@
 package java.io
 
-import scala.util.Try
-
 object FileWriterSuite extends tests.Suite {
 
-  test(
-    "throws FileNotFoundException when writing to a directory (or non-existing path)") {
-    assertThrows[FileNotFoundException] {
-      new FileWriter("/etc")
-    }
+  test("write to new file") {
+    val path    = "/tmp/new.file"
+    val toWrite = "something"
+
+    val fw = new FileWriter(path)
+    fw.write(toWrite)
+    fw.close()
+
+    val buffer    = new Array[Char](20)
+    val fr        = new FileReader(path)
+    val charsRead = fr.read(buffer)
+    assert(charsRead == toWrite.length)
+    assert(buffer.take(charsRead).mkString == toWrite)
+    fr.close()
   }
 }
