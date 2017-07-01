a45ebdbd4eaf78f0ff7ab6a73c6d8b91b577d10a
matil019 <yohashi1991@gmail.com>
2017-07-01 11:15:27 +0900

    Take escape sequences into account when counting the length of Val.Chars
    Escape sequences in CString literal (i.e. c"...") should be counted
    as one byte each.

--- nir/src/main/scala/scala/scalanative/nir/Vals.scala
+++ nir/src/main/scala/scala/scalanative/nir/Vals.scala
@@ -19,13 +19,54 @@ sealed abstract class Val {
     case Val.Double(_)          => Type.Double
     case Val.Struct(name, vals) => Type.Struct(name, vals.map(_.ty))
     case Val.Array(ty, vals)    => Type.Array(ty, vals.length)
-    case Val.Chars(s)           => Type.Array(Type.Byte, s.getBytes.length + 1)
+    case Val.Chars(s)           => Type.Array(Type.Byte, countBytes(s))
     case Val.Local(_, ty)       => ty
     case Val.Global(_, ty)      => ty
 
     case Val.Unit      => Type.Unit
     case Val.Const(_)  => Type.Ptr
     case Val.String(_) => Rt.String
+  }
+
+  private def countBytes(s: String): Int = {
+    import Character.isDigit
+
+    // TODO: should be a compilation error, how to report?
+    def malformed() = ???
+
+    // Subtracts from the length of the bytes for each escape sequence
+    // uses String, not Seq[Byte], but should be okay since we handle ASCII only
+    def uncountEscapes(from: Int, accum: Int): Int =
+      s.indexOf('\\', from) match {
+        case -1 => accum
+        case idx if idx == s.length - 1 =>
+          malformed()
+        case idx =>
+          def isOct(c: Char): Boolean = isDigit(c) && c != '8' && c != '9'
+          def isHex(c: Char): Boolean =
+            isDigit(c) ||
+              c == 'a' || c == 'b' || c == 'c' || c == 'd' || c == 'e' || c == 'f' ||
+              c == 'A' || c == 'B' || c == 'C' || c == 'D' || c == 'E' || c == 'F'
+          s(idx + 1) match {
+            case d if isOct(d) =>
+              // octal ("\O", "\OO", "\OOO")
+              val digitNum = s.drop(idx + 1).take(3).takeWhile(isOct).length
+              uncountEscapes(idx + 1 + digitNum, accum - digitNum)
+            case 'x' =>
+              // hexademical ("\xH", "\xHH")
+              val digitNum = s.drop(idx + 2).takeWhile(isHex).length
+              // mimic clang, which reports compilation error against too many hex digits
+              if (digitNum >= 3)
+                malformed()
+              uncountEscapes(idx + 2 + digitNum, accum - 1 - digitNum)
+            // TODO: support unicode?
+            // case 'u' =>
+            // case 'U' =>
+            case _ =>
+              uncountEscapes(idx + 2, accum - 1)
+          }
+      }
+    uncountEscapes(0, s.getBytes.length + 1)
   }
 
   final def show: String = nir.Show(this)
