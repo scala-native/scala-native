3a67f70085e83a66bc478b84fd00d3c4d59b75c0
Andrzej Sołtysik <andrzej.soltysik17@gmail.com>
2017-07-14 17:31:59 +0200

    remove debug println

--- scripted-tests/run/java-net-socket/tests/IsReachableTest.scala
+++ scripted-tests/run/java-net-socket/tests/IsReachableTest.scala
@@ -3,7 +3,6 @@ package java.net
 object IsReachableTest {
 
   def main(args: Array[String]): Unit = {
-    println(SocketHelpers.isReachableByEcho("127.0.0,1", 1000, 5832))
     assert(SocketHelpers.isReachableByEcho("127.0.0.1", 1000, 5832) == true)
   }
 
