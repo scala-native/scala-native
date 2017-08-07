490c04f42ba28f2187b034a35a34ed4be438933e
Remi Coudert <remi.coudert@epfl.ch>
2017-08-07 13:45:20 +0200

    initial commit for pthreads

--- nativelib/src/main/scala/scala/scalanative/posix/sys/time.scala
+++ nativelib/src/main/scala/scala/scalanative/posix/sys/time.scala
@@ -2,8 +2,9 @@ package scala.scalanative
 package posix
 package sys
 
-import scalanative.native.CLongInt
+import scala.scalanative.native.{CLong, CLongInt, CStruct2}
 
 object time {
-  type time_t = CLongInt
+  type time_t   = CLongInt
+  type timespec = CStruct2[time_t, CLong]
 }
