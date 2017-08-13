dbe7ad2416e0e0d4e307bf011148c65ac0dd2715
Remi Coudert <remi.coudert@epfl.ch>
2017-08-13 20:26:34 +0200

    java atomics

--- javalib/src/main/scala/java/util/concurrent/atomic/AtomicBoolean.scala
+++ javalib/src/main/scala/java/util/concurrent/atomic/AtomicBoolean.scala
@@ -1,34 +1,45 @@
 package java.util.concurrent.atomic
 
+import scala.scalanative.native.CInt
+import scala.scalanative.runtime.CAtomicInt
+
 class AtomicBoolean(private[this] var value: Boolean) extends Serializable {
   def this() = this(false)
 
-  final def get(): Boolean = value
+  private[this] val inner = CAtomicInt(value)
+
+  final def get(): Boolean = inner.load()
 
   final def compareAndSet(expect: Boolean, update: Boolean): Boolean = {
-    if (expect != value) false
-    else {
-      value = update
-      true
-    }
+    inner.compareAndSwapStrong(expect, update)._1
   }
 
   // For some reason, this method is not final
   def weakCompareAndSet(expect: Boolean, update: Boolean): Boolean =
-    compareAndSet(expect, update)
+    inner.compareAndSwapWeak(expect, update)._1
 
   final def set(newValue: Boolean): Unit =
-    value = newValue
+    inner.store(newValue)
 
   final def lazySet(newValue: Boolean): Unit =
-    set(newValue)
+    inner.store(newValue)
 
   final def getAndSet(newValue: Boolean): Boolean = {
-    val old = value
-    value = newValue
+    val old = inner.load()
+    inner.store(newValue)
     old
   }
 
   override def toString(): String =
-    value.toString()
+    inner.toString()
+
+  private implicit def toInt(b: Boolean): Int = if (b) 1 else 0
+
+  private implicit def toBool(i: Int): Boolean = i != 0
+}
+
+object AtomicBoolean {
+
+  private final val serialVersionUID: Long = 4654671469794556979L
+
 }
