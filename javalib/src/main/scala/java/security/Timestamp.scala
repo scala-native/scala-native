575e364551977e050e1ff72c0bb1dde2ce63fd19
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2017-08-28 03:13:50 +0200

    Fix #959: Port `java.security.Timestamp` from Apache Harmony

--- javalib/src/main/scala/java/security/Timestamp.scala
+++ javalib/src/main/scala/java/security/Timestamp.scala
@@ -1,7 +1,68 @@
 package java.security
 
-import java.util.Date
-import java.security.cert.CertPath
+// Ported from Harmony
 
-final class Timestamp(timestamp: Date, signerCertPath: CertPath)
-    extends Serializable
+import java.security.cert.CertPath
+import java.util.Date
+
+final class Timestamp private (private val signerCertPath: CertPath,
+                               private val timestamp: Date)
+    extends Serializable {
+
+  if (signerCertPath eq null) {
+    throw new NullPointerException("signerCertPath cannot be null")
+  }
+
+  /** Constructor overload with null checking and timestamp cloning **/
+  @throws[NullPointerException]
+  def this(timestamp: Date, signerCertPath: CertPath) =
+    this(
+      signerCertPath,
+      TimestampConstructorHelper.validateAndCloneInputTimestamp(timestamp)
+    )
+
+  @inline def getSignerCertPath: CertPath = signerCertPath
+
+  @inline def getTimestamp: Date = new Date(timestamp.getTime)
+
+  override def equals(obj: Any): Boolean =
+    obj match {
+      case objRef: AnyRef if this eq objRef =>
+        true
+      case that: Timestamp =>
+        timestamp.equals(that.getTimestamp) &&
+          signerCertPath.equals(that.getSignerCertPath)
+      case _ =>
+        false
+    }
+
+  @transient
+  override lazy val hashCode: Int = timestamp.hashCode ^ signerCertPath.hashCode
+
+  override def toString: String = {
+    val buf = new java.lang.StringBuilder()
+    // Dump only the first certificate
+    buf.append("Timestamp [")
+    buf.append(timestamp.toString)
+    buf.append(" certPath=")
+    val certificates = signerCertPath.getCertificates
+    if (certificates.isEmpty) {
+      buf.append(certificates.get(0).toString)
+    } else {
+      buf.append("<empty>")
+    }
+    buf.append("]")
+    buf.toString
+  }
+}
+
+private object TimestampConstructorHelper {
+
+  @throws[NullPointerException]
+  def validateAndCloneInputTimestamp(timestamp: Date): Date =
+    if (timestamp eq null) {
+      throw new NullPointerException("Timestamp cannot be null")
+    } else {
+      new Date(timestamp.getTime)
+    }
+}
--- javalib/src/main/scala/java/security/cert/CertPath.scala
+++ javalib/src/main/scala/java/security/cert/CertPath.scala
@@ -1,7 +1,16 @@
 package java.security.cert
 
-import java.util.List
-
 abstract class CertPath protected (`type`: String) {
-  def getCertificates(): List[_ <: Certificate]
+
+  def getCertificates(): java.util.List[_ <: Certificate]
+
+  def getType(): String = `type`
+
+  override def equals(other: Any): Boolean =
+    other match {
+      case otherRef: AnyRef if this eq otherRef => true
+      case otherCp: CertPath if otherCp.getType.equals(`type`) =>
+        getCertificates().equals(otherCp.getCertificates)
+      case _ => false
+    }
 }
