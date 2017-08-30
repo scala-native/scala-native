4f1f8fa0c5ee74d3826455c0317aa5452188e73a
Martin Duhem <martin.duhem@gmail.com>
2017-08-30 15:37:44 +0200

    Fix #866: Add support for scala.Enumeration (#976)
    Port the phase `PrepJSInterop` from ScalaJS and remove all JS-specific
    transformations and checks. All that is left is the rewriting of calls
    to `scala.Enumeration.Value`.
    
    Fixes #866

--- nscplugin/src/main/scala/scala/scalanative/nscplugin/NirPlugin.scala
+++ nscplugin/src/main/scala/scala/scalanative/nscplugin/NirPlugin.scala
@@ -7,11 +7,18 @@ import scala.tools.nsc.plugins._
 class NirPlugin(val global: Global) extends Plugin { self =>
   val name        = "nir"
   val description = "Compile to Scala Native IR (NIR)"
-  val components  = List[PluginComponent](nirGen)
+  val components  = List[PluginComponent](prepNativeInterop, nirGen)
 
   object nirAddons extends {
     val global: NirPlugin.this.global.type = NirPlugin.this.global
   } with NirGlobalAddons
+
+  object prepNativeInterop extends {
+    val global: self.global.type                 = self.global
+    val nirAddons: NirPlugin.this.nirAddons.type = NirPlugin.this.nirAddons
+    override val runsAfter                       = List("typer")
+    override val runsBefore                      = List("pickler")
+  } with PrepNativeInterop
 
   object nirGen extends {
     val global: self.global.type                 = self.global
