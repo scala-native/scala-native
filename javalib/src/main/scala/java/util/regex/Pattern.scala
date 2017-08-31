54409d22b134f6f7142646b7aca1245682cf1d51
matil019 <yohashi1991@gmail.com>
2017-08-31 21:00:37 +0900

    Integrate CompiledPatternStore into Pattern to remove finalize
    Pattern.finalize is removed.
    The implementation of Pattern.compile is moved to
    CompiledPatternStore.doCompile with minor improvements in resource
    management.

--- javalib/src/main/scala/java/util/regex/Pattern.scala
+++ javalib/src/main/scala/java/util/regex/Pattern.scala
@@ -19,88 +19,14 @@ object Pattern {
 
   def compile(regex: String): Pattern = compile(regex, 0)
 
-  def compile(regex: String, flags: Int): Pattern = Zone {
-    implicit z =>
-      def notSupported(flag: Int, flagName: String): Unit = {
-        if ((flags & flag) == flag) {
-          assert(false, s"regex flag $flagName is not supported")
-        }
-      }
+  def compile(regex: String, flags: Int): Pattern = {
+    // make sure the provided regex is compiled
+    CompiledPatternStore.get(regex, flags)
 
-      notSupported(CANON_EQ, "CANON_EQ(canonical equivalences)")
-      notSupported(COMMENTS, "COMMENTS")
-      notSupported(UNICODE_CASE, "UNICODE_CASE")
-      notSupported(UNICODE_CHARACTER_CLASS, "UNICODE_CHARACTER_CLASS")
-      notSupported(UNIX_LINES, "UNIX_LINES")
-
-      val options = cre2.optNew()
-      cre2.setCaseSensitive(options, flags & CASE_INSENSITIVE)
-      cre2.setDotNl(options, flags & DOTALL)
-      cre2.setLiteral(options, flags & LITERAL)
-      cre2.setLogErrors(options, 0)
-
-      // setOneLine(false) is only available when limiting ourself to posix_syntax
-      // https://github.com/google/re2/blob/2017-03-01/re2/re2.h#L548
-      // regex flag MULTILINE cannot be disabled
-
-      val re2 = {
-        val regexre2 = alloc[cre2.string_t]
-        toRE2String(regex, regexre2)
-        cre2.compile(regexre2.data, regexre2.length, options)
-      }
-
-      val code = cre2.errorCode(re2)
-
-      if (code != ERROR_NO_ERROR) {
-        val errorPattern = {
-          val arg = stackalloc[cre2.string_t]
-          cre2.errorArg(re2, arg)
-          fromRE2String(arg)
-        }
-
-        // we try to find the index of the parsing error
-        // this could return the wrong index it only finds the first match
-        // see https://groups.google.com/forum/#!topic/re2-dev/rnvFZ9Ki8nk
-        val index =
-          if (code == ERROR_TRAILING_BACKSLASH) regex.size - 1
-          else regex.indexOfSlice(errorPattern)
-
-        val reText = fromCString(cre2.errorString(re2))
-
-        val description =
-          code match {
-            case ERROR_INTERNAL           => "Internal Error"
-            case ERROR_BAD_ESCAPE         => "Illegal/unsupported escape sequence"
-            case ERROR_BAD_CHAR_CLASS     => "Illegal/unsupported character class"
-            case ERROR_BAD_CHAR_RANGE     => "Illegal character range"
-            case ERROR_MISSING_BRACKET    => "Unclosed character class"
-            case ERROR_MISSING_PAREN      => "Missing parenthesis"
-            case ERROR_TRAILING_BACKSLASH => "Trailing Backslash"
-            case ERROR_REPEAT_ARGUMENT    => "Dangling meta character '*'"
-            case ERROR_REPEAT_SIZE        => "Bad repetition argument"
-            case ERROR_REPEAT_OP          => "Bad repetition operator"
-            case ERROR_BAD_PERL_OP        => "Bad perl operator"
-            case ERROR_BAD_UTF8           => "Invalid UTF-8 in regexp"
-            case ERROR_BAD_NAMED_CAPTURE  => "Bad named capture group"
-            case ERROR_PATTERN_TOO_LARGE =>
-              "Pattern too large (compilation failed)"
-            case _ => reText
-          }
-
-        throw new PatternSyntaxException(
-          description,
-          regex,
-          index
-        )
-      }
-
-      cre2.optDelete(options)
-
-      new Pattern(
-        _pattern = regex,
-        _flags = flags,
-        _regex = re2
-      )
+    new Pattern(
+      _pattern = regex,
+      _flags = flags
+    )
   }
 
   def matches(regex: String, input: CharSequence): Boolean =
@@ -143,20 +69,98 @@ object Pattern {
       next = if (next + 1 >= cache.size) 0 else next + 1
     }
 
-    private def doCompile(regex: String, flags: Int): Ptr[cre2.regexp_t] = {
-      // TODO move Pattern$.compile here
-      ???
+    private def doCompile(regex: String, flags: Int): Ptr[cre2.regexp_t] = Zone { implicit z =>
+      def notSupported(flag: Int, flagName: String): Unit = {
+        if ((flags & flag) == flag) {
+          assert(false, s"regex flag $flagName is not supported")
+        }
+      }
+
+      notSupported(CANON_EQ, "CANON_EQ(canonical equivalences)")
+      notSupported(COMMENTS, "COMMENTS")
+      notSupported(UNICODE_CASE, "UNICODE_CASE")
+      notSupported(UNICODE_CHARACTER_CLASS, "UNICODE_CHARACTER_CLASS")
+      notSupported(UNIX_LINES, "UNIX_LINES")
+
+      val options = cre2.optNew()
+      try {
+        cre2.setCaseSensitive(options, flags & CASE_INSENSITIVE)
+        cre2.setDotNl(options, flags & DOTALL)
+        cre2.setLiteral(options, flags & LITERAL)
+        cre2.setLogErrors(options, 0)
+
+        // setOneLine(false) is only available when limiting ourself to posix_syntax
+        // https://github.com/google/re2/blob/2017-03-01/re2/re2.h#L548
+        // regex flag MULTILINE cannot be disabled
+
+        val re2 = {
+          val regexre2 = alloc[cre2.string_t]
+          toRE2String(regex, regexre2)
+          cre2.compile(regexre2.data, regexre2.length, options)
+        }
+
+        val code = cre2.errorCode(re2)
+
+        if (code != ERROR_NO_ERROR) {
+          val errorPattern = {
+            val arg = alloc[cre2.string_t]
+            cre2.errorArg(re2, arg)
+            fromRE2String(arg)
+          }
+
+          // we try to find the index of the parsing error
+          // this could return the wrong index it only finds the first match
+          // see https://groups.google.com/forum/#!topic/re2-dev/rnvFZ9Ki8nk
+          val index =
+            if (code == ERROR_TRAILING_BACKSLASH) regex.size - 1
+            else regex.indexOfSlice(errorPattern)
+
+          val reText = fromCString(cre2.errorString(re2))
+
+          val description =
+            code match {
+              case ERROR_INTERNAL           => "Internal Error"
+              case ERROR_BAD_ESCAPE         => "Illegal/unsupported escape sequence"
+              case ERROR_BAD_CHAR_CLASS     => "Illegal/unsupported character class"
+              case ERROR_BAD_CHAR_RANGE     => "Illegal character range"
+              case ERROR_MISSING_BRACKET    => "Unclosed character class"
+              case ERROR_MISSING_PAREN      => "Missing parenthesis"
+              case ERROR_TRAILING_BACKSLASH => "Trailing Backslash"
+              case ERROR_REPEAT_ARGUMENT    => "Dangling meta character '*'"
+              case ERROR_REPEAT_SIZE        => "Bad repetition argument"
+              case ERROR_REPEAT_OP          => "Bad repetition operator"
+              case ERROR_BAD_PERL_OP        => "Bad perl operator"
+              case ERROR_BAD_UTF8           => "Invalid UTF-8 in regexp"
+              case ERROR_BAD_NAMED_CAPTURE  => "Bad named capture group"
+              case ERROR_PATTERN_TOO_LARGE =>
+                "Pattern too large (compilation failed)"
+              case _ => reText
+            }
+
+          cre2.delete(re2)
+          throw new PatternSyntaxException(
+            description,
+            regex,
+            index
+          )
+        }
+
+        re2
+      } finally {
+        cre2.optDelete(options)
+      }
     }
   }
 }
 
 final class Pattern private[regex] (
     _pattern: String,
-    _flags: Int,
-    _regex: Ptr[cre2.regexp_t]
+    _flags: Int
 ) {
 
-  private[regex] def regex: Ptr[cre2.regexp_t] = _regex
+  // TODO make this function a loan pattern so that a Ptr is kept alive while in use
+  private[regex] def regex: Ptr[cre2.regexp_t] =
+    Pattern.CompiledPatternStore.get(_pattern, _flags)
 
   def split(input: CharSequence): Array[String] =
     split(input, 0)
@@ -206,10 +210,4 @@ final class Pattern private[regex] (
   def flags: Int                = _flags
   def pattern: String           = _pattern
   override def toString: String = _pattern
-
-  override protected def finalize(): Unit = {
-    // TODO remove
-    super.finalize()
-    cre2.delete(_regex)
-  }
 }
