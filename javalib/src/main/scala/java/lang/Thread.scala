721ef1f34af306b4d4fc47f3c0aca6af71e266c8
Valdis <valdisxp1@gmail.com>
2017-09-08 11:07:10 +0200

    removed duplicate yield definitions

--- javalib/src/main/scala/java/lang/Thread.scala
+++ javalib/src/main/scala/java/lang/Thread.scala
@@ -548,12 +548,6 @@ object Thread {
     threadOrdinalNum
   }
 
-  def `yield`(): Unit = ???
-
-  def `yield`(): Unit = ???
-
-  def `yield`(): Unit = ???
-
   def interrupted(): scala.Boolean = {
     val ret = currentThread().isInterrupted
     currentThread().interruptedState = false
