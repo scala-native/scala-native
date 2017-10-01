8f46fd673df9d325807e280d163182c186ca2c59
Valdis <valdisxp1@gmail.com>
2017-10-01 13:07:29 +0200

    test threads interating with the outside

--- unit-tests/src/test/scala/java/lang/ThreadSuite.scala
+++ unit-tests/src/test/scala/java/lang/ThreadSuite.scala
@@ -21,4 +21,48 @@ object ThreadSuite extends tests.Suite {
 
   }
 
+  test("Thread should be able to change a shared var") {
+    var shared: Int = 0
+    new Thread(new Runnable {
+      def run(): Unit = {
+        shared = 1
+      }
+    }).start()
+    Thread.sleep(100)
+    assertEquals(shared, 1)
+  }
+
+  test("Thread should be able to change runnable's internal state") {
+    class StatefulRunnable extends Runnable {
+      var internal = 0
+      def run(): Unit = {
+        internal = 1
+      }
+    }
+    val runnable = new StatefulRunnable
+    new Thread(runnable).start()
+    Thread.sleep(100)
+    assertEquals(runnable.internal, 1)
+  }
+
+  test("Thread should be able to call a method") {
+    object hasTwoArgMethod {
+      var timesCalled = 0
+      def call(arg: String, arg2: Int): Unit = {
+        assertEquals("abc", arg)
+        assertEquals(123, arg2)
+        synchronized {
+          timesCalled += 1
+        }
+      }
+    }
+    new Thread(new Runnable {
+      def run(): Unit = {
+        hasTwoArgMethod.call("abc", 123)
+        hasTwoArgMethod.call("abc", 123)
+      }
+    }).start()
+    Thread.sleep(100)
+    assertEquals(hasTwoArgMethod.timesCalled, 2)
+  }
 }
