2c5963fd4e4dbf1fbeea3763d25afa4b68cebf13
Ethan Atkins <ethan.atkins@gmail.com>
2017-11-15 20:18:26 -0800

    Whitespace: alphabetize unistd.scala
    No functional change.

--- nativelib/src/main/scala/scala/scalanative/posix/unistd.scala
+++ nativelib/src/main/scala/scala/scalanative/posix/unistd.scala
@@ -9,37 +9,25 @@ object unistd {
 
   type off_t = CLongLong
 
-  def sleep(seconds: CUnsignedInt): CUnsignedInt                  = extern
-  def usleep(usecs: CUnsignedInt): CInt                           = extern
-  def unlink(path: CString): CInt                                 = extern
   def access(pathname: CString, mode: CInt): CInt                 = extern
-  def readlink(path: CString, buf: CString, bufsize: CSize): CInt = extern
-  def getcwd(buf: CString, size: CSize): CString                  = extern
-  def write(fildes: CInt, buf: Ptr[_], nbyte: CSize): CInt        = extern
-  def read(fildes: CInt, buf: Ptr[_], nbyte: CSize): CInt         = extern
+  def chdir(path: CString): CInt                                  = extern
   def close(fildes: CInt): CInt                                   = extern
   def fsync(fildes: CInt): CInt                                   = extern
-  def lseek(fildes: CInt, offset: off_t, whence: CInt): off_t     = extern
   def ftruncate(fildes: CInt, length: off_t): CInt                = extern
-  def truncate(path: CString, length: off_t): CInt                = extern
+  def getcwd(buf: CString, size: CSize): CString                  = extern
   def gethostname(name: CString, len: CSize): CInt                = extern
+  def lseek(fildes: CInt, offset: off_t, whence: CInt): off_t     = extern
+  def read(fildes: CInt, buf: Ptr[_], nbyte: CSize): CInt         = extern
+  def readlink(path: CString, buf: CString, bufsize: CSize): CInt = extern
   def sethostname(name: CString, len: CSize): CInt                = extern
-  def chdir(path: CString): CInt                                  = extern
+  def sleep(seconds: CUnsignedInt): CUnsignedInt                  = extern
+  def truncate(path: CString, length: off_t): CInt                = extern
+  def unlink(path: CString): CInt                                 = extern
+  def usleep(usecs: CUnsignedInt): CInt                           = extern
+  def write(fildes: CInt, buf: Ptr[_], nbyte: CSize): CInt        = extern
 
-  @name("scalanative_stdin_fileno")
-  def STDIN_FILENO: CInt = extern
-
-  @name("scalanative_stdout_fileno")
-  def STDOUT_FILENO: CInt = extern
-
-  @name("scalanative_stderr_fileno")
-  def STDERR_FILENO: CInt = extern
-
-  @name("scalanative_symlink")
-  def symlink(path1: CString, path2: CString): CInt = extern
-
-  @name("scalanative_symlinkat")
-  def symlinkat(path1: CString, fd: CInt, path2: CString): CInt = extern
+  @name("scalanative_chown")
+  def chown(path: CString, owner: uid_t, group: gid_t): CInt = extern
 
   @name("scalanative_link")
   def link(path1: CString, path2: CString): CInt = extern
@@ -51,8 +39,20 @@ object unistd {
              path2: CString,
              flag: CInt): CInt = extern
 
-  @name("scalanative_chown")
-  def chown(path: CString, owner: uid_t, group: gid_t): CInt = extern
+  @name("scalanative_stderr_fileno")
+  def STDERR_FILENO: CInt = extern
+
+  @name("scalanative_stdin_fileno")
+  def STDIN_FILENO: CInt = extern
+
+  @name("scalanative_stdout_fileno")
+  def STDOUT_FILENO: CInt = extern
+
+  @name("scalanative_symlink")
+  def symlink(path1: CString, path2: CString): CInt = extern
+
+  @name("scalanative_symlinkat")
+  def symlinkat(path1: CString, fd: CInt, path2: CString): CInt = extern
 
   // Macros
 
