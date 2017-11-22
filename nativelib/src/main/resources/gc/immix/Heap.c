2aae1006f01c13676e5fdff08be90e2ff02b27ae
Denys Shabalin <denys.shabalin@epfl.ch>
2017-11-22 11:03:23 +0100

    Use 1MB as initial large heap size

--- nativelib/src/main/resources/gc/immix/Heap.c
+++ nativelib/src/main/resources/gc/immix/Heap.c
@@ -42,9 +42,11 @@ word_t *Heap_mapAndAlign(size_t memoryLimit, size_t alignmentSize) {
 /**
  * Allocates the heap struct and initializes it
  */
-Heap *Heap_Create(size_t initialSize) {
-    assert(initialSize >= 2 * BLOCK_TOTAL_SIZE);
-    assert(initialSize % BLOCK_TOTAL_SIZE == 0);
+Heap *Heap_Create(size_t initialSmallHeapSize, size_t initialLargeHeapSize) {
+    assert(initialSmallHeapSize >= 2 * BLOCK_TOTAL_SIZE);
+    assert(initialSmallHeapSize % BLOCK_TOTAL_SIZE == 0);
+    assert(initialLargeHeapSize >= 2 * BLOCK_TOTAL_SIZE);
+    assert(initialLargeHeapSize % BLOCK_TOTAL_SIZE == 0);
 
     Heap *heap = malloc(sizeof(Heap));
 
@@ -54,18 +56,20 @@ Heap *Heap_Create(size_t initialSize) {
     word_t *smallHeapStart = Heap_mapAndAlign(memoryLimit, BLOCK_TOTAL_SIZE);
 
     // Init heap for small objects
-    heap->smallHeapSize = initialSize;
+    heap->smallHeapSize = initialSmallHeapSize;
     heap->heapStart = smallHeapStart;
-    heap->heapEnd = smallHeapStart + initialSize / WORD_SIZE;
+    heap->heapEnd = smallHeapStart + initialSmallHeapSize / WORD_SIZE;
     heap->allocator =
-        Allocator_Create(smallHeapStart, initialSize / BLOCK_TOTAL_SIZE);
+        Allocator_Create(smallHeapStart, initialSmallHeapSize / BLOCK_TOTAL_SIZE);
 
     // Init heap for large objects
     word_t *largeHeapStart = Heap_mapAndAlign(memoryLimit, MIN_BLOCK_SIZE);
-    heap->largeHeapSize = initialSize;
-    heap->largeAllocator = LargeAllocator_Create(largeHeapStart, initialSize);
+    heap->largeHeapSize = initialLargeHeapSize;
+    heap->largeAllocator =
+        LargeAllocator_Create(largeHeapStart, initialLargeHeapSize);
     heap->largeHeapStart = largeHeapStart;
-    heap->largeHeapEnd = (word_t *)((ubyte_t *)largeHeapStart + initialSize);
+    heap->largeHeapEnd =
+        (word_t *)((ubyte_t *)largeHeapStart + initialLargeHeapSize);
 
     return heap;
 }
--- nativelib/src/main/resources/gc/immix/ImmixGC.c
+++ nativelib/src/main/resources/gc/immix/ImmixGC.c
@@ -9,13 +9,12 @@
 #include "Object.h"
 #include "State.h"
 #include "utils/MathUtils.h"
-
-#define INITIAL_HEAP_SIZE (4 * 1024 * 1024UL)
+#include "Constants.h"
 
 void scalanative_collect();
 
 void scalanative_init() {
-    heap = Heap_Create(INITIAL_HEAP_SIZE);
+    heap = Heap_Create(INITIAL_SMALL_HEAP_SIZE, INITIAL_LARGE_HEAP_SIZE);
     stack = Stack_Alloc(INITIAL_STACK_SIZE);
 }
 
