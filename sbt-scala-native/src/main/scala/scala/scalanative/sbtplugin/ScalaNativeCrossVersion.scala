scala-native/scala-native

commit 884d0c7ac955a8bea069c171032c8bebde5e5b95

Fix #915: Upgrade plugins to sbt 1.X (#1039)

sbt-scala-native/src/main/scala/scala/scalanative/sbtplugin/ScalaNativeCrossVersion.scala
@@ -5,38 +5,21 @@ package sbtplugin
 
 import sbt._
 import scala.scalanative.nir.Versions
+import SBTCompat._
 
 object ScalaNativeCrossVersion {
+  private final val ReleaseVersion =
+    raw"""(\d+)\.(\d+)\.(\d+)""".r
 
-  val currentBinaryVersion = {
-    val ReleaseVersion = raw"""(\d+)\.(\d+)\.(\d+)""".r
+  val currentBinaryVersion = binaryVersion(Versions.current)
 
-    def binaryVersion(full: String): String = full match {
-      case ReleaseVersion(major, minor, _) => s"$major.$minor"
-      case _                               => full
-    }
-
-    binaryVersion(Versions.current)
+  def binaryVersion(full: String): String = full match {
+    case ReleaseVersion(major, _, _) => major
+    case _                           => full
   }
 
-  def scalaNativeMapped(cross: CrossVersion): CrossVersion = {
-    val scalaNativeVersionUnmapped: String => String =
-      _ => s"native$currentBinaryVersion"
-
-    val scalaNativeVersionMap: String => String =
-      version => s"native${currentBinaryVersion}_$version"
-
-    cross match {
-      case CrossVersion.Disabled =>
-        CrossVersion.binaryMapped(scalaNativeVersionUnmapped)
-      case cross: CrossVersion.Binary =>
-        CrossVersion.binaryMapped(
-          cross.remapVersion andThen scalaNativeVersionMap)
-      case cross: CrossVersion.Full =>
-        CrossVersion.fullMapped(
-          cross.remapVersion andThen scalaNativeVersionMap)
-    }
-  }
+  def scalaNativeMapped(cross: CrossVersion): CrossVersion =
+    crossVersionAddPlatformPart(cross, "native" + currentBinaryVersion)
 
   val binary: CrossVersion = scalaNativeMapped(CrossVersion.binary)
 

.travis.yml
@@ -3,21 +3,18 @@ sudo: required
 scala:
   - "2.11.8"
 
-matrix:
-  include:
-    - os: linux
-      dist: trusty
-      jdk: oraclejdk8
-      language: scala
-      env: SCALANATIVE_GC=boehm
-
-    - os: linux
-      dist: trusty
-      jdk: oraclejdk8
-      language: scala
-      env: SCALANATIVE_GC=immix
+os: linux
+dist: trusty
+jdk: oraclejdk8
+language: scala
 
 env:
+  matrix:
+    - SCALANATIVE_GC=boehm SBT_VERSION=0.13.16
+    - SCALANATIVE_GC=boehm SBT_VERSION=1.0.2
+    - SCALANATIVE_GC=immix SBT_VERSION=0.13.16
+    - SCALANATIVE_GC=immix SBT_VERSION=1.0.2
+
   global:
     - MAVEN_REALM="Sonatype Nexus Repository Manager"
     - MAVEN_DOMAIN="oss.sonatype.org"
@@ -49,7 +46,14 @@ script:
   - java -version
   - if [ -n "$CLANG_FORMAT_PATH" ]; then bin/clangfmt --test; fi
   - bin/scalafmt --test
-  - bash sbt -J-Xmx2G rebuild "set scriptedBufferLog in sbtScalaNative := false" test-all
+  - > 
+    sbt \
+      "-no-colors" \
+      "-J-Xmx2G" \
+      "^^ $SBT_VERSION" \
+      rebuild \
+      "set scriptedBufferLog in sbtScalaNative := false" \
+      test-all
 
 before_cache:
   - find $HOME/.sbt -name "*.lock" | xargs rm

build.sbt
@@ -4,7 +4,6 @@ import scalanative.tools.OptimizerReporter
 import scalanative.sbtplugin.ScalaNativePluginInternal._
 import scalanative.io.packageNameFromPath
 
-val toolScalaVersion      = "2.10.6"
 val libScalaVersion       = "2.11.11"
 val libCrossScalaVersions = Seq("2.11.8", "2.11.11")
 
@@ -163,7 +162,13 @@ lazy val noPublishSettings = Seq(
 lazy val toolSettings =
   baseSettings ++
     Seq(
-      scalaVersion := toolScalaVersion,
+      crossSbtVersions := List("0.13.16", "1.0.2"),
+      scalaVersion := {
+        (sbtBinaryVersion in pluginCrossBuild).value match {
+          case "0.13" => "2.10.6"
+          case _      => "2.12.3"
+        }
+      },
       scalacOptions ++= Seq(
         "-deprecation",
         "-unchecked",
@@ -224,7 +229,7 @@ lazy val tools =
         "com.lihaoyi"    %% "scalaparse" % "0.4.2",
         "org.scalacheck" %% "scalacheck" % "1.13.4" % "test",
         compilerPlugin(
-          "org.scalamacros" % "paradise" % "2.0.1" cross CrossVersion.full),
+          "org.scalamacros" % "paradise" % "2.1.0" cross CrossVersion.full),
         "org.scalatest" %% "scalatest" % "3.0.0" % "test"
       ),
       fullClasspath in Test := ((fullClasspath in Test) dependsOn setUpTestingCompiler).value,
@@ -273,11 +278,14 @@ lazy val sbtScalaNative =
     .in(file("sbt-scala-native"))
     .settings(sbtPluginSettings)
     .settings(
-      addSbtPlugin("org.scala-native" % "sbt-crossproject" % "0.1.0"),
+      crossScalaVersions := libCrossScalaVersions,
+      // fixed in https://github.com/sbt/sbt/pull/3397 (for sbt 0.13.17)
+      sbtBinaryVersion in update := (sbtBinaryVersion in pluginCrossBuild).value,
+      addSbtPlugin("org.scala-native" % "sbt-crossproject" % "0.2.2"),
       moduleName := "sbt-scala-native",
       sbtTestDirectory := (baseDirectory in ThisBuild).value / "scripted-tests",
       // `testInterfaceSerialization` needs to be available from the sbt plugin,
-      // but it's a Scala Native project (and thus 2.11), and the plugin is 2.10.
+      // but it's a Scala Native project (and thus 2.11), and the plugin is 2.10 or 2.12.
       // We simply add the sources to mimic cross-compilation.
       sources in Compile ++= (sources in Compile in testInterfaceSerialization).value,
       // publish the other projects before running scripted tests.

nir/src/main/scala/scala/scalanative/nir/serialization/BinaryDeserializer.scala
@@ -100,6 +100,7 @@ final class BinaryDeserializer(_buffer: => ByteBuffer) {
         case T.PinIfAttr =>
           deps += Dep.Conditional(getGlobalNoDep, getGlobalNoDep)
       }
+      ()
     }
 
     Attrs.fromSeq(buf)

project/build.sbt
@@ -1,7 +1,8 @@
 unmanagedSourceDirectories in Compile ++= {
   val root = baseDirectory.value.getParentFile
 
-  Seq(
+  (root / "sbt-scala-native/src/main/scala-sbt-0.13") +:
+    Seq(
     "util",
     "nir",
     "tools",
@@ -16,10 +17,10 @@ libraryDependencies ++= Seq(
   "com.lihaoyi"      %% "fastparse"           % "0.4.2",
   "com.lihaoyi"      %% "scalaparse"          % "0.4.2",
   compilerPlugin(
-    "org.scalamacros" % "paradise" % "2.0.1" cross CrossVersion.full)
+    "org.scalamacros" % "paradise" % "2.1.0" cross CrossVersion.full)
 )
 
-addSbtPlugin("org.scala-native" % "sbt-crossproject" % "0.1.0")
+addSbtPlugin("org.scala-native" % "sbt-crossproject" % "0.2.2")
 addSbtPlugin("com.eed3si9n"     % "sbt-dirty-money"  % "0.1.0")
 addSbtPlugin("me.lessis"        % "bintray-sbt"      % "0.3.0")
 addSbtPlugin("com.jsuereth"     % "sbt-pgp"          % "1.0.0")

sbt-scala-native/src/main/scala-sbt-0.13/scala/scalanative/sbtplugin/SBTCompat.scala
@@ -1,0 +1,31 @@
+package scala.scalanative
+package sbtplugin
+
+import sbt._, Keys._
+import scala.sys.process.{ProcessBuilder, ProcessLogger}
+import scala.language.implicitConversions
+
+// Ported from Scala.js
+// https://github.com/scala-js/scala-js/blob/v0.6.20/sbt-plugin/src/main/scala-sbt-0.13/org/scalajs/sbtplugin/SBTCompat.scala#L1
+
+private[sbtplugin] object SBTCompat {
+  def crossVersionAddPlatformPart(cross: CrossVersion,
+                                  part: String): CrossVersion = {
+    cross match {
+      case CrossVersion.Disabled =>
+        CrossVersion.binaryMapped(_ => part)
+      case cross: CrossVersion.Binary =>
+        CrossVersion.binaryMapped(cross.remapVersion.andThen(part + "_" + _))
+      case cross: CrossVersion.Full =>
+        CrossVersion.fullMapped(cross.remapVersion.andThen(part + "_" + _))
+    }
+  }
+
+  implicit class RichProcessBuilder(pb: ProcessBuilder) {
+    def lineStream_!(logger: ProcessLogger) = pb.lines_!(logger)
+    def lineStream_! : Stream[String]       = pb.lines_!
+  }
+
+  implicit def promoteProcessLogger(logger: sbt.Logger): ProcessLogger =
+    ProcessLogger(msg => logger.info(msg), msg => logger.error(msg))
+}

sbt-scala-native/src/main/scala-sbt-1.0/scala/scalanative/sbtplugin/SBTCompat.scala
@@ -1,0 +1,23 @@
+package scala.scalanative
+package sbtplugin
+
+import sbt._
+
+// https://github.com/scala-js/scala-js/blob/v0.6.20/sbt-plugin/src/main/scala-sbt-1.0/org/scalajs/sbtplugin/SBTCompat.scala
+
+private[sbtplugin] object SBTCompat {
+
+  def crossVersionAddPlatformPart(cross: CrossVersion,
+                                  part: String): CrossVersion = {
+    cross match {
+      case CrossVersion.Disabled =>
+        CrossVersion.constant(part)
+      case cross: sbt.librarymanagement.Constant =>
+        cross.withValue(part + "_" + cross.value)
+      case cross: CrossVersion.Binary =>
+        cross.withPrefix(part + "_" + cross.prefix)
+      case cross: CrossVersion.Full =>
+        cross.withPrefix(part + "_" + cross.prefix)
+    }
+  }
+}

sbt-scala-native/src/main/scala/scala/scalanative/sbtplugin/ScalaNativePluginInternal.scala
@@ -21,6 +21,10 @@ import scala.util.Try
 import System.{lineSeparator => nl}
 import java.io.ByteArrayInputStream
 
+import scala.sys.process.Process
+
+import SBTCompat._
+
 object ScalaNativePluginInternal {
 
   val nativeWarnOldJVM =
@@ -97,7 +101,7 @@ object ScalaNativePluginInternal {
     nativeCompileOptions := {
       val includes = {
         val includedir =
-          Try(Process("llvm-config --includedir").lines_!.toSeq)
+          Try(Process("llvm-config --includedir").lineStream_!.toSeq)
             .getOrElse(Seq.empty)
         ("/usr/local/include" +: includedir).map(s => s"-I$s")
       }
@@ -107,7 +111,7 @@ object ScalaNativePluginInternal {
     nativeLinkingOptions := {
       val libs = {
         val libdir =
-          Try(Process("llvm-config --libdir").lines_!.toSeq)
+          Try(Process("llvm-config --libdir").lineStream_!.toSeq)
             .getOrElse(Seq.empty)
         ("/usr/local/lib" +: libdir).map(s => s"-L$s")
       }
@@ -434,24 +438,28 @@ object ScalaNativePluginInternal {
       (nativeExternalDependencies.value.toSet --
         nativeAvailableDependencies.value.toSet).toList.sorted
     },
-    nativeAvailableDependencies := ResourceScope { implicit scope =>
-      val forceCompile = compile.value
+    nativeAvailableDependencies := {
+      val fcp = fullClasspath.value
+      ResourceScope { implicit scope =>
+        val globals = fcp
+          .collect { case p if p.data.exists => p.data }
+          .flatMap(p =>
+            tools.LinkerPath(VirtualDirectory.real(p)).globals.toSeq)
 
-      val globals = fullClasspath.value
-        .collect { case p if p.data.exists => p.data }
-        .flatMap(p => tools.LinkerPath(VirtualDirectory.real(p)).globals.toSeq)
-
-      globals.map(_.show).sorted
+        globals.map(_.show).sorted
+      }
     },
-    nativeExternalDependencies := ResourceScope { implicit scope =>
+    nativeExternalDependencies := {
       val forceCompile = compile.value
       val classDir     = classDirectory.value
-      val globals      = linker.ClassPath(VirtualDirectory.real(classDir)).globals
 
-      val config = tools.Config.empty.withPaths(Seq(classDir))
-      val result = (linker.Linker(config)).link(globals.toSeq)
+      ResourceScope { implicit scope =>
+        val globals = linker.ClassPath(VirtualDirectory.real(classDir)).globals
+        val config  = tools.Config.empty.withPaths(Seq(classDir))
+        val result  = (linker.Linker(config)).link(globals.toSeq)
 
-      result.unresolved.map(_.show).sorted
+        result.unresolved.map(_.show).sorted
+      }
     }
   )
 

commit 95e6ff67e201b023baa31fcd20b5ba2329b68fab

Fix binary version regression introduced in #1039
Binary version must include both major and minor versions of
Scala Native, not just major.

sbt-scala-native/src/main/scala/scala/scalanative/sbtplugin/ScalaNativeCrossVersion.scala
@@ -14,8 +14,8 @@ object ScalaNativeCrossVersion {
   val currentBinaryVersion = binaryVersion(Versions.current)
 
   def binaryVersion(full: String): String = full match {
-    case ReleaseVersion(major, _, _) => major
-    case _                           => full
+    case ReleaseVersion(major, minor, _) => s"$major.$minor"
+    case _                               => full
   }
 
   def scalaNativeMapped(cross: CrossVersion): CrossVersion =