52fe06b4c9816303c2002d4e5ea803fbdd910a0a
Sandeep Singh <sandeep@techaddict.me>
2017-11-30 00:59:25 +0530

    load and store methods

--- javalib/src/main/scala/java/util/Properties.scala
+++ javalib/src/main/scala/java/util/Properties.scala
@@ -1,8 +1,15 @@
+/**
+ * Ported from Harmony
+ */
 package java.util
+
+//
 
 import java.io.{PrintStream, PrintWriter}
 import java.{util => ju}
 
+import scala.annotation.{switch, tailrec}
+import scala.collection.immutable.{Map => SMap}
 import scala.collection.JavaConverters._
 import scala.scalanative.annotation.stub
 
@@ -14,8 +21,22 @@ class Properties(protected val defaults: Properties)
   def setProperty(key: String, value: String): AnyRef =
     put(key, value)
 
-  @stub
-  def load(inStream: java.io.InputStream): Unit = ???
+  def load(inStream: java.io.InputStream): Unit = {
+    if (inStream == null) {
+      throw new NullPointerException()
+    }
+    val stream = new BufferedInputStream(inStream)
+    stream.mark(Int.MaxValue)
+
+    val _isEbcdic = isEbcdic(stream)
+    stream.reset()
+
+    if (!_isEbcdic) {
+      loadImpl(new InputStreamReader(stream, "ISO8859-1")); //$NON-NLS-1$
+    } else {
+      loadImpl(new InputStreamReader(stream)); //$NON-NLS-1$
+    }
+  }
 
   @stub
   def load(reader: java.io.Reader): Unit = ???
@@ -76,10 +97,319 @@ class Properties(protected val defaults: Properties)
     entrySet().asScala.foreach { entry => out.println(format(entry)) }
   }
 
+  def load(reader: Reader): Unit = {
+    if (reader == null) {
+      throw new NullPointerException()
+    }
+    loadImpl(reader)
+  }
+
+  def store(out: OutputStream, comments: String): Unit = {
+    val buffer = new StringBuilder(200)
+    val writer = new OutputStreamWriter(out, "ISO8859_1") //$NON-NLS-1$
+    if (comments != null)
+      writeComments(writer, comments)
+    writer.write('#')
+    writer.write(new Date().toString)
+    writer.write(System.lineSeparator)
+
+    entrySet().asScala.foreach { entry =>
+      val key = entry.getKey.asInstanceOf[String]
+      dumpString(buffer, key, true, true)
+      buffer.append('=')
+      dumpString(buffer, entry.getValue.asInstanceOf[String], false, true)
+      buffer.append(System.lineSeparator)
+      writer.write(buffer.toString)
+      buffer.setLength(0)
+    }
+    writer.flush()
+  }
+
+  def store(writer: Writer, comments: String): Unit = {
+    if (comments != null)
+      writeComments(writer, comments)
+    writer.write('#')
+    writer.write(new Date().toString)
+    writer.write(System.lineSeparator)
+
+    val buffer = new StringBuilder(200)
+    entrySet().asScala.foreach { entry =>
+      val key = entry.getKey.asInstanceOf[String]
+      dumpString(buffer, key, true, false)
+      buffer.append('=')
+      dumpString(buffer, entry.getValue.asInstanceOf[String], false, false)
+      buffer.append(System.lineSeparator)
+      writer.write(buffer.toString)
+      buffer.setLength(0)
+    }
+    writer.flush()
+  }
+
+  @deprecated("", "") def save(out: OutputStream, comments: String): Unit =
+    store(out, comments)
+
+  private val NONE     = 0
+  private val SLASH    = 1
+  private val UNICODE  = 2
+  private val CONTINUE = 3 // when \r is encountered looks for next \n
+  private val KEY_DONE = 4
+  private val IGNORE   = 5
+  private lazy val nextCharMap =
+    SMap('b' -> '\b', 'f' -> '\f', 'n' -> '\n', 'r' -> '\r', 't' -> '\t')
+
+  private def isEbcdic(in: BufferedInputStream): Boolean = {
+    var b = in.read
+    while (b != 0xffffffff) {
+      if (b == 0x23 || b == 0x0a || b == 0x3d) { //ascii: newline/#/=
+        return false
+      }
+      if (b == 0x15) { //EBCDIC newline
+        return true
+      }
+      b = in.read
+    }
+    //we found no ascii newline, '#', neither '=', relative safe to consider it
+    //as non-ascii, the only exception will be a single line with only key(no value and '=')
+    //in this case, it should be no harm to read it in default charset
+    false
+  }
+
+  private def loadImpl(reader: Reader): Unit = {
+    var mode           = NONE
+    var unicode        = 0
+    var count          = 0
+    var nextChar: Char = 0
+    var buf            = new Array[Char](80)
+    var offset         = 0
+    var keyLength      = -1
+    val br             = new BufferedReader(reader)
+    @tailrec def processNext(isFirstChar: Boolean): Unit = {
+      val intVal = br.read()
+      if (intVal == -1) {
+        if (mode == UNICODE && count <= 4) {
+          throw new IllegalArgumentException(
+            "Invalid Unicode sequence: expected format")
+        }
+        if (keyLength == -1 && offset > 0)
+          keyLength = offset
+        if (keyLength >= 0) {
+          val key   = new String(buf, 0, keyLength)
+          val value = new String(buf, keyLength, offset - keyLength)
+          put(key, if (mode == SLASH) value + '\u0000' else value)
+        }
+      } else {
+        nextChar = intVal.toChar
+        if (offset == buf.length) {
+          val newBuf = new Array[Char](buf.length << 1)
+          System.arraycopy(buf, 0, newBuf, 0, offset)
+          buf = newBuf
+        }
+        val _bool = if (mode == SLASH) {
+          mode = NONE
+          (nextChar: @switch) match {
+            case '\r' =>
+              mode = CONTINUE // Look for a following \n
+              isFirstChar
+            case '\u0085' | '\n' =>
+              mode = IGNORE // Ignore whitespace on the next line
+              isFirstChar
+            case c @ ('b' | 'f' | 'n' | 'r' | 't') =>
+              nextChar = nextCharMap(c)
+              buf(offset) = nextChar
+              offset += 1
+              false
+            case 'u' =>
+              mode = UNICODE
+              unicode = 0
+              count = 0
+              isFirstChar
+            case _ =>
+              buf(offset) = nextChar
+              offset += 1
+              false
+          }
+        } else {
+          def fn(_nextChar: Char): Boolean = (_nextChar: @switch) match {
+            case '#' | '!' if isFirstChar =>
+              @tailrec def ignoreCharsTillEOL(tempVal: Char): Unit = {
+                if (tempVal != 0xFFFF) { // -1.toChar
+                  nextChar = tempVal.toChar
+
+                  // not
+                  // required
+                  if (nextChar != '\r' && nextChar != '\n' && nextChar != '\u0085') {
+                    ignoreCharsTillEOL(br.read().toChar)
+                  }
+                }
+              }
+
+              ignoreCharsTillEOL(br.read().toChar)
+              isFirstChar
+            case c @ ('\n' | '\u0085' | '\r') =>
+              if (c == '\n' && mode == CONTINUE) { // Part of a \r\n sequence
+                mode = IGNORE
+                isFirstChar
+              } else {
+                mode = NONE
+                if (offset > 0 || (offset == 0 && keyLength == 0)) {
+                  if (keyLength == -1) keyLength = offset
+                  val key   = new String(buf, 0, keyLength)
+                  val value = new String(buf, keyLength, offset - keyLength)
+                  put(key, value)
+                }
+                keyLength = -1
+                offset = 0
+                true
+              }
+            case '\\' =>
+              if (mode == KEY_DONE) keyLength = offset
+              mode = SLASH
+              isFirstChar
+            case ':' | '=' if keyLength == -1 =>
+              // if parsing the key
+              mode = NONE
+              keyLength = offset
+              isFirstChar
+            case _ =>
+              if (nextChar < 256 && Character.isWhitespace(nextChar)) {
+                if (mode == CONTINUE) mode = IGNORE
+                // if key length == 0 or value length == 0
+                if (offset == 0 || offset == keyLength || mode == IGNORE)
+                  isFirstChar
+                else if (keyLength == -1) {
+                  mode = KEY_DONE
+                  isFirstChar
+                } else {
+                  if (mode == IGNORE || mode == CONTINUE) mode = NONE
+                  else if (mode == KEY_DONE) {
+                    keyLength = offset
+                    mode = NONE
+                  }
+                  buf(offset) = nextChar
+                  offset += 1
+                  false
+                }
+              } else {
+                if (mode == IGNORE || mode == CONTINUE) mode = NONE
+                else if (mode == KEY_DONE) {
+                  keyLength = offset
+                  mode = NONE
+                }
+                buf(offset) = nextChar
+                offset += 1
+                false
+              }
+          }
+          if (mode == UNICODE) {
+            val digit = Character.digit(nextChar, 16)
+            if (digit >= 0) {
+              unicode = (unicode << 4) + digit
+              count += 1
+            } else if (count <= 4) {
+              throw new IllegalArgumentException(
+                "Invalid Unicode sequence: illegal character")
+            }
+            if (digit >= 0 && count < 4) {
+              isFirstChar
+            } else {
+              mode = NONE
+              buf(offset) = unicode.toChar
+              offset += 1
+              if (nextChar != '\n' && nextChar != '\u0085')
+                isFirstChar
+              else
+                fn(nextChar)
+            }
+          } else {
+            fn(nextChar)
+          }
+        }
+        processNext(_bool)
+      }
+    }
+
+    processNext(true)
+
+  }
+
+  private def writeComments(writer: Writer, comments: String): Unit = {
+    writer.write('#')
+    val chars = comments.toCharArray
+    var index = 0
+    while (index < chars.length) {
+      if (chars(index) < 256)
+        if (chars(index) == '\r' || chars(index) == '\n') {
+          val indexPlusOne = index + 1
+          if (chars(index) == '\r' && indexPlusOne < chars.length && chars(
+                indexPlusOne) == '\n') { // "\r\n"
+//          continue //todo: continue is not supported
+          }
+          writer.write(System.lineSeparator)
+          if (indexPlusOne < chars.length && (chars(indexPlusOne) == '#' || chars(
+                indexPlusOne) == '!')) { // return char with either '#' or '!' afterward
+//          continue //todo: continue is not supported
+          }
+          writer.write('#')
+        } else writer.write(chars(index))
+      else writer.write(unicodeToHexaDecimal(chars(index)))
+      index += 1
+    }
+    writer.write(System.lineSeparator)
+  }
+
+  private def dumpString(buffer: StringBuilder,
+                         string: String,
+                         isKey: Boolean,
+                         toHexaDecimal: Boolean): Unit = {
+    var index  = 0
+    val length = string.length
+    if (!isKey && index < length && string.charAt(index) == ' ') {
+      buffer.append("\\ ") //$NON-NLS-1$
+      index += 1
+    }
+
+    while (index < length) {
+      val ch = string.charAt(index)
+      (ch: @switch) match {
+        case '\t' =>
+          buffer.append("\\t")
+        case '\n' =>
+          buffer.append("\\n")
+        case '\f' =>
+          buffer.append("\\f")
+        case '\r' =>
+          buffer.append("\\r")
+        case _ =>
+          if ("\\#!=:".indexOf(ch) >= 0 || (isKey && ch == ' '))
+            buffer.append('\\')
+          if (ch >= ' ' && ch <= '~') {
+            buffer.append(ch)
+          } else if (toHexaDecimal) {
+            buffer.appendAll(unicodeToHexaDecimal(ch))
+          } else {
+            buffer.append(ch)
+          }
+      }
+      index += 1
+    }
+  }
+
+  private def unicodeToHexaDecimal(ch: Int): Array[Char] = {
+    val hexChars = Array('\\', 'u', '0', '0', '0', '0')
+    var hexChar  = 0
+    var index    = hexChars.length
+    var copyOfCh = ch
+    do {
+      hexChar = copyOfCh & 15
+      if (hexChar > 9) hexChar = hexChar - 10 + 'A'
+      else hexChar += '0'
+      index -= 1
+      hexChars(index) = hexChar.toChar
+    } while ({ copyOfCh >>>= 4; copyOfCh } != 0)
+    hexChars
+  }
+
   // TODO:
-  // @deprecated("", "") def save(out: OutputStream, comments: String): Unit
-  // def store(writer: Writer, comments: String): Unit
-  // def store(out: OutputStream, comments: String): Unit
   // def loadFromXML(in: InputStream): Unit
   // def storeToXML(os: OutputStream, comment: String): Unit
   // def storeToXML(os: OutputStream, comment: String, encoding: String): Unit
--- unit-tests/src/test/scala/java/util/PropertiesSuite.scala
+++ unit-tests/src/test/scala/java/util/PropertiesSuite.scala
@@ -1,3 +1,6 @@
+/**
+ * Ported from Harmony
+ */
 package java.util
 
 import java.io._
@@ -48,5 +51,207 @@ object PropertiesSuite extends tests.Suite {
         |p000000000011111111112222222222333333...=40
       """.stripMargin
     assertResult(result2)
+  }
+
+  private val dummyProps = {
+    val bout = new ByteArrayOutputStream()
+    val ps   = new PrintStream(bout)
+    ps.println("#commented.key=dummy_value")
+    ps.println("key1=value1")
+    ps.println("key2=value1")
+    ps.close()
+    bout.toByteArray
+  }
+
+  test("load(InputStream) with null input") {
+    val prop = new java.util.Properties()
+    assertThrows[NullPointerException] {
+      prop.load(null: java.io.InputStream)
+    }
+  }
+
+  test("load(InputStream)") {
+    val is: InputStream = new ByteArrayInputStream(dummyProps)
+    val prop            = new Properties()
+    prop.load(is)
+    is.close()
+
+    assertEquals("value1", prop.getProperty("key1"))
+    assertNull(prop.getProperty("commented.key"))
+    assertEquals("default_value",
+                 prop.getProperty("commented.key", "default_value"))
+  }
+
+  test("load(InputStream) for empty keys") {
+    var prop = new java.util.Properties()
+    prop.load(new ByteArrayInputStream("=".getBytes()))
+    assertEquals("", prop.get(""))
+
+    prop = new java.util.Properties()
+    prop.load(new ByteArrayInputStream(" = ".getBytes()))
+    assertEquals("", prop.get(""))
+  }
+
+  test("load(InputStream) handle whitespace") {
+    var prop = new java.util.Properties()
+    prop.load(new ByteArrayInputStream(" a= b".getBytes()))
+    assertEquals("b", prop.get("a"))
+
+    prop = new java.util.Properties()
+    prop.load(new ByteArrayInputStream(" a b".getBytes()))
+    assertEquals("b", prop.get("a"))
+  }
+
+  test("load(InputStream) handle special chars") {
+    var prop = new java.util.Properties()
+    prop.load(
+      new ByteArrayInputStream(
+        "#\u008d\u00d2\na=\u008d\u00d3".getBytes("ISO8859_1")))
+    assertEquals("\u008d\u00d3", prop.get("a"))
+
+    prop = new java.util.Properties()
+    prop.load(
+      new ByteArrayInputStream(
+        "#properties file\r\nfred=1\r\n#last comment".getBytes("ISO8859_1")))
+    assertEquals("1", prop.get("fred"))
+  }
+
+  test("load(InputStream) with file input") {
+    val file =
+      new File("unit-tests/src/test/resources/properties-load-test.properties")
+    val is: InputStream = new FileInputStream(file)
+    val prop            = new Properties()
+    prop.load(is)
+    is.close()
+
+    assertEquals("\n \t \f", prop.getProperty(" \r"))
+    assertEquals("a", prop.getProperty("a"))
+    assertEquals("bb as,dn   ", prop.getProperty("b"))
+    assertEquals(":: cu", prop.getProperty("c\r \t\nu"))
+    assertEquals("bu", prop.getProperty("bu"))
+    assertEquals("d\r\ne=e", prop.getProperty("d"))
+    assertEquals("fff", prop.getProperty("f"))
+    assertEquals("g", prop.getProperty("g"))
+    assertEquals("", prop.getProperty("h h"))
+    assertEquals("i=i", prop.getProperty(" "))
+    assertEquals("   j", prop.getProperty("j"))
+    assertEquals("   c", prop.getProperty("space"))
+    assertEquals("\\", prop.getProperty("dblbackslash"))
+  }
+
+  test("load(Reader) with null input") {
+    val prop = new java.util.Properties()
+    assertThrows[NullPointerException] {
+      prop.load(null: Reader)
+    }
+  }
+
+  test("load(Reader)") {
+    val is: InputStream = new ByteArrayInputStream(dummyProps)
+    val prop            = new Properties()
+    prop.load(new InputStreamReader(is))
+    is.close()
+
+    assertEquals("value1", prop.getProperty("key1"))
+    assertNull(prop.getProperty("commented.key"))
+    assertEquals("default_value",
+                 prop.getProperty("commented.key", "default_value"))
+  }
+
+  test("load(Reader) handle special chars") {
+    var prop = new java.util.Properties()
+    prop.load(
+      new InputStreamReader(new ByteArrayInputStream(
+        "#\u008d\u00d2\na=\u008d\u00d3".getBytes("UTF-8"))))
+    assertEquals("\u008d\u00d3", prop.get("a"))
+
+    prop = new java.util.Properties()
+    prop.load(
+      new InputStreamReader(new ByteArrayInputStream(
+        "#properties file\r\nfred=1\r\n#last comment".getBytes("UTF-8"))))
+    assertEquals("1", prop.get("fred"))
+  }
+
+  test("load(Reader) with file input") {
+    val file =
+      new File("unit-tests/src/test/resources/properties-load-test.properties")
+    val is: InputStream = new FileInputStream(file)
+    val prop            = new Properties()
+    prop.load(new InputStreamReader(is))
+    is.close()
+
+    assertEquals("\n \t \f", prop.getProperty(" \r"))
+    assertEquals("a", prop.getProperty("a"))
+    assertEquals("bb as,dn   ", prop.getProperty("b"))
+    assertEquals(":: cu", prop.getProperty("c\r \t\nu"))
+    assertEquals("bu", prop.getProperty("bu"))
+    assertEquals("d\r\ne=e", prop.getProperty("d"))
+    assertEquals("fff", prop.getProperty("f"))
+    assertEquals("g", prop.getProperty("g"))
+    assertEquals("", prop.getProperty("h h"))
+    assertEquals("i=i", prop.getProperty(" "))
+    assertEquals("   j", prop.getProperty("j"))
+    assertEquals("   c", prop.getProperty("space"))
+    assertEquals("\\", prop.getProperty("dblbackslash"))
+  }
+
+  test("store(OutputStream, comments) with null input") {
+    val prop = new java.util.Properties()
+    assertThrows[NullPointerException] {
+      prop.store(null: OutputStream, "")
+    }
+  }
+
+  test("store(OutputStream, comments)") {
+    val prop1 = new Properties()
+    val prop2 = new Properties()
+
+    prop1.put("Property A", " aye\\\f\t\n\r\b")
+    prop1.put("Property B", "b ee#!=:")
+    prop1.put("Property C", "see")
+
+    val out = new ByteArrayOutputStream()
+    prop1.store(out, "A Header")
+    out.close()
+
+    val in = new ByteArrayInputStream(out.toByteArray)
+    prop2.load(in)
+    in.close()
+
+    val e = prop1.propertyNames()
+    while (e.hasMoreElements) {
+      val nextKey = e.nextElement().asInstanceOf[String]
+      assertEquals(prop2.getProperty(nextKey), prop1.getProperty(nextKey))
+    }
+  }
+
+  test("store(Writer, comments) with null input") {
+    val prop = new java.util.Properties()
+    assertThrows[NullPointerException] {
+      prop.store(null: OutputStream, "")
+    }
+  }
+
+  test("store(Writer, comments)") {
+    val prop1 = new Properties()
+    val prop2 = new Properties()
+
+    prop1.put("Property A", " aye\\\f\t\n\r\b")
+    prop1.put("Property B", "b ee#!=:")
+    prop1.put("Property C", "see")
+
+    val out = new ByteArrayOutputStream()
+    prop1.store(new OutputStreamWriter(out), "A Header")
+    out.close()
+
+    val in = new ByteArrayInputStream(out.toByteArray)
+    prop2.load(in)
+    in.close()
+
+    val e = prop1.propertyNames()
+    while (e.hasMoreElements) {
+      val nextKey = e.nextElement().asInstanceOf[String]
+      assertEquals(prop2.getProperty(nextKey), prop1.getProperty(nextKey))
+    }
   }
 }
