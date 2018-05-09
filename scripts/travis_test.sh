1af81dedda32d7438009b4dab73b7c33a434aa5a
Shadaj Laddad <shadaj@users.noreply.github.com>
2018-05-09 13:23:17 -0700

    Use prebuilt images for testing containers

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -3,8 +3,6 @@ java -version;
 
 docker run --rm --privileged multiarch/qemu-user-static:register;
 
-docker build -t $TEST_CONTAINER $TEST_CONTAINER
-
 sudo chmod a+rwx -R $HOME;
 
 docker run -v $HOME/.ivy2:/home/scala-native/.ivy2 -v $HOME/.sbt:/home/scala-native/.sbt -v $PWD:/home/scala-native/scala-native -e SCALANATIVE_GC=$SCALANATIVE_GC -e SBT_VERSION=$SBT_VERSION -e TEST_COMMAND=$TEST_COMMAND -it $TEST_CONTAINER;
