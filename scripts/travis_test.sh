d29914900465f40f06229ee6082c1fb86af24cd8
Shadaj Laddad <shadaj@users.noreply.github.com>
2018-05-09 12:28:14 -0700

    Only run scripted tests when testing SBT 1.0.4

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -7,4 +7,4 @@ docker build -t $TEST_CONTAINER $TEST_CONTAINER
 
 sudo chmod a+rwx -R $HOME;
 
-docker run -v $HOME/.ivy2:/home/scala-native/.ivy2 -v $HOME/.sbt:/home/scala-native/.sbt -v $PWD:/home/scala-native/scala-native -e SCALANATIVE_GC=$SCALANATIVE_GC -e SBT_VERSION=$SBT_VERSION -it $TEST_CONTAINER;
+docker run -v $HOME/.ivy2:/home/scala-native/.ivy2 -v $HOME/.sbt:/home/scala-native/.sbt -v $PWD:/home/scala-native/scala-native -e SCALANATIVE_GC=$SCALANATIVE_GC -e SBT_VERSION=$SBT_VERSION -e TEST_COMMAND=$TEST_COMMAND -it $TEST_CONTAINER;
