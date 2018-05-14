39e7945e305a56d1f1462d245ba9b3a1dc1008ba
Shadaj Laddad <shadaj@users.noreply.github.com>
2018-05-14 16:50:10 -0700

    Limit CPUs for ARM32

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -5,4 +5,4 @@ docker run --rm --privileged multiarch/qemu-user-static:register;
 
 sudo chmod a+rwx -R $HOME;
 
-docker run -v $HOME/.ivy2:/home/scala-native/.ivy2 -v $HOME/.sbt:/home/scala-native/.sbt -v $PWD:/home/scala-native/scala-native -e SCALANATIVE_GC=$SCALANATIVE_GC -e SBT_VERSION=$SBT_VERSION -e TEST_COMMAND=$TEST_COMMAND -it $TEST_CONTAINER;
+docker run -v $HOME/.ivy2:/home/scala-native/.ivy2 -v $HOME/.sbt:/home/scala-native/.sbt -v $PWD:/home/scala-native/scala-native -e SCALANATIVE_GC=$SCALANATIVE_GC -e SBT_VERSION=$SBT_VERSION -e TEST_COMMAND=$TEST_COMMAND $DOCKER_OPTIONS -it $TEST_CONTAINER;
