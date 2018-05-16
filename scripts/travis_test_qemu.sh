6c8c50d50b35c50bd65fe30b9ffe0461287351c7
Shadaj Laddad <shadaj@users.noreply.github.com>
2018-05-16 16:40:26 -0700

    Limit CPUs to prevent segfault

--- scripts/travis_test_qemu.sh
+++ scripts/travis_test_qemu.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 sudo chmod a+rwx -R $HOME;
 
-docker run -v /usr/bin/qemu-$QEMU_NAME-static:/usr/bin/qemu-$QEMU_NAME-static -it $TEST_CONTAINER uname -a
+docker run --cpuset-cpus="0" -v /usr/bin/qemu-$QEMU_NAME-static:/usr/bin/qemu-$QEMU_NAME-static -it $TEST_CONTAINER uname -a
 
-docker run -v /usr/bin/qemu-$QEMU_NAME-static:/usr/bin/qemu-$QEMU_NAME-static -it $TEST_CONTAINER java -version
+docker run --cpuset-cpus="0" -v /usr/bin/qemu-$QEMU_NAME-static:/usr/bin/qemu-$QEMU_NAME-static -it $TEST_CONTAINER java -version
 
-docker run -v /usr/bin/qemu-$QEMU_NAME-static:/usr/bin/qemu-$QEMU_NAME-static -v $HOME/.ivy2:/home/scala-native/.ivy2 -v $HOME/.sbt:/home/scala-native/.sbt -v $PWD:/home/scala-native/scala-native -e SCALANATIVE_GC=$SCALANATIVE_GC -e SBT_VERSION=$SBT_VERSION -e TEST_COMMAND=$TEST_COMMAND -it $TEST_CONTAINER;
+docker run --cpuset-cpus="0" -v /usr/bin/qemu-$QEMU_NAME-static:/usr/bin/qemu-$QEMU_NAME-static -v $HOME/.ivy2:/home/scala-native/.ivy2 -v $HOME/.sbt:/home/scala-native/.sbt -v $PWD:/home/scala-native/scala-native -e SCALANATIVE_GC=$SCALANATIVE_GC -e SBT_VERSION=$SBT_VERSION -e TEST_COMMAND=$TEST_COMMAND -it $TEST_CONTAINER;
