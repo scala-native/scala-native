6db16e4eab862f978c1a0d4b71441514cda028ce
Shadaj Laddad <shadaj@users.noreply.github.com>
2018-05-15 21:34:15 -0700

    Don't use variable for Docker options

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo chmod a+rwx -R $HOME;
 
-docker run $DOCKER_OPTIONS -it $TEST_CONTAINER java -version
+docker run -it $TEST_CONTAINER java -version
 
-docker run -v $HOME/.ivy2:/home/scala-native/.ivy2 -v $HOME/.sbt:/home/scala-native/.sbt -v $PWD:/home/scala-native/scala-native -e SCALANATIVE_GC=$SCALANATIVE_GC -e SBT_VERSION=$SBT_VERSION -e TEST_COMMAND=$TEST_COMMAND $DOCKER_OPTIONS -it $TEST_CONTAINER;
+docker run -v $HOME/.ivy2:/home/scala-native/.ivy2 -v $HOME/.sbt:/home/scala-native/.sbt -v $PWD:/home/scala-native/scala-native -e SCALANATIVE_GC=$SCALANATIVE_GC -e SBT_VERSION=$SBT_VERSION -e TEST_COMMAND=$TEST_COMMAND -it $TEST_CONTAINER;
