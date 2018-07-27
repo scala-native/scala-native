249e9734b0e7e82354f12695a5635c43d8f4f401
Denys Shabalin <denys.shabalin@epfl.ch>
2018-07-27 16:51:19 +0200

    Minimize use of fields within top-level objects

---
+++ scalalib/overrides-2.11/scala/collection/immutable/LinearSeq.scala
@@ -0,0 +1,46 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2003-2013, LAMP/EPFL             **
+**  __\ \/ /__/ __ |/ /__/ __ |    http://scala-lang.org/               **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+\*                                                                      */
+
+
+
+package scala
+package collection
+package immutable
+
+import generic._
+import mutable.Builder
+
+/** A subtrait of `collection.LinearSeq` which represents sequences that
+ *  are guaranteed immutable.
+ *  $linearSeqInfo
+ */
+trait LinearSeq[+A] extends Seq[A]
+                            with scala.collection.LinearSeq[A]
+                            with GenericTraversableTemplate[A, LinearSeq]
+                            with LinearSeqLike[A, LinearSeq[A]] {
+  override def companion: GenericCompanion[LinearSeq] = LinearSeq
+  override def seq: LinearSeq[A] = this
+}
+
+/** $factoryInfo
+ *  The current default implementation of a $Coll is a `List`.
+ *  @define coll immutable linear sequence
+ *  @define Coll `immutable.LinearSeq`
+ */
+object LinearSeq extends SeqFactory[LinearSeq] {
+  @inline override def ReusableCBF: GenericCanBuildFrom[Nothing] = ReusableCBFInstance
+  private object ReusableCBFInstance extends GenericCanBuildFrom[Nothing] {
+    @inline override def apply() = newBuilder[Nothing]
+  }
+
+  @inline implicit def canBuildFrom[A]: CanBuildFrom[Coll, A, LinearSeq[A]] =
+    ReusableCBF.asInstanceOf[GenericCanBuildFrom[A]]
+
+  @inline def newBuilder[A]: Builder[A, LinearSeq[A]] =
+    new mutable.ListBuffer
+}
