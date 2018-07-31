682126b17c6966831c68d2df44e0195fea82fbb0
ekrich <ekrichardson@gmail.com>
2018-07-31 10:21:09 -0400

    Initial commit to add signal support

---
+++ nativelib/src/main/resources/posix/signal.c
@@ -0,0 +1,10 @@
+#include <signal.h>
+
+// symbolic constants
+
+int scalanative_sigev_none() { return SIGEV_NONE; }
+int scalanative_sigev_signal() { return SIGEV_SIGNAL; }
+int scalanative_sigev_thread() { return SIGEV_THREAD; }
+
+
+// scala native functions
