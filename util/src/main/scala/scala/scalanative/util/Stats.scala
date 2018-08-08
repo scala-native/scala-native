1bc3b1b332405dc64939f9ac6eb7fcaa1ef476d8
Denys Shabalin <denys.shabalin@epfl.ch>
2018-08-08 15:17:17 +0200

    Make stats traking thread-aware

--- util/src/main/scala/scala/scalanative/util/Stats.scala
+++ util/src/main/scala/scala/scalanative/util/Stats.scala
@@ -4,35 +4,80 @@ package util
 import scala.collection.mutable
 
 object Stats {
-  private val times    = mutable.Map.empty[String, Long]
-  private val counters = mutable.Map.empty[String, Long]
+  private val times  = mutable.Map.empty[String, Long]
+  private val counts = mutable.Map.empty[String, Long]
+  private def printTotal(): Unit = {
+    val totalTimes   = mutable.Map.empty[String, Long]
+    val totalCounts  = mutable.Map.empty[String, Long]
+    val totalThreads = mutable.Map.empty[String, Long]
+    times.foreach {
+      case (k, v) =>
+        val key = k.split(":")(1)
+        totalTimes(key) = totalTimes.getOrElse(key, 0L) + v
+        totalThreads(key) = totalThreads.getOrElse(key, 0L) + 1
+    }
+    counts.foreach {
+      case (k, v) =>
+        val key = k.split(":")(1)
+        totalCounts(key) = totalCounts.getOrElse(key, 0L) + v
+    }
+    println("--- Total")
+    totalTimes.toSeq.sortBy(_._1).foreach {
+      case (key, time) =>
+        val ms      = (time / 1000000D).toString
+        val count   = totalCounts(key)
+        val threads = totalThreads(key)
+        println(s"$key: $ms ms, $count times, $threads threads")
+    }
+  }
+  private def printThread(id: String): Unit = {
+    println(s"--- Thread $id")
+    times.toSeq.sortBy(_._1).foreach {
+      case (key, time) if key.startsWith(id) =>
+        val ms    = (time / 1000000D).toString
+        val count = counts(key)
+        val k     = key.split(":")(1)
+        println(s"$k: $ms ms, $count times")
+      case _ =>
+        ()
+    }
+  }
+  private def printThreads(): Unit = {
+    val threads = mutable.Set.empty[String]
+    times.keys.foreach { k =>
+      threads += k.split(":")(0)
+    }
+    threads.toSeq.sorted.foreach(printThread)
+  }
+  private def print(): Unit = synchronized {
+    printTotal()
+    printThreads()
+  }
+  private def clear(): Unit = synchronized {
+    times.clear()
+    counts.clear()
+  }
+  private def threadKey(key: String): String =
+    java.lang.Thread.currentThread.getId + ":" + key
+  def in[T](f: => T): T = {
+    clear()
+    val res = f
+    print()
+    res
+  }
   def time[T](key: String)(f: => T): T = {
     import System.nanoTime
     val start = nanoTime()
     val res   = f
     val end   = nanoTime()
-    synchronized {
-      times(key) = times.getOrElse(key, 0L) + (end - start)
+    val t     = end - start
+    val k     = threadKey(key)
+    times.synchronized {
+      times(k) = times.getOrElse(k, 0L) + t
+    }
+    counts.synchronized {
+      counts(k) = counts.getOrElse(k, 0L) + 1
     }
     res
-  }
-  def count(key: String): Unit = synchronized {
-    counters(key) = counters.getOrElse(key, 0L) + 1L
-  }
-  def print(): Unit = synchronized {
-    println("--- Times")
-    times.toSeq.sortBy(_._1).foreach {
-      case (key, time) =>
-        println(key + ": " + (time / 1000000D).toString + " ms")
-    }
-    println("--- Counters")
-    counters.toSeq.sortBy(_._1).foreach {
-      case (key, n) =>
-        println(key + ": " + n + " times")
-    }
-  }
-  def clear(): Unit = synchronized {
-    times.clear()
-    counters.clear()
   }
 }
