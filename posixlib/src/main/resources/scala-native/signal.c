333147888445c78a1ae6b702a4a625b865888a6a
ekrich <ekrichardson@gmail.com>
2018-08-20 11:44:45 -0400

    Add signal constants and functions

--- posixlib/src/main/resources/scala-native/signal.c
+++ posixlib/src/main/resources/scala-native/signal.c
@@ -1,10 +1,42 @@
 #include <signal.h>
 
 // symbolic constants
+void *scalanative_sig_dfl(int s) { return SIG_DFL; }
+void *scalanative_sig_err(int s) { return SIG_ERR; }
+void *scalanative_sig_hold(int s) { return SIG_HOLD; }
+void *scalanative_sig_ign(int s) { return SIG_IGN; }
 
 int scalanative_sigev_none() { return SIGEV_NONE; }
 int scalanative_sigev_signal() { return SIGEV_SIGNAL; }
 int scalanative_sigev_thread() { return SIGEV_THREAD; }
 
+int scalanative_sigabrt() { return SIGABRT; }
+int scalanative_sigalrm() { return SIGALRM; }
+int scalanative_sigbus() { return SIGBUS; }
+int scalanative_sigchld() { return SIGCHLD; }
+int scalanative_sigcont() { return SIGCONT; }
+int scalanative_sigfpe() { return SIGFPE; }
+int scalanative_sighup() { return SIGHUP; }
+int scalanative_sigill() { return SIGILL; }
+int scalanative_sigint() { return SIGINT; }
+int scalanative_sigkill() { return SIGKILL; }
+int scalanative_sigpipe() { return SIGPIPE; }
+int scalanative_sigquit() { return SIGQUIT; }
+int scalanative_sigsegv() { return SIGSEGV; }
+int scalanative_sigstop() { return SIGSTOP; }
+int scalanative_sigterm() { return SIGTERM; }
+int scalanative_sigtstp() { return SIGTSTP; }
+int scalanative_sigttin() { return SIGTTIN; }
+int scalanative_sigttou() { return SIGTTOU; }
+int scalanative_sigusr1() { return SIGUSR1; }
+int scalanative_sigusr2() { return SIGUSR2; }
+// int scalanative_sigpoll() { return SIGPOLL; }
+int scalanative_sigprof() { return SIGPROF; }
+int scalanative_sigsys() { return SIGSYS; }
+int scalanative_sigtrap() { return SIGTRAP; }
+int scalanative_sigurg() { return SIGURG; }
+int scalanative_sigtalrm() { return SIGVTALRM; }
+int scalanative_sigxcpu() { return SIGXCPU; }
+int scalanative_sigxfsz() { return SIGXFSZ; }
 
 // scala native functions
