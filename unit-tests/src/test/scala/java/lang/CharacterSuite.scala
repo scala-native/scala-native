be6a2b2875f256e8e379bb52a2e5899b8acb5ea8
Lee Tibbert <LeeTibbert@users.noreply.github>
2018-08-30 11:42:39 +0000

    Fix #1189 "Character.digit is too restrictive in what it considers digits"
      * The presenting problem was expressed in issue #1189
        "Character.digit is too restrictive in what it considers digits "
        This issue is now fixed. ScalaNative now can correctly categorize
        digits from additional Unicode 16 bit blocks.
    
      * The code in this pull request is ported, with gratitude and minor
        modifications, from ScalaJS, especially ScalaJS commit:
            https://github.com/scala-js/scala-js/commit/
            396ff7287d06a7690ac12ed906b878e377fba91d
    
        Thank you, @sjrd, for both permission to use the code and, especially,
        notification that it existed.
    
      * The prior code had been missing a Java 8 method
        `def digit(codePoint: Int, radix: Int): Int`. That method has now
        been ported.
    
      * An unexpected but welcome consequence of this PR is that extended
        Unicode strings are now supported by j.l.Integer.parseInt,
        j.l.Integer.parseUnsignedInt, j.l.Long.parseLong, &
        j.l.Long.parseUnsignedLong. I also checked that at least
        j.l.Integer.parseInt rejected, as it should, Unicode supplemental
        characters, even though they might be reported as digits by
        Character.digit.
    
        I was prompted to explore the parse* methods parsing Unicode by
        a ScalaJs commit to Character.scala after the Character.digit
        commit I was porting.  It appears that ScalaJs change is not needed
        by ScalaNative.
    
      * The ScalaJs digit test case was added, with minor modifications to avoid
        naming conflicts, to CharacterSuite.scala.
    
    64/32 bit issues:
    
          None known.
    
    Documentation:
    
          Deserves a release note.
    
    Testing:
    
    	* Built and tested ("test-all") on X86_64. All tests passed.

--- unit-tests/src/test/scala/java/lang/CharacterSuite.scala
+++ unit-tests/src/test/scala/java/lang/CharacterSuite.scala
@@ -13,6 +13,91 @@ package java.lang
 object CharacterSuite extends tests.Suite {
   import java.lang.Character._
 
+  // Ported, with gratitude & possibly modifications
+  // from ScalaJs CharacterTest.scala
+  // https://github.com/scala-js/scala-js/blob/master/
+  //         test-suite/shared/src/test/scala/org/scalajs/testsuite/
+  //         javalib/lang/CharacterTest.scala
+
+  test("digit") {
+
+    // method test() in ScalaJS, but that name already in use in test Suite.
+    // To assay is, roughly "to judge the worth of" or "try, attempt".
+
+    def assay(expected: Int, codePoint: Int): Unit = {
+      assertEquals(expected, Character.digit(codePoint, MAX_RADIX))
+      if (codePoint <= Char.MaxValue)
+        assertEquals(expected, Character.digit(codePoint.toChar, MAX_RADIX))
+
+      if (expected != -1) {
+        assertEquals(
+          expected,
+          Character.digit(codePoint, Math.max(expected + 1, MIN_RADIX)))
+
+        if (expected >= MIN_RADIX)
+          assertEquals(-1, Character.digit(codePoint, expected))
+      }
+    }
+
+    // Invalid radix
+
+    assertEquals(-1, Character.digit('0', MIN_RADIX - 1))
+    assertEquals(-1, Character.digit('0', MAX_RADIX + 1))
+    assertEquals(-1, Character.digit('0', -1))
+
+    assertEquals(-1, Character.digit('0'.toInt, MIN_RADIX - 1))
+    assertEquals(-1, Character.digit('0'.toInt, MAX_RADIX + 1))
+    assertEquals(-1, Character.digit('0'.toInt, -1))
+
+    // A few invalid digits
+    assay(-1, '}')
+    assay(-1, -4)
+    assay(-1, 0xffffff)
+    assay(-1, '0' - 1)
+    assay(-1, '9' + 1)
+    assay(-1, 'A' - 1)
+    assay(-1, 'Z' + 1)
+    assay(-1, 'a' - 1)
+    assay(-1, 'z' + 1)
+    assay(-1, 0xff20)
+    assay(-1, 0xff3b)
+    assay(-1, 0xff40)
+    assay(-1, 0xff5b)
+    assay(-1, 0xbe5)
+    assay(-1, 0xbf0)
+    assay(-1, 0x11065)
+    assay(-1, 0x11070)
+    assay(-1, Int.MinValue)
+    assay(-1, Int.MaxValue)
+
+    // Every single valid digit
+
+// format: off
+    val All0s = Array[Int]('0', 0x660, 0x6f0, 0x7c0, 0x966, 0x9e6, 0xa66,
+        0xae6, 0xb66, 0xbe6, 0xc66, 0xce6, 0xd66, 0xe50, 0xed0, 0xf20, 0x1040,
+        0x1090, 0x17e0, 0x1810, 0x1946, 0x19d0, 0x1a80, 0x1a90, 0x1b50, 0x1bb0,
+        0x1c40, 0x1c50, 0xa620, 0xa8d0, 0xa900, 0xa9d0, 0xaa50, 0xabf0, 0xff10,
+        0x104a0, 0x11066, 0x110f0, 0x11136, 0x111d0, 0x116c0, 0x1d7ce, 0x1d7d8,
+        0x1d7e2, 0x1d7ec, 0x1d7f6)
+// format: on
+
+    for {
+      zero   <- All0s
+      offset <- 0 to 9
+    } {
+      assay(offset, zero + offset)
+    }
+
+    val AllAs = Array[Int]('A', 'a', 0xff21, 0xff41)
+
+    for {
+      a      <- AllAs
+      offset <- 0 to 25
+    } {
+      assay(10 + offset, a + offset)
+    }
+  }
+
   def toInt(hex: String): Int = Integer.parseInt(hex, 16)
 
   /** toUpperCase/toLowerCase based on Unicode 7 case folding.
@@ -49,6 +134,7 @@ object CharacterSuite extends tests.Suite {
     assert(toLowerCase(toInt("10FFFF")) equals toInt("10FFFF"))
     assert(toLowerCase(toInt("110000")) equals toInt("110000"))
   }
+
   test("toUpperCase") {
     // low chars
     assert(toUpperCase('\n') equals '\n')
@@ -76,4 +162,5 @@ object CharacterSuite extends tests.Suite {
     assert(toUpperCase(toInt("10FFFF")) equals toInt("10FFFF"))
     assert(toUpperCase(toInt("110000")) equals toInt("110000"))
   }
+
 }
