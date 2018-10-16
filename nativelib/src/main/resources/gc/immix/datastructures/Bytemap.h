65d80ff1c662ef132b06f8fb708c60729294d5a5
Valdis <valdisxp1@gmail.com>
2018-10-16 15:16:14 +0200

    more readable explanation about this optimization

--- nativelib/src/main/resources/gc/immix/datastructures/Bytemap.h
+++ nativelib/src/main/resources/gc/immix/datastructures/Bytemap.h
@@ -93,7 +93,7 @@ static inline void Bytemap_ClearBlock(Bytemap *bytemap, word_t *start) {
     implements this, just with hardcoded constants:
 
     size_t startIndex = Bytemap_index(bytemap, start);
-    size_t endIndex = startIndex + WORDS_IN_LINE;
+    size_t endIndex = startIndex + WORDS_IN_LINE / ALLOCATION_ALIGNMENT_WORDS;
     ubyte_t *data = bytemap->data;
     for (size_t i = startIndex; i < endIndex; i++) {
         if (data[i] == bm_marked) {
@@ -105,7 +105,7 @@ static inline void Bytemap_ClearBlock(Bytemap *bytemap, word_t *start) {
 */
 #define SWEEP_MASK 0x0404040404040404UL
 static inline void Bytemap_SweepLineAt(ubyte_t *start) {
-    assert(WORDS_IN_LINE / ALLOCATION_ALIGNMENT == 2);
+    assert(WORDS_IN_LINE / ALLOCATION_ALIGNMENT_WORDS / 8 == 2);
     uint64_t *first = (uint64_t *)start;
     first[0] = (first[0] & SWEEP_MASK) >> 1;
     first[1] = (first[1] & SWEEP_MASK) >> 1;
