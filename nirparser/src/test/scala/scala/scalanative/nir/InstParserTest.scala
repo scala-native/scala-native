7366c3bc01716641c95516a68555f141f8b02ec2
Denys Shabalin <denys.shabalin@epfl.ch>
2018-11-21 10:49:50 +0100

    Fix nir parser Fix nir parser tests

--- nirparser/src/test/scala/scala/scalanative/nir/InstParserTest.scala
+++ nirparser/src/test/scala/scala/scalanative/nir/InstParserTest.scala
@@ -14,13 +14,14 @@ class InstParserTest extends FunSuite {
     Inst.Label(local, Seq.empty),
     Inst.Let(local, Op.As(noTpe, Val.None), Next.None),
     Inst.Let(local, Op.As(noTpe, Val.None), Next.Unwind(Local(0))),
-    Inst.Unreachable,
     Inst.Ret(Val.None),
     Inst.Jump(next),
     Inst.If(Val.None, next, next),
     Inst.Switch(Val.None, next, Seq.empty),
     Inst.Throw(Val.Zero(Type.Ptr), Next.Unwind(Local(0))),
-    Inst.Throw(Val.Zero(Type.Ptr), Next.None)
+    Inst.Throw(Val.Zero(Type.Ptr), Next.None),
+    Inst.Unreachable(Next.Unwind(Local(0))),
+    Inst.Unreachable(Next.None)
   ).foreach { inst =>
     test(s"parse inst `${inst.show}`") {
       val Parsed.Success(result, _) = parser.Inst.parser.parse(inst.show)
