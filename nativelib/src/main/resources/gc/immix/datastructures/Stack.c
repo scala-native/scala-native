a50856360e2915e8cadfb446e49b59d8c0d7e45a
Valdis <valdisxp1@gmail.com>
2018-11-29 15:14:56 +0100

    removed no longer relevant assert

--- nativelib/src/main/resources/gc/immix/datastructures/Stack.c
+++ nativelib/src/main/resources/gc/immix/datastructures/Stack.c
@@ -34,7 +34,6 @@ bool Stack_IsEmpty(Stack *stack) {
 
 NOINLINE
 void Stack_doubleSize(Stack *stack) {
-    assert(stack->current == 0);
     size_t nb_words = stack->nb_words * 2;
     stack->nb_words = nb_words;
     stack->bottom = realloc(stack->bottom, nb_words * sizeof(Stack_Type));
