912bfdb2d3eef90485fb35e451cb7eab68a9bae1
Denys Shabalin <denys.shabalin@epfl.ch>
2018-12-13 16:33:28 +0100

    Drop block param reduction

--- tools/src/main/scala/scala/scalanative/optimizer/pass/DeadCodeElimination.scala
+++ tools/src/main/scala/scala/scalanative/optimizer/pass/DeadCodeElimination.scala
@@ -11,23 +11,19 @@ class DeadCodeElimination extends Pass {
   import DeadCodeElimination._
 
   override def onInsts(insts: Seq[Inst]): Seq[Inst] = {
-    val fresh      = Fresh(insts)
-    val cfg        = ControlFlow.Graph(insts)
-    val usedef     = UseDef(cfg)
-    val removeArgs = new ArgRemover(usedef, cfg.entry.name)
-    val buf        = new nir.Buffer()(fresh)
+    val fresh  = Fresh(insts)
+    val cfg    = ControlFlow.Graph(insts)
+    val usedef = UseDef(cfg)
+    val buf    = new nir.Buffer()(fresh)
 
     cfg.all.foreach { block =>
       if (usedef(block.name).alive) {
-        val newParams = block.params.filter { p =>
-          (block.name == cfg.entry.name) || usedef(p.name).alive
-        }
-        buf += block.label.copy(params = newParams)
+        buf += block.label
         block.insts.foreach {
           case inst @ Inst.Let(n, _, _) =>
             if (usedef(n).alive) buf += inst
           case inst: Inst.Cf =>
-            buf += removeArgs.onInst(inst)
+            buf += inst
           case _ =>
             ()
         }
@@ -44,25 +40,4 @@ object DeadCodeElimination extends PassCompanion {
 
   override def apply(config: build.Config, linked: linker.Result): Pass =
     new DeadCodeElimination()
-
-  class ArgRemover(usedef: Map[Local, UseDef.Def], entryName: Local)
-      extends Transform {
-    override def onNext(next: Next) = next match {
-      case Next.Label(name, args) if (name != entryName) =>
-        usedef(name) match {
-          case UseDef.BlockDef(_, _, _, params) =>
-            val newArgs = args.zip(params).collect {
-              case (arg, param) if param.alive => arg
-            }
-            Next.Label(name, newArgs)
-
-          case _ =>
-            throw new IllegalStateException(
-              s"Expected a BlockDef in usedef for ${name.show}")
-        }
-
-      case _ =>
-        next
-    }
-  }
 }
