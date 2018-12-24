45096b3cedebed95dbf9f2fc14304c0ad564a4f3
Eric Richardson <ekrichardson@gmail.com>
2018-12-24 18:25:51 -0500

    Added IdentityHashMapSuite, forwarders and fix equals

---
+++ javalib/src/main/scala/java/util/IdentityHashMap.scala
@@ -0,0 +1,19 @@
+package java.util
+
+class IdentityHashMap[K, V] extends HashMap[K, V] {
+
+  override def boxKey(key: K): AnyRef =
+    IdentityBox(key)
+  override def unboxKey(box: AnyRef): K =
+    box match {
+      case IdentityBox(value) => value.asInstanceOf[K]
+      case _                  => null.asInstanceOf[K]
+    }
+
+  // forwarders - https://github.com/scala-native/scala-native/issues/375
+  override def containsKey(key: Any): Boolean     = super.containsKey(key)
+  override def containsValue(value: Any): Boolean = super.containsValue(value)
+  override def get(key: Any): V                   = super.get(key)
+  override def put(key: K, value: V): V           = super.put(key, value)
+  override def remove(key: Any): V                = super.remove(key)
+}
