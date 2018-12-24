77c6fe8ef85033fcc274c7f7e60030e3f9c74b22
Eric Richardson <ekrichardson@gmail.com>
2018-12-24 18:25:51 -0500

    Added IdentityHashMapSuite, forwarders and fix equals

---
+++ unit-tests/src/test/scala/java/util/IdentityHashMapSuite.scala
@@ -0,0 +1,77 @@
+package java.util
+
+// Ported from Scala.js
+
+import java.{util => ju}
+import scala.reflect.ClassTag
+
+//object IdentityHashMapSuite extends MapSuite {
+//  def factory(): IdentityHashMapSuite = new IdentityHashMapSuite
+//}
+//
+//class IdentityHashMapSuite extends AbstractMapSuiteFactory {
+//  override def implementationName: String =
+//    "java.util.IdentityHashMap"
+//
+//  override def empty[K: ClassTag, V: ClassTag]: ju.IdentityHashMap[K, V] =
+//    new ju.IdentityHashMap[K, V]
+//
+//  def allowsNullKeys: Boolean   = true
+//  def allowsNullValues: Boolean = true
+//
+//}
+
+object IdentityHashMapSuite extends tests.Suite {
+
+  def empty[K: ClassTag, V: ClassTag]: ju.IdentityHashMap[K, V] =
+    new ju.IdentityHashMap[K, V]
+
+  // tests with null keys and values
+  val map                  = empty[AnyRef, AnyRef]
+  var result: AnyRef       = _
+  var value: AnyRef        = _
+  var anothervalue: AnyRef = _
+
+  test("null key and value") { //
+    result = map.put(null, null)
+    assertTrue(map.containsKey(null))
+    assertTrue(map.containsValue(null))
+    assertNull(map.get(null))
+    assertNull(result)
+  }
+
+  test("null key replaces") {
+    value = "a value"
+    result = map.put(null, value)
+    assertTrue(map.containsKey(null))
+    assertTrue(map.containsValue(value))
+    assertTrue(map.get(null) eq value)
+    assertNull(result)
+  }
+
+  test("null value") {
+    val key = "a key"
+    result = map.put(key, null)
+    assertTrue(map.containsKey(key))
+    assertTrue(map.containsValue(null))
+    assertNull(map.get(key))
+    assertNull(result)
+  }
+
+  test("another null key replaces old val") {
+    anothervalue = "another value"
+    result = map.put(null, anothervalue)
+    assertTrue(map.containsKey(null))
+    assertTrue(map.containsValue(anothervalue))
+    assertTrue(map.get(null) eq anothervalue)
+    assertTrue(result eq value)
+  }
+
+  test("remove a null key") {
+    result = map.remove(null)
+    assertTrue(result eq anothervalue)
+    assertTrue(!map.containsKey(null))
+    assertTrue(!map.containsValue(anothervalue))
+    assertNull(map.get(null))
+  }
+}
