f86251543b9da5dd7bd8f23bd9db1904daaca0bd
ekrich <ekrichardson@gmail.com>
2019-03-21 09:26:33 -0700

    Add stubs to URL and URLConnection

--- javalib/src/main/scala/java/net/URLConnection.scala
+++ javalib/src/main/scala/java/net/URLConnection.scala
@@ -1,6 +1,16 @@
 package java.net
 
+import scalanative.native.stub
+
 class URLConnection {
-  @scalanative.native.stub
+  @stub
   def getLastModified(): scala.Long = ???
+  @stub
+  def connect(): Unit = ???
+  @stub
+  def getContentType(): String = ???
+  @stub
+  def getInputStream(): java.io.InputStream = ???
+  @stub
+  def setRequestProperty(key: String, value: String): Unit = ???
 }
