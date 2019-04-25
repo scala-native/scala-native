ba5bf51c1137f462b5ed1468a2ba09f63e329e30
Lee Tibbert <lee.tibbert@gmail.com>
2019-04-25 19:52:48 -0400

    Fix #1560, part 2: Implement AtomicReference getAndUpdate & updateAndGet
      * This pull request is the second of two which address Issue #1560.
        The first in the series was PR #1566 by @ekrich and the 0.3.9
        variant, PR #1567.
    
        The intent was to enable a SN build of scalafmt.
    
        That issue is now fixed.
    
      * Almost all complex code in SN java.util.concurrent.atomic
        relies upon the fact that SN is now single threaded.
        This code makes the same assumption.
    
        All of this code will be a hard to find hazard and hindrance
        once SN supports multithreading.
    
      * A small test Suite to exercise the methods implemented was created.
        The tests in the suite excercise (only) the newly implemented methods and
        check that the expected values are returned. There was no obvious way
        to test concurrency.
    
        When it is run test-only all tests run and pass.
    
        The new Suite seems to drive the testing framwork over its
        (memory?) limit.  In my private build the new Suite runs
        just fine if I delete some existing Suites.  Another manifestation
        of the same issue I encountered whilst implementing regex.
    
        I am submitting the unit-tests but naming the file
        AtomicReferenceSuite.scala.disabled to pass Travis CI
        until the test-runner is fixed. It took me a while to figure
        them out and they may prove useful to a future developer.
    
    Documentation:
    
        A one line entry in the change log is suggested.
    
    Testing:
    
      * Built and tested ("test-all") in debug mode using sbt 1.2.6 on
        X86_64 only . All test expected to pass do so.

--- javalib/src/main/scala/java/util/concurrent/atomic/AtomicReference.scala
+++ javalib/src/main/scala/java/util/concurrent/atomic/AtomicReference.scala
@@ -1,5 +1,14 @@
 package java.util.concurrent.atomic
 
+// 2019-04-25 Note Well!
+//    Almost all of the methods in this and other Atomic*.scala files
+//    are manifestly not atomic.  The two methods added to day
+//    and the prior art all rely upon the fact that Scala Native is
+//    currently single threaded.  They will break, bring great gnashing
+//    of teeth & horrid pain if/when SN becomes multi-threaded.
+
+import java.util.function.UnaryOperator
+
 class AtomicReference[T <: AnyRef](private[this] var value: T)
     extends Serializable {
 
@@ -28,6 +37,17 @@ class AtomicReference[T <: AnyRef](private[this] var value: T)
     val old = value
     value = newValue
     old
+  }
+
+  final def getAndUpdate(updateFunction: UnaryOperator[T]): T = {
+    val old = value
+    value = updateFunction(old)
+    old
+  }
+
+  final def updateAndGet(updateFunction: UnaryOperator[T]): T = {
+    value = updateFunction(value)
+    value
   }
 
   override def toString(): String =
