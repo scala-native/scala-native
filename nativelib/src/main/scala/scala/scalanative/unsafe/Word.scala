e4e07bd5695a97d2d70150228e3d8f696f5d50c3
Shadaj Laddad <shadaj@users.noreply.github.com>
2019-05-26 21:30:37 -0700

    Add unary and shift operations to Word

--- nativelib/src/main/scala/scala/scalanative/unsafe/Word.scala
+++ nativelib/src/main/scala/scala/scalanative/unsafe/Word.scala
@@ -25,6 +25,112 @@ final class Word(private[scalanative] val rawWord: RawWord) {
   @alwaysinline def toULong: ULong   = toLong.toULong
   @alwaysinline def toUWord: UWord   = new UWord(rawWord)
 
+  override def hashCode: Int = java.lang.Long.hashCode(toLong)
+
+  override def equals(other: Any): Boolean =
+    (this eq other.asInstanceOf[AnyRef]) || (other match {
+      case other: Word =>
+        other.rawWord == rawWord
+      case _ =>
+        false
+    })
+
+  override def toString(): String = toLong.toString
+
+  /**
+   * Returns the bitwise negation of this value.
+   * @example {{{
+   * ~5 == 4294967290
+   * // in binary: ~00000101 ==
+   * //             11111010
+   * }}}
+   */
+  @alwaysinline def unary_~ : Word =
+    (~toLong).toWord // TODO(shadaj): intrinsify
+
+  /** Returns the negated version of this value. */
+  @alwaysinline def unary_- : Word = 0 - this // TODO(shadaj): intrinsify
+
+  /**
+   * Returns this value bit-shifted left by the specified number of bits,
+   *         filling in the new right bits with zeroes.
+   * @example {{{ 6 << 3 == 48 // in binary: 0110 << 3 == 0110000 }}}
+   */
+  @alwaysinline def <<(x: Int): Word =
+    (toLong << x).toWord // TODO(shadaj): intrinsify
+
+  /**
+   * Returns this value bit-shifted left by the specified number of bits,
+   *         filling in the new right bits with zeroes.
+   * @example {{{ 6 << 3 == 48 // in binary: 0110 << 3 == 0110000 }}}
+   */
+  @alwaysinline def <<(x: Long): Word =
+    (toLong << x).toWord // TODO(shadaj): intrinsify
+
+  /**
+   * Returns this value bit-shifted right by the specified number of bits,
+   *         filling the new left bits with zeroes.
+   * @example {{{ 21 >>> 3 == 2 // in binary: 010101 >>> 3 == 010 }}}
+   * @example {{{
+   * 4294967275 >>> 3 == 536870909
+   * // in binary: 11111111 11111111 11111111 11101011 >>> 3 ==
+   * //            00011111 11111111 11111111 11111101
+   * }}}
+   */
+  @alwaysinline def >>>(x: Int): Word =
+    (toLong >>> x).toWord // TODO(shadaj): intrinsify
+
+  /**
+   * Returns this value bit-shifted right by the specified number of bits,
+   *         filling the new left bits with zeroes.
+   * @example {{{ 21 >>> 3 == 2 // in binary: 010101 >>> 3 == 010 }}}
+   * @example {{{
+   * 4294967275 >>> 3 == 536870909
+   * // in binary: 11111111 11111111 11111111 11101011 >>> 3 ==
+   * //            00011111 11111111 11111111 11111101
+   * }}}
+   */
+  @alwaysinline def >>>(x: Long): Word =
+    (toLong >>> x).toWord // TODO(shadaj): intrinsify
+
+  /**
+   * Returns this value bit-shifted left by the specified number of bits,
+   *         filling in the right bits with the same value as the left-most bit of this.
+   * @example {{{
+   * 4294967275 >> 3 == 4294967293
+   * // in binary: 11111111 11111111 11111111 11101011 >> 3 ==
+   * //            11111111 11111111 11111111 11111101
+   * }}}
+   */
+  @inline final def >>(x: Int): Word = (toLong >> x).toWord
+
+  /**
+   * Returns this value bit-shifted left by the specified number of bits,
+   *         filling in the right bits with the same value as the left-most bit of this.
+   * @example {{{
+   * 4294967275 >> 3 == 4294967293
+   * // in binary: 11111111 11111111 11111111 11101011 >> 3 ==
+   * //            11111111 11111111 11111111 11111101
+   * }}}
+   */
+  @inline final def >>(x: Long): Word = (toLong >> x).toWord
+
+  /** Returns `true` if this value is equal to x, `false` otherwise. */
+  @alwaysinline def ==(x: Byte): Boolean = this == x.toWord
+
+  /** Returns `true` if this value is equal to x, `false` otherwise. */
+  @alwaysinline def ==(x: Short): Boolean = this == x.toWord
+
+  /** Returns `true` if this value is equal to x, `false` otherwise. */
+  @alwaysinline def ==(x: Int): Boolean = this == x.toWord
+
+  /** Returns `true` if this value is equal to x, `false` otherwise. */
+  @alwaysinline def ==(x: Long): Boolean = this.toLong == x
+
+  /** Returns `true` if this value is equal to x, `false` otherwise. */
+  @alwaysinline def ==(other: Word): Boolean =
+    this.toLong == other.toLong // TODO(shadaj): intrinsify
+
   /** Returns `true` if this value is greater than x, `false` otherwise. */
   @alwaysinline def >(x: Byte): Boolean = this > x.toWord
 
@@ -64,18 +170,6 @@ final class Word(private[scalanative] val rawWord: RawWord) {
 
   @alwaysinline def /(other: Word): Word =
     new Word(divRawWords(rawWord, other.rawWord))
-
-  override def hashCode: Int = java.lang.Long.hashCode(toLong)
-
-  override def equals(other: Any): Boolean =
-    (this eq other.asInstanceOf[AnyRef]) || (other match {
-      case other: Word =>
-        other.rawWord == rawWord
-      case _ =>
-        false
-    })
-
-  override def toString(): String = toLong.toString
 }
 
 object Word {
