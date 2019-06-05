eb23ce7bfe4d01f0adc076d2d8a6a4e154a771f1
Eric K Richardson <ekrichardson@gmail.com>
2019-06-05 02:28:00 -0700

    Fix #1612: remove code that is hiding the true exception (#1617)

--- unit-tests/src/test/scala/scala/scalanative/regex/GoTestUtils.scala
+++ unit-tests/src/test/scala/scala/scalanative/regex/GoTestUtils.scala
@@ -1,8 +1,6 @@
 package scala.scalanative
 package regex
 
-import java.io.UnsupportedEncodingException
-
 // Utilities to make JUnit act a little more like Go's "testing" package.
 object GoTestUtils {
   def len[A](array: Array[A]): Int =
@@ -17,41 +15,25 @@ object GoTestUtils {
     if (array == null) 0
     else array.length
 
-  def utf8(s: String): Array[Byte] =
-    try {
-      s.getBytes("UTF-8")
-    } catch {
-      case e: UnsupportedEncodingException =>
-        throw new IllegalStateException("can't happen")
-    }
+  def utf8(s: String): Array[Byte] = s.getBytes("UTF-8")
 
   // Beware: logically this operation can fail, but Java doesn't detect it.
-  def fromUTF8(b: Array[Byte]): String =
-    try new String(b, "UTF-8")
-    catch {
-      case e: UnsupportedEncodingException =>
-        throw new IllegalStateException("can't happen")
-    }
+  def fromUTF8(b: Array[Byte]): String = new String(b, "UTF-8")
 
   // Convert |idx16|, which are Java (UTF-16) string indices, into the
   // corresponding indices in the UTF-8 encoding of |text|.
-  //
-  def utf16IndicesToUtf8(idx16: Array[Int], text: String): Array[Int] =
-    try {
-      val idx8 = new Array[Int](idx16.length)
-      var i    = 0
-      while (i < idx16.length) {
-        idx8(i) =
-          if (idx16(i) == -1) -1
-          else text.substring(0, idx16(i)).getBytes("UTF-8").length // yikes
+  def utf16IndicesToUtf8(idx16: Array[Int], text: String): Array[Int] = {
+    val idx8 = new Array[Int](idx16.length)
+    var i    = 0
+    while (i < idx16.length) {
+      idx8(i) =
+        if (idx16(i) == -1) -1
+        else text.substring(0, idx16(i)).getBytes("UTF-8").length // yikes
 
-        {
-          i += 1; i
-        }
+      {
+        i += 1; i
       }
-      idx8
-    } catch {
-      case e: UnsupportedEncodingException =>
-        throw new IllegalStateException(e)
     }
+    idx8
+  }
 }
