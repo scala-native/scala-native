6fc9b9673370762fdd6d4ccf0ad3ad4a47873958
Shadaj Laddad <shadaj@users.noreply.github.com>
2019-06-27 19:00:48 -0700

    Add more Word unit tests

--- unit-tests/src/test/scala/scala/scalanative/unsafe/WordSuite.scala
+++ unit-tests/src/test/scala/scala/scalanative/unsafe/WordSuite.scala
@@ -6,12 +6,47 @@ object WordSuite extends tests.Suite {
     assertEquals(~(5.toWord).toInt, -6)
   }
 
-  test("Decimal negation") {
+  test("Numerical negation") {
     assertEquals(-(5.toWord).toInt, -5)
   }
 
   test("Shift left") {
     assertEquals((5.toWord << 2).toInt, 20)
+  }
+
+  test("Logical shift right") {
+    assertEquals((6.toWord >> 1).toInt, 3)
+  }
+
+  test("Signed shift right") {
+    assertEquals((-6.toWord >> 1).toInt, -3)
+  }
+
+  test("Equals") {
+    assert(6.toWord == 6.toWord)
+    assert(6.toWord.equals(6.toWord))
+  }
+
+  test("Not equals") {
+    assert(6.toWord != 5.toWord)
+  }
+
+  test("Less than") {
+    assert(5.toWord < 6.toWord)
+  }
+
+  test("Less than or equal") {
+    assert(5.toWord <= 6.toWord)
+    assert(5.toWord <= 5.toWord)
+  }
+
+  test("Greater than") {
+    assert(6.toWord > 5.toWord)
+  }
+
+  test("Greater than or equal") {
+    assert(6.toWord >= 5.toWord)
+    assert(5.toWord >= 5.toWord)
   }
 
   test("Bitwise and") {
@@ -26,23 +61,23 @@ object WordSuite extends tests.Suite {
     assertEquals((123.toWord ^ 456.toWord).toInt, 435)
   }
 
-  test("Decimal addition") {
+  test("Addition") {
     assertEquals((123.toWord + 456.toWord).toInt, 579)
   }
 
-  test("Decimal subtraction") {
+  test("Subtraction") {
     assertEquals((123.toWord - 456.toWord).toInt, -333)
   }
 
-  test("Decimal multiplication") {
+  test("Multiplication") {
     assertEquals((123.toWord * 3.toWord).toInt, 369)
   }
 
-  test("Decimal division") {
+  test("Division") {
     assertEquals((123.toWord / 2.toWord).toInt, 61)
   }
 
-  test("Modulo") {
+  test("Remainder") {
     assertEquals((123.toWord % 13.toWord).toInt, 6)
   }
 }
