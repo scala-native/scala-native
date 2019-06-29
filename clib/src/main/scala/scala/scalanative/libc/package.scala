fb207c9faa447123aafe117ed23c54dab1cd5465
Richard Whaling <rwhaling@Richards-MacBook-Pro-2.local>
2019-06-29 16:30:46 -0500

    ran scalafmt

--- clib/src/main/scala/scala/scalanative/libc/package.scala
+++ clib/src/main/scala/scala/scalanative/libc/package.scala
@@ -3,38 +3,38 @@ import scalanative.unsafe._
 import scalanative.libc.stdio
 
 package object libc {
-  implicit class StdioHelpers(val _stdio:libc.stdio.type) extends AnyVal {
+  implicit class StdioHelpers(val _stdio: libc.stdio.type) extends AnyVal {
     def printf(format: CString, args: CVarArg*): CInt =
       Zone { implicit z =>
         stdio.vprintf(format, toCVarArgList(args.toSeq))
       }
 
-    def sprintf(s:CString, format:CString, args:CVarArg*): CInt = 
+    def sprintf(s: CString, format: CString, args: CVarArg*): CInt =
       Zone { implicit z =>
         stdio.vsprintf(s, format, toCVarArgList(args.toSeq))
       }
 
-    def snprintf(s:CString, n:CSize, format:CString, args:CVarArg*): CInt =
+    def snprintf(s: CString, n: CSize, format: CString, args: CVarArg*): CInt =
       Zone { implicit z =>
         stdio.vsnprintf(s, n.toInt, format, toCVarArgList(args.toSeq))
       }
 
-    def fprintf(f:Ptr[stdio.FILE], format:CString, args:CVarArg*): CInt = 
+    def fprintf(f: Ptr[stdio.FILE], format: CString, args: CVarArg*): CInt =
       Zone { implicit z =>
         stdio.vfprintf(f, format, toCVarArgList(args.toSeq))
       }
 
-    def scanf(format:CString, args: CVarArg*): CInt =
+    def scanf(format: CString, args: CVarArg*): CInt =
       Zone { implicit z =>
         stdio.vscanf(format, toCVarArgList(args.toSeq))
       }
 
-    def sscanf(s:CString, format:CString, args: CVarArg*): CInt = 
+    def sscanf(s: CString, format: CString, args: CVarArg*): CInt =
       Zone { implicit z =>
-        stdio.vsscanf(s,format, toCVarArgList(args.toSeq))
+        stdio.vsscanf(s, format, toCVarArgList(args.toSeq))
       }
 
-    def fscanf(f:Ptr[stdio.FILE], format:CString, args:CVarArg*): CInt = 
+    def fscanf(f: Ptr[stdio.FILE], format: CString, args: CVarArg*): CInt =
       Zone { implicit z =>
         stdio.vfscanf(f, format, toCVarArgList(args.toSeq))
       }
