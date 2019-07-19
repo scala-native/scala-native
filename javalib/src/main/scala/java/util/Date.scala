e25d44c331f5f7ecbcf143723352fbe1d0f5f4b0
Lee Tibbert <lee.tibbert@gmail.com>
2019-07-19 06:09:02 -0400

    j.u.Date#toString output now matches JVM
    * This PR fixes a problem reported by @ekrich on gitter. Previously
      Scala Native java.util.Date method toString would return a result
      of the form "Date(1234567)". This differed from the form returned
      by Java "Thu Jul 18 09:16:29 PDT 2019".
    
      The difference meant that the output of SN Properties#store differed
      from the JVM, complicating Properties.scala development.
    
      Scala Native now returns the same result as if it had executed on
      the JVM.
    
    * Note Well:  In order for the translation from Date object to string
      to succeed, the C timezone and locale environments need to be
      set up appropriately as to the expected result.  This is not
      an issue on most systems.
    
      Concretely, a system set up to use GMT would show GMT as the timezone name.
      A system set up to use, say, French weekday names would show them.
    
      This is NOT general (Java) Locale support.
    
    * The test case for Date.toString was updated in `java/util/Suite.scala`
    
      The test case gives a sanity check for the resultant format rather
      than testing for an exact output.  The latter can change depending
      upon the environment in which the test is executed. A good example
      is the three character timezone name.
    
      The regex pattern used for the sanity check will need to be revisited
      in twenty years and then again in 100 and 1000 years.  That only
      my code should last so long.
    
    * This PR gives user visible parity with Java VM practice.  The
      implementation may need to change as methods in java.time mature
      or become available.
    
      The current secondsToString() implementation derives from unpublished
      design studies for Instant.scala & kin.
    
    Documentation:
    
    * The standard changelog entry is requested.
    
    Testing:
    
    * Built and tested ("test-all") in debug mode using sbt 1.2.8 on
        X86_64 only . All tests pass.

--- javalib/src/main/scala/java/util/Date.scala
+++ javalib/src/main/scala/java/util/Date.scala
@@ -1,8 +1,14 @@
 package java.util
 
+import scalanative.libc.{errno, string}
+import scalanative.posix.time._
+import scalanative.unsafe._
+
+import java.io.IOException
+
 /** Ported from Scala JS and Apache Harmony
  * - omits deprecated methods
- * - toString not jdk compatible
+ * - toString code is new for Scala Native.
  */
 class Date(var milliseconds: Long)
     extends Object
@@ -33,6 +39,33 @@ class Date(var milliseconds: Long)
   def setTime(time: Long): Unit =
     milliseconds = time
 
-  override def toString(): String = s"Date($milliseconds)"
+  private var tzsetDone = false
+
+  private def secondsToString(seconds: Long): String = Zone { implicit z =>
+    val ttPtr = alloc[time_t]
+    !ttPtr = seconds
+    val tmPtr = alloc[tm]
+
+    if (!tzsetDone) {
+      tzset() // needed in order to portably use localtime_r
+      tzsetDone = true
+    }
+
+    if (localtime_r(ttPtr, tmPtr) == null) {
+      throw new IOException(fromCString(string.strerror(errno.errno)))
+    } else {
+      val bufSize = "Thu Jul 18 09:16:29 PDT 2019".length + 1
+      val buf     = alloc[Byte](bufSize)
+
+      val n = strftime(buf, bufSize, c"%a %b %d %T %Z %Y", tmPtr)
+
+      // strftime does not set errno on error
+      var result = if (n == 0) "" else fromCString(buf)
+
+      result
+    }
+  }
+
+  override def toString(): String = secondsToString(milliseconds / 1000L)
 
 }
