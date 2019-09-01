c934ad75f7ca3988e2d61437bb50e3421609126a
Lee Tibbert <Lee Tibbert>
2019-09-01 16:50:13 -0400

    Remove unnecessary & complicating semi-colons left over from java port.

--- nativelib/src/main/scala/scala/scalanative/regex/Parser.scala
+++ nativelib/src/main/scala/scala/scalanative/regex/Parser.scala
@@ -10,9 +10,9 @@ package regex
 
 import java.util.ArrayList
 import java.util.Arrays
-import java.util.HashMap;
+import java.util.HashMap
 import java.util.List
-import java.util.Map;
+import java.util.Map
 
 import java.util.regex.PatternSyntaxException
 
