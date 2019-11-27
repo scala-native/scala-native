d8b363bf0eb5c16cf12dc8036196cfc0d2b48d86
ekrich <ekrichardson@gmail.com>
2019-11-26 17:23:14 -0800

    Use organization and project name to provide namespaces for native code

--- sbt-scala-native/src/main/scala/scala/scalanative/sbtplugin/ScalaNativePluginInternal.scala
+++ sbt-scala-native/src/main/scala/scala/scalanative/sbtplugin/ScalaNativePluginInternal.scala
@@ -99,8 +99,9 @@ object ScalaNativePluginInternal {
       val nativeCodeProject = {
         if (nativeCodeInclude.value) {
           val projName   = name.value
+          val orgName    = organization.value
           val classesDir = crossTarget.value / "classes"
-          Some(NativeLib(projName, classesDir.toPath()))
+          Some(NativeLib(LibId(orgName, projName), classesDir.toPath()))
         } else {
           None
         }
