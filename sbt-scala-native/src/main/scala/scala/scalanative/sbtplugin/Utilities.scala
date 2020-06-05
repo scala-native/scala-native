de0a559b9a8f236b2a7c132aec9382ef09870ff1
Lee Tibbert <lee.tibbert@gmail.com>
2020-06-04 20:21:41 -0400

    ScalaNativePlugin: remove private Process, SBTCompat.scala, etc.
      * This PR is another in my long series of PRs concerned with
        the Sbt/ScalaNative communications handshake in test-interface.
    
        It removes a private third implementation of the Process class.
        This makes it easier, for me at least, to understand which
        Process implementation is actually being used in ComRunner.scala.
        My intention & desire is to use scala.sys.process.Process, which
        rests on the Scala Native javalib Process (which rests on Pthreads,
        not real processes).
    
        Removing this private implementation allowed, with a few edits,
        the removal of SBTCompat.scala.  That file originally came
        from Scala.js and had the Process implementation added for SN.
        Scala.js 1.n no longer uses SBTCompat.scala.  Removing it
        clears away some shrubbery.
    
      * Similarly, Utilities.scala was simplified to remove obfuscation
        that may have been useful at one time but no longer is. I cleared
        away the easy bits. Someone with more skill, patience, & time may
        be able to eliminate Utilities.scala all together.
    
    Documentation:
    
      * The affected files are all internal to the Scala Native build itself.
        Any change visible to a developer using Scala Native artifacts
        is unintentional and a bug.
    
    Testing:
    
      * Built ("rebuild")and tested ("test-all") in debug mode using sbt 1.3.12
        & Java 8 on X86_64 only . All tests pass.
    
      * After a half day of development using the test-interface and,
        in particular, ComRunner.scala, I have noticed no problems.

--- sbt-scala-native/src/main/scala/scala/scalanative/sbtplugin/Utilities.scala
+++ sbt-scala-native/src/main/scala/scala/scalanative/sbtplugin/Utilities.scala
@@ -7,30 +7,8 @@ import scala.language.postfixOps
 
 import sbt._
 
-import scalanative.sbtplugin.SBTCompat.{Process, ProcessLogger, _}
-
 object Utilities {
-
-  object SilentLogger extends ProcessLogger {
-    def info(s: => String): Unit  = ()
-    def error(s: => String): Unit = ()
-    def buffer[T](f: => T): T     = f
-  }
-
-  implicit class RichFile(file: File) {
-    def abs: String = file.getAbsolutePath
-  }
-
   implicit class RichLogger(logger: Logger) {
-    def time[T](msg: String)(f: => T): T = {
-      import java.lang.System.nanoTime
-      val start = nanoTime()
-      val res   = f
-      val end   = nanoTime()
-      logger.info(s"$msg (${(end - start) / 1000000} ms)")
-      res
-    }
-
     def toLogger: build.Logger =
       build.Logger(logger.debug(_),
                    logger.info(_),
