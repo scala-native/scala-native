scala-native/scala-native

commit db0631cab1d8f3f590b70fd7bc60e787ca22436c

Fix #1279: Enable reflective instantiation via static initializers (#1728)
This commit enables users to reflectively instantiate objects, by
making use of static class initialisers (not to be confused with JVM
class initialisers).

To achieve the above:

* The NIR member signatures are enhanced with an extra signature kind,
  called `clinit`, which is mangled with `I`. Static initialiser
  functions take no arguments and return `Unit`.
* For the static initialiser functions to be called, they must be made
  reachable. For this to work, we need to iterate over all program
  classes (each of them in a separate `.nir` file) and check whether
  they contain a `clinit` function. To avoid deserialising the whole
  file for each class, we add a single byte at the beginning of each
  file, to indicate whether the type it contains has a `clinit`
  function. If that byte is set, then the `clinit` function of the type
  in that file is added to the reachable set.
* When generating the code for the `main` method (native), we have to
  call the discovered `clinit` fuctions. We do this just before calling
  the main Scala method.

On top of that, we implement the same `Reflect` API and
`@EnableReflectiveInstantiation`-based mechanism that Scala.js has.

util/src/main/scala/scala/scalanative/io/VirtualDirectory.scala
@@ -18,6 +18,9 @@ sealed trait VirtualDirectory {
   /** Reads a contents of file with given path. */
   def read(path: Path): ByteBuffer
 
+  /** Reads up to len bytes from the file with the given path. */
+  def read(path: Path, len: Int): ByteBuffer
+
   /** Replaces contents of file with given value. */
   def write(path: Path, buffer: ByteBuffer): Unit
 
@@ -74,6 +77,13 @@ object VirtualDirectory {
       buffer
     }
 
+    override def read(path: Path, len: Int): ByteBuffer = {
+      val stream = Files.newInputStream(resolve(path))
+      val bytes  = new Array[Byte](len)
+      val read   = stream.read(bytes)
+      ByteBuffer.wrap(bytes, 0, read)
+    }
+
     override def write(path: Path, buffer: ByteBuffer): Unit = {
       val channel = open(resolve(path))
       try channel.write(buffer)
@@ -126,6 +136,8 @@ object VirtualDirectory {
     override def read(path: Path): ByteBuffer =
       throw new UnsupportedOperationException(
         "Can't read from empty directory.")
+
+    override def read(path: Path, len: Int): ByteBuffer = read(path)
 
     override def write(path: Path, buffer: ByteBuffer): Unit =
       throw new UnsupportedOperationException("Can't write to empty directory.")

nativelib/src/main/scala/scala/scalanative/reflect/Reflect.scala
@@ -1,0 +1,194 @@
+package scala.scalanative.reflect
+
+import scala.collection.mutable
+
+final class LoadableModuleClass private[reflect] (
+    val runtimeClass: Class[_],
+    loadModuleFun: Function0[Any]
+) {
+
+  /** Loads the module instance and returns it. */
+  def loadModule(): Any = loadModuleFun()
+}
+
+final class InstantiatableClass private[reflect] (
+    val runtimeClass: Class[_],
+    val declaredConstructors: List[InvokableConstructor]
+) {
+
+  /** Instantiates a new instance of this class using the zero-argument
+   *  constructor.
+   *
+   *  @throws java.lang.InstantiationException (caused by a
+   *    `NoSuchMethodException`)
+   *    If this class does not have a public zero-argument constructor.
+   */
+  def newInstance(): Any = {
+    getConstructor().fold[Any] {
+      throw new InstantiationException(runtimeClass.getName).initCause(
+        new NoSuchMethodException(runtimeClass.getName + ".<init>()"))
+    } { ctor =>
+      ctor.newInstance()
+    }
+  }
+
+  /** Looks up a public constructor identified by the types of its formal
+   *  parameters.
+   *
+   *  If no such public constructor exists, returns `None`.
+   */
+  def getConstructor(parameterTypes: Class[_]*): Option[InvokableConstructor] =
+    declaredConstructors.find(_.parameterTypes.sameElements(parameterTypes))
+}
+
+final class InvokableConstructor private[reflect] (
+    val parameterTypes: Array[Class[_]],
+    newInstanceFun: Function1[Array[Any], Any]
+) {
+  def newInstance(args: Any*): Any = {
+    /* Check the number of actual arguments. We let the casts and unbox
+     * operations inside `newInstanceFun` take care of the rest.
+     */
+    require(
+      args.size == parameterTypes.size,
+      "Reflect: wrong number of arguments for InvokableConstructor"
+    )
+    val adaptedArgs = (args zip parameterTypes).map {
+      case (arg, tpe) => wideningPrimConversionIfRequired(arg, tpe)
+    }
+    newInstanceFun.apply(adaptedArgs.toArray)
+  }
+
+  /** Perform a widening primitive conversion if required.
+   *
+   *  According to
+   *  https://docs.oracle.com/javase/specs/jls/se8/html/jls-5.html#jls-5.1.2
+   */
+  private def wideningPrimConversionIfRequired(arg: Any,
+                                               paramType: Class[_]): Any = {
+    paramType match {
+      case java.lang.Short.TYPE =>
+        arg match {
+          case arg: Byte => arg.toShort
+          case _         => arg
+        }
+      case java.lang.Integer.TYPE =>
+        arg match {
+          case arg: Byte  => arg.toInt
+          case arg: Short => arg.toInt
+          case arg: Char  => arg.toInt
+          case _          => arg
+        }
+      case java.lang.Long.TYPE =>
+        arg match {
+          case arg: Byte  => arg.toLong
+          case arg: Short => arg.toLong
+          case arg: Int   => arg.toLong
+          case arg: Char  => arg.toLong
+          case _          => arg
+        }
+      case java.lang.Float.TYPE =>
+        arg match {
+          case arg: Byte  => arg.toFloat
+          case arg: Short => arg.toFloat
+          case arg: Int   => arg.toFloat
+          case arg: Long  => arg.toFloat
+          case arg: Char  => arg.toFloat
+          case _          => arg
+        }
+      case java.lang.Double.TYPE =>
+        arg match {
+          case arg: Byte  => arg.toDouble
+          case arg: Short => arg.toDouble
+          case arg: Int   => arg.toDouble
+          case arg: Long  => arg.toDouble
+          case arg: Float => arg.toDouble
+          case arg: Char  => arg.toDouble
+          case _          => arg
+        }
+      case _ =>
+        arg
+    }
+  }
+
+  override def toString: String = {
+    val builder = new mutable.StringBuilder("InvokableContructor")
+    builder.append("(")
+    for (tpe <- parameterTypes) {
+      builder.append(tpe.getName)
+      builder.append(", ")
+    }
+    if (parameterTypes.length > 0) {
+      builder.setLength(builder.length - 2)
+    }
+    builder.append(")")
+    builder.toString
+  }
+}
+
+object Reflect {
+  private val loadableModuleClasses =
+    mutable.Map.empty[String, LoadableModuleClass]
+
+  private val instantiatableClasses =
+    mutable.Map.empty[String, InstantiatableClass]
+
+  // `protected[reflect]` makes it public in the IR
+  protected[reflect] def registerLoadableModuleClass[T](
+      fqcn: String,
+      runtimeClass: Class[T],
+      loadModuleFun: Function0[T]): Unit = {
+    loadableModuleClasses(fqcn) =
+      new LoadableModuleClass(runtimeClass, loadModuleFun)
+  }
+
+  protected[reflect] def registerInstantiatableClass[T](
+      fqcn: String,
+      runtimeClass: Class[T],
+      constructors: Array[(Array[Class[_]], Function1[Array[Any], Any])])
+    : Unit = {
+    val invokableConstructors = constructors.map { c =>
+      new InvokableConstructor(c._1, c._2)
+    }
+    instantiatableClasses(fqcn) =
+      new InstantiatableClass(runtimeClass, invokableConstructors.toList)
+  }
+
+  /** Reflectively looks up a loadable module class.
+   *
+   *  A module class is the technical term referring to the class of a Scala
+   *  `object`. The object or one of its super types (classes or traits) must
+   *  be annotated with
+   *  [[scala.scalajs.reflect.annotation.EnableReflectiveInstantiation @EnableReflectiveInstantiation]].
+   *  Moreover, the object must be "static", i.e., declared at the top-level of
+   *  a package or inside a static object.
+   *
+   *  If the module class cannot be found, either because it does not exist,
+   *  was not `@EnableReflectiveInstantiation` or was not static, this method
+   *  returns `None`.
+   *
+   *  @param fqcn
+   *    Fully-qualified name of the module class, including its trailing `$`
+   */
+  def lookupLoadableModuleClass(fqcn: String): Option[LoadableModuleClass] =
+    loadableModuleClasses.get(fqcn)
+
+  /** Reflectively looks up an instantiable class.
+   *
+   *  The class or one of its super types (classes or traits) must be annotated
+   *  with
+   *  [[scala.scalajs.reflect.annotation.EnableReflectiveInstantiation @EnableReflectiveInstantiation]].
+   *  Moreover, the class must not be abstract, nor be a local class (i.e., a
+   *  class defined inside a `def`). Inner classes (defined inside another
+   *  class) are supported.
+   *
+   *  If the class cannot be found, either because it does not exist,
+   *  was not `@EnableReflectiveInstantiation` or was abstract or local, this
+   *  method returns `None`.
+   *
+   *  @param fqcn
+   *    Fully-qualified name of the class
+   */
+  def lookupInstantiatableClass(fqcn: String): Option[InstantiatableClass] =
+    instantiatableClasses.get(fqcn)
+}

nativelib/src/main/scala/scala/scalanative/reflect/annotation/EnableReflectiveInstantiation.scala
@@ -1,0 +1,10 @@
+package scala.scalanative
+package reflect.annotation
+
+/** Enables reflective instantiation for the annotated class, trait or object,
+ *  and all its descendants.
+ *
+ *  Affected classes can be identified at run-time through methods of
+ *  [[scala.scalanative.reflect.Reflect]].
+ */
+class EnableReflectiveInstantiation extends scala.annotation.StaticAnnotation

nir/src/main/scala/scala/scalanative/nir/Mangle.scala
@@ -51,6 +51,9 @@ object Mangle {
         str("R")
         types.foreach(mangleType)
         str("E")
+      case Sig.Clinit() =>
+        str("I")
+        str("E")
       case Sig.Method(id, types) =>
         str("D")
         mangleIdent(id)

nir/src/main/scala/scala/scalanative/nir/Sig.scala
@@ -27,6 +27,7 @@ final class Sig(val mangle: String) {
 
   final def isField: Boolean     = mangle(0) == 'F'
   final def isCtor: Boolean      = mangle(0) == 'R'
+  final def isClinit: Boolean    = mangle(0) == 'I'
   final def isImplCtor: Boolean  = mangle.startsWith("M6$init$")
   final def isMethod: Boolean    = mangle(0) == 'D'
   final def isProxy: Boolean     = mangle(0) == 'P'
@@ -40,6 +41,7 @@ object Sig {
   }
   final case class Field(id: String)                    extends Unmangled
   final case class Ctor(types: Seq[Type])               extends Unmangled
+  final case class Clinit()                             extends Unmangled
   final case class Method(id: String, types: Seq[Type]) extends Unmangled
   final case class Proxy(id: String, types: Seq[Type])  extends Unmangled
   final case class Extern(id: String)                   extends Unmangled

nir/src/main/scala/scala/scalanative/nir/Types.scala
@@ -153,4 +153,28 @@ object Type {
     isArray(clsTy.name)
   def isArray(clsName: Global): Boolean =
     arrayToType.contains(clsName)
+
+  def typeToName(tpe: Type): Global = tpe match {
+    case Rt.BoxedUnit       => Global.Top("scala.scalanative.runtime.PrimitiveUnit")
+    case Bool               => Global.Top("scala.scalanative.runtime.PrimitiveBoolean")
+    case Char               => Global.Top("scala.scalanative.runtime.PrimitiveChar")
+    case Byte               => Global.Top("scala.scalanative.runtime.PrimitiveByte")
+    case Short              => Global.Top("scala.scalanative.runtime.PrimitiveShort")
+    case Int                => Global.Top("scala.scalanative.runtime.PrimitiveInt")
+    case Long               => Global.Top("scala.scalanative.runtime.PrimitiveLong")
+    case Float              => Global.Top("scala.scalanative.runtime.PrimitiveFloat")
+    case Double             => Global.Top("scala.scalanative.runtime.PrimitiveDouble")
+    case Ref(name, _, _)    => name
+    case Array(tpe, _)      => toArrayClass(tpe)
+    case ArrayValue(tpe, _) => toArrayClass(tpe)
+    case Function(args, _)  => Global.Top(s"scala.Function${args.length}")
+  }
+
+  def primConvSig(tpe: Type): Sig.Method = tpe match {
+    case Short  => Sig.Method("shortValue", Seq(tpe))
+    case Int    => Sig.Method("intValue", Seq(tpe))
+    case Long   => Sig.Method("longValue", Seq(tpe))
+    case Float  => Sig.Method("floatValue", Seq(tpe))
+    case Double => Sig.Method("doubleValue", Seq(tpe))
+  }
 }

nir/src/main/scala/scala/scalanative/nir/Unmangle.scala
@@ -24,6 +24,8 @@ object Unmangle {
         Sig.Field(readIdent())
       case 'R' =>
         Sig.Ctor(readTypes())
+      case 'I' =>
+        Sig.Clinit()
       case 'D' =>
         Sig.Method(readIdent(), readTypes())
       case 'P' =>

nir/src/main/scala/scala/scalanative/nir/serialization/BinaryDeserializer.scala
@@ -12,6 +12,8 @@ final class BinaryDeserializer(buffer: ByteBuffer) {
   private val header: Map[Global, Int] = {
     buffer.position(0)
 
+    val hasEntryPoint = getBool
+
     val magic    = getInt
     val compat   = getInt
     val revision = getInt

commit 7542585424f5834550a02d38190bde7009c0c670

Fix "too many open files" after consecutive runs
This bug was introduced by PR #1728. It basically leaves
the files open in the VirtualDirectory.read(path, len) method.
After 3-4 consecutive runs in sbt, the build would complain with
"too many open files", which makes sense since the above method
is used to look for classes with entry points, so it is essentially
used to open every single .nir file in the build.

util/src/main/scala/scala/scalanative/io/VirtualDirectory.scala
@@ -79,9 +79,11 @@ object VirtualDirectory {
 
     override def read(path: Path, len: Int): ByteBuffer = {
       val stream = Files.newInputStream(resolve(path))
-      val bytes  = new Array[Byte](len)
-      val read   = stream.read(bytes)
-      ByteBuffer.wrap(bytes, 0, read)
+      try {
+        val bytes = new Array[Byte](len)
+        val read  = stream.read(bytes)
+        ByteBuffer.wrap(bytes, 0, read)
+      } finally stream.close()
     }
 
     override def write(path: Path, buffer: ByteBuffer): Unit = {