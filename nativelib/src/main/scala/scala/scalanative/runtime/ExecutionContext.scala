3cc1733a7fd37fb926d4872f5dae763c0ed73892
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2020-07-15 19:50:19 +0200

    Factor out duplication from loop and loopRunOnce

--- nativelib/src/main/scala/scala/scalanative/runtime/ExecutionContext.scala
+++ nativelib/src/main/scala/scala/scalanative/runtime/ExecutionContext.scala
@@ -14,27 +14,26 @@ object ExecutionContext {
 
   private val queue: ListBuffer[Runnable] = new ListBuffer
 
+  @inline
+  private def loopRunOnceUnsafe(): Unit = {
+    val runnable = queue.remove(0)
+    try {
+      runnable.run()
+    } catch {
+      case t: Throwable =>
+        QueueExecutionContext.reportFailure(t)
+    }
+  }
+
   private[runtime] def loop(): Unit = {
     while (queue.nonEmpty) {
-      val runnable = queue.remove(0)
-      try {
-        runnable.run()
-      } catch {
-        case t: Throwable =>
-          QueueExecutionContext.reportFailure(t)
-      }
+      loopRunOnceUnsafe()
     }
   }
 
   private[runtime] def loopRunOnce(): Int = {
     if (queue.nonEmpty) {
-      val runnable = queue.remove(0)
-      try {
-        runnable.run()
-      } catch {
-        case t: Throwable =>
-          QueueExecutionContext.reportFailure(t)
-      }
+      loopRunOnceUnsafe()
     }
     queue.size
   }
