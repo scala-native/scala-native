3b30efe2d80095e807be31bebdaaf8618455ad48
wojciechmazur <wojciech.mazur95@gmail.com>
2020-08-21 13:21:06 +0200

    Reverted sandbox/Test

--- sandbox/Test.scala
+++ sandbox/Test.scala
@@ -1,11 +1,5 @@
 object Test {
-  def foo(x: Int): Unit = {
-    val y = x
-    println("hello native")
-  }
-
   def main(args: Array[String]): Unit = {
     println("Hello, World!")
-    foo(1)
   }
 }
