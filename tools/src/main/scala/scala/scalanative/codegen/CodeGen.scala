3a9c57fe44683f9365f533743bf9a19bfd18406c
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2020-08-27 11:59:54 +0200

    Make mutable fields private

--- tools/src/main/scala/scala/scalanative/codegen/CodeGen.scala
+++ tools/src/main/scala/scala/scalanative/codegen/CodeGen.scala
@@ -85,14 +85,14 @@ object CodeGen {
                            defns: Seq[Defn])(implicit meta: Metadata) {
     import Impl._
 
-    var currentBlockName: Local = _
-    var currentBlockSplit: Int  = _
+    private var currentBlockName: Local = _
+    private var currentBlockSplit: Int  = _
 
-    val copies           = mutable.Map.empty[Local, Val]
-    val deps             = mutable.Set.empty[Global]
-    val generated        = mutable.Set.empty[String]
-    val externSigMembers = mutable.Map.empty[Sig, Global.Member]
-    val builder          = new ShowBuilder
+    private val copies           = mutable.Map.empty[Local, Val]
+    private val deps             = mutable.Set.empty[Global]
+    private val generated        = mutable.Set.empty[String]
+    private val externSigMembers = mutable.Map.empty[Sig, Global.Member]
+    private val builder          = new ShowBuilder
     import builder._
 
     def gen(): ByteBuffer = {
@@ -473,8 +473,8 @@ object CodeGen {
         unsupported(ty)
     }
 
-    val constMap = mutable.Map.empty[Val, Global]
-    val constTy  = mutable.Map.empty[Global, Type]
+    private val constMap = mutable.Map.empty[Val, Global]
+    private val constTy  = mutable.Map.empty[Global, Type]
     def constFor(v: Val): Global =
       if (constMap.contains(v)) {
         constMap(v)
