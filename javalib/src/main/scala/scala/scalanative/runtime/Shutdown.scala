f55a1b35d80d789072c83c8bb94b1000aad48a31
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2020-09-26 12:40:54 +0200

    Implement shutdown using atexit instead of c++

--- javalib/src/main/scala/scala/scalanative/runtime/Shutdown.scala
+++ javalib/src/main/scala/scala/scalanative/runtime/Shutdown.scala
@@ -1,6 +1,7 @@
 package scala.scalanative.runtime
 
 import scala.collection.mutable
+import scala.scalanative.libc.stdlib.atexit
 import scala.scalanative.unsafe._
 
 private[runtime] object Shutdown {
@@ -14,11 +15,5 @@ private[runtime] object Shutdown {
         case e: Exception => // Maybe add a system propery that adds logging of exceptions?
       }
     }
-  NativeShutdown.init(() => runHooks())
-}
-
-@extern
-private[runtime] object NativeShutdown {
-  @name("scalanative_native_shutdown_init")
-  def init(func: CFuncPtr0[Unit]): Unit = extern
+  atexit(() => runHooks())
 }
