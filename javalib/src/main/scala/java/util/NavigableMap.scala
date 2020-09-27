b6901c4d8a3af6e8eaccb624ced98c8431d9ce4b
Lee Tibbert <lee.tibbert@gmail.com>
2020-09-27 11:11:56 -0400

    Remove Scala.js copyright, as directed in another PR
    In recently accepted PR #1899 it was explained to me that the Scala.js
    copyright statement need not be retained when porting Scala.js code
    to Scala Native.  I presume & believe the same applies here.

--- javalib/src/main/scala/java/util/NavigableMap.scala
+++ javalib/src/main/scala/java/util/NavigableMap.scala
@@ -4,18 +4,6 @@
 //   Minor scalafmt change to subMap() declaraton in order to pass current
 //   Scala Native Travis CI scalafmt check stage.
 
-/*
- * Scala.js (https://www.scala-js.org/)
- *
- * Copyright EPFL.
- *
- * Licensed under Apache License 2.0
- * (https://www.apache.org/licenses/LICENSE-2.0).
- *
- * See the NOTICE file distributed with this work for
- * additional information regarding copyright ownership.
- */
-
 package java.util
 
 trait NavigableMap[K, V] extends SortedMap[K, V] {
