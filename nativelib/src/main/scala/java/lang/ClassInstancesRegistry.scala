47d367fd17e2d60c61b6d4a66d107284765ec42b
wojciechmazur <wojciech.mazur95@gmail.com>
2020-10-07 18:18:41 +0200

    Styling and access fixes to ClassInstancesRegistry. Use System.arraycopy instead of Array.copy

--- nativelib/src/main/scala/java/lang/ClassInstancesRegistry.scala
+++ nativelib/src/main/scala/java/lang/ClassInstancesRegistry.scala
@@ -1,20 +1,19 @@
 package java.lang
 
 /** Registry for created instances of java.lang.Class
- * It's only purpose is to prevent GC from collecting instances of java.lang.Class
+ * Its only purpose is to prevent the GC from collecting instances of java.lang.Class
  **/
-object ClassInstancesRegistry {
-  type Arr[T] = scala.Array[T]
-  type Bucket = Arr[_Class[_]]
+private[lang] object ClassInstancesRegistry {
+  type Bucket = Array[_Class[_]]
 
-  final val BucketSize = 256
-  private var buckets  = new Arr[Bucket](2)
-  private var lastId   = -1
-  private var bucketId = -1
-  private var itemId   = -1
+  private final val BucketSize = 256
+  private var buckets          = new Array[Bucket](2)
+  private var lastId           = -1
+  private var bucketId         = -1
+  private var itemId           = -1
 
   @inline def nextIds(): Unit = {
-    lastId += 1;
+    lastId += 1
     bucketId = lastId / BucketSize
     itemId = lastId % BucketSize
   }
@@ -23,8 +22,8 @@ object ClassInstancesRegistry {
     nextIds()
     if (bucketId >= buckets.length) {
       val newSize: Int = buckets.length * 2
-      val newArr       = new Arr[Bucket](newSize)
-      Array.copy(buckets, 0, newArr, 0, buckets.length)
+      val newArr       = new Array[Bucket](newSize)
+      System.arraycopy(buckets, 0, newArr, 0, buckets.length)
       buckets = newArr
     }
     if (buckets(bucketId) == null) {
