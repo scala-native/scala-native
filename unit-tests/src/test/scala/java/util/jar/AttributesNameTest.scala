f338fdb2fdd0f13a3f108d2babb0200be34a2c5e
ekrich <ekrichardson@gmail.com>
2020-10-12 12:24:16 -0700

    Fix formatting

--- unit-tests/src/test/scala/java/util/jar/AttributesNameTest.scala
+++ unit-tests/src/test/scala/java/util/jar/AttributesNameTest.scala
@@ -9,9 +9,9 @@ import scala.scalanative.junit.utils.AssertThrows._
 class AttributesNameTest {
 
   @Test def constructor(): Unit = {
-    assertThrows(classOf[IllegalArgumentException],
+    assertThrows(
+      classOf[IllegalArgumentException],
       new Attributes.Name(
-        "01234567890123456789012345678901234567890123456789012345678901234567890")
-    )
+        "01234567890123456789012345678901234567890123456789012345678901234567890"))
   }
 }
