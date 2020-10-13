8ab290d6be035086256b279d36da12af6f7fe546
ekrich <ekrichardson@gmail.com>
2020-10-12 21:39:36 -0700

    Port InflaterInputStreamTest

--- unit-tests/src/test/scala/java/util/zip/InflaterInputStreamTest.scala
+++ unit-tests/src/test/scala/java/util/zip/InflaterInputStreamTest.scala
@@ -2,14 +2,15 @@ package java.util.zip
 
 // Ported from Apache Harmony
 
-import java.io.{
-  ByteArrayInputStream,
-  ByteArrayOutputStream,
-  IOException,
-  InputStream
-}
+import java.io._
 
-object InflaterInputStreamSuite extends tests.Suite {
+import org.junit.Test
+import org.junit.Assert._
+
+import scala.scalanative.junit.utils.AssertThrows._
+
+class InflaterInputStreamTest {
+
   var outPutBuf = new Array[Byte](500)
 
   private class MyInflaterInputStream(in: InputStream,
@@ -21,18 +22,17 @@ object InflaterInputStreamSuite extends tests.Suite {
     def myFill(): Unit = fill()
   }
 
-  test("Constructor(InputStream, Inflater)") {
+  @Test def constructorInputStreamInflater(): Unit = {
     val byteArray = new Array[Byte](100)
     val infile = new ByteArrayInputStream(
       Array(0x9C, 0x78, 0x64, 0x63, 0x61, 0x66, 0x28, 0xe7, 0xc8, 0xc9, 0x66,
         0x62, 0x00, 0x03, 0xde, 0x05, 0x67, 0x01).map(_.toByte))
     val inflate = new Inflater
-    assertThrows[IllegalArgumentException] {
-      new InflaterInputStream(infile, inflate, -1)
-    }
+    assertThrows(classOf[IllegalArgumentException],
+                 new InflaterInputStream(infile, inflate, -1))
   }
 
-  test("mark()") {
+  @Test def mark(): Unit = {
     val is  = new ByteArrayInputStream(new Array[Byte](10))
     val iis = new InflaterInputStream(is)
     // mark do nothing, do no check
@@ -41,14 +41,14 @@ object InflaterInputStreamSuite extends tests.Suite {
     iis.mark(10000000)
   }
 
-  test("markSupported()") {
+  @Test def markSupported(): Unit = {
     val is  = new ByteArrayInputStream(new Array[Byte](10))
     val iis = new InflaterInputStream(is)
-    assert(!iis.markSupported())
-    assert(is.markSupported())
+    assertTrue(!iis.markSupported())
+    assertTrue(is.markSupported())
   }
 
-  test("read()") {
+  @Test def read(): Unit = {
     var result    = 0
     var buffer    = new Array[Int](5000)
     val orgBuffer = Array[Byte](1, 3, 4, 7, 8)
@@ -67,12 +67,12 @@ object InflaterInputStreamSuite extends tests.Suite {
 
     var j = 0
     while (j < orgBuffer.length) {
-      assert(orgBuffer(j) == buffer(j))
+      assertTrue(orgBuffer(j) == buffer(j))
       j += 1
     }
   }
 
-  test("read(Array[Byte], Int, Int)") {
+  @Test def readArrayByteIntInt(): Unit = {
     var result = 0
     val infile = new ByteArrayInputStream(
       Array(0x9c, 0x78, 0x61, 0x66, 0x00, 0xe7, 0x00, 0x00, 0x00, 0x38, 0x00,
@@ -82,55 +82,51 @@ object InflaterInputStreamSuite extends tests.Suite {
 
     val b = new Array[Byte](3)
 
-    assert(0 == inflatIP.read(b, 0, 0))
+    assertTrue(0 == inflatIP.read(b, 0, 0))
 
-    assertThrows[IndexOutOfBoundsException] {
-      inflatIP.read(b, 5, 2)
-    }
+    assertThrows(classOf[IndexOutOfBoundsException], inflatIP.read(b, 5, 2))
 
     inflatIP.close()
-    assertThrows[IOException] {
-      inflatIP.read(b, 0, 1)
-    }
+    assertThrows(classOf[IOException], inflatIP.read(b, 0, 1))
   }
 
-  test("AvailableNonEmptySource") {
+  @Test def availableNonEmptySource(): Unit = {
     // this byte[] is a deflation of these bytes: {1, 3, 4, 6 }
     val deflated =
       Array[Byte](72, -119, 99, 100, 102, 97, 3, 0, 0, 31, 0, 15, 0)
     val in = new InflaterInputStream(new ByteArrayInputStream(deflated))
-    assert(1 == in.read())
-    assert(1 == in.available())
-    assert(3 == in.read())
-    assert(1 == in.available())
-    assert(4 == in.read())
-    assert(1 == in.available())
-    assert(6 == in.read())
-    assert(0 == in.available())
-    assert(-1 == in.read())
-    assert(-1 == in.read())
+    assertTrue(1 == in.read())
+    assertTrue(1 == in.available())
+    assertTrue(3 == in.read())
+    assertTrue(1 == in.available())
+    assertTrue(4 == in.read())
+    assertTrue(1 == in.available())
+    assertTrue(6 == in.read())
+    assertTrue(0 == in.available())
+    assertTrue(-1 == in.read())
+    assertTrue(-1 == in.read())
   }
 
-  test("AvailableSkip") {
+  @Test def availableSkip(): Unit = {
     // this byte[] is a deflation of these bytes: {1, 3, 4, 6 }
     val deflated =
       Array[Byte](72, -119, 99, 100, 102, 97, 3, 0, 0, 31, 0, 15, 0)
     val in = new InflaterInputStream(new ByteArrayInputStream(deflated))
-    assert(1 == in.available())
-    assert(4 == in.skip(4))
-    assert(0 == in.available())
+    assertTrue(1 == in.available())
+    assertTrue(4 == in.skip(4))
+    assertTrue(0 == in.available())
   }
 
-  test("AvailableEmptySource") {
+  @Test def availableEmptySource(): Unit = {
     // this byte[] is a deflation of the empty file
     val deflated = Array[Byte](120, -100, 3, 0, 0, 0, 0, 1)
     val in       = new InflaterInputStream(new ByteArrayInputStream(deflated))
-    assert(-1 == in.read())
-    assert(-1 == in.read())
-    assert(0 == in.available())
+    assertTrue(-1 == in.read())
+    assertTrue(-1 == in.read())
+    assertTrue(0 == in.available())
   }
 
-  test("read(Array[Byte], Int, Int)") {
+  @Test def readArrayByteIntInt1(): Unit = {
     val test = new Array[Byte](507)
     var i    = 0
     while (i < 256) {
@@ -156,12 +152,10 @@ object InflaterInputStreamSuite extends tests.Suite {
         eofReached = true
       }
     }
-    assertThrows[IndexOutOfBoundsException] {
-      iis.read(outBuf, -1, 10)
-    }
+    assertThrows(classOf[IndexOutOfBoundsException], iis.read(outBuf, -1, 10))
   }
 
-  test("read(Array[Byte], Int, Int) 2") {
+  @Test def readArrayByteIntInt2(): Unit = {
 
     val bis    = new ByteArrayInputStream(ZipBytes.brokenManifestBytes)
     val iis    = new InflaterInputStream(bis)
@@ -169,58 +163,47 @@ object InflaterInputStreamSuite extends tests.Suite {
     iis.close()
 
     // Input data is malformed
-    assertThrows[IOException] {
-      iis.read(outBuf, 0, 5)
-    }
-
+    assertThrows(classOf[IOException], iis.read(outBuf, 0, 5))
   }
 
-  test("read(Array[Byte], Int, Int) 3") {
+  @Test def readArrayByteIntInt3(): Unit = {
     val bis    = new ByteArrayInputStream(ZipBytes.brokenManifestBytes)
     val iis    = new InflaterInputStream(bis)
     val outBuf = new Array[Byte](530)
 
-    assertThrows[IOException] {
-      iis.read()
-    }
+    assertThrows(classOf[IOException], iis.read())
   }
 
-  test("reset()") {
+  @Test def reset(): Unit = {
     val bis = new ByteArrayInputStream(new Array[Byte](10))
     val iis = new InflaterInputStream(bis)
 
-    assertThrows[IOException] {
-      iis.reset()
-    }
+    assertThrows(classOf[IOException], iis.reset())
   }
 
-  test("skipJ()") {
+  @Test def skipJ(): Unit = {
     val bytes = Array(0x78, 0x9c, 0x63, 0x65, 0x61, 0x66, 0xe2, 0x60, 0x67,
       0xe0, 0x64, 0x03, 0x00, 0x00, 0xd3, 0x00, 0x2d, 0x00).map(_.toByte)
     val bis = new ByteArrayInputStream(bytes)
     val iis = new InflaterInputStream(bis)
 
-    assertThrows[IllegalArgumentException] {
-      iis.skip(-3)
-    }
-    assert(iis.read() == 5)
+    assertThrows(classOf[IllegalArgumentException], iis.skip(-3))
+    assertTrue(iis.read() == 5)
 
-    assertThrows[IllegalArgumentException] {
-      iis.skip(Int.MinValue)
-    }
-    assert(iis.read() == 4)
+    assertThrows(classOf[IllegalArgumentException], iis.skip(Int.MinValue))
+    assertTrue(iis.read() == 4)
 
-    assert(iis.skip(3) == 3)
-    assert(iis.read() == 7)
-    assert(iis.skip(0) == 0)
-    assert(iis.read() == 0)
+    assertTrue(iis.skip(3) == 3)
+    assertTrue(iis.read() == 7)
+    assertTrue(iis.skip(0) == 0)
+    assertTrue(iis.read() == 0)
 
-    assert(iis.skip(4) == 2)
-    assert(iis.read() == -1)
+    assertTrue(iis.skip(4) == 2)
+    assertTrue(iis.read() == -1)
     iis.close()
   }
 
-  test("skipJ2") {
+  @Test def skipJ2(): Unit = {
     var result    = 0
     val buffer    = new Array[Int](100)
     val orgBuffer = Array[Byte](1, 3, 4, 7, 8)
@@ -232,9 +215,7 @@ object InflaterInputStreamSuite extends tests.Suite {
     val inflatIP   = new InflaterInputStream(infile, inflate, 10)
     var skip: Long = 0L
 
-    assertThrows[IllegalArgumentException] {
-      inflatIP.skip(Int.MinValue)
-    }
+    assertThrows(classOf[IllegalArgumentException], inflatIP.skip(Int.MinValue))
     inflatIP.close()
 
     val infile2 = new ByteArrayInputStream(
@@ -243,7 +224,7 @@ object InflaterInputStreamSuite extends tests.Suite {
     val inflatIP2 = new InflaterInputStream(infile2)
 
     skip = inflatIP2.skip(Int.MaxValue)
-    assert(skip == 5)
+    assertTrue(skip == 5)
     inflatIP2.close()
 
     val infile3 = new ByteArrayInputStream(
@@ -251,7 +232,7 @@ object InflaterInputStreamSuite extends tests.Suite {
         0x00, 0x18, 0x00).map(_.toByte))
     val inflatIP3 = new InflaterInputStream(infile3)
     skip = inflatIP3.skip(2)
-    assert(2 == skip)
+    assertTrue(2 == skip)
 
     var i = 0
     result = 0
@@ -263,12 +244,12 @@ object InflaterInputStreamSuite extends tests.Suite {
 
     var j = 2
     while (j < orgBuffer.length) {
-      assert(orgBuffer(j) == buffer(j - 2))
+      assertTrue(orgBuffer(j) == buffer(j - 2))
       j += 1
     }
   }
 
-  test("available()") {
+  @Test def available(): Unit = {
     val bytes = Array(0x78, 0x9c, 0x63, 0x65, 0x61, 0x66, 0xe2, 0x60, 0x67,
       0xe0, 0x64, 0x03, 0x00, 0x00, 0xd3, 0x00, 0x2d, 0x00).map(_.toByte)
     val bis       = new ByteArrayInputStream(bytes)
@@ -280,25 +261,21 @@ object InflaterInputStreamSuite extends tests.Suite {
       iis.read()
       available = iis.available()
       if (available == 0) {
-        assert(-1 == iis.read())
+        assertTrue(-1 == iis.read())
       } else {
-        assert(available == 1)
+        assertTrue(available == 1)
       }
       i += 1
     }
-
     iis.close()
 
-    assertThrows[IOException] {
-      iis.available()
-    }
+    assertThrows(classOf[IOException], iis.available())
   }
 
-  test("close()") {
+  @Test def close(): Unit = {
     val iin =
       new InflaterInputStream(new ByteArrayInputStream(new Array[Byte](0)))
     iin.close()
     iin.close()
   }
-
 }
