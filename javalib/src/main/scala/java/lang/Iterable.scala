6929a2289f15030b32555263deda9801275eb572
LeeTibbert <LeeTibbert@users.noreply.github.com>
2020-10-12 22:53:59 -0400

    Add default j.l.Iterable ForEach method (#1934)

--- javalib/src/main/scala/java/lang/Iterable.scala
+++ javalib/src/main/scala/java/lang/Iterable.scala
@@ -1,7 +1,16 @@
+// Influenced by and corresponds to Scala.js commit SHA: f9fc1ae
+
 package java.lang
 
 import java.util.Iterator
+import java.util.function.Consumer
 
 trait Iterable[T] {
   def iterator(): Iterator[T]
+
+  def forEach(action: Consumer[_ >: T]): Unit = {
+    val iter = iterator()
+    while (iter.hasNext())
+      action.accept(iter.next())
+  }
 }
