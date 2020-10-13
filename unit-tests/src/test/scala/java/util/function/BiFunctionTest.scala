9da9a563118fe11d6a852678d1ea15475834f3f2
ekrich <ekrichardson@gmail.com>
2020-10-13 13:45:58 -0700

    Update BiFunctionTest

--- unit-tests/src/test/scala/java/util/function/BiFunctionTest.scala
+++ unit-tests/src/test/scala/java/util/function/BiFunctionTest.scala
@@ -1,24 +1,24 @@
 package java.util.function
 
-object BiFunctionSuite extends tests.Suite {
+import org.junit.Test
+import org.junit.Assert._
+
+class BiFunctionTest {
   val f = new BiFunction[Integer, Integer, Integer] {
     override def apply(x: Integer, y: Integer): Integer = {
       x + y
     }
   }
 
-  test("BiFunction.apply(Integer, Integer)") {
+  @Test def biFunctionApplyIntegerInteger(): Unit = {
     assertEquals(f(1, 2), 3)
   }
 
-  // andThen doesn't work currently
-  /*
   val ff = new Function[Integer, Integer] {
     override def apply(x: Integer): Integer = x + 1
   }
 
-  test("BiFunction.andThen") {
+  @Test def biFunctionAndThen(): Unit = {
     assertEquals(f.andThen(ff)(1, 2), 4)
   }
- */
 }
