31a259f48e277d583158ee4bc941bd8f8c81a275
ekrich <ekrichardson@gmail.com>
2020-10-13 13:31:48 -0700

    Update BiConsumerTest

--- unit-tests/src/test/scala/java/util/function/BiConsumerTest.scala
+++ unit-tests/src/test/scala/java/util/function/BiConsumerTest.scala
@@ -1,11 +1,16 @@
 package java.util.function
 
-import org.junit.Ignore
+import org.junit.Before
 import org.junit.Test
 import org.junit.Assert._
 
 class BiConsumerTest {
   var result = 0
+
+  @Before
+  protected def setUp(): Unit = {
+    result = 0
+  }
 
   val addT = new BiConsumer[Int, Int] {
     override def accept(t: Int, u: Int): Unit = {
@@ -25,14 +30,12 @@ class BiConsumerTest {
     assertTrue(result == 4)
   }
 
-  @Ignore("andThen doesn't work currently")
   @Test def biConsumerAndThen(): Unit = {
-    // result = 0
-    // assertTrue(result == 0)
-    // addT.andThen(timesT).accept(2, 3)
+    assertTrue(result == 0)
+    addT.andThen(timesT).accept(2, 3)
 
-    // // addT will add 2 + 2 = 4 to result which is zero = 4
-    // // then 2*3 = 6 will be added to result which is 4, =  11
-    // assertTrue(result == 11)
+    // addT will add 2 + 2 = 4 to result which is zero = 4
+    // then 2*3 = 6 will be added to result which is 4, =  11
+    assertTrue(result == 11)
   }
 }
