a8981c84644b386160e56c5aa036bb8f5153d96c
ekrich <ekrichardson@gmail.com>
2020-10-18 21:33:38 -0700

    Change asserts

--- unit-tests/src/test/scala/java/lang/RuntimeTest.scala
+++ unit-tests/src/test/scala/java/lang/RuntimeTest.scala
@@ -11,20 +11,20 @@ class RuntimeTest {
   @Test def execCommand(): Unit = {
     val proc = Runtime.getRuntime.exec(Array("ls", resourceDir))
     val out  = readInputStream(proc.getInputStream)
-    assert(proc.waitFor(5, TimeUnit.SECONDS))
-    assert(out.split("\n").toSet == Set("echo.sh", "err.sh", "hello.sh", "ls"))
+    assertTrue(proc.waitFor(5, TimeUnit.SECONDS))
+    assertTrue(out.split("\n").toSet == Set("echo.sh", "err.sh", "hello.sh", "ls"))
   }
   @Test def execEnvp(): Unit = {
     val envp = Array(s"PATH=$resourceDir")
     val proc = Runtime.getRuntime.exec(Array("ls"), envp)
     val out  = readInputStream(proc.getInputStream)
-    assert(proc.waitFor(5, TimeUnit.SECONDS))
-    assert(out == "1")
+    assertTrue(proc.waitFor(5, TimeUnit.SECONDS))
+    assertTrue(out == "1")
   }
   @Test def execDir(): Unit = {
     val proc = Runtime.getRuntime.exec(Array("ls"), null, new File(resourceDir))
     val out  = readInputStream(proc.getInputStream)
-    assert(proc.waitFor(5, TimeUnit.SECONDS))
-    assert(out.split("\n").toSet == Set("echo.sh", "err.sh", "hello.sh", "ls"))
+    assertTrue(proc.waitFor(5, TimeUnit.SECONDS))
+    assertTrue(out.split("\n").toSet == Set("echo.sh", "err.sh", "hello.sh", "ls"))
   }
 }
