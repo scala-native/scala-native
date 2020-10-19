4b0b631eb85b9465533c55e75d449c9404f353f6
ekrich <ekrichardson@gmail.com>
2020-10-18 21:32:32 -0700

    Port RuntimeTest

--- unit-tests/src/test/scala/java/lang/RuntimeTest.scala
+++ unit-tests/src/test/scala/java/lang/RuntimeTest.scala
@@ -3,22 +3,25 @@ package java.lang
 import java.util.concurrent.TimeUnit
 import java.io.File
 
-object RuntimeSuite extends tests.Suite {
+import org.junit.Test
+import org.junit.Assert._
+
+class RuntimeTest {
   import ProcessSuite._
-  test("exec command") {
+  @Test def execCommand(): Unit = {
     val proc = Runtime.getRuntime.exec(Array("ls", resourceDir))
     val out  = readInputStream(proc.getInputStream)
     assert(proc.waitFor(5, TimeUnit.SECONDS))
     assert(out.split("\n").toSet == Set("echo.sh", "err.sh", "hello.sh", "ls"))
   }
-  test("exec envp") {
+  @Test def execEnvp(): Unit = {
     val envp = Array(s"PATH=$resourceDir")
     val proc = Runtime.getRuntime.exec(Array("ls"), envp)
     val out  = readInputStream(proc.getInputStream)
     assert(proc.waitFor(5, TimeUnit.SECONDS))
     assert(out == "1")
   }
-  test("exec dir") {
+  @Test def execDir(): Unit = {
     val proc = Runtime.getRuntime.exec(Array("ls"), null, new File(resourceDir))
     val out  = readInputStream(proc.getInputStream)
     assert(proc.waitFor(5, TimeUnit.SECONDS))
