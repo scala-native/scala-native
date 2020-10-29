d5bc214babbed45a820e9fba6fe4e2a9765480a9
ekrich <ekrichardson@gmail.com>
2020-10-29 16:40:25 -0700

    JUnit port for ObjectCloneTest

--- unit-tests/src/test/scala/scala/ObjectCloneTest.scala
+++ unit-tests/src/test/scala/scala/ObjectCloneTest.scala
@@ -1,33 +1,36 @@
 package scala
 
-object ObjectCloneSuite extends tests.Suite {
+import org.junit.Test
+import org.junit.Assert._
+
+class ObjectCloneTest {
 
   case class I(var i: Int) {
     def copy(): I = this.clone().asInstanceOf[I]
   }
 
-  test("clone with primitive") {
+  @Test def cloneWithPrimitive(): Unit = {
     val obj   = I(123)
     val clone = obj.copy()
 
     obj.i = 124
 
-    assert(obj.i == 124)
-    assert(clone.i == 123)
+    assertTrue(obj.i == 124)
+    assertTrue(clone.i == 123)
   }
 
   case class Arr(val arr: Array[Int]) {
     def copy(): Arr = this.clone().asInstanceOf[Arr]
   }
 
-  test("clone with ref") {
+  @Test def cloneWithRef(): Unit = {
     val obj   = Arr(Array(123))
     val clone = obj.copy()
 
     obj.arr(0) = 124
 
-    assert(obj.arr(0) == 124)
-    assert(clone.arr(0) == 124)
+    assertTrue(obj.arr(0) == 124)
+    assertTrue(clone.arr(0) == 124)
   }
 
 }
