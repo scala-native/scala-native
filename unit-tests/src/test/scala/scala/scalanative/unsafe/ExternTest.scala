2c5e9fd43109609d658234f294d1e7748aa47505
ekrich <ekrichardson@gmail.com>
2020-11-01 13:58:44 -0800

    Port ExternTest

--- unit-tests/src/test/scala/scala/scalanative/unsafe/ExternTest.scala
+++ unit-tests/src/test/scala/scala/scalanative/unsafe/ExternTest.scala
@@ -1,11 +1,27 @@
 package scala.scalanative
 package unsafe
 
+import org.junit.Test
+import org.junit.Assert._
+
 import scalanative.unsafe._
 import scalanative.libc.{stdio, stdlib, string}
 
-object ExternSuite extends tests.Suite {
-  test("extern variable read and assign") {
+// these can be nested inside an object but not a class - see #897
+@extern
+object Ext1 {
+  def snprintf(buf: CString, size: CSize, format: CString, l: CString): Int =
+    extern
+}
+@extern
+object Ext2 {
+  @name("snprintf")
+  def p(buf: CString, size: CSize, format: CString, i: Int): Int = extern
+}
+
+class ExternTest {
+
+  @Test def externVariableReadAndAssign(): Unit = {
     import scala.scalanative.posix.getopt
 
     val args = Seq("skipped", "skipped", "skipped", "-b", "-f", "farg")
@@ -22,42 +38,33 @@ object ExternSuite extends tests.Suite {
       getopt.optind = 3
 
       val bOpt = getopt.getopt(args.length, argv, c"bf:")
-      assert(bOpt == 'b')
+      assertTrue(bOpt == 'b')
 
       val fOpt = getopt.getopt(args.length, argv, c"bf:")
-      assert(fOpt == 'f')
+      assertTrue(fOpt == 'f')
       val fArg = fromCString(getopt.optarg)
-      assert(fArg == "farg")
+      assertTrue(fArg == "farg")
     }
   }
 
-  @extern
-  object Ext1 {
-    def snprintf(buf: CString, size: CSize, format: CString, l: CString): Int =
-      extern
-  }
-  @extern
-  object Ext2 {
-    @name("snprintf")
-    def p(buf: CString, size: CSize, format: CString, i: Int): Int = extern
-  }
-  test("same extern name in two different objects") {
+  @Test def sameExternNameInTwoDifferentObjects(): Unit = {
     val bufsize = 10L
     val buf1    = stackalloc[Byte](bufsize)
     val buf2    = stackalloc[Byte](bufsize)
     Ext1.snprintf(buf1, bufsize, c"%s", c"hello")
-    assert(string.strcmp(buf1, c"hello") == 0)
+    assertTrue(string.strcmp(buf1, c"hello") == 0)
     Ext2.p(buf2, bufsize, c"%d", 1)
-    assert(string.strcmp(buf2, c"1") == 0)
+    assertTrue(string.strcmp(buf2, c"1") == 0)
   }
 
   val cb: CFuncPtr0[CInt] = new CFuncPtr0[Int] {
     override def apply(): Int = 42
   }
-  test("allows to use generic function as argument") {
+
+  @Test def allowsToUseGenericFunctionAsArgument(): Unit = {
     val res0 = testlib.exec0(cb) //expected CFuncPtr0[Int]
     val res1 = testlib.exec(cb)  //expected CFuncPtr
-    assert(res0 == 42)
-    assert(res1 == 42)
+    assertTrue(res0 == 42)
+    assertTrue(res1 == 42)
   }
 }
