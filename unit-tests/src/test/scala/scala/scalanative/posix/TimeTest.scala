fc7407e6e963d125c4715a6f996fe8a298c0da4c
ekrich <ekrichardson@gmail.com>
2020-11-01 04:36:40 -0800

    Port TimeTest

--- unit-tests/src/test/scala/scala/scalanative/posix/TimeTest.scala
+++ unit-tests/src/test/scala/scala/scalanative/posix/TimeTest.scala
@@ -1,49 +1,52 @@
 package scala.scalanative.posix
 
+import org.junit.Test
+import org.junit.Assert._
+
 import scala.scalanative.unsafe._
 
 import time._
 import timeOps.tmOps
 
-object TimeSuite extends tests.Suite {
+class TimeTest {
   tzset()
   //In 2.11/2.12 time was resolved to posix.time.type, in 2.13 to posix.time.time method
   val now_time_t: time_t = scala.scalanative.posix.time.time(null)
   val epoch: time_t      = 0L
 
-  test("asctime() with a given known state should match its representation") {
+  @Test def asctimeWithGivenKnownStateShouldMatchItsRepresentation(): Unit = {
     Zone { implicit z =>
       val anno_zero_ptr = alloc[tm]
       anno_zero_ptr.tm_mday = 1
       anno_zero_ptr.tm_wday = 1
       val cstr: CString = asctime(anno_zero_ptr)
       val str: String   = fromCString(cstr)
-      assert("Mon Jan  1 00:00:00 1900\n".equals(str))
+      assertTrue("Mon Jan  1 00:00:00 1900\n".equals(str))
     }
   }
 
-  test("asctime_r() with a given known state should match its representation") {
+  @Test def asctime_rWithGivenKnownStateShouldMatchItsRepresentation(): Unit = {
     Zone { implicit z =>
       val anno_zero_ptr = alloc[tm]
       anno_zero_ptr.tm_mday = 1
       anno_zero_ptr.tm_wday = 1
       val cstr: CString = asctime_r(anno_zero_ptr, alloc[Byte](26))
       val str: String   = fromCString(cstr)
-      assert("Mon Jan  1 00:00:00 1900\n".equals(str))
+      assertTrue("Mon Jan  1 00:00:00 1900\n".equals(str))
     }
   }
 
-  test("localtime() should transform the epoch to localtime") {
+  @Test def localtimeShouldTransformTheEpochToLocaltime(): Unit = {
     val time_ptr = stackalloc[time_t]
     !time_ptr = epoch + timezone
     val time: Ptr[tm] = localtime(time_ptr)
     val cstr: CString = asctime(time)
     val str: String   = fromCString(cstr)
 
-    assert("Thu Jan  1 00:00:00 1970\n".equals(str))
+    assertTrue("Thu Jan  1 00:00:00 1970\n".equals(str))
   }
 
-  test("localtime_r() should transform the epoch to localtime") {
+  @Test def localtime_rShouldTransformTheEpochToLocaltime(): Unit = {
     Zone { implicit z =>
       val time_ptr = stackalloc[time_t]
       !time_ptr = epoch + timezone
@@ -51,21 +54,21 @@ object TimeSuite extends tests.Suite {
       val cstr: CString = asctime_r(time, alloc[Byte](26))
       val str: String   = fromCString(cstr)
 
-      assert("Thu Jan  1 00:00:00 1970\n".equals(str))
+      assertTrue("Thu Jan  1 00:00:00 1970\n".equals(str))
     }
   }
 
-  test(
-    "difftime() between epoch and now should be bigger than the timestamp when I wrote this code") {
-    assert(difftime(now_time_t, epoch) > 1502752688)
+  @Test def difftimeBetweenEpochAndNowGreaterThanTimestampWhenCodeWasWritten()
+      : Unit = {
+    assertTrue(difftime(now_time_t, epoch) > 1502752688)
   }
 
-  test("time() should be bigger than the timestamp when I wrote this code") {
+  @Test def timeNowGreaterThanTimestampWhenCodeWasWritten(): Unit = {
     // arbitrary date set at the time when I was writing this.
-    assert(now_time_t > 1502752688)
+    assertTrue(now_time_t > 1502752688)
   }
 
-  test("strftime() for 1900-01-01T00:00:00Z") {
+  @Test def strftimeForJanOne1900ZeroZulu(): Unit = {
     Zone { implicit z =>
       val isoDatePtr: Ptr[CChar] = alloc[CChar](70)
       val timePtr                = alloc[tm]
@@ -76,11 +79,11 @@ object TimeSuite extends tests.Suite {
 
       val isoDateString: String = fromCString(isoDatePtr)
 
-      assert("1900-01-01T00:00:00Z".equals(isoDateString))
+      assertTrue("1900-01-01T00:00:00Z".equals(isoDateString))
     }
   }
 
-  test("strftime() for Monday Mon Jan  1 00:00:00 1900") {
+  @Test def strftimeForMondayJanOne1990ZeroTime(): Unit = {
     Zone { implicit z =>
       val timePtr             = alloc[tm]
       val datePtr: Ptr[CChar] = alloc[CChar](70)
@@ -91,11 +94,11 @@ object TimeSuite extends tests.Suite {
       strftime(datePtr, 70, c"%A %c", timePtr)
 
       val dateString: String = fromCString(datePtr)
-      assert("Monday Mon Jan  1 00:00:00 1900".equals(dateString))
+      assertTrue("Monday Mon Jan  1 00:00:00 1900".equals(dateString))
     }
   }
 
-  test(s"strptime() - detect invalid format") {
+  @Test def strptimeDetectsInvalidFormat(): Unit = {
     Zone { implicit z =>
       val tmPtr = alloc[tm]
 
@@ -103,11 +106,11 @@ object TimeSuite extends tests.Suite {
       val result =
         strptime(c"December 31, 2016 23:59:60", c"%B %d, %Y %Q", tmPtr)
 
-      assert(result == null, s"expected null result, got pointer")
+      assertTrue(s"expected null result, got pointer", result == null)
     }
   }
 
-  test(s"strptime() - detect invalid string") {
+  @Test def strptimeDetectsInvalidString(): Unit = {
     Zone { implicit z =>
       val tmPtr = alloc[tm]
 
@@ -115,22 +118,22 @@ object TimeSuite extends tests.Suite {
       val result =
         strptime(c"December 32, 2016 23:59:60", c"%B %d, %Y %T", tmPtr)
 
-      assert(result == null, s"expected null result, got pointer")
+      assertTrue(s"expected null result, got pointer", result == null)
     }
   }
 
-  test(s"strptime() - detect string shorter than format") {
+  @Test def strptimeDetectsStringShorterThanFormat(): Unit = {
     Zone { implicit z =>
       val tmPtr = alloc[tm]
 
       val result =
         strptime(c"December 32, 2016 23:59", c"%B %d, %Y %T", tmPtr)
 
-      assert(result == null, s"expected null result, got pointer")
+      assertTrue(s"expected null result, got pointer", result == null)
     }
   }
 
-  test("strptime() for December 31, 2016 23:59:60") {
+  @Test def strptimeFor31December2016Time235960(): Unit = {
     Zone { implicit z =>
       val tmPtr = alloc[tm]
 
@@ -138,50 +141,50 @@ object TimeSuite extends tests.Suite {
       val result =
         strptime(c"December 31, 2016 23:59:60", c"%B %d, %Y %T", tmPtr)
 
-      assert(result != null, s"error: unexpected null returned")
+      assertTrue(s"error: unexpected null returned", result != null)
 
       val expectedYear = 116
-      assert(tmPtr.tm_year == expectedYear,
-             s"tm_year: ${tmPtr.tm_year} != expected: ${expectedYear}")
+      assertTrue(s"tm_year: ${tmPtr.tm_year} != expected: ${expectedYear}",
+                 tmPtr.tm_year == expectedYear)
 
       val expectedMonth = 11
-      assert(tmPtr.tm_mon == expectedMonth,
-             s"tm_mon: ${tmPtr.tm_mon} != expected: ${expectedMonth}")
+      assertTrue(s"tm_mon: ${tmPtr.tm_mon} != expected: ${expectedMonth}",
+                 tmPtr.tm_mon == expectedMonth)
 
       val expectedMday = 31
-      assert(tmPtr.tm_mday == expectedMday,
-             s"tm_mon: ${tmPtr.tm_mday} != expected: ${expectedMday}")
+      assertTrue(s"tm_mon: ${tmPtr.tm_mday} != expected: ${expectedMday}",
+                 tmPtr.tm_mday == expectedMday)
 
       val expectedHour = 23
-      assert(tmPtr.tm_hour == expectedHour,
-             s"tm_mon: ${tmPtr.tm_hour} != expected: ${expectedHour}")
+      assertTrue(s"tm_mon: ${tmPtr.tm_hour} != expected: ${expectedHour}",
+                 tmPtr.tm_hour == expectedHour)
 
       val expectedMin = 59
-      assert(tmPtr.tm_min == expectedMin,
-             s"tm_min: ${tmPtr.tm_min} != expected: ${expectedMin}")
+      assertTrue(s"tm_min: ${tmPtr.tm_min} != expected: ${expectedMin}",
+                 tmPtr.tm_min == expectedMin)
 
       val expectedSec = 60
-      assert(tmPtr.tm_sec == expectedSec,
-             s"tm_sec: ${tmPtr.tm_sec} != expected: ${expectedSec}")
+      assertTrue(s"tm_sec: ${tmPtr.tm_sec} != expected: ${expectedSec}",
+                 tmPtr.tm_sec == expectedSec)
 
       val expectedIsdst = 0
-      assert(tmPtr.tm_isdst == expectedIsdst,
-             s"tm_isdst: ${tmPtr.tm_isdst} != expected: ${expectedIsdst}")
+      assertTrue(s"tm_isdst: ${tmPtr.tm_isdst} != expected: ${expectedIsdst}",
+                 tmPtr.tm_isdst == expectedIsdst)
     }
   }
 
-  test("strptime() -- extra text after date string is OK") {
+  @Test def strptimeExtraTextAfterDateStringIsOK(): Unit = {
     Zone { implicit z =>
       val tmPtr = alloc[tm]
 
       val result =
         strptime(c"December 31, 2016 23:59:60 UTC", c"%B %d, %Y %T ", tmPtr)
 
-      assert(result != null, s"error: null returned")
+      assertTrue(s"error: null returned", result != null)
 
       val expected = 'U'
-      assert(!result == expected,
-             s"character: ${!result} != expected: ${expected}")
+      assertTrue(s"character: ${!result} != expected: ${expected}",
+                 !result == expected)
     }
   }
 
