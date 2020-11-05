3a995665894e4f40995f028f48b0228561f74426
ekrich <ekrichardson@gmail.com>
2020-11-05 08:26:15 -0800

    Port RE2QuoteMetaTest

--- unit-tests/src/test/scala/scala/scalanative/regex/RE2QuoteMetaTest.scala
+++ unit-tests/src/test/scala/scala/scalanative/regex/RE2QuoteMetaTest.scala
@@ -3,9 +3,10 @@ package regex
 
 import java.util.regex.PatternSyntaxException
 
-import ScalaTestCompat.fail
+import org.junit.Test
+import org.junit.Assert._
 
-object RE2QuoteMetaSuite extends tests.Suite {
+class RE2QuoteMetaTest {
 
   // (pattern, output, literal, isLiteral)
   private val META_TESTS = Array(
@@ -21,14 +22,14 @@ object RE2QuoteMetaSuite extends tests.Suite {
           "false")
   )
 
-  test("QuoteMeta") {
+  @Test def quoteMeta(): Unit = {
     // Verify that quoteMeta returns the expected string.
     for (Array(pattern, output, _, _) <- META_TESTS) {
       val quoted = RE2.quoteMeta(pattern)
-      if (!(quoted == output))
-        fail(
-          "RE2.quoteMeta(\"%s\") = \"%s\"; want \"%s\""
-            .format(pattern, quoted, output))
+      assertTrue("RE2.quoteMeta(\"%s\") = \"%s\"; want \"%s\""
+                   .format(pattern, quoted, output),
+                 quoted == output)
+
       // Verify that the quoted string is in fact treated as expected
       // by compile -- i.e. that it matches the original, unquoted string.
       if (!pattern.isEmpty) {
@@ -44,27 +45,22 @@ object RE2QuoteMetaSuite extends tests.Suite {
         val repl     = "xyz"
         val replaced = re.replaceAll(src, repl)
         val expected = "abcxyzdef"
-        if (!(replaced == expected))
-          fail(
-            "quoteMeta(`%s`).replace(`%s`,`%s`) = `%s`; want `%s`"
-              .format(pattern, src, repl, replaced, expected))
+        assertTrue("quoteMeta(`%s`).replace(`%s`,`%s`) = `%s`; want `%s`"
+                     .format(pattern, src, repl, replaced, expected),
+                   replaced == expected)
       }
     }
   }
 
-  test("LiteralPrefix") {
+  @Test def literalPrefix(): Unit = {
     // Literal method needs to scan the pattern.
     for (Array(pattern, output, literal, _isLiteral) <- META_TESTS) {
       val isLiteral = java.lang.Boolean.parseBoolean(_isLiteral)
       val re        = RE2.compile(pattern)
-      if (re.prefixComplete != isLiteral)
-        fail(
-          "literalPrefix(\"%s\") = %s; want %s"
-            .format(pattern, re.prefixComplete, isLiteral))
-      if (!(re.prefix == literal))
-        fail(
-          "literalPrefix(\"%s\") = \"%s\"; want \"%s\""
-            .format(pattern, re.prefix, literal))
+      val msg       = "literalPrefix(\"%s\") = \"%s\"; want \"%s\""
+      assertFalse(msg.format(pattern, re.prefixComplete, isLiteral),
+                  re.prefixComplete != isLiteral)
+      assertTrue(msg.format(pattern, re.prefix, literal), re.prefix == literal)
     }
   }
 }
