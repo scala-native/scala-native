10c0da63124f819b63a82c6dbd42df354e4f6050
ekrich <ekrichardson@gmail.com>
2020-11-09 10:00:31 -0800

    Port MatcherTest and change comment to remove file

--- unit-tests/src/test/scala/utils/ThrowsHelper.scala
+++ unit-tests/src/test/scala/utils/ThrowsHelper.scala
@@ -2,6 +2,9 @@ package scala.scalanative.junit.utils
 
 import AssertThrows._
 
+// Calls to this should probably be changed to expectThrows.
+// This was added as it was all over the place in the pre
+// JUnit code.
 object ThrowsHelper {
   def assertThrowsAnd[T <: Throwable, U](
       expectedThrowable: Class[T],
