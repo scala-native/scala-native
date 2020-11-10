67daf3b7560dc599f45ea6cd346a8a73aede2c1f
ekrich <ekrichardson@gmail.com>
2020-11-10 14:08:36 -0800

    Revert changes as the issue was CI only

--- unit-tests/src/test/scala/scala/scalanative/unsafe/ExternTest.scala
+++ unit-tests/src/test/scala/scala/scalanative/unsafe/ExternTest.scala
@@ -1,7 +1,6 @@
 package scala.scalanative
 package unsafe
 
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.Assert._
 
@@ -48,15 +47,14 @@ class ExternTest {
     }
   }
 
-  @Ignore("linking error issue #1652")
-  @Test def sameExternNameInTwoDifferentObjects(): Unit = {
+  @Test def sameExternNameInTwoDifferentObjectsIssue1652(): Unit = {
     val bufsize = 10L
     val buf1    = stackalloc[Byte](bufsize)
     val buf2    = stackalloc[Byte](bufsize)
-    // Ext1.snprintf(buf1, bufsize, c"%s", c"hello")
-    // assertTrue(string.strcmp(buf1, c"hello") == 0)
-    // Ext2.p(buf2, bufsize, c"%d", 1)
-    // assertTrue(string.strcmp(buf2, c"1") == 0)
+    Ext1.snprintf(buf1, bufsize, c"%s", c"hello")
+    assertTrue(string.strcmp(buf1, c"hello") == 0)
+    Ext2.p(buf2, bufsize, c"%d", 1)
+    assertTrue(string.strcmp(buf2, c"1") == 0)
   }
 
   val cb: CFuncPtr0[CInt] = new CFuncPtr0[Int] {
