24708fbc40a798affdfbe2200bf6aa18860359ab
wojciechmazur <wojciech.mazur95@gmail.com>
2020-11-11 11:13:35 +0100

    Add missing newline in io.package. Remove ClassTag type bound. Use Seq.newBuilder instead of UnrolledBuffer

--- util/src/main/scala/scala/scalanative/io/package.scala
+++ util/src/main/scala/scala/scalanative/io/package.scala
@@ -1,18 +1,16 @@
 package scala.scalanative
 
 import java.nio.file.Path
-import scala.collection.mutable
-import scala.reflect.ClassTag
 
 package object io {
-  private[io] def jIteratorToSeq[T: ClassTag](
-      it: java.util.Iterator[T]): Seq[T] = {
-    val buf = mutable.UnrolledBuffer.empty[T]
+  private[io] def jIteratorToSeq[T](it: java.util.Iterator[T]): Seq[T] = {
+    val buf = Seq.newBuilder[T]
     while (it.hasNext) {
       buf += it.next()
     }
-    buf.toSeq
+    buf.result()
   }
+
   def packageNameFromPath(path: Path): String = {
     val fileName = path.getFileName.toString
     val base     = fileName.split('.').init.mkString(".")
