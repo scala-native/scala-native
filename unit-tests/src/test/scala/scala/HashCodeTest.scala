28f8a0357fd4b852066a27d62dd408b86b7e6e7f
wojciechmazur <wojciech.mazur95@gmail.com>
2020-11-11 17:40:08 +0100

    Revert Scala version dependent HashCode tests after rebase

--- unit-tests/src/test/scala/scala/HashCodeTest.scala
+++ unit-tests/src/test/scala/scala/HashCodeTest.scala
@@ -2,15 +2,29 @@ package scala
 
 import org.junit.Test
 import org.junit.Assert._
+import scala.scalanative.buildinfo.ScalaNativeBuildInfo
 
 class HashCodeTest {
   case class MyData(string: String, num: Int)
+
+  def scala212orOlder: Boolean =
+    ScalaNativeBuildInfo.scalaVersion
+      .split('.')
+      .take(3)
+      .map(_.toInt) match {
+      case Array(2, n, _) if n <= 12 => true
+      case _                         => false
+    }
 
   @Test def hashCodeOfStringMatchesScalaJVM(): Unit = {
     assertTrue("hello".hashCode == 99162322)
   }
 
   @Test def hashCodeOfCaseClassMatchesScalaJVM(): Unit = {
-    assertTrue(MyData("hello", 12345).hashCode == -1824015247)
+    val expectedHashCode =
+      if (scala212orOlder) -1824015247
+      else -715875225
+    assertTrue(MyData("hello", 12345).hashCode == expectedHashCode)
   }
+
 }
