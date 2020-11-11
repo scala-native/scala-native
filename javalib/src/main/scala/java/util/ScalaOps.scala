a5916ee4e73fa095c154de62439fa1addad6a706
wojciechmazur <wojciech.mazur95@gmail.com>
2020-11-11 13:04:26 +0100

    Extend ScalaOps Iterator ops with support for map, min/max/toSeq, head/last(Option)

--- javalib/src/main/scala/java/util/ScalaOps.scala
+++ javalib/src/main/scala/java/util/ScalaOps.scala
@@ -50,6 +50,27 @@ private[java] object ScalaOps {
 
     @inline def mkString(start: String, sep: String, end: String): String =
       __self.iterator().scalaOps.mkString(start, sep, end)
+
+    @inline def min(comp: Comparator[_ >: A]): A =
+      __self.iterator().scalaOps.min(comp)
+
+    @inline def max(comp: Comparator[_ >: A]): A =
+      __self.iterator().scalaOps.max(comp)
+
+    @inline def headOption: Option[A] =
+      __self.iterator().scalaOps.headOption
+
+    @inline def head: A =
+      __self.iterator().scalaOps.head
+
+    @inline def lastOption: Option[A] =
+      __self.iterator().scalaOps.lastOption
+
+    @inline def last: A =
+      __self.iterator().scalaOps.last
+
+    @inline def toSeq: Seq[A] =
+      __self.iterator().scalaOps.toSeq
   }
 
   implicit class ToJavaIteratorOps[A] private[ScalaOps] (
@@ -64,6 +85,11 @@ private[java] object ScalaOps {
     @inline def foreach[U](f: A => U): Unit = {
       while (__self.hasNext())
         f(__self.next())
+    }
+
+    @inline def map[U](f: A => U): Iterator[U] = new Iterator[U] {
+      override def hasNext: Boolean = __self.hasNext
+      override def next(): U        = f(__self.next())
     }
 
     @inline def count(f: A => Boolean): Int =
@@ -123,6 +149,47 @@ private[java] object ScalaOps {
         result += __self.next()
       }
       result + end
+    }
+
+    @inline def headOption: Option[A] = {
+      if (__self.hasNext()) Some(__self.next())
+      else None
+    }
+
+    @inline def head: A = {
+      if (__self.hasNext()) __self.next()
+      else throw new NoSuchElementException("empty.head")
+    }
+
+    @inline def lastOption: Option[A] = {
+      if (!__self.hasNext()) None
+      else {
+        var last: A = __self.next()
+        while (__self.hasNext()) {
+          last = __self.next()
+        }
+        Some(last)
+      }
+    }
+
+    @inline def last: A =
+      if (__self.hasNext()) lastOption.get
+      else throw new NoSuchElementException("empty.last")
+
+    @inline def min(comp: Comparator[_ >: A]): A =
+      reduceLeft[A] {
+        case (l, r) => if (comp.compare(l, r) <= 0) l else r
+      }
+
+    @inline def max(comp: Comparator[_ >: A]): A =
+      reduceLeft[A] {
+        case (l, r) => if (comp.compare(l, r) >= 0) l else r
+      }
+
+    @inline def toSeq: Seq[A] = {
+      val buf = Seq.newBuilder[A]
+      foreach(buf += _)
+      buf.result()
     }
   }
 
