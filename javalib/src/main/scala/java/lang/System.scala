4ad48f5d435e51b52a07c96dd6e625fbca30c36c
wojciechmazur <wojciech.mazur95@gmail.com>
2020-11-11 19:26:08 +0100

    Remove laziness of System.systemProperties, it was fixed by scalalib 2.13 overrides

--- javalib/src/main/scala/java/lang/System.scala
+++ javalib/src/main/scala/java/lang/System.scala
@@ -91,11 +91,7 @@ object System {
   var err: PrintStream =
     new PrintStream(new FileOutputStream(FileDescriptor.err))
 
-  /* Laziness for this val was enforced due to changes in Scala 2.13 Vector implementation
-   * Vector uses system property to define some of its default parameters and to allow user to tune it.
-   * This problem exists because current implementation of java.lang.System depends on Scala collections,
-   * this problem should be addressed in the future */
-  private lazy val systemProperties = loadProperties()
+  private val systemProperties = loadProperties()
   Platform.setOSProps(new CFuncPtr2[CString, CString, Unit] {
     def apply(key: CString, value: CString): Unit =
       systemProperties.setProperty(fromCString(key), fromCString(value))
