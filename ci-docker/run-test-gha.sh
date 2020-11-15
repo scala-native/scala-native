7661469e2f28cc0e766d10e82e7e211d28995b46
wojciechmazur <wojciech.mazur95@gmail.com>
2020-11-15 21:50:16 +0100

    Don't build image if it already exists

--- ci-docker/run-test-gha.sh
+++ ci-docker/run-test-gha.sh
@@ -2,24 +2,15 @@
 set -e
 set -x
 
+if [ $# -eq 0 ]
+  then echo "Expected single argument with docker image version"
+  exit 1
+fi
+IMAGE_VERSION=$1
+
 sudo chmod a+rwx -R $HOME;
 
-docker run \
- --rm \
- --privileged \
- multiarch/qemu-user-static:register
-
-docker images | grep scala-native-testing
-
-docker build \
-  --cache-from scala-native-testing:linux-$TARGET_DOCKER_PLATFORM \
-  -t scala-native-testing:linux-$TARGET_DOCKER_PLATFORM \
-  --build-arg TARGET_DOCKER_PLATFORM=$TARGET_DOCKER_PLATFORM \
-  --build-arg HOST_ARCHITECTURE=amd64 \
-  --cpuset-cpus=0 \
-  ci-docker
-
-docker run -i scala-native-testing:linux-$TARGET_DOCKER_PLATFORM java -version
+docker run -i scala-native-testing/linux-$TARGET_DOCKER_PLATFORM:${IMAGE_VERSION} java -version
 
 docker run -v $HOME/.ivy2:/home/scala-native/.ivy2:z \
            -v $HOME/.sbt:/home/scala-native/.sbt:z \
@@ -29,5 +20,5 @@ docker run -v $HOME/.ivy2:/home/scala-native/.ivy2:z \
            -e SCALANATIVE_OPTIMIZE=$SCALANATIVE_OPTIMIZE \
            -e TEST_COMMAND=$TEST_COMMAND \
            -e SCALA_VERSION=$SCALA_VERSION \
-           -i scala-native-testing:linux-$TARGET_DOCKER_PLATFORM;
+           -i scala-native-testing/linux-$TARGET_DOCKER_PLATFORM:${IMAGE_VERSION}
 
