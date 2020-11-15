87f2dac29d67f85187cad7707cd7efe624261a01
Lee Tibbert <lee.tibbert@gmail.com>
2020-11-15 15:48:15 -0500

    WIP: Annotate  & IOU-test j.u.List default methods
    This PR builds upon merged PR #1937 by adding the JavaDefaultMethod
    annotation to the trait
    
    IOU: The discerning reader will notice that there is no ListTest.scala
         in this WIP.
    
         Before this PR can move out of WIP state the corresponding
         Scala.js ListTest.scala should be ported. Currently ScalaNative
         has no ListTest.
    
         Scala.js ListTest has a number of prerequisites, few of which
         are currently ported (such as TestMainBase).  I believe
         porting those items in a series of separate PRs. Tedious, but
         it will leave a better audit trail.
    
    See Issue #1972 for background & discussion of JavaDefaultMethod
    annotation.
    
    This PR is WIP because it will fail to build until after PR #1997
    is merged and this PR re-based upon it.

--- javalib/src/main/scala/java/util/List.scala
+++ javalib/src/main/scala/java/util/List.scala
@@ -1,9 +1,45 @@
+// Ported from Scala.js commit: ad7d82f dated: 2020-10-05
+
 package java.util
 
+import java.util.function.UnaryOperator
+
+import scala.scalanative.annotation.JavaDefaultMethod
+
 trait List[E] extends Collection[E] {
+  @JavaDefaultMethod
+  def replaceAll(operator: UnaryOperator[E]): Unit = {
+    val iter = listIterator()
+    while (iter.hasNext())
+      iter.set(operator.apply(iter.next()))
+  }
+
+  @JavaDefaultMethod
+  def sort(c: Comparator[_ >: E]): Unit = {
+    val arrayBuf = toArray()
+    Arrays.sort[AnyRef with E](arrayBuf.asInstanceOf[Array[AnyRef with E]], c)
+
+    val len = arrayBuf.length
+
+    if (this.isInstanceOf[RandomAccess]) {
+      var i = 0
+      while (i != len) {
+        set(i, arrayBuf(i).asInstanceOf[E])
+        i += 1
+      }
+    } else {
+      var i    = 0
+      val iter = listIterator()
+      while (i != len) {
+        iter.next()
+        iter.set(arrayBuf(i).asInstanceOf[E])
+        i += 1
+      }
+    }
+  }
+
   def get(index: Int): E
   def set(index: Int, element: E): E
-  def add(element: E): Boolean
   def add(index: Int, element: E): Unit
   def remove(index: Int): E
   def indexOf(o: Any): Int
@@ -11,11 +47,5 @@ trait List[E] extends Collection[E] {
   def listIterator(): ListIterator[E]
   def listIterator(index: Int): ListIterator[E]
   def subList(fromIndex: Int, toIndex: Int): List[E]
-  def addAll(c: Collection[_ <: E]): Boolean
   def addAll(index: Int, c: Collection[_ <: E]): Boolean
-  def clear(): Unit
-  def isEmpty(): Boolean
-  def iterator(): Iterator[E]
-  def contains(o: Any): Boolean
-  def size(): Int
 }
