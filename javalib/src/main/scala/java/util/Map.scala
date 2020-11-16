85641d410cb96499798a103c4e302dc074321b81
Lee Tibbert <lee.tibbert@gmail.com>
2020-11-16 17:33:49 -0500

    Add required JavaDefaultMethod annotations

--- javalib/src/main/scala/java/util/Map.scala
+++ javalib/src/main/scala/java/util/Map.scala
@@ -1,12 +1,10 @@
-/*
- * Ported from Scala.js
- *   commit SHA1: f7be410
- *   dated: 2020-10-12
- */
+// Ported from Scala.js  commit f7be410 dated: 2020-10-07
 
 package java.util
 
 import java.util.function.{BiConsumer, BiFunction, Function}
+
+import scala.scalanative.annotation.JavaDefaultMethod
 
 import ScalaOps._
 
@@ -26,20 +24,24 @@ trait Map[K, V] {
   def equals(o: Any): Boolean
   def hashCode(): Int
 
+  @JavaDefaultMethod
   def getOrDefault(key: Any, defaultValue: V): V =
     if (containsKey(key)) get(key)
     else defaultValue
 
+  @JavaDefaultMethod
   def forEach(action: BiConsumer[_ >: K, _ >: V]): Unit = {
     for (entry <- entrySet().scalaOps)
       action.accept(entry.getKey(), entry.getValue())
   }
 
+  @JavaDefaultMethod
   def replaceAll(function: BiFunction[_ >: K, _ >: V, _ <: V]): Unit = {
     for (entry <- entrySet().scalaOps)
       entry.setValue(function.apply(entry.getKey(), entry.getValue()))
   }
 
+  @JavaDefaultMethod
   def putIfAbsent(key: K, value: V): V = {
     val prevValue = get(key)
     if (prevValue == null)
@@ -48,6 +50,7 @@ trait Map[K, V] {
       prevValue
   }
 
+  @JavaDefaultMethod
   def remove(key: Any, value: Any): Boolean = {
     if (containsKey(key) && Objects.equals(get(key), value)) {
       remove(key)
@@ -57,6 +60,7 @@ trait Map[K, V] {
     }
   }
 
+  @JavaDefaultMethod
   def replace(key: K, oldValue: V, newValue: V): Boolean = {
     if (containsKey(key) && Objects.equals(get(key), oldValue)) {
       put(key, newValue)
@@ -66,10 +70,12 @@ trait Map[K, V] {
     }
   }
 
+  @JavaDefaultMethod
   def replace(key: K, value: V): V =
     if (containsKey(key)) put(key, value)
     else null.asInstanceOf[V]
 
+  @JavaDefaultMethod
   def computeIfAbsent(key: K, mappingFunction: Function[_ >: K, _ <: V]): V = {
     val oldValue = get(key)
     if (oldValue != null) {
@@ -82,6 +88,7 @@ trait Map[K, V] {
     }
   }
 
+  @JavaDefaultMethod
   def computeIfPresent(
       key: K,
       remappingFunction: BiFunction[_ >: K, _ >: V, _ <: V]): V = {
@@ -95,6 +102,7 @@ trait Map[K, V] {
     }
   }
 
+  @JavaDefaultMethod
   def compute(key: K,
               remappingFunction: BiFunction[_ >: K, _ >: V, _ <: V]): V = {
     val oldValue = get(key)
@@ -116,6 +124,7 @@ trait Map[K, V] {
     newValue
   }
 
+  @JavaDefaultMethod
   def merge(key: K,
             value: V,
             remappingFunction: BiFunction[_ >: V, _ >: V, _ <: V]): V = {
