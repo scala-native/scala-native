c088ed754f7e933d4bc0fb2ff44290de3721c04f
wojciechmazur <wojciech.mazur95@gmail.com>
2020-11-16 11:52:37 +0100

    Use local registry to store images instead of using caching layers action

--- ci-docker/run-test-gha.sh
+++ ci-docker/run-test-gha.sh
@@ -6,19 +6,25 @@ if [ $# -eq 0 ]
   then echo "Expected single argument with docker image version"
   exit 1
 fi
-IMAGE_VERSION=$1
 
-sudo chmod 777 -R $HOME;
+IMAGE_NAME=$1
+FULL_IMAGE_NAME="localhost:5000/${IMAGE_NAME}"
+sudo chmod 777 -R "$HOME"
 
-docker run -i scala-native-testing/linux-$TARGET_DOCKER_PLATFORM:${IMAGE_VERSION} java -version
+docker run -d -p 5000:5000 \
+  --restart=always \
+  --name registry \
+  -v /tmp/docker-registry:/var/lib/registry \
+  registry:2 && \
+  npx wait-on tcp:5000
 
+docker run -i "${FULL_IMAGE_NAME}" java -version
 docker run -v $HOME/.ivy2:/home/scala-native/.ivy2 \
            -v $HOME/.sbt:/home/scala-native/.sbt \
            -v $PWD:/home/scala-native/scala-native \
-           -e SCALANATIVE_MODE=$SCALANATIVE_MODE \
-           -e SCALANATIVE_GC=$SCALANATIVE_GC \
-           -e SCALANATIVE_OPTIMIZE=$SCALANATIVE_OPTIMIZE \
-           -e TEST_COMMAND=$TEST_COMMAND \
-           -e SCALA_VERSION=$SCALA_VERSION \
-           -i scala-native-testing/linux-$TARGET_DOCKER_PLATFORM:${IMAGE_VERSION}
-
+           -e SCALANATIVE_MODE="$SCALANATIVE_MODE" \
+           -e SCALANATIVE_GC="$SCALANATIVE_GC" \
+           -e SCALANATIVE_OPTIMIZE="$SCALANATIVE_OPTIMIZE" \
+           -e TEST_COMMAND="$TEST_COMMAND" \
+           -e SCALA_VERSION="$SCALA_VERSION" \
+           -i "${FULL_IMAGE_NAME}"
