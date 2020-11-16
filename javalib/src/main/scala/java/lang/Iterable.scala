61beff020c0074178b80df7552a1361394270787
Lee Tibbert <lee.tibbert@gmail.com>
2020-11-16 12:28:31 -0500

    This PR builds upon merged PR #1937 by re-porting Iterable.scala in order to pick up the JavaDefaultMethod annotation.
    The corresponding test was ported from Scala.js by Lorenzo G (lolgab)
    as part of his PR #1994.  To keep the JavaDefaultMethod changes
    together, I am submitting it here by his kind permission. He did
    the port; I added a bit of audit trail information at the top.
    Thank you Lolgab.
    
    See Issue #1972 for background & discussion of JavaDefaultMethod annotation.
    
    This PR is WIP because it will fail to build until after PR #1997
    is merged and this PR re-based upon it.

--- javalib/src/main/scala/java/lang/Iterable.scala
+++ javalib/src/main/scala/java/lang/Iterable.scala
@@ -1,13 +1,16 @@
-// Influenced by and corresponds to Scala.js commit SHA: f9fc1ae
+// Ported from Scala.js commit: f9fc1a dated: 2020-03-06
 
 package java.lang
 
 import java.util.Iterator
 import java.util.function.Consumer
 
+import scala.scalanative.annotation.JavaDefaultMethod
+
 trait Iterable[T] {
   def iterator(): Iterator[T]
 
+  @JavaDefaultMethod
   def forEach(action: Consumer[_ >: T]): Unit = {
     val iter = iterator()
     while (iter.hasNext())
