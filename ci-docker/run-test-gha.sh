119a9af720b825b779c6c4fa302d27365c21aec8
wojciechmazur <wojciech.mazur95@gmail.com>
2020-11-16 11:13:13 +0100

    Share compiled sources in pipeline

--- ci-docker/run-test-gha.sh
+++ ci-docker/run-test-gha.sh
@@ -8,13 +8,13 @@ if [ $# -eq 0 ]
 fi
 IMAGE_VERSION=$1
 
-sudo chmod a+rwx -R $HOME;
+sudo chmod 777 -R $HOME;
 
 docker run -i scala-native-testing/linux-$TARGET_DOCKER_PLATFORM:${IMAGE_VERSION} java -version
 
-docker run -v $HOME/.ivy2:/home/scala-native/.ivy2:z \
-           -v $HOME/.sbt:/home/scala-native/.sbt:z \
-           -v $PWD:/home/scala-native/scala-native:z \
+docker run -v $HOME/.ivy2:/home/scala-native/.ivy2 \
+           -v $HOME/.sbt:/home/scala-native/.sbt \
+           -v $PWD:/home/scala-native/scala-native \
            -e SCALANATIVE_MODE=$SCALANATIVE_MODE \
            -e SCALANATIVE_GC=$SCALANATIVE_GC \
            -e SCALANATIVE_OPTIMIZE=$SCALANATIVE_OPTIMIZE \
