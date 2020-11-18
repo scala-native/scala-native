41d5a38699bb16af1af574517a2c4dedbdcbc1d8
Lee Tibbert <lee.tibbert@gmail.com>
2020-11-18 17:41:40 -0500

    Annotate & test j.u.f.BiConsumer default methods.
    This PR builds upon merged PR #1937 by annotating the existing
    j.u.f.BiConsumer.scala and porting its corresponding BiConsumerTest
    from Scala.js.
    
    The existing file was edited rather than porting the most current
    Scala.js commit because the latter uses lambdas and those
    do not play well when using Scala Native with Scala 2.11.
    
    See Issue #1972 for background & discussion of JavaDefaultMethod
    annotation.

--- javalib/src/main/scala/java/util/function/BiConsumer.scala
+++ javalib/src/main/scala/java/util/function/BiConsumer.scala
@@ -1,10 +1,16 @@
+// Corresponds to Scala.js commit: f86ed6 c2f5a43 dated: 2020-09-06
+// Design note: Do not use lambdas with Scala Native and Scala 2.11
+
 package java.util.function
+
+import scala.scalanative.annotation.JavaDefaultMethod
 
 trait BiConsumer[T, U] {
   self =>
 
   def accept(t: T, u: U): Unit
 
+  @JavaDefaultMethod
   def andThen(after: BiConsumer[T, U]): BiConsumer[T, U] =
     new BiConsumer[T, U]() {
       override def accept(t: T, u: U): Unit = {
@@ -12,5 +18,4 @@ trait BiConsumer[T, U] {
         after.accept(t, u)
       }
     }
-
 }
