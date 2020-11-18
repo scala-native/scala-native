cb549320b203cf48838470c8412f669835e7b6ee
LeeTibbert <LeeTibbert@users.noreply.github.com>
2020-11-18 18:55:08 -0500

    Revert "Annotate & test j.l.Iterable default methods (#2009)"
    This reverts commit a0a68d19d65f82b7857ac51ffb831a111fac2f39.

--- javalib/src/main/scala/java/lang/Iterable.scala
+++ javalib/src/main/scala/java/lang/Iterable.scala
@@ -1,16 +1,13 @@
-// Ported from Scala.js commit: f9fc1a dated: 2020-03-06
+// Influenced by and corresponds to Scala.js commit SHA: f9fc1ae
 
 package java.lang
 
 import java.util.Iterator
 import java.util.function.Consumer
 
-import scala.scalanative.annotation.JavaDefaultMethod
-
 trait Iterable[T] {
   def iterator(): Iterator[T]
 
-  @JavaDefaultMethod
   def forEach(action: Consumer[_ >: T]): Unit = {
     val iter = iterator()
     while (iter.hasNext())
