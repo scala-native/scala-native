ca464d592e9b48636cde5b321cd69ccce5b975ce
Lee Tibbert <lee.tibbert@gmail.com>
2020-12-01 18:35:14 -0500

    New file TrivialImmutableMap.scala needs to be in the mix.

---
+++ unit-tests/src/test/scala/java/util/TrivialImmutableMap.scala
@@ -0,0 +1,39 @@
+// Ported from Scala.js commit: 6819668 on 2020-10-O7
+
+package org.scalanative.testsuite.javalib.util
+
+import java.{util => ju}
+import java.util.Map.Entry
+
+final class TrivialImmutableMap[K, V] private (contents: List[Entry[K, V]])
+    extends ju.AbstractMap[K, V] {
+
+  def entrySet(): ju.Set[Entry[K, V]] = {
+    new ju.AbstractSet[Entry[K, V]] {
+      def size(): Int = contents.size
+
+      def iterator(): ju.Iterator[Entry[K, V]] = {
+        new ju.Iterator[Entry[K, V]] {
+          private var remaining: List[Entry[K, V]] = contents
+
+          def hasNext(): Boolean = remaining.nonEmpty
+
+          def next(): Entry[K, V] = {
+            val head = remaining.head
+            remaining = remaining.tail
+            head
+          }
+        }
+      }
+    }
+  }
+}
+
+object TrivialImmutableMap {
+  def apply[K, V](contents: List[Entry[K, V]]): TrivialImmutableMap[K, V] =
+    new TrivialImmutableMap(contents)
+
+  def apply[K, V](contents: (K, V)*): TrivialImmutableMap[K, V] =
+    apply(contents.toList.map(kv =>
+      new ju.AbstractMap.SimpleImmutableEntry(kv._1, kv._2)))
+}
