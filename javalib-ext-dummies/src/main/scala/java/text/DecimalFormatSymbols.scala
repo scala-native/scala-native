592cdd2dfb02de98321dbff7f7c76f23676cc3a3
Wojciech Mazur <wojciech.mazur95@gmail.com>
2020-12-16 16:45:59 +0100

    Port java.text from Scala.js and remove no longer needed parts

--- javalib-ext-dummies/src/main/scala/java/text/DecimalFormatSymbols.scala
+++ javalib-ext-dummies/src/main/scala/java/text/DecimalFormatSymbols.scala
@@ -3,9 +3,37 @@ package java.text
 import java.util.Locale
 
 class DecimalFormatSymbols(locale: Locale) {
-  // basic implementation for java.util.Formatter with Locale.US only
-  def getDecimalSeparator(): Char  = '.'
-  def getGroupingSeparator(): Char = ','
-  def getMinusSign(): Char         = '-'
-  def getZeroDigit(): Char         = '0'
+  def getZeroDigit(): Char = {
+    val ext = locale.getExtension('u')
+    if (ext != null && ext.contains("nu-deva"))
+      '\u0966' // '०' DEVANAGARI DIGIT ZERO
+    else
+      '0'
+  }
+
+  def getGroupingSeparator(): Char = {
+    locale.getLanguage() match {
+      case "fr"             => '\u00A0' // NO-BREAK SPACE
+      case "" | "en" | "hi" => ','
+      case _                => unsupported()
+    }
+  }
+
+  def getDecimalSeparator(): Char = {
+    locale.getLanguage() match {
+      case "fr"             => ','
+      case "" | "en" | "hi" => '.'
+      case _                => unsupported()
+    }
+  }
+
+  def getMinusSign(): Char = '-'
+
+  private def unsupported(): Nothing =
+    throw new Error(s"Unsupported locale '$locale' in DecimalFormatSymbols")
+}
+
+object DecimalFormatSymbols {
+  def getInstance(locale: Locale) =
+    new DecimalFormatSymbols(locale)
 }
--- javalib-ext-dummies/src/main/scala/java/text/Format.scala
+++ javalib-ext-dummies/src/main/scala/java/text/Format.scala
@@ -1,14 +1,6 @@
 package java.text
 
-abstract class Format {
-  final def format(obj: Object): String =
-    format(obj, new StringBuffer(), new FieldPosition(0)).toString()
-
-  def format(obj: Object,
-             toAppendTo: StringBuffer,
-             pos: FieldPosition): StringBuffer
-}
-
-object Format {
-  class Field protected (name: String)
-}
+abstract class Format protected ()
+    extends AnyRef
+    with java.io.Serializable
+    with java.lang.Cloneable
