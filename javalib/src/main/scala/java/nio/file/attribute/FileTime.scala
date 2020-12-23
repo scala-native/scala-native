1f59b807346f97e6a665b810d3354cc17b30dea2
Wojciech Mazur <wojciech.mazur95@gmail.com>
2020-12-23 12:38:17 +0100

    Remove usage of Instant form FileTime

--- javalib/src/main/scala/java/nio/file/attribute/FileTime.scala
+++ javalib/src/main/scala/java/nio/file/attribute/FileTime.scala
@@ -3,37 +3,43 @@ package java.nio.file.attribute
 import java.util.concurrent.TimeUnit
 import java.time.Instant
 
-final class FileTime private (instant: Instant) extends Comparable[FileTime] {
-  def compareTo(other: FileTime) =
-    instant.compareTo(other.toInstant())
+final class FileTime private (private val epochMillis: Long,
+                              private val nanos: Int)
+    extends Comparable[FileTime] {
+
+  def compareTo(other: FileTime) = {
+    val compareMillis = epochMillis.compareTo(other.epochMillis)
+    if (compareMillis == 0) {
+      nanos.compareTo(other.nanos)
+    } else compareMillis
+  }
 
   override def equals(obj: Any): Boolean =
     obj match {
-      case other: FileTime => instant == other.toInstant()
+      case other: FileTime => compareTo(other) == 0
       case _               => false
     }
 
   override def hashCode(): Int =
-    instant.hashCode
+    epochMillis.## + nanos.##
 
   def to(unit: TimeUnit): Long =
     unit.convert(toMillis(), TimeUnit.MILLISECONDS)
 
-  def toMillis(): Long =
-    instant.toEpochMilli()
+  def toMillis(): Long = epochMillis
 
-  def toInstant(): Instant =
-    instant
+  def toInstant(): Instant = Instant.ofEpochMilli(epochMillis).plusNanos(nanos)
 
-  override def toString(): String =
-    instant.toString
+  override def toString(): String = s"FileTime($epochMillis, $nanos)"
 }
 
 object FileTime {
   def from(value: Long, unit: TimeUnit): FileTime =
     fromMillis(TimeUnit.MILLISECONDS.convert(value, unit))
+
   def fromMillis(value: Long): FileTime =
-    from(Instant.ofEpochMilli(value))
+    new FileTime(value, 0)
+
   def from(instant: Instant): FileTime =
-    new FileTime(instant)
+    new FileTime(instant.toEpochMilli, instant.getNano)
 }
