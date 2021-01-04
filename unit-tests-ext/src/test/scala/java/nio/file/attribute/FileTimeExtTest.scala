fd7444c6d9cb755a3b7d61f8a6e39ef74e626ea3
Wojciech Mazur <wojciech.mazur95@gmail.com>
2021-01-04 18:23:06 +0100

    Extend FileTime.fromInstant tests to check big Instant values overflows

--- unit-tests-ext/src/test/scala/java/nio/file/attribute/FileTimeExtTest.scala
+++ unit-tests-ext/src/test/scala/java/nio/file/attribute/FileTimeExtTest.scala
@@ -35,6 +35,60 @@ class FileTimeExtTest {
       Instant.ofEpochSecond(timestampAsSeconds, nanosAdjustment))
   }
 
+  @Test def fromInstantMaxPrecision(): Unit = {
+    val maxSecondsWithNanos  = Long.MaxValue / 1e9.toLong - 1L
+    val naxSecondsWithMicros = Long.MaxValue / 1e6.toLong - 1L
+    val maxSecondsWithMillis = Long.MaxValue / 1e3.toLong - 1L
+
+    def toUnit(seconds: Long, nanos: Long, unit: TimeUnit) = {
+      val fromSeconds = unit.convert(seconds, TimeUnit.SECONDS)
+      val fromNanos   = unit.convert(nanos, TimeUnit.NANOSECONDS)
+      fromSeconds + fromNanos
+    }
+
+    def fromSeconds(seconds: Long, unit: TimeUnit) =
+      FileTime.from(toUnit(seconds, Instant.MAX.getNano, unit), unit)
+
+    def testPrecision(seconds: Long,
+                      expectedPrecision: TimeUnit,
+                      lowerPrecision: TimeUnit) = {
+      val overflowSeconds = seconds + 1L
+
+      val fileTimeMax = FileTime.from(
+        Instant.ofEpochSecond(seconds, Instant.MAX.getNano)
+      )
+      val fileTimeOverflow = FileTime.from(
+        Instant.ofEpochSecond(overflowSeconds, Instant.MAX.getNano)
+      )
+
+      val expectedMax   = fromSeconds(seconds, expectedPrecision)
+      val overflowValue = fromSeconds(overflowSeconds, lowerPrecision)
+
+      assertEquals(expectedMax, fileTimeMax)
+      assertEquals(overflowValue, fileTimeOverflow)
+
+      assertNotEquals(Long.MaxValue, fileTimeMax.to(expectedPrecision))
+      if (expectedPrecision == TimeUnit.MILLISECONDS) {
+        // in this case it does not overflow yet (it's exactly Long.MaxValue - 807L), but any greater precision would cause overflow
+        assertNotEquals(Long.MaxValue,
+                        fileTimeOverflow.to(TimeUnit.MILLISECONDS))
+        assertEquals(Long.MaxValue, fileTimeOverflow.to(TimeUnit.MICROSECONDS))
+      } else {
+        assertEquals(Long.MaxValue, fileTimeOverflow.to(expectedPrecision))
+      }
+    }
+
+    testPrecision(maxSecondsWithNanos,
+                  TimeUnit.NANOSECONDS,
+                  TimeUnit.MICROSECONDS)
+
+    testPrecision(naxSecondsWithMicros,
+                  TimeUnit.MICROSECONDS,
+                  TimeUnit.MILLISECONDS)
+
+    testPrecision(maxSecondsWithMillis, TimeUnit.MILLISECONDS, TimeUnit.SECONDS)
+
+  }
   @Test def toInstant(): Unit = {
     val fromNanos =
       FileTime.from(timestampAsNanos + nanosAdjustment, TimeUnit.NANOSECONDS)
