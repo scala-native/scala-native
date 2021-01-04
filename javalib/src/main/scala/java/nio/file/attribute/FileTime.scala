a3e18acc35294a27789924f2612164b09a645dc1
Wojciech Mazur <wojciech.mazur95@gmail.com>
2021-01-04 18:28:54 +0100

    Adjust FileTime to handle large Instant values

--- javalib/src/main/scala/java/nio/file/attribute/FileTime.scala
+++ javalib/src/main/scala/java/nio/file/attribute/FileTime.scala
@@ -6,6 +6,7 @@ import java.time.Instant
 final class FileTime private (private val value: Long,
                               private val unit: TimeUnit)
     extends Comparable[FileTime] {
+  import java.nio.file.attribute.FileTime.NanosToSecond
 
   @inline
   private def compareTo(other: FileTime, unit: TimeUnit) =
@@ -37,22 +38,39 @@ final class FileTime private (private val value: Long,
   def toInstant(): Instant = {
     val asNanos = to(TimeUnit.NANOSECONDS)
     val seconds = to(TimeUnit.SECONDS)
-    Instant.ofEpochSecond(
-      seconds,
-      asNanos - TimeUnit.NANOSECONDS.convert(seconds, TimeUnit.SECONDS))
+    Instant.ofEpochSecond(seconds, Math.floorMod(asNanos, NanosToSecond))
   }
 
   override def toString(): String = s"FileTime($value, $unit)"
 }
 
 object FileTime {
+  private final val NanosToSecond        = 1000000000L
+  private final val MicrosToSecond       = 1000000L
+  private final val MillisToSecond       = 1000L
+  private final val MaxSecondsWithMillis = Long.MaxValue / MillisToSecond - 1L
+  private final val MaxSecondsWithMicros = Long.MaxValue / MicrosToSecond - 1L
+  private final val MaxSecondsWithNanos  = Long.MaxValue / NanosToSecond - 1L
+
   def from(value: Long, unit: TimeUnit): FileTime = new FileTime(value, unit)
 
   def fromMillis(value: Long): FileTime = from(value, TimeUnit.MILLISECONDS)
 
   def from(instant: Instant): FileTime = {
-    val secondsAsNanos =
-      TimeUnit.NANOSECONDS.convert(instant.getEpochSecond, TimeUnit.SECONDS)
-    new FileTime(secondsAsNanos + instant.getNano, TimeUnit.NANOSECONDS)
+    val s = instant.getEpochSecond
+
+    /* Direct conversion of instant seconds to nanoseconds with their nanos adjustment might cause overflow.
+     * To handle such cases precision of TimeFile is limited for large values
+     */
+    def withPrecision(unit: TimeUnit) = {
+      val adjustment  = unit.convert(instant.getNano, TimeUnit.NANOSECONDS)
+      val fromSeconds = unit.convert(s, TimeUnit.SECONDS)
+      new FileTime(fromSeconds + adjustment, unit)
+    }
+
+    if (s <= MaxSecondsWithNanos) withPrecision(TimeUnit.NANOSECONDS)
+    else if (s <= MaxSecondsWithMicros) withPrecision(TimeUnit.MICROSECONDS)
+    else if (s <= MaxSecondsWithMillis) withPrecision(TimeUnit.MILLISECONDS)
+    else withPrecision(TimeUnit.SECONDS)
   }
 }
