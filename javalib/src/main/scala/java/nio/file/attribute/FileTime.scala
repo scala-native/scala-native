e705838a95f7f690da29958b279dd078c4a8adf8
Wojciech Mazur <wojciech.mazur95@gmail.com>
2021-01-05 16:41:14 +0100

    Change implementation of FileTime. Store days and dayNanos to support maximal range of values

--- javalib/src/main/scala/java/nio/file/attribute/FileTime.scala
+++ javalib/src/main/scala/java/nio/file/attribute/FileTime.scala
@@ -3,23 +3,15 @@ package java.nio.file.attribute
 import java.util.concurrent.TimeUnit
 import java.time.Instant
 
-final class FileTime private (private val value: Long,
-                              private val unit: TimeUnit)
+final class FileTime private (private val epochDays: Long,
+                              private val dayNanos: Long)
     extends Comparable[FileTime] {
   import java.nio.file.attribute.FileTime.NanosToSecond
 
-  @inline
-  private def compareTo(other: FileTime, unit: TimeUnit) =
-    this.to(unit).compareTo(other.to(unit))
-
   def compareTo(other: FileTime): Int = {
-    if (this.unit == other.unit) {
-      this.value.compareTo(other.value)
-    } else {
-      val daysComp = compareTo(other, TimeUnit.DAYS)
-      if (daysComp != 0) daysComp
-      else compareTo(other, TimeUnit.NANOSECONDS)
-    }
+    val daysComp = epochDays.compareTo(other.epochDays)
+    if (daysComp == 0) dayNanos.compareTo(other.dayNanos)
+    else daysComp
   }
 
   override def equals(obj: Any): Boolean =
@@ -29,48 +21,47 @@ final class FileTime private (private val value: Long,
     }
 
   override def hashCode(): Int =
-    value.## + unit.##
+    epochDays.## + dayNanos.##
 
-  def to(unit: TimeUnit): Long = unit.convert(value, this.unit)
+  def to(unit: TimeUnit): Long = {
+    val fromDays  = unit.convert(epochDays, TimeUnit.DAYS)
+    val fromNanos = unit.convert(dayNanos, TimeUnit.NANOSECONDS)
+    try {
+      Math.addExact(fromDays, fromNanos)
+    } catch {
+      // In case of overflow return Long.MaxValue if positive or else Long.MinValue
+      case _: ArithmeticException =>
+        if (fromDays > 0) Long.MaxValue
+        else Long.MinValue
+    }
+  }
 
   def toMillis(): Long = to(TimeUnit.MILLISECONDS)
 
   def toInstant(): Instant = {
-    val asNanos = to(TimeUnit.NANOSECONDS)
     val seconds = to(TimeUnit.SECONDS)
-    Instant.ofEpochSecond(seconds, Math.floorMod(asNanos, NanosToSecond))
+    Instant.ofEpochSecond(seconds, Math.floorMod(dayNanos, NanosToSecond))
   }
 
-  override def toString(): String = s"FileTime($value, $unit)"
+  override def toString(): String = s"FileTime($epochDays, $dayNanos)"
 }
 
 object FileTime {
-  private final val NanosToSecond        = 1000000000L
-  private final val MicrosToSecond       = 1000000L
-  private final val MillisToSecond       = 1000L
-  private final val MaxSecondsWithMillis = Long.MaxValue / MillisToSecond - 1L
-  private final val MaxSecondsWithMicros = Long.MaxValue / MicrosToSecond - 1L
-  private final val MaxSecondsWithNanos  = Long.MaxValue / NanosToSecond - 1L
+  private final val SecondsInDay  = 86400L
+  private final val NanosInDay    = 86400000000000L
+  private final val NanosToSecond = 1000000000L
 
-  def from(value: Long, unit: TimeUnit): FileTime = new FileTime(value, unit)
+  def from(value: Long, unit: TimeUnit): FileTime =
+    new FileTime(unit.toDays(value),
+                 Math.floorMod(unit.toNanos(value), NanosInDay))
 
   def fromMillis(value: Long): FileTime = from(value, TimeUnit.MILLISECONDS)
 
   def from(instant: Instant): FileTime = {
-    val s = instant.getEpochSecond
-
-    /* Direct conversion of instant seconds to nanoseconds with their nanos adjustment might cause overflow.
-     * To handle such cases precision of TimeFile is limited for large values
-     */
-    def withPrecision(unit: TimeUnit) = {
-      val adjustment  = unit.convert(instant.getNano, TimeUnit.NANOSECONDS)
-      val fromSeconds = unit.convert(s, TimeUnit.SECONDS)
-      new FileTime(fromSeconds + adjustment, unit)
-    }
-
-    if (s <= MaxSecondsWithNanos) withPrecision(TimeUnit.NANOSECONDS)
-    else if (s <= MaxSecondsWithMicros) withPrecision(TimeUnit.MICROSECONDS)
-    else if (s <= MaxSecondsWithMillis) withPrecision(TimeUnit.MILLISECONDS)
-    else withPrecision(TimeUnit.SECONDS)
+    val s          = instant.getEpochSecond
+    val daySeconds = Math.floorMod(s, SecondsInDay)
+    val days       = TimeUnit.SECONDS.toDays(s)
+    val dayNanos   = TimeUnit.SECONDS.toNanos(daySeconds) + instant.getNano
+    new FileTime(days, dayNanos)
   }
 }
