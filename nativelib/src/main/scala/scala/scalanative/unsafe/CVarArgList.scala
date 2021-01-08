33ab3252b94b88c6006a675a737f7968d5653163
Shadaj Laddad <shadaj@users.noreply.github.com>
2021-01-07 16:43:23 -0800

    Implement basic support for varargs

--- nativelib/src/main/scala/scala/scalanative/unsafe/CVarArgList.scala
+++ nativelib/src/main/scala/scala/scalanative/unsafe/CVarArgList.scala
@@ -37,81 +37,120 @@ object CVarArgList {
   /** Construct C-style vararg list from Scala sequence. */
   private[scalanative] def fromSeq(varargs: Seq[CVarArg])(
       implicit z: Zone): CVarArgList = {
-    var storage         = new Array[Long](registerSaveWords)
-    var wordsUsed       = storage.size
-    var gpRegistersUsed = 0
-    var fpRegistersUsed = 0
+    if (sizeof[Word].toInt == 8) {
+      var storage         = new Array[Long](registerSaveWords)
+      var wordsUsed       = storage.size
+      var gpRegistersUsed = 0
+      var fpRegistersUsed = 0
 
-    def isPassedAsDouble(vararg: CVarArg): Boolean =
-      vararg.tag == Tag.Float || vararg.tag == Tag.Double
+      def isPassedAsDouble(vararg: CVarArg): Boolean =
+        vararg.tag == Tag.Float || vararg.tag == Tag.Double
 
-    def encode[T](value: T)(implicit tag: Tag[T]): Array[Long] = value match {
-      case value: Byte =>
-        encode(value.toLong)
-      case value: Short =>
-        encode(value.toLong)
-      case value: Int =>
-        encode(value.toLong)
-      case value: UByte =>
-        encode(value.toULong)
-      case value: UShort =>
-        encode(value.toULong)
-      case value: UInt =>
-        encode(value.toULong)
-      case value: Float =>
-        encode(value.toDouble)
-      case _ =>
-        val count =
-          ((sizeof(tag) + sizeof[Long] - 1) / sizeof[Long]).toInt
-        val words = new Array[Long](count)
-        val start = words.asInstanceOf[LongArray].at(0).asInstanceOf[Ptr[T]]
-        tag.store(start, value)
-        words
-    }
-
-    def append(word: Long): Unit = {
-      if (wordsUsed == storage.size) {
-        val newstorage = new Array[Long](storage.size * 2)
-        System.arraycopy(storage, 0, newstorage, 0, storage.size)
-        storage = newstorage
+      def encode[T](value: T)(implicit tag: Tag[T]): Array[Long] = value match {
+        case value: Byte =>
+          encode(value.toLong)
+        case value: Short =>
+          encode(value.toLong)
+        case value: Int =>
+          encode(value.toLong)
+        case value: UByte =>
+          encode(value.toULong)
+        case value: UShort =>
+          encode(value.toULong)
+        case value: UInt =>
+          encode(value.toULong)
+        case value: Float =>
+          encode(value.toDouble)
+        case _ =>
+          val count =
+            ((sizeof(tag) + sizeof[Long] - 1) / sizeof[Long]).toInt
+          val words = new Array[Long](count)
+          val start = words.asInstanceOf[LongArray].at(0).asInstanceOf[Ptr[T]]
+          tag.store(start, value)
+          words
       }
-      storage(wordsUsed) = word
-      wordsUsed += 1
-    }
 
-    varargs.foreach { vararg =>
-      val encoded  = encode(vararg.value)(vararg.tag)
-      val isDouble = isPassedAsDouble(vararg)
-
-      if (isDouble && fpRegistersUsed < countFPRegisters) {
-        var startIndex = countGPRegisters + (fpRegistersUsed * fpRegisterWords)
-        encoded.foreach { w =>
-          storage(startIndex) = w
-          startIndex += 1
+      def append(word: Long): Unit = {
+        if (wordsUsed == storage.size) {
+          val newstorage = new Array[Long](storage.size * 2)
+          System.arraycopy(storage, 0, newstorage, 0, storage.size)
+          storage = newstorage
         }
-        fpRegistersUsed += 1
-      } else if (encoded.size == 1 && !isDouble && gpRegistersUsed < countGPRegisters) {
-        val startIndex = gpRegistersUsed
-        storage(startIndex) = encoded(0)
-        gpRegistersUsed += 1
-      } else {
-        encoded.foreach(append)
+        storage(wordsUsed) = word
+        wordsUsed += 1
       }
+
+      varargs.foreach { vararg =>
+        val encoded  = encode(vararg.value)(vararg.tag)
+        val isDouble = isPassedAsDouble(vararg)
+
+        if (isDouble && fpRegistersUsed < countFPRegisters) {
+          var startIndex =
+            countGPRegisters + (fpRegistersUsed * fpRegisterWords)
+          encoded.foreach { w =>
+            storage(startIndex) = w
+            startIndex += 1
+          }
+          fpRegistersUsed += 1
+        } else if (encoded.size == 1 && !isDouble && gpRegistersUsed < countGPRegisters) {
+          val startIndex = gpRegistersUsed
+          storage(startIndex) = encoded(0)
+          gpRegistersUsed += 1
+        } else {
+          encoded.foreach(append)
+        }
+      }
+
+      val resultStorage =
+        z.alloc(sizeof[Long] * storage.size).asInstanceOf[Ptr[Long]]
+      val storageStart = storage.asInstanceOf[LongArray].at(0)
+      libc.memcpy(toRawPtr(resultStorage),
+                  toRawPtr(storageStart),
+                  wordsUsed.toUWord * sizeof[Long])
+
+      val resultHeader = z.alloc(sizeof[Header]).asInstanceOf[Ptr[Header]]
+      resultHeader.gpOffset = 0.toUInt
+      resultHeader.fpOffset = (countGPRegisters.toUWord * sizeof[Long]).toUInt
+      resultHeader.regSaveArea = resultStorage
+      resultHeader.overflowArgArea = resultStorage + registerSaveWords.toUWord
+
+      new CVarArgList(toRawPtr(resultHeader))
+    } else {
+      val resizedArgs = varargs.map { arg =>
+        arg.value match {
+          case value: Byte =>
+            value.toInt: CVarArg
+          case value: Short =>
+            value.toInt: CVarArg
+          case value: Long =>
+            value.toInt: CVarArg
+          case value: UByte =>
+            value.toUInt: CVarArg
+          case value: UShort =>
+            value.toUInt: CVarArg
+          case value: ULong =>
+            value.toUInt: CVarArg
+          case value: Float =>
+            value.toDouble: CVarArg
+          case o => arg
+        }
+      }
+
+      var totalSize = 0.toUWord
+      resizedArgs.foreach { vararg =>
+        totalSize = Tag.align(totalSize, vararg.tag.alignment) + vararg.tag.size
+      }
+
+      val argListStorage = z.alloc(totalSize).asInstanceOf[Ptr[Byte]]
+      var currentIndex   = 0.toUWord
+      resizedArgs.foreach { vararg =>
+        currentIndex = Tag.align(currentIndex, vararg.tag.alignment)
+        vararg.tag.store((argListStorage + currentIndex).asInstanceOf[Ptr[Any]],
+                         vararg.value)
+        currentIndex += vararg.tag.size
+      }
+
+      new CVarArgList(toRawPtr(argListStorage))
     }
-
-    val resultStorage =
-      z.alloc(sizeof[Long] * storage.size).asInstanceOf[Ptr[Long]]
-    val storageStart = storage.asInstanceOf[LongArray].at(0)
-    libc.memcpy(toRawPtr(resultStorage),
-                toRawPtr(storageStart),
-                wordsUsed.toUWord * sizeof[Long])
-
-    val resultHeader = z.alloc(sizeof[Header]).asInstanceOf[Ptr[Header]]
-    resultHeader.gpOffset = 0.toUInt
-    resultHeader.fpOffset = (countGPRegisters.toUWord * sizeof[Long]).toUInt
-    resultHeader.regSaveArea = resultStorage
-    resultHeader.overflowArgArea = resultStorage + registerSaveWords.toUWord
-
-    new CVarArgList(toRawPtr(resultHeader))
   }
 }
