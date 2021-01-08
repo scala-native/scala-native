cdac9a80c3b87dadd3180b98476a6aab742b381d
Shadaj Laddad <shadaj@users.noreply.github.com>
2021-01-07 21:54:37 -0800

    Correct bug in time struct

--- posixlib/src/main/scala/scala/scalanative/posix/time.scala
+++ posixlib/src/main/scala/scala/scalanative/posix/time.scala
@@ -10,7 +10,10 @@ object time {
   type time_t   = types.time_t
   type clock_t  = types.clock_t
   type timespec = CStruct2[time_t, CLong]
-  type tm       = CStruct9[CInt, CInt, CInt, CInt, CInt, CInt, CInt, CInt, CInt]
+  type tm       = CStruct11[
+    CInt, CInt, CInt, CInt, CInt, CInt, CInt, CInt, CInt,
+    CLong, Ptr[Byte] // additional glibc fields
+  ]
 
   @name("scalanative_asctime")
   def asctime(time_ptr: Ptr[tm]): CString = extern
