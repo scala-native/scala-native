b9b83c63323fb42e96d243a04bebc8a65f30669b
Sébastien Doeraene <sjrdoeraene@gmail.com>
2021-01-19 17:26:14 +0100

    Towards 0.4.1. (#2120)

--- nir/src/main/scala/scala/scalanative/nir/Versions.scala
+++ nir/src/main/scala/scala/scalanative/nir/Versions.scala
@@ -25,5 +25,5 @@ object Versions {
   final val revision: Int = 8 // a.k.a. MINOR version
 
   /* Current public release version of Scala Native. */
-  final val current: String = "0.4.0"
+  final val current: String = "0.4.1-SNAPSHOT"
 }
