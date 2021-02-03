bd38f896620450a7f3156f16c270f6f3c50dc75d
Wojciech Mazur <wojciech.mazur95@gmail.com>
2021-02-03 13:01:05 +0100

    Rename SharedLibrary to LibraryDynamic

--- tools/src/main/scala/scala/scalanative/build/BuildTarget.scala
+++ tools/src/main/scala/scala/scalanative/build/BuildTarget.scala
@@ -3,14 +3,14 @@ package scala.scalanative.build
 sealed trait BuildTarget
 
 object BuildTarget {
-  private[scalanative] case object Application   extends BuildTarget
-  private[scalanative] case object SharedLibrary extends BuildTarget
+  private[scalanative] case object Application    extends BuildTarget
+  private[scalanative] case object LibraryDynamic extends BuildTarget
 
   /** Link code as application */
   def application: BuildTarget = Application
 
-  /** Link code as shared library */
-  def sharedLibrary: BuildTarget = SharedLibrary
+  /** Link code as shared/dynamic library */
+  def libraryDynamic: BuildTarget = LibraryDynamic
 
   /** The default build target. */
   def default: BuildTarget = application
