e613224065029f7852e97620060be0a7afb2da8a
Wojciech Mazur <wojciech.mazur95@gmail.com>
2021-02-03 17:19:56 +0100

    Formatting fixes

--- scripted-tests/run/build-library/src/main/c/libtest.h
+++ scripted-tests/run/build-library/src/main/c/libtest.h
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 
 struct Foo {
-	short arg1;
-	int arg2;
-	long arg3;
-	double arg4;
-	char* arg5;
+    short arg1;
+    int arg2;
+    long arg3;
+    double arg4;
+    char *arg5;
 };
 
-int ScalaNativeInit(); // needs to be called before first SN heap allocation (GC)
+int ScalaNativeInit(); // needs to be called before first SN heap allocation
+                       // (GC)
 void sayHello(void);
 long addLongs(long l, long r);
-struct Foo* retStructPtr(void);
-void updateStruct(struct Foo* p);
+struct Foo *retStructPtr(void);
+void updateStruct(struct Foo *p);
 void sn_runGC(void);
