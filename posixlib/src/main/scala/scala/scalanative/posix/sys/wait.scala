8304a35d0f6f6acccc59816c0d80ce4649d368b2
Mark Hammons <mark.hammons@the-ica.com>
2021-02-13 01:44:27 +0100

    Clangfmt fails on file unrelated to me?

--- posixlib/src/main/scala/scala/scalanative/posix/sys/wait.scala
+++ posixlib/src/main/scala/scala/scalanative/posix/sys/wait.scala
@@ -1,3 +1,4 @@
+//POSIX.1-2017: https://pubs.opengroup.org/onlinepubs/9699919799/
 package scala.scalanative.posix.sys
 
 import scala.scalanative.unsafe._
@@ -5,6 +6,7 @@ import scala.scalanative.posix.sys.types.pid_t
 
 @extern
 object wait {
+  //XSI
   @name("scala_native_wait_WCONTINUED")
   val WCONTINUED: CInt = extern
 
@@ -32,6 +34,7 @@ object wait {
   @name("scala_native_wait_WSTOPSIG")
   def WSTOPSIG(statVal: CInt): CInt = extern
 
+  //XSI
   @name("scala_native_wait_WIFCONTINUED")
   def WIFCONTINUED(statVal: CInt): CInt = extern
 
--- unit-tests/src/test/scala/scala/scalanative/posix/sys/WaitTest.scala
+++ unit-tests/src/test/scala/scala/scalanative/posix/sys/WaitTest.scala
@@ -6,6 +6,8 @@ import scala.scalanative.unsafe._
 
 import org.junit.Test
 import org.junit.Assert._
+import scala.scalanative.libc.errno.{errno, errno_=}
+import scala.scalanative.libc.string.strerror
 
 class WaitTest {
   @Test def waitWaitsChildExit(): Unit = {
@@ -13,7 +15,7 @@ class WaitTest {
 
     def blackHole(a: Any): Unit = ()
 
-    /** these are just here to test the bindings compile and link **/
+    // these are just here to test the bindings compile and link
     blackHole(WCONTINUED)
     blackHole(WNOHANG)
     blackHole(WUNTRACED)
@@ -26,24 +28,34 @@ class WaitTest {
 
     //done
 
+    errno = 0
     val pid = vfork()
     if (pid == 0) {
       sys.exit(0)
-    } else if (pid > 0) {
-      val childPid = waitpid(statLoc)
+    } else if (pid < 0) {
+      val err = errno
+      val str = fromCString(strerror(err))
+      assert(false, s"forking failed: $err - $str")
+
+    } else {
+      val childPid = wait(statLoc)
 
       assert(
         pid == childPid,
         "child that was observed exiting was not the child spawned by fork")
       assert(WIFEXITED(!statLoc) == 1, "child exited abnormally")
-    } else {
-      assert(false, "forking failed")
+
     }
 
+    errno = 0
     val pid2 = vfork()
     if (pid2 == 0) {
       sys.exit(1)
-    } else if (pid > 0) {
+    } else if (pid < 0) {
+      val err = errno
+      val str = fromCString(strerror(err))
+      assert(false, s"forking failed: $err - $str")
+    } else {
       val childPid = waitpid(pid2, statLoc, 0)
 
       assert(
@@ -51,8 +63,6 @@ class WaitTest {
         "child that was observed exiting was not the child spawned by fork")
       assert(WIFEXITED(!statLoc) == 1, "child exited abnormally")
       assert(WEXITSTATUS(!statLoc) == 1, "status doesn't match expected status")
-    } else {
-      assert(false, "forking failed")
     }
   }
 }
