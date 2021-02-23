92162548c47a77b323c49a5c7622a31bdaa6ac2e
Wojciech Mazur <wojciech.mazur95@gmail.com>
2021-02-23 18:33:10 +0100

    Add Scala 2.13.5 to cross-build matrix

--- project/ScalaVersions.scala
+++ project/ScalaVersions.scala
@@ -1,11 +1,15 @@
 package build
 
 object ScalaVersions {
+  val crossScala213 = Seq("2.13.4", "2.13.5")
+
   val scala211: String = "2.11.12"
   val scala212: String = "2.12.13"
-  val scala213: String = "2.13.4"
+  val scala213: String = crossScala213.last
 
-  val sbt10Version: String               = "1.1.6" // minimum version
-  val sbt10ScalaVersion: String          = scala212
-  val libCrossScalaVersions: Seq[String] = Seq(scala211, scala212, scala213)
+  val sbt10Version: String      = "1.1.6" // minimum version
+  val sbt10ScalaVersion: String = scala212
+
+  val libCrossScalaVersions: Seq[String] =
+    Seq(scala211, scala212) ++ crossScala213
 }
