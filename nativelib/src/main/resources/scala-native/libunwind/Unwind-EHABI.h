289573235130ed3798a4fa30528aa03e6bd65985
ekrich <ekrichardson@gmail.com>
2021-03-14 16:35:11 -0700

    Fix to allow 32bit branch to compile and run on Raspberry PI

--- nativelib/src/main/resources/scala-native/libunwind/Unwind-EHABI.h
+++ nativelib/src/main/resources/scala-native/libunwind/Unwind-EHABI.h
@@ -16,7 +16,9 @@
 #if defined(_LIBUNWIND_ARM_EHABI)
 
 #include <stdint.h>
-#include <unwind.h>
+// #include <unwind.h>
+// Change from original, point to local header file
+#include "include-libunwind/unwind.h"
 
 // Unable to unwind in the ARM index table (section 5 EHABI).
 #define UNW_EXIDX_CANTUNWIND 0x1
