5055147efb10130fb450e9a7ee799243f2fbb583
ekrich <ekrichardson@gmail.com>
2021-04-21 08:00:07 -1000

    Use fold rather than case

--- tools/src/main/scala/scala/scalanative/build/Filter.scala
+++ tools/src/main/scala/scala/scalanative/build/Filter.scala
@@ -26,42 +26,40 @@ object Filter {
                       allPaths: Seq[Path]): Seq[Path] = {
     val codePath = destPath.resolve(codeDir)
     // check if filtering is needed, o.w. return all paths
-    findFilterProperties(codePath) match {
-      case None => allPaths
-      case Some(file) =>
-        val paths = allPaths.map(_.abs)
+    findFilterProperties(codePath).fold(allPaths) { file =>
+      val paths = allPaths.map(_.abs)
 
-        // predicate to check if given file path shall be compiled
-        // we only include sources of the current gc and exclude
-        // all optional dependencies if they are not necessary
-        val optPath = codePath.resolve("optional").abs
-        val (gcPath, gcSelPath) = {
-          val gcPath    = codePath.resolve("gc")
-          val gcSelPath = gcPath.resolve(config.gc.name)
-          (gcPath.abs, gcSelPath.abs)
+      // predicate to check if given file path shall be compiled
+      // we only include sources of the current gc and exclude
+      // all optional dependencies if they are not necessary
+      val optPath = codePath.resolve("optional").abs
+      val (gcPath, gcSelPath) = {
+        val gcPath    = codePath.resolve("gc")
+        val gcSelPath = gcPath.resolve(config.gc.name)
+        (gcPath.abs, gcSelPath.abs)
+      }
+
+      def include(path: String) = {
+        if (path.contains(optPath)) {
+          val name = Paths.get(path).toFile.getName.split("\\.").head
+          linkerResult.links.map(_.name).contains(name)
+        } else if (path.contains(gcPath)) {
+          path.contains(gcSelPath)
+        } else {
+          true
         }
+      }
 
-        def include(path: String) = {
-          if (path.contains(optPath)) {
-            val name = Paths.get(path).toFile.getName.split("\\.").head
-            linkerResult.links.map(_.name).contains(name)
-          } else if (path.contains(gcPath)) {
-            path.contains(gcSelPath)
-          } else {
-            true
-          }
-        }
+      val (includePaths, excludePaths) = paths.partition(include(_))
 
-        val (includePaths, excludePaths) = paths.partition(include(_))
+      // delete .o files for all excluded source files
+      excludePaths.foreach { path =>
+        val opath = Paths.get(path + oExt) // path.resolve
+        if (Files.exists(opath))
+          Files.delete(opath)
+      }
 
-        // delete .o files for all excluded source files
-        excludePaths.foreach { path =>
-          val opath = Paths.get(path + oExt) // path.resolve
-          if (Files.exists(opath))
-            Files.delete(opath)
-        }
-
-        includePaths.map(Paths.get(_))
+      includePaths.map(Paths.get(_))
     }
   }
 
