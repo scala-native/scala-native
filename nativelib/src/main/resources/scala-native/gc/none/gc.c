446c4cf15194e08157a5acd4b36344f1d1394c57
Teodor Dimov <teodimoff@gmail.com>
2021-05-17 19:24:28 +0300

    Add a configurable env flag that controls memory preallocation when GC.none is selected (#2205)
    * Add a configurable env variable that controls memory preallocation.

--- nativelib/src/main/resources/scala-native/gc/none/gc.c
+++ nativelib/src/main/resources/scala-native/gc/none/gc.c
@@ -1,16 +1,54 @@
 #include <stdlib.h>
 #include "MemoryMap.h"
+#include "MemoryInfo.h"
+#include "Parsing.h"
 
 // Dummy GC that maps chunks of 4GB and allocates but never frees.
-
-// Map 4GB
-#define CHUNK (4 * 1024 * 1024 * 1024L)
 
 void *current = 0;
 void *end = 0;
 
+static size_t DEFAULT_CHUNK;
+static size_t PREALLOC_CHUNK;
+static size_t CHUNK;
+static size_t TO_NORMAL_MMAP = 1L;
+static size_t DO_PREALLOC = 0L; // No Preallocation.
+
+void Prealloc_Or_Default() {
+
+    if (TO_NORMAL_MMAP == 1L) { // Check if we have prealloc env varible
+                                // or execute default mmap settings
+        size_t memorySize = getMemorySize();
+
+        DEFAULT_CHUNK = // Default Maximum allocation Map 4GB
+            Choose_IF(Parse_Env_Or_Default_String("GC_MAXIMUM_HEAP_SIZE", "4G"),
+                      Less_OR_Equal, memorySize);
+
+        PREALLOC_CHUNK = // Preallocation
+            Choose_IF(Parse_Env_Or_Default("GC_INITIAL_HEAP_SIZE", 0L),
+                      Less_OR_Equal, DEFAULT_CHUNK);
+
+        if (PREALLOC_CHUNK == 0L) { // no prealloc settings.
+            CHUNK = DEFAULT_CHUNK;
+            TO_NORMAL_MMAP = 0L;
+
+        } else { // config prealloc settings and the flag to reset the
+                 // mmap settings the next iteration.
+            CHUNK = PREALLOC_CHUNK;
+            DO_PREALLOC = 1L;    // Do Preallocate.
+            TO_NORMAL_MMAP = 2L; // Return settings to normal on next iteration.
+        }
+    } else if (TO_NORMAL_MMAP == 2L) {
+        DO_PREALLOC = 0L;
+        CHUNK = DEFAULT_CHUNK;
+        TO_NORMAL_MMAP = 0L; // break the cycle and return to normal mmap alloc
+    } else {
+    }
+}
+
 void scalanative_init() {
-    current = memoryMap(CHUNK);
+    Prealloc_Or_Default();
+    current = memoryMapPrealloc(CHUNK, DO_PREALLOC);
     end = current + CHUNK;
 }
 
