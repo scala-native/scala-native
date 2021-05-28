0a1674dbdcb2c0d35bf77689e8cb32648e7b9990
Wojciech Mazur <wojciech.mazur95@gmail.com>
2021-05-28 15:36:13 +0200

    Fix tools tests

--- tools/src/test/scala/scala/scalanative/linker/package.scala
+++ tools/src/test/scala/scala/scalanative/linker/package.scala
@@ -3,7 +3,7 @@ package scala.scalanative
 import scala.scalanative.nir._
 
 package object linker {
-  val linktimeInfoProperties = "scalanative.meta.linktimeinfo"
+  val linktimeInfoProperties = "scala.scalanative.meta.linktimeinfo"
   // Defaults values for linktime resolved properties
   val linktimeInfoDefaults: Map[String, Boolean] = {
     Map(
