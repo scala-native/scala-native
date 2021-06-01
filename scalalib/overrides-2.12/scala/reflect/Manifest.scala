b9800a1c6e390286363ac091ed8d304257b466d9
Wojciech Mazur <wojciech.mazur95@gmail.com>
2021-06-01 13:59:48 +0200

    Fix cyclic dependency in `scala.reflect.{AnyVal, Phantom}Manifest` - use inlined def for hashcode instead of computed value

--- scalalib/overrides-2.12/scala/reflect/Manifest.scala
+++ scalalib/overrides-2.12/scala/reflect/Manifest.scala
@@ -76,8 +76,8 @@ abstract class AnyValManifest[T <: AnyVal](override val toString: String) extend
     case _                    => false
   }
   override def equals(that: Any): Boolean = this eq that.asInstanceOf[AnyRef]
-  @transient
-  override val hashCode = System.identityHashCode(this)
+  @transient @inline
+  override def hashCode = System.identityHashCode(this)
 }
 
 /** `ManifestFactory` defines factory methods for manifests.
@@ -323,8 +323,8 @@ object ManifestFactory {
   private abstract class PhantomManifest[T](_runtimeClass: Predef.Class[_],
                                             override val toString: String) extends ClassTypeManifest[T](None, _runtimeClass, Nil) {
     override def equals(that: Any): Boolean = this eq that.asInstanceOf[AnyRef]
-    @transient
-    override val hashCode = System.identityHashCode(this)
+    @transient @inline
+    override def hashCode = System.identityHashCode(this)
   }
 
   /** Manifest for the class type `clazz[args]`, where `clazz` is
