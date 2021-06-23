5dba0e85c02ae38703d5690ea2d11046dc6becb1
ekrich <ekrichardson@gmail.com>
2021-06-22 18:21:44 -0700

    Fix formatting

--- tools/src/main/scala/scala/scalanative/build/plugin/BuildPluginFactory.scala
+++ tools/src/main/scala/scala/scalanative/build/plugin/BuildPluginFactory.scala
@@ -6,7 +6,7 @@ import scalanative.build.{BuildException, Filter}
 object BuildPluginFactory {
   def create(name: String): BuildPlugin =
     name match {
-      case "gc" => new GcBuildPlugin
+      case "gc"      => new GcBuildPlugin
       case "javalib" => new JavalibBuildPlugin
       case _ =>
         throw new BuildException(
