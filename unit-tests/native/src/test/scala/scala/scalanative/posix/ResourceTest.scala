aeaa9b6142af8013d11fee3fd5b4e9843c8101ea
Wojciech Mazur <wojciech.mazur95@gmail.com>
2021-08-04 10:42:17 +0200

    Replace all usages of Posix specific functions with stubs for future Windows implementation Allow to use zlib on Windows Adjust CI to run unit tests

--- unit-tests/native/src/test/scala/scala/scalanative/posix/ResourceTest.scala
+++ unit-tests/native/src/test/scala/scala/scalanative/posix/ResourceTest.scala
@@ -66,8 +66,8 @@ class ResourceTest {
   @Test def testGetpriority() = if (!isWindows) {
     val cases = Array(
       TestInfo("PRIO_PROCESS", PRIO_PROCESS),
-      TestInfo("PRIO_PGRP", PRIO_PGRP),
-      TestInfo("PRIO_USER", PRIO_USER)
+      TestInfo("PRIO_PGRP",    PRIO_PGRP),
+      TestInfo("PRIO_USER",    PRIO_USER)
     )
 
     for (c <- cases) {
@@ -101,12 +101,12 @@ class ResourceTest {
     Zone { implicit z =>
       val cases = Array(
         TestInfo("RLIMIT_AS", RLIMIT_AS),
-        TestInfo("RLIMIT_CORE", RLIMIT_CORE),
-        TestInfo("RLIMIT_CPU", RLIMIT_CPU),
-        TestInfo("RLIMIT_DATA", RLIMIT_DATA),
-        TestInfo("RLIMIT_FSIZE", RLIMIT_FSIZE),
+        TestInfo("RLIMIT_CORE",   RLIMIT_CORE),
+        TestInfo("RLIMIT_CPU",    RLIMIT_CPU),
+        TestInfo("RLIMIT_DATA",   RLIMIT_DATA),
+        TestInfo("RLIMIT_FSIZE",  RLIMIT_FSIZE),
         TestInfo("RLIMIT_NOFILE", RLIMIT_NOFILE),
-        TestInfo("RLIMIT_STACK", RLIMIT_STACK)
+        TestInfo("RLIMIT_STACK",  RLIMIT_STACK)
       )
 
       for (c <- cases) {
