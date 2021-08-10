25df48a23f84dff5fac4e5229b31b0fa97abb932
Wojciech Mazur <wojciech.mazur95@gmail.com>
2021-08-10 11:57:05 +0200

    Adjust tests to pass on Windows

--- unit-tests/shared/src/test/scala/javalib/io/FileOutputStreamTest.scala
+++ unit-tests/shared/src/test/scala/javalib/io/FileOutputStreamTest.scala
@@ -5,7 +5,7 @@ import java.io._
 import org.junit.Test
 import org.junit.Assert._
 import org.junit.Assume._
-import org.scalanative.testsuite.utils.Platform.isWindows
+import org.scalanative.testsuite.utils.Platform._
 
 import scalanative.junit.utils.AssertThrows.assertThrows
 
@@ -93,6 +93,10 @@ class FileOutputStreamTest {
   }
 
   @Test def attemptToOpenDirectory(): Unit = {
+    assumeTrue(
+      "Uses not yet implemented java.nio methods",
+      !isWindows || executingInJVM
+    )
     withTempDirectory { dir =>
       assertThrows(classOf[FileNotFoundException], new FileOutputStream(dir))
     }
