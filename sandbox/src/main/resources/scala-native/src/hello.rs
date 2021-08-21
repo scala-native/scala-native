6c8cd64604ae4cb98ee600ccdf75fa783948062f
Wojciech Mazur <wojciech.mazur95@gmail.com>
2021-08-22 01:03:30 +0200

    Clenaup

--- sandbox/src/main/resources/scala-native/src/hello.rs
+++
@@ -1,7 +0,0 @@
-#[no_mangle]
-pub extern "C" fn rust_hello() {
-  // Statements here are executed when the compiled binary is called
-  
-  // Print text to the console
-  println!("Hello World from Rust!");
-}
--- sandbox/src/main/resources/scala-native/src/myIsEven.rs
+++
@@ -1,7 +0,0 @@
-extern crate is_even;
-use is_even::IsEven;
-
-#[no_mangle]
-pub extern "C" fn isEven(x: i32) -> bool {
-  x.is_even()
-}
