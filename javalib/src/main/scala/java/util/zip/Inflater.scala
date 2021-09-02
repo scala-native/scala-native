ae3c4948d7df431a37528caf73a504be3d2abe70
Wojciech Mazur <wojciech.mazur95@gmail.com>
2021-09-02 18:15:09 +0200

    Allow to use java.util.zip in Windows

--- javalib/src/main/scala/java/util/zip/Inflater.scala
+++ javalib/src/main/scala/java/util/zip/Inflater.scala
@@ -4,6 +4,9 @@ import scala.scalanative.unsigned._
 import scala.scalanative.unsafe._
 import scala.scalanative.libc._
 import scala.scalanative.runtime.{ByteArray, zlib}
+import scala.scalanative.runtime.zlibExt.z_stream
+import scala.scalanative.runtime.zlibOps._
+import zlib._
 
 // Ported from Apache Harmony
 
@@ -14,7 +17,7 @@ class Inflater(noHeader: Boolean) {
   private[zip] var inLength: Int = 0
   private[zip] var inRead: Int = 0
   private var doesNeedDictionary: Boolean = false
-  private var stream: zlib.z_streamp = Inflater.createStream(noHeader)
+  private var stream: z_streamp = Inflater.createStream(noHeader)
 
   def this() = this(noHeader = false)
 
@@ -38,21 +41,21 @@ class Inflater(noHeader: Boolean) {
     if (stream == null) {
       throw new IllegalStateException()
     } else {
-      stream._13.toInt
+      stream.adler.toInt
     }
 
   def getBytesRead(): Long =
     if (stream == null) {
       throw new NullPointerException()
     } else {
-      stream._3.toLong
+      stream.totalIn.toLong
     }
 
   def getBytesWritten(): Long =
     if (stream == null) {
       throw new NullPointerException()
     } else {
-      stream._6.toLong
+      stream.totalOut.toLong
     }
 
   def getRemaining(): Int =
@@ -143,23 +146,23 @@ class Inflater(noHeader: Boolean) {
       inRead = 0
       inLength = nbytes
       if (buf.length == 0) {
-        stream._1 = Inflater.empty.asInstanceOf[ByteArray].at(off)
+        stream.nextIn = Inflater.empty.asInstanceOf[ByteArray].at(off)
       } else {
-        stream._1 = buf.asInstanceOf[ByteArray].at(off)
+        stream.nextIn = buf.asInstanceOf[ByteArray].at(off)
       }
-      stream._2 = nbytes.toUInt
+      stream.availableIn = nbytes.toUInt
     } else {
       throw new ArrayIndexOutOfBoundsException()
     }
 
   private def inflateImpl(buf: Array[Byte], off: Int, nbytes: Int): Int = {
-    stream._5 = nbytes.toUInt
-    val sin = stream._3
-    val sout = stream._6
+    stream.availableOut = nbytes.toUInt
+    val sin = stream.totalIn
+    val sout = stream.totalOut
     if (buf.length == 0) {
-      stream._4 = Inflater.empty.asInstanceOf[ByteArray].at(off)
+      stream.nextOut = Inflater.empty.asInstanceOf[ByteArray].at(off)
     } else {
-      stream._4 = buf.asInstanceOf[ByteArray].at(off)
+      stream.nextOut = buf.asInstanceOf[ByteArray].at(off)
     }
     val err = zlib.inflate(stream, zlib.Z_SYNC_FLUSH)
 
@@ -167,7 +170,7 @@ class Inflater(noHeader: Boolean) {
       if (err == zlib.Z_STREAM_ERROR) {
         0
       } else if (err == zlib.Z_STREAM_END || err == zlib.Z_NEED_DICT) {
-        val totalIn = stream._3
+        val totalIn = stream.totalIn
         inRead += (totalIn - sin).toInt
 
         if (err == zlib.Z_STREAM_END) {
@@ -175,14 +178,14 @@ class Inflater(noHeader: Boolean) {
         } else {
           doesNeedDictionary = true
         }
-        val totalOut = stream._6
+        val totalOut = stream.totalOut
         (totalOut - sout).toInt
       } else {
         throw new DataFormatException(err.toString)
       }
     } else {
-      val totalIn = stream._3
-      val totalOut = stream._6
+      val totalIn = stream.totalIn
+      val totalOut = stream.totalOut
       inRead += (totalIn - sin).toInt
       (totalOut - sout).toInt
     }
@@ -194,10 +197,10 @@ private object Inflater {
   // Used when we try to read to a zero-sized array.
   val empty = new Array[Byte](1)
 
-  def createStream(noHeader: Boolean): zlib.z_streamp = {
+  def createStream(noHeader: Boolean): z_streamp = {
     val stream = stdlib
-      .calloc(1.toULong, sizeof[zlib.z_stream])
-      .asInstanceOf[zlib.z_streamp]
+      .calloc(1.toULong, z_stream.size)
+      .asInstanceOf[z_streamp]
     val wbits: Int =
       if (noHeader) 15 / -1
       else 15
