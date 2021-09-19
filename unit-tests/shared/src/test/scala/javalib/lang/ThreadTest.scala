b7161471915b3f5a1ef4af8c6d66585fdcd6f097
Wojciech Mazur <wojciech.mazur95@gmail.com>
2021-09-19 14:09:31 +0200

    Assume implementation of Thread.sleep(0, nanos) not JVM compliant in tests

--- unit-tests/shared/src/test/scala/javalib/lang/ThreadTest.scala
+++ unit-tests/shared/src/test/scala/javalib/lang/ThreadTest.scala
@@ -13,10 +13,8 @@ import java.lang._
 import org.junit.Test
 import org.junit.Assert._
 
-import org.scalanative.testsuite.utils.Platform.{
-  executingInJVM,
-  executingInScalaNative
-}
+import org.scalanative.testsuite.utils.Platform._
+import scala.scalanative.junit.utils.AssumesHelper._
 
 class ThreadTest {
 
@@ -66,6 +64,13 @@ class ThreadTest {
   }
 
   @Test def sleepShouldSuspendForAtLeastSpecifiedNanos(): Unit = {
+    if (isWindows) {
+      // Behaviour for Thread.sleep(0, nanos) is not well documented on the JVM
+      // when executing on Windows. Local tests have proven that sleep might
+      // take undefined amount of time, in multiple cases less then expected.
+      // In SN for Windows we assume minimal granuality of sleep to be 1ms
+      assumeNotJVMCompliant()
+    }
     val sleepForNanos = 500000 // 0.5ms
     val start = System.nanoTime()
     Thread.sleep(0, sleepForNanos)
