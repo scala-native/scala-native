4e38eb8b6082a169fd6f5c9f5ee831d6697f1185
Wojciech Mazur <wojciech.mazur95@gmail.com>
2021-09-23 15:50:01 +0200

    Use WinNls Api to get use locale info (country, lang)

--- windowslib/src/main/resources/scala-native/windows/winnls.c
+++ windowslib/src/main/resources/scala-native/windows/winnls.c
@@ -2,8 +2,26 @@
 #define WIN32_LEAN_AND_MEAN
 #include "Windows.h"
 
- int scalanative_win32_LocaleName_Invariant(){return LOCALE_NAME_INVARIANT;}
- int scalanative_win32_LocaleName_SystemDefault(){{return LOCALE_NAME_SYSTEM_DEFAULT;}}
- int scalanative_win32_LocaleName_UserDefault(){{return LOCALE_NAME_USER_DEFAULT;}}
+int scalanative_win32_LocaleName_Invariant() { return LOCALE_NAME_INVARIANT; }
+int scalanative_win32_LocaleName_SystemDefault() {
+    return LOCALE_NAME_SYSTEM_DEFAULT;
+}
+int scalanative_win32_LocaleName_UserDefault() {
+    return LOCALE_NAME_USER_DEFAULT;
+}
+
+int scalanative_win32_Locale_SISO_LangName() { return LOCALE_SISO639LANGNAME; }
+
+int scalanative_win32_Locale_SISO_LangName2() {
+    return LOCALE_SISO639LANGNAME2;
+}
+
+int scalanative_win32_Locale_SISO_CountryName() {
+    return LOCALE_SISO639LANGNAME;
+}
+
+int scalanative_win32_Locale_SISO_CountryName2() {
+    return LOCALE_SISO639LANGNAME2;
+}
 
 #endif
--- windowslib/src/main/scala/scala/scalanative/windows/WinNlsApi.scala
+++ windowslib/src/main/scala/scala/scalanative/windows/WinNlsApi.scala
@@ -1,6 +1,7 @@
 package scala.scalanative.windows
 
 import scala.scalanative.unsafe._
+import scala.scalanative.unsigned._
 
 @link("Kernel32")
 @extern()
@@ -10,13 +11,25 @@ object WinNlsApi {
       localeName: CWString,
       lcType: LCType,
       buffer: CWString,
-      bufferSize: Int
+      bufferSize: UInt
   ): Int = extern
 
   @name("scalanative_win32_LocaleName_Invariant")
-  def LOCALE_NAME_INVARIANT = extern
+  def LOCALE_NAME_INVARIANT: CWString = extern
   @name("scalanative_win32_LocaleName_SystemDefault")
-  def LOCALE_NAME_SYSTEM_DEFAULT = extern
+  def LOCALE_NAME_SYSTEM_DEFAULT: CWString = extern
   @name("scalanative_win32_LocaleName_UserDefault")
-  def LOCALE_NAME_USER_DEFAULT = extern
+  def LOCALE_NAME_USER_DEFAULT: CWString = extern
+
+  @name("scalanative_win32_Locale_SISO_LangName")
+  def LOCALE_SISO639LANGNAME: LCType = extern
+
+  @name("scalanative_win32_Locale_SISO_LangName2")
+  def LOCALE_SISO639LANGNAME2: LCType = extern
+
+  @name("scalanative_win32_Locale_SISO_CountryName")
+  def LOCALE_SISO3166CTRYNAME: LCType = extern
+
+  @name("scalanative_win32_Locale_SISO_CountryName2")
+  def LOCALE_SISO3166CTRYNAME2: LCType = extern
 }
