1122972d769ec4793e486a3ea395387c720cd320
Wojciech Mazur <wojciech.mazur95@gmail.com>
2021-09-23 15:55:11 +0200

    Fix types in winnls.c

--- windowslib/src/main/resources/scala-native/windows/winnls.c
+++ windowslib/src/main/resources/scala-native/windows/winnls.c
@@ -2,25 +2,29 @@
 #define WIN32_LEAN_AND_MEAN
 #include "Windows.h"
 
-int scalanative_win32_LocaleName_Invariant() { return LOCALE_NAME_INVARIANT; }
-int scalanative_win32_LocaleName_SystemDefault() {
+PCWSTR scalanative_win32_LocaleName_Invariant() {
+    return LOCALE_NAME_INVARIANT;
+}
+PCWSTR scalanative_win32_LocaleName_SystemDefault() {
     return LOCALE_NAME_SYSTEM_DEFAULT;
 }
-int scalanative_win32_LocaleName_UserDefault() {
+PCWSTR scalanative_win32_LocaleName_UserDefault() {
     return LOCALE_NAME_USER_DEFAULT;
 }
 
-int scalanative_win32_Locale_SISO_LangName() { return LOCALE_SISO639LANGNAME; }
-
-int scalanative_win32_Locale_SISO_LangName2() {
-    return LOCALE_SISO639LANGNAME2;
-}
-
-int scalanative_win32_Locale_SISO_CountryName() {
+LCTYPE scalanative_win32_Locale_SISO_LangName() {
     return LOCALE_SISO639LANGNAME;
 }
 
-int scalanative_win32_Locale_SISO_CountryName2() {
+LCTYPE scalanative_win32_Locale_SISO_LangName2() {
+    return LOCALE_SISO639LANGNAME2;
+}
+
+LCTYPE scalanative_win32_Locale_SISO_CountryName() {
+    return LOCALE_SISO639LANGNAME;
+}
+
+LCTYPE scalanative_win32_Locale_SISO_CountryName2() {
     return LOCALE_SISO639LANGNAME2;
 }
 
