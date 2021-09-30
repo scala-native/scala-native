65f9591a44815c714a82a6d537bd8de9e86fe9c0
Wojciech Mazur <wojciech.mazur95@gmail.com>
2021-09-30 17:12:47 +0200

    Improve formatting

--- tools/src/main/scala/scala/scalanative/build/LLVM.scala
+++ tools/src/main/scala/scala/scalanative/build/LLVM.scala
@@ -62,8 +62,8 @@ private[scalanative] object LLVM {
             Seq("-c", inpath, "-o", outpath)
 
         config.logger.running(compilec)
-        val result = Process(compilec, config.workdir.toFile) ! Logger
-          .toProcessLogger(config.logger)
+        val result = Process(compilec, config.workdir.toFile) !
+          Logger.toProcessLogger(config.logger)
         if (result != 0) {
           throw new BuildException(s"Failed to compile ${inpath}")
         }
@@ -131,8 +131,8 @@ private[scalanative] object LLVM {
       s"Linking native code (${config.gc.name} gc, ${config.LTO.name} lto)"
     ) {
       config.logger.running(compile)
-      val result = Process(compile, config.workdir.toFile) ! Logger
-        .toProcessLogger(config.logger)
+      val result = Process(compile, config.workdir.toFile) !
+        Logger.toProcessLogger(config.logger)
       if (result != 0) {
         throw new BuildException(s"Failed to link ${outpath}")
       }
