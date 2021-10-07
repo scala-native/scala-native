9ba78a3a92c38ca81298d18d2b32ebba930986f7
Jan Chyb <jan.chyb.99@gmail.com>
2021-10-07 12:30:07 +0200

    Change previous "Any" caching translation setup to ADT

--- sbt-scala-native/src/main/scala/scala/scalanative/sbtplugin/NativeLinkCacheImplicits.scala
+++ sbt-scala-native/src/main/scala/scala/scalanative/sbtplugin/NativeLinkCacheImplicits.scala
@@ -1,67 +1,117 @@
 package scala.scalanative.sbtplugin
 
 import sjsonnew._
-import java.nio.file.Path
+import java.nio.file.{Path, Paths}
 import scala.scalanative.build
-import sbt.{pathJsonFormatter => _, _}
+import sbt.util.CacheImplicits._
 
-// Definitions of isomorphic functions used to covert cached values
-// from and to json of types not covered by the sjsonnew.BasicJsonProtocol 
-// package. Used by the sjsonnew library, which sbt caching also depends on.
+// Definitions of isomorphic functions used to convert cached values
+// from and to json of types not covered by the sjsonnew.BasicJsonProtocol
+// package. Used by the sjsonnew library, on which sbt caching also depends.
 // Due to the structure of build.Config and build.NativeConfig,
-// name values have to be added manually.
+// new values have to be added manually.
 // Logger is ignored when caching.
 private[sbtplugin] object NativeLinkCacheImplicits {
-  import sbt.util.CacheImplicits._
 
-  // TypeTag aware pseudo-union. Based on the allowed types
-  // specified in NativeConfig.
-  // Meant to replace "Any" when caching LinktimeProperties.
-  private type LinktimePropertyValue = (
-    Option[Boolean],
-    Option[Byte],
-    Option[Char],
-    Option[Short],
-    Option[Int],
-    Option[Long],
-    Option[Float],
-    Option[Double],
-    Option[String]
+  // Meant to replace the "Any" when caching LinktimeProperties.
+  // Based on the allowed types defined in NativeConfig.
+  sealed abstract class LinktimePropertyValue(val any: Any)
+  final case class LinktimePropertyBoolean(val value: Boolean)
+      extends LinktimePropertyValue(value)
+  final case class LinktimePropertyByte(val value: Byte)
+      extends LinktimePropertyValue(value)
+  final case class LinktimePropertyChar(val value: Char)
+      extends LinktimePropertyValue(value)
+  final case class LinktimePropertyShort(val value: Short)
+      extends LinktimePropertyValue(value)
+  final case class LinktimePropertyInt(val value: Int)
+      extends LinktimePropertyValue(value)
+  final case class LinktimePropertyLong(val value: Long)
+      extends LinktimePropertyValue(value)
+  final case class LinktimePropertyFloat(val value: Float)
+      extends LinktimePropertyValue(value)
+  final case class LinktimePropertyDouble(val value: Double)
+      extends LinktimePropertyValue(value)
+  final case class LinktimePropertyString(val value: String)
+      extends LinktimePropertyValue(value)
+
+  private def toLinktimeValue(any: Any): LinktimePropertyValue =
+    any match {
+      case b: Boolean => LinktimePropertyBoolean(b)
+      case b: Byte    => LinktimePropertyByte(b)
+      case b: Char    => LinktimePropertyChar(b)
+      case b: Short   => LinktimePropertyShort(b)
+      case b: Int     => LinktimePropertyInt(b)
+      case b: Long    => LinktimePropertyLong(b)
+      case b: Float   => LinktimePropertyFloat(b)
+      case b: Double  => LinktimePropertyDouble(b)
+      case b: String  => LinktimePropertyString(b)
+    }
+
+  implicit val ltvBooleanIso =
+    LList.iso[LinktimePropertyBoolean, Boolean :*: LNil](
+      { ltv: LinktimePropertyBoolean => ("boolean", ltv.value) :*: LNil },
+      { case (_, value) :*: LNil => LinktimePropertyBoolean(value) }
+    )
+  implicit val ltvByteIso = LList.iso[LinktimePropertyByte, Byte :*: LNil](
+    { ltv: LinktimePropertyByte => ("byte", ltv.value) :*: LNil },
+    { case (_, value) :*: LNil => LinktimePropertyByte(value) }
   )
+  implicit val ltvCharIso = LList.iso[LinktimePropertyChar, Char :*: LNil](
+    { ltv: LinktimePropertyChar => ("char", ltv.value) :*: LNil },
+    { case (_, value) :*: LNil => LinktimePropertyChar(value) }
+  )
+  implicit val ltvShortIso = LList.iso[LinktimePropertyShort, Short :*: LNil](
+    { ltv: LinktimePropertyShort => ("short", ltv.value) :*: LNil },
+    { case (_, value) :*: LNil => LinktimePropertyShort(value) }
+  )
+  implicit val ltvIntIso = LList.iso[LinktimePropertyInt, Int :*: LNil](
+    { ltv: LinktimePropertyInt => ("int", ltv.value) :*: LNil },
+    { case (_, value) :*: LNil => LinktimePropertyInt(value) }
+  )
+  implicit val ltvLongIso = LList.iso[LinktimePropertyLong, Long :*: LNil](
+    { ltv: LinktimePropertyLong => ("long", ltv.value) :*: LNil },
+    { case (_, value) :*: LNil => LinktimePropertyLong(value) }
+  )
+  implicit val ltvFloatIso = LList.iso[LinktimePropertyFloat, Float :*: LNil](
+    { ltv: LinktimePropertyFloat => ("float", ltv.value) :*: LNil },
+    { case (_, value) :*: LNil => LinktimePropertyFloat(value) }
+  )
+  implicit val ltvDoubleIso =
+    LList.iso[LinktimePropertyDouble, Double :*: LNil](
+      { ltv: LinktimePropertyDouble => ("double", ltv.value) :*: LNil },
+      { case (_, ltv) :*: LNil => LinktimePropertyDouble(ltv) }
+    )
+  implicit val ltvStringIso =
+    LList.iso[LinktimePropertyString, String :*: LNil](
+      { ltv: LinktimePropertyString => ("string", ltv.value) :*: LNil },
+      { case (_, value) :*: LNil => LinktimePropertyString(value) }
+    )
 
-  private def toLinktimeValue(a: Any): LinktimePropertyValue = {
-    val n = None
-    a match {
-      case b: Boolean => (Some(b), n, n, n, n, n, n, n, n)
-      case b: Byte    => (n, Some(b), n, n, n, n, n, n, n)
-      case b: Char    => (n, n, Some(b), n, n, n, n, n, n)
-      case b: Short   => (n, n, n, Some(b), n, n, n, n, n)
-      case b: Int     => (n, n, n, n, Some(b), n, n, n, n)
-      case b: Long    => (n, n, n, n, n, Some(b), n, n, n)
-      case b: Float   => (n, n, n, n, n, n, Some(b), n, n)
-      case b: Double  => (n, n, n, n, n, n, n, Some(b), n)
-      case b: String  => (n, n, n, n, n, n, n, n, Some(b))
-      case _          => throw new IllegalArgumentException()
-    }
-  }
+  implicit val ltpValueUnion =
+    flatUnionFormat9[
+      LinktimePropertyValue,
+      LinktimePropertyBoolean,
+      LinktimePropertyByte,
+      LinktimePropertyChar,
+      LinktimePropertyShort,
+      LinktimePropertyInt,
+      LinktimePropertyLong,
+      LinktimePropertyFloat,
+      LinktimePropertyDouble,
+      LinktimePropertyString
+    ]("LinktimePropertyValue")
 
-  private def toAny(ltv: LinktimePropertyValue): Any = ltv match {
-      case (Some(a), _, _, _, _, _, _, _, _) => a
-      case (_, Some(a), _, _, _, _, _, _, _) => a
-      case (_, _, Some(a), _, _, _, _, _, _) => a
-      case (_, _, _, Some(a), _, _, _, _, _) => a
-      case (_, _, _, _, Some(a), _, _, _, _) => a
-      case (_, _, _, _, _, Some(a), _, _, _) => a
-      case (_, _, _, _, _, _, Some(a), _, _) => a
-      case (_, _, _, _, _, _, _, Some(a), _) => a
-      case (_, _, _, _, _, _, _, _, Some(a)) => a
-      case _ => throw new IllegalArgumentException()
-    }
+  implicit val pathIso =
+    LList.iso[Path, String :*: LNil](
+      { path: Path => ("path", path.toString()) :*: LNil },
+      { case (_, pathStr) :*: LNil => Paths.get(pathStr) }
+    )
 
   implicit val linktimePropertiesAnyIso =
     LList.iso[Any, LinktimePropertyValue :*: LNil](
       { any: Any => ("any", toLinktimeValue(any)) :*: LNil },
-      { case (_, ltv) :*: LNil => toAny(ltv) }
+      { case (_, ltv) :*: LNil => ltv.any }
     )
 
   implicit val gcIso = LList.iso[build.GC, String :*: LNil](
