78882b3cf61d446384a8a785bc37cba686e4f3b2
Wojciech Mazur <wojciech.mazur95@gmail.com>
2021-10-19 18:34:11 +0200

    Recreate and clean patches for scalalib

---
+++ scalalib/old-collections/scala/collection/mutable/Iterable.scala
@@ -0,0 +1,50 @@
+/*
+ * Scala (https://www.scala-lang.org)
+ *
+ * Copyright EPFL and Lightbend, Inc.
+ *
+ * Licensed under Apache License 2.0
+ * (http://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ */
+
+package scala
+package collection
+package mutable
+
+import generic._
+import parallel.mutable.ParIterable
+
+/** A base trait for iterable collections that can be mutated.
+ *  $iterableInfo
+ */
+trait Iterable[A] extends Traversable[A]
+//                     with GenIterable[A]
+                     with scala.collection.Iterable[A]
+                     with GenericTraversableTemplate[A, Iterable]
+                     with IterableLike[A, Iterable[A]]
+                     with Parallelizable[A, ParIterable[A]]
+{
+  override def companion: GenericCompanion[Iterable] = Iterable
+  protected[this] override def parCombiner = ParIterable.newCombiner[A] // if `mutable.IterableLike` gets introduced, please move this there!
+  override def seq: Iterable[A] = this
+}
+
+/** $factoryInfo
+ *  The current default implementation of a $Coll is an `ArrayBuffer`.
+ *  @define coll mutable iterable collection
+ *  @define Coll `mutable.Iterable`
+ */
+object Iterable extends TraversableFactory[Iterable] {
+  @inline override def ReusableCBF: GenericCanBuildFrom[Nothing] = ReusableCBFInstance
+  private object ReusableCBFInstance extends GenericCanBuildFrom[Nothing] {
+    @inline override def apply() = newBuilder[Nothing]
+  }
+  @inline implicit def canBuildFrom[A]: CanBuildFrom[Coll, A, Iterable[A]] = ReusableCBF.asInstanceOf[GenericCanBuildFrom[A]]
+  @inline def newBuilder[A]: Builder[A, Iterable[A]] = new ArrayBuffer
+}
+
+/** Explicit instantiation of the `Iterable` trait to reduce class file size in subclasses. */
+abstract class AbstractIterable[A] extends scala.collection.AbstractIterable[A] with Iterable[A]
