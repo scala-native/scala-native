4a743fe9f57b9eb5ff9bcce8436419b936b38f00
Denys Shabalin <320966+densh@users.noreply.github.com>
2021-12-05 17:18:12 +0100

    Fix #735: improve throughput of zone allocation
    This change replaces naive implementation of the zone allocator
    we had previously with one that performs bump allocation for
    small allocations (<2K).
    
    Allocation throughput after this change:
    
    ```
    2 bytes : 21115 ns / 1000 allocations
    4 bytes : 20467 ns / 1000 allocations
    8 bytes : 20535 ns / 1000 allocations
    16 bytes : 20005 ns / 1000 allocations
    32 bytes : 19696 ns / 1000 allocations
    64 bytes : 19915 ns / 1000 allocations
    128 bytes : 20309 ns / 1000 allocations
    256 bytes : 27632 ns / 1000 allocations
    512 bytes : 41469 ns / 1000 allocations
    1024 bytes : 55459 ns / 1000 allocations
    2048 bytes : 83120 ns / 1000 allocations
    4096 bytes : 773972 ns / 1000 allocations
    8192 bytes : 1755135 ns / 1000 allocations
    16384 bytes : 4041602 ns / 1000 allocations
    ```
    
    Allocation throughput before this change:
    
    ```
    2 bytes : 81216 ns / 1000 allocations
    4 bytes : 81147 ns / 1000 allocations
    8 bytes : 80590 ns / 1000 allocations
    16 bytes : 82284 ns / 1000 allocations
    32 bytes : 85628 ns / 1000 allocations
    64 bytes : 80658 ns / 1000 allocations
    128 bytes : 90329 ns / 1000 allocations
    256 bytes : 91666 ns / 1000 allocations
    512 bytes : 93411 ns / 1000 allocations
    1024 bytes : 122811 ns / 1000 allocations
    2048 bytes : 419922 ns / 1000 allocations
    4096 bytes : 1087628 ns / 1000 allocations
    8192 bytes : 2239114 ns / 1000 allocations
    16384 bytes : 4660193 ns / 1000 allocations
    ```

--- nativelib/src/main/scala/scala/scalanative/unsafe/Zone.scala
+++ nativelib/src/main/scala/scala/scalanative/unsafe/Zone.scala
@@ -3,6 +3,8 @@ package unsafe
 
 import scala.annotation.implicitNotFound
 import scalanative.runtime.{libc, RawPtr, fromRawPtr}
+import scalanative.runtime.{MemoryPool, MemoryPoolZone}
+import scalanative.unsigned._
 
 /** Zone allocator which manages memory allocations. */
 @implicitNotFound("Given method requires an implicit zone.")
@@ -26,47 +28,11 @@ object Zone {
 
   /** Run given function with a fresh zone and destroy it afterwards. */
   final def apply[T](f: Zone => T): T = {
-    val zone = Zone.open()
+    val zone = open()
     try f(zone)
     finally zone.close()
   }
 
   /** Create a new zone allocator. Use Zone#close to free allocations. */
-  final def open(): Zone = new ZoneImpl
-
-  /** Minimalistic zone allocator that uses underlying system allocator for
-   *  allocations, and frees all of the allocations once the zone is closed.
-   */
-  private class ZoneImpl extends Zone {
-    final class Node(val head: RawPtr, val tail: Node)
-
-    private var node: Node = null
-    private var closed: Boolean = false
-
-    final override def isClosed: Boolean = closed
-
-    final def alloc(size: CSize): Ptr[Byte] = {
-      if (isClosed) {
-        throw new IllegalStateException("zone allocator is closed")
-      }
-      val rawptr = libc.malloc(size)
-      if (rawptr == null) {
-        throw new OutOfMemoryError(s"Unable to allocate $size bytes")
-      }
-      node = new Node(rawptr, node)
-      fromRawPtr[Byte](rawptr)
-    }
-
-    final override def close(): Unit = {
-      if (closed) {
-        throw new IllegalStateException("zone allocator is closed")
-      }
-      closed = true
-      while (node != null) {
-        val head = node.head
-        node = node.tail
-        libc.free(head)
-      }
-    }
-  }
+  final def open(): Zone = MemoryPoolZone.open(MemoryPool.defaultMemoryPool)
 }
