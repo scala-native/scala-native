1a4305b3c67661d2585aa2af3cd715e2717eb836
Wojciech Mazur <wojciech.mazur95@gmail.com>
2021-12-11 15:01:02 +0100

    Duplicate java.lang.ProcessBuilder for scala 2 and 3, due to inner definition of enums

--- javalib/src/main/scala-3/java/lang/ProcessBuilder.scala
+++ javalib/src/main/scala-3/java/lang/ProcessBuilder.scala
@@ -1,3 +1,7 @@
+// Any changes to this files needs to be synced with it's Scala2 counter part.
+// Due to enum restrictions it is impossible to create common implementation
+// for both Scala versions
+
 package java.lang
 
 import java.util.{ArrayList, List}
@@ -150,14 +154,14 @@ object ProcessBuilder {
   }
 
   object Redirect {
-    private class RedirectImpl(tpe: Redirect.Type, file: File)
+    private class RedirectImpl(tpe: Redirect.Type, redirectFile: File)
         extends Redirect {
       override def `type`(): Type = tpe
 
-      override def file(): File = file
+      override def file(): File = redirectFile
 
       override def toString =
-        s"Redirect.$tpe${if (file != null) s": ${file}" else ""}"
+        s"Redirect.$tpe${if (redirectFile != null) s": ${redirectFile}" else ""}"
     }
 
     val INHERIT: Redirect = new RedirectImpl(Type.INHERIT, null)
@@ -179,30 +183,12 @@ object ProcessBuilder {
       new RedirectImpl(Type.WRITE, file)
     }
 
-    class Type private (name: String, ordinal: Int)
-        extends Enum[Type](name, ordinal)
-
-    object Type {
-      final val PIPE = new Type("PIPE", 0)
-      final val INHERIT = new Type("INHERIT", 1)
-      final val READ = new Type("READ", 2)
-      final val WRITE = new Type("WRITE", 3)
-      final val APPEND = new Type("APPEND", 4)
-
-      def valueOf(name: String): Type = {
-        if (name == null) throw new NullPointerException()
-        _values.toSeq.find(_.name() == name) match {
-          case Some(t) => t
-          case None =>
-            throw new IllegalArgumentException(
-              s"$name is not a valid Type name"
-            )
-        }
-      }
-
-      def values(): Array[Type] = _values
-
-      private val _values = Array(PIPE, INHERIT, READ, WRITE, APPEND)
+    enum Type extends Enum[Type]() {
+      case PIPE extends Type
+      case INHERIT extends Type
+      case READ extends Type
+      case WRITE extends Type
+      case APPEND extends Type
     }
   }
 }
