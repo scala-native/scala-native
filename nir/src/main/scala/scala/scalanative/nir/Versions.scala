0b5f904e6455dbbbb4507b17b35e20501dc08999
Wojciech Mazur <wojciech.mazur95@gmail.com>
2021-12-13 12:24:17 +0100

    Bump up NIR version

--- nir/src/main/scala/scala/scalanative/nir/Versions.scala
+++ nir/src/main/scala/scala/scalanative/nir/Versions.scala
@@ -21,8 +21,8 @@ object Versions {
    * when 2.3-based release happens all of the code needs to recompiled with
    * new version of the toolchain.
    */
-  final val compat: Int = 5 // a.k.a. MAJOR version
-  final val revision: Int = 8 // a.k.a. MINOR version
+  final val compat: Int = 6 // a.k.a. MAJOR version
+  final val revision: Int = 9 // a.k.a. MINOR version
 
   /* Current public release version of Scala Native. */
   final val current: String = "0.5.0-SNAPSHOT"
