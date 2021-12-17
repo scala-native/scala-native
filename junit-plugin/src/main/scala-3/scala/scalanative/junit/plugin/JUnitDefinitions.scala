6f0306cda4e18db93becb45b85ecfef4ff0101e9
Wojciech Mazur <wojciech.mazur95@gmail.com>
2021-12-17 14:08:12 +0100

    Run scalafmt

--- junit-plugin/src/main/scala-3/scala/scalanative/junit/plugin/JUnitDefinitions.scala
+++ junit-plugin/src/main/scala-3/scala/scalanative/junit/plugin/JUnitDefinitions.scala
@@ -22,8 +22,8 @@ object JUnitDefinitions {
 /** Definitions required by JUnit plugin
  *
  *  Scala.js port based on dotty.tools.backend.sjs.JSDefinitions#junit from
- *  Scala release 3.1.0. It contains the same definitions as original class which
- *  cannot be used directly in Scala Native plugin
+ *  Scala release 3.1.0. It contains the same definitions as original class
+ *  which cannot be used directly in Scala Native plugin
  */
 final class JUnitDefinitions()(using ctx: Context) {
   // scalafmt: { maxColumn = 120}
