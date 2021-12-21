1a57bde5976f11c924abfac12b4d9fc95728c4d4
Jan Chyb <jan.chyb.99@gmail.com>
2021-12-21 11:13:22 +0100

    clangfmt fix

--- nativelib/src/main/resources/scala-native/EmbeddedResourceReader.c
+++ nativelib/src/main/resources/scala-native/EmbeddedResourceReader.c
@@ -1,21 +1,21 @@
 #include <stdint.h>
-extern int** __resources_all_path;
-extern int** __resources_all_content;
-extern int32_t* __resources_all_content_lengths;
-extern int32_t* __resources_all_path_lengths;
+extern int **__resources_all_path;
+extern int **__resources_all_content;
+extern int32_t *__resources_all_content_lengths;
+extern int32_t *__resources_all_path_lengths;
 extern int32_t __resources_amount;
 
-int scalanative_resource_get_content_byte(int32_t embeddedFileId, int32_t byteIndex) {
+int scalanative_resource_get_content_byte(int32_t embeddedFileId,
+                                          int32_t byteIndex) {
     return __resources_all_content[embeddedFileId][byteIndex];
 }
 
-int scalanative_resource_get_path_byte(int32_t embeddedFileId, int32_t byteIndex) {
+int scalanative_resource_get_path_byte(int32_t embeddedFileId,
+                                       int32_t byteIndex) {
     return __resources_all_path[embeddedFileId][byteIndex];
 }
 
-int32_t scalanative_resource_get_embedded_size() {
-    return __resources_amount;
-}
+int32_t scalanative_resource_get_embedded_size() { return __resources_amount; }
 
 int32_t scalanative_resource_get_path_length(int32_t embeddedFileId) {
     return __resources_all_path_lengths[embeddedFileId];
