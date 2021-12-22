bba12b074fa30ea8c08f31040265fbf1327511c6
Wojciech Mazur <wojciech.mazur95@gmail.com>
2021-12-22 23:26:44 +0100

    Allow to use call static methods

--- nscplugin/src/main/scala-2/scala/scalanative/nscplugin/NirGenExpr.scala
+++ nscplugin/src/main/scala-2/scala/scalanative/nscplugin/NirGenExpr.scala
@@ -2279,6 +2279,8 @@ trait NirGenExpr[G <: nsc.Global with Singleton] { self: NirGenPhase[G] =>
     )(implicit pos: nir.Position): Val = {
       if (sym.owner.isExternModule && sym.isAccessor) {
         genApplyExternAccessor(sym, argsp)
+      } else if (sym.isStaticMember) {
+        genApplyStaticMethod(sym, selfp, argsp)
       } else if (isImplClass(sym.owner)) {
         genApplyMethod(sym, statically = true, Val.Null, argsp)
       } else if (sym.isStaticMember) {
