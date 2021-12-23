bc4968534bde141dabca67481aad67d56b9543bb
Wojciech Mazur <wojciech.mazur95@gmail.com>
2021-12-23 10:01:58 +0100

    Add runtime test checking that we can use macros

---
+++ unit-tests/shared/src/test/scala-3/scala/issues/EnumTest.scala
@@ -0,0 +1,13 @@
+package scala.issues
+
+import org.junit.Test
+import org.junit.Assert._
+
+class Scala3Issues:
+
+  // Test itslf does not have a large value, it does however assert that
+  // usage of macros in the code, does not break compiler plugin
+  @Test def canUseMacros(): Unit = {
+    val result = Macros.test("foo")
+    assertEquals(List(1, 2, 3), result)
+  }
