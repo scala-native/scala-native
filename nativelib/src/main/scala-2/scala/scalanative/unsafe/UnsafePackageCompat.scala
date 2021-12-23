7fe87ffc1cb12ab8fa9e76a9166114813d0ad789
Wojciech Mazur <wojciech.mazur95@gmail.com>
2021-12-23 18:18:00 +0100

    Make deprecation warning port of macro definition (otherwise it would be ignored)

--- nativelib/src/main/scala-2/scala/scalanative/unsafe/UnsafePackageCompat.scala
+++ nativelib/src/main/scala-2/scala/scalanative/unsafe/UnsafePackageCompat.scala
@@ -79,6 +79,19 @@ private object MacroImpl {
   import scala.reflect.macros.blackbox.Context
 
   def alloc1[T: c.WeakTypeTag](c: Context)(tag: c.Tree, z: c.Tree): c.Tree = {
+    c.warning(
+      c.enclosingPosition,
+      s"Empty param method `alloc[T]` is deprecated, " +
+        "in Scala 3 `alloc[T](n)` can be interpretted as " +
+        "`alloc[T].apply(n)` leading to runtime erros, " +
+        "use `alloc[T]()` instead "
+    )
+    alloc1Impl(c)(tag, z)
+  }
+
+  private def alloc1Impl[T: c.WeakTypeTag](
+      c: Context
+  )(tag: c.Tree, z: c.Tree): c.Tree = {
     import c.universe._
     val T = weakTypeOf[T]
 
@@ -97,7 +110,7 @@ private object MacroImpl {
 
   def allocSingle[T: c.WeakTypeTag](
       c: Context
-  )()(tag: c.Tree, z: c.Tree): c.Tree = alloc1(c)(tag, z)
+  )()(tag: c.Tree, z: c.Tree): c.Tree = alloc1Impl(c)(tag, z)
 
   def allocN[T: c.WeakTypeTag](
       c: Context
@@ -121,9 +134,21 @@ private object MacroImpl {
   }
 
   def stackallocSingle[T: c.WeakTypeTag](c: Context)()(tag: c.Tree): c.Tree =
-    stackalloc1(c)(tag)
+    stackalloc1Impl(c)(tag)
 
   def stackalloc1[T: c.WeakTypeTag](c: Context)(tag: c.Tree): c.Tree = {
+    c.warning(
+      c.enclosingPosition,
+      s"Empty param method `stackalloc[T]` is deprecated, " +
+        "in Scala 3 `stackalloc[T](n)` can be interpretted as " +
+        "`stackalloc[T].apply(n)` leading to runtime erros, " +
+        "use `stackalloc[T]()` instead "
+    )
+    stackalloc1Impl(c)(tag)
+  }
+
+  private def stackalloc1Impl[T: c.WeakTypeTag](c: Context)(tag: c.Tree): c.Tree = {
+
     import c.universe._
 
     val T = weakTypeOf[T]
