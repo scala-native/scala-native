f874ff80c4a0eb7c8b4255bdb0c5365385178c01
Wojciech Mazur <wojciech.mazur95@gmail.com>
2021-12-23 10:51:09 +0100

    Fix typo

--- unit-tests/shared/src/test/scala-3/scala/issues/Scala3IssuesTest.scala
+++ unit-tests/shared/src/test/scala-3/scala/issues/Scala3IssuesTest.scala
@@ -5,7 +5,7 @@ import org.junit.Assert._
 
 class Scala3IssuesTest:
 
-  // Test itslf does not have a large value, it does however assert that
+  // Test itself does not have a large value, it does however assert that
   // usage of macros in the code, does not break compiler plugin
   @Test def canUseMacros(): Unit = {
     val result = Macros.test("foo")
