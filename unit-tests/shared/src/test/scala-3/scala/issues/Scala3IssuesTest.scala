cc2e206d9c8ccbed97937c67df58f6a28d235eab
Wojciech Mazur <wojciech.mazur95@gmail.com>
2021-12-28 22:07:24 +0100

    Allow to resolve `unsafe.Tag` introduced via `given` (#2488)
    * Add test for failing issue
    * Fix resolving of Tags introduces via given
    * Move test from shared sources

--- unit-tests/shared/src/test/scala-3/scala/issues/Scala3IssuesTest.scala
+++ unit-tests/shared/src/test/scala-3/scala/issues/Scala3IssuesTest.scala
@@ -11,3 +11,37 @@ class Scala3IssuesTest:
     val result = Macros.test("foo")
     assertEquals(List(1, 2, 3), result)
   }
+
+  @Test def test_Issue803(): Unit = {
+    val x1: String = null
+    var x2: String = "right"
+    assertTrue(x1 + x2 == "nullright")
+
+    val x3: String = "left"
+    val x4: String = null
+    assertTrue(x3 + x4 == "leftnull")
+
+    val x5: AnyRef = new { override def toString = "custom" }
+    val x6: String = null
+    assertEquals("customnull", x5.toString + x6)
+
+    val x7: String = null
+    val x8: AnyRef = new { override def toString = "custom" }
+    assertEquals("nullcustom", x7 + x8)
+
+    val x9: String = null
+    val x10: String = null
+    assertEquals("nullnull", x9 + x10)
+
+    // This syntax operation does not compile in Scala 3
+    // When using `toString` on null it might throw NullPointerException
+    // val x11: AnyRef = null
+    // val x12: String = null
+    // assertEquals("nullnull", x11 + x12)
+
+    val x13: String = null
+    val x14: AnyRef = null
+    assertEquals("nullnull", x13 + x14)
+  }
+
+end Scala3IssuesTest
