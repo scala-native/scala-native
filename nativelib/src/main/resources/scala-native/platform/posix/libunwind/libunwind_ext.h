634d7d07047f69806d91e1beabc1ecc2a3953351
Shadaj Laddad <shadaj@users.noreply.github.com>
2022-01-04 15:51:12 -0800

    Update libunwind to LLVM 12.0.1

--- nativelib/src/main/resources/scala-native/platform/posix/libunwind/libunwind_ext.h
+++ nativelib/src/main/resources/scala-native/platform/posix/libunwind/libunwind_ext.h
@@ -1,11 +1,11 @@
-#if defined(__unix__) || defined(__unix) || defined(unix) ||                   \
+// clang-format off
+#if defined(__unix__) || defined(__unix) || defined(unix) || \
     (defined(__APPLE__) && defined(__MACH__))
 //===------------------------ libunwind_ext.h -----------------------------===//
 //
-//                     The LLVM Compiler Infrastructure
-//
-// This file is dual licensed under the MIT and the University of Illinois Open
-// Source Licenses. See LICENSE.TXT for details.
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //
 //  Extensions to libunwind API.
@@ -16,25 +16,46 @@
 #define __LIBUNWIND_EXT__
 
 #include "config.h"
-#include "include-libunwind/libunwind.h"
-#include "include-libunwind/unwind.h"
+#include "libunwind.h"
+#include "unwind.h"
 
 #define UNW_STEP_SUCCESS 1
-#define UNW_STEP_END 0
+#define UNW_STEP_END     0
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+extern int __unw_getcontext(unw_context_t *);
+extern int __unw_init_local(unw_cursor_t *, unw_context_t *);
+extern int __unw_step(unw_cursor_t *);
+extern int __unw_get_reg(unw_cursor_t *, unw_regnum_t, unw_word_t *);
+extern int __unw_get_fpreg(unw_cursor_t *, unw_regnum_t, unw_fpreg_t *);
+extern int __unw_set_reg(unw_cursor_t *, unw_regnum_t, unw_word_t);
+extern int __unw_set_fpreg(unw_cursor_t *, unw_regnum_t, unw_fpreg_t);
+extern int __unw_resume(unw_cursor_t *);
+
+#ifdef __arm__
+/* Save VFP registers in FSTMX format (instead of FSTMD). */
+extern void __unw_save_vfp_as_X(unw_cursor_t *);
+#endif
+
+extern const char *__unw_regname(unw_cursor_t *, unw_regnum_t);
+extern int __unw_get_proc_info(unw_cursor_t *, unw_proc_info_t *);
+extern int __unw_is_fpreg(unw_cursor_t *, unw_regnum_t);
+extern int __unw_is_signal_frame(unw_cursor_t *);
+extern int __unw_get_proc_name(unw_cursor_t *, char *, size_t, unw_word_t *);
+
 // SPI
-extern void unw_iterate_dwarf_unwind_cache(void (*func)(
+extern void __unw_iterate_dwarf_unwind_cache(void (*func)(
     unw_word_t ip_start, unw_word_t ip_end, unw_word_t fde, unw_word_t mh));
 
 // IPI
-extern void _unw_add_dynamic_fde(unw_word_t fde);
-extern void _unw_remove_dynamic_fde(unw_word_t fde);
+extern void __unw_add_dynamic_fde(unw_word_t fde);
+extern void __unw_remove_dynamic_fde(unw_word_t fde);
 
 #if defined(_LIBUNWIND_ARM_EHABI)
-extern const uint32_t *decode_eht_entry(const uint32_t *, size_t *, size_t *);
+extern const uint32_t* decode_eht_entry(const uint32_t*, size_t*, size_t*);
 extern _Unwind_Reason_Code _Unwind_VRS_Interpret(_Unwind_Context *context,
                                                  const uint32_t *data,
                                                  size_t offset, size_t len);
@@ -45,4 +66,4 @@ extern _Unwind_Reason_Code _Unwind_VRS_Interpret(_Unwind_Context *context,
 #endif
 
 #endif // __LIBUNWIND_EXT__
-#endif // Unix or Mac OS)
+#endif
---
+++ scripts/update_libunwind.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+shopt -s globstar
+
+LIBUNWIND_FOLDER=$1
+
+TARGET_FOLDER=nativelib/src/main/resources/scala-native/platform/posix/libunwind
+
+rm -rf $TARGET_FOLDER
+
+mkdir $TARGET_FOLDER
+
+cp -r $LIBUNWIND_FOLDER/src/* $TARGET_FOLDER/
+cp -r $LIBUNWIND_FOLDER/include/* $TARGET_FOLDER/
+
+nl='
+'
+
+for source in $TARGET_FOLDER/**/*.{c,cpp,h,hpp,S}; do
+  sed -i '1i\// clang-format off'"\\${nl}"'#if defined(__unix__) || defined(__unix) || defined(unix) || \\'"\\${nl}"'    (defined(__APPLE__) && defined(__MACH__))' "$source"
+  echo "#endif" >> "$source"
+
+  sed -i 's/<mach-o\/dyld.h>/"mach-o\/dyld.h"/g' "$source"
+  sed -i 's/<mach-o\/compact_unwind_encoding.h>/"mach-o\/compact_unwind_encoding.h"/g' "$source"
+  sed -i 's/<__libunwind_config.h>/"__libunwind_config.h"/g' "$source"
+  sed -i 's/<libunwind.h>/"libunwind.h"/g' "$source"
+  sed -i 's/<unwind.h>/"unwind.h"/g' "$source"
+done
+
+SN_FOLDER=$(pwd)
+
+cd $LIBUNWIND_FOLDER
+REV=$(git describe --tags)
+cd $SN_FOLDER
+
+echo $REV > $TARGET_FOLDER/rev.txt
