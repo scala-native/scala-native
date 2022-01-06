8a38c563c5db2208c83128e0abe2db3a4eb39bfe
ekrich <ekrichardson@gmail.com>
2022-01-06 14:34:05 -0800

    Fix max offset in test

--- unit-tests/native/src/test/scala/scala/scalanative/runtime/TimeTest.scala
+++ unit-tests/native/src/test/scala/scala/scalanative/runtime/TimeTest.scala
@@ -8,9 +8,9 @@ import scalanative.runtime.time.scalanative_time_zone_offset
 class TimeTest {
   @Test def testTimeZoneOffset(): Unit = {
     val offset = scalanative_time_zone_offset()
-    // Max 12 hours +- in seconds. Offset is 0s (UTC) in CI
+    // Between -12 and +14 hrs in seconds. Offset is 0s (UTC) in CI
     // println(s"Time zone offset: ${offset}s")
     assertTrue("time_zone_offset >= -43200", offset >= -43200)
-    assertTrue("time_zone_offset <= 43200", offset <= 43200)
+    assertTrue("time_zone_offset <= 50400", offset <= 50400)
   }
 }
