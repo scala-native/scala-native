ae397b4166f67df992a24700fa98afa20ba28fde
ekrich <ekrichardson@gmail.com>
2022-01-05 21:22:02 -0800

    Comment out print

--- unit-tests/native/src/test/scala/scala/scalanative/runtime/TimeTest.scala
+++ unit-tests/native/src/test/scala/scala/scalanative/runtime/TimeTest.scala
@@ -8,9 +8,8 @@ import scalanative.runtime.time.scalanative_time_zone_offset
 class TimeTest {
   @Test def testTimeZoneOffset(): Unit = {
     val offset = scalanative_time_zone_offset()
-    // Max 12 hours +- in seconds
-    // Offset is 0s (UTC) in CI
-    println(s"Time zone offset: ${offset}s")
+    // Max 12 hours +- in seconds. Offset is 0s (UTC) in CI
+    // println(s"Time zone offset: ${offset}s")
     assertTrue("time_zone_offset >= -43200", offset >= -43200)
     assertTrue("time_zone_offset <= 43200", offset <= 43200)
   }
