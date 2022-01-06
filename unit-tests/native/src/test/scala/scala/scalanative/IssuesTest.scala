0114ada0775d2c235ad8499c1d4715734c82cb84
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-01-06 18:18:00 +0100

    Run scalafmt

--- unit-tests/native/src/test/scala/scala/scalanative/IssuesTest.scala
+++ unit-tests/native/src/test/scala/scala/scalanative/IssuesTest.scala
@@ -437,7 +437,8 @@ class IssuesTest {
     val singletonMap = Collections.singletonMap(value, value)
     val singletonSet = Collections.singleton(value)
     val singletonList = Collections.singletonList(value)
-    val unmodifiableMap = Collections.unmodifiableMap[String, String](singletonMap)
+    val unmodifiableMap =
+      Collections.unmodifiableMap[String, String](singletonMap)
     val unmodifiableSet = Collections.unmodifiableSet(singletonSet)
     val unmodifiableList = Collections.unmodifiableList(singletonList)
     val unmodifiableCollection =
