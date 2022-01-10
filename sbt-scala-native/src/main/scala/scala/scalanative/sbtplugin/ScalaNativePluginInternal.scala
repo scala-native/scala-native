3f524ce376ea95a082c81c8159e0d794aea3651d
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-01-10 20:54:02 +0100

    Add `-test` suffix to artifact path of test executables (#2524)
    * Add -test suffix to artifact path of test executables
    * Add suffix for `native` workdir

--- sbt-scala-native/src/main/scala/scala/scalanative/sbtplugin/ScalaNativePluginInternal.scala
+++ sbt-scala-native/src/main/scala/scala/scalanative/sbtplugin/ScalaNativePluginInternal.scala
@@ -89,13 +89,13 @@ object ScalaNativePluginInternal {
     }
   )
 
-  lazy val scalaNativeConfigSettings: Seq[Setting[_]] = Seq(
+  def scalaNativeConfigSettings(nameSuffix: String): Seq[Setting[_]] = Seq(
     nativeLink / artifactPath := {
       val ext = if (Platform.isWindows) ".exe" else ""
-      crossTarget.value / (moduleName.value + "-out" + ext)
+      crossTarget.value / s"${moduleName.value}$nameSuffix-out$ext"
     },
     nativeWorkdir := {
-      val workdir = crossTarget.value / "native"
+      val workdir = crossTarget.value / s"native$nameSuffix"
       if (!workdir.exists) {
         IO.createDirectory(workdir)
       }
@@ -225,10 +225,10 @@ object ScalaNativePluginInternal {
   )
 
   lazy val scalaNativeCompileSettings: Seq[Setting[_]] =
-    scalaNativeConfigSettings
+    scalaNativeConfigSettings(nameSuffix = "")
 
   lazy val scalaNativeTestSettings: Seq[Setting[_]] =
-    scalaNativeConfigSettings ++
+    scalaNativeConfigSettings(nameSuffix = "-test") ++
       Seq(
         mainClass := Some("scala.scalanative.testinterface.TestMain"),
         loadedTestFrameworks := {
