9cd76c7ad7b54014d59a5533cbfba4df7861ee61
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-01-10 17:22:45 +0100

    Add reproducer test for issue 2520

--- unit-tests/native/src/test/scala/scala/scalanative/IssuesTest.scala
+++ unit-tests/native/src/test/scala/scala/scalanative/IssuesTest.scala
@@ -469,6 +469,23 @@ class IssuesTest {
     }
   }
 
+  @Test def test_Issue2520(): Unit = {
+    import issue2520._
+    import issue2520.Kleisli.KleisliOpt
+    implicit def instance: Strong[Function1] = new Strong[Function1] {
+      override def first[A, B, C](f: A => B): Function1[(A, C), (B, C)] = {
+        case (a1, a2) => (f(a1), a2)
+      }
+    }
+
+    assertNotNull {
+      val res =
+        implicitly[Strong[KleisliOpt]].law.test[Int, Int, Int, Int](x => x)
+      println(res)
+      res
+    }
+  }
+
 }
 
 package issue1090 {
@@ -518,4 +535,34 @@ package issue1909 {
 package issue1950 {
   final class ValueClass(val value: Float) extends AnyVal
   final case class ValueClass2(string: String) extends AnyVal
+}
+
+package issue2520 {
+  case class Kleisli[F[_], A, B](run: A => F[B])
+
+  object Kleisli {
+    type KleisliOpt[A, B] = Kleisli[Option, A, B]
+
+    implicit def instance: Strong[KleisliOpt] = new Strong[KleisliOpt] {
+      override def first[A, B, C](
+          f: KleisliOpt[A, B]
+      ): KleisliOpt[(A, C), (B, C)] =
+        Kleisli[Option, (A, C), (B, C)] {
+          case (a1, a2) => f.run(a1).map(_ -> a2)
+        }
+    }
+  }
+
+  trait Strong[F[_, _]] {
+    def first[A, B, C](fa: F[A, B]): F[(A, C), (B, C)]
+
+    trait Law {
+      def test[A, B, C, D](f: C => D)(implicit PF: Strong[Function1]) = {
+        val x: ((C, B)) => (D, B) = PF.first[C, D, B](f)
+      }
+    }
+
+    def law: Law = new Law {}
+  }
+
 }
