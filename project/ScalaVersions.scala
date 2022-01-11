14dadbe6284c9f75f9ebe7661e6d314ef33b9559
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-01-11 11:16:43 +0100

    Add Scala 2.13.8 to the build (#2525)
    * Add Scala 2.13.8 to cross build, use as default
    * Add Scala 2.13.8 support to the docs
    * Use Scala 2.13.8 in the CI
    * Add missing scala partests test
    * Fix partest task
    * Update scala partest junit tests blacklists
    * Add additional ScalaRunTime overrides for partest junit tests

--- project/ScalaVersions.scala
+++ project/ScalaVersions.scala
@@ -3,7 +3,7 @@ package build
 object ScalaVersions {
   val crossScala211 = Seq("2.11.12")
   val crossScala212 = Seq("2.12.13", "2.12.14", "2.12.15")
-  val crossScala213 = Seq("2.13.4", "2.13.5", "2.13.6", "2.13.7")
+  val crossScala213 = Seq("2.13.4", "2.13.5", "2.13.6", "2.13.7", "2.13.8")
   val crossScala3 = Seq("3.1.0")
 
   val scala211: String = crossScala211.last
