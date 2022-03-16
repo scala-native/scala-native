40601aef60df4080c5ffcb30d75b0cbf3946d5ad
110416 <ito.yo16uh90616@gmail.com>
2022-03-17 06:16:38 +0900

    Add bindings for fenv.h (#2591)
    * add fenv.h
    * parametarize tests
    * add debuggable assertion
    * remove redundant fenv

---
+++ clib/src/main/resources/scala-native/fenv.c
@@ -0,0 +1,12 @@
+#include <fenv.h>
+
+int scalanative_fe_divbyzero() { return FE_DIVBYZERO; }
+int scalanative_fe_inexact() { return FE_INEXACT; }
+int scalanative_fe_invalid() { return FE_INVALID; }
+int scalanative_fe_overflow() { return FE_OVERFLOW; }
+int scalanative_fe_underflow() { return FE_UNDERFLOW; }
+int scalanative_fe_all_except() { return FE_ALL_EXCEPT; }
+int scalanative_fe_downward() { return FE_DOWNWARD; }
+int scalanative_fe_tonearest() { return FE_TONEAREST; }
+int scalanative_fe_towardzero() { return FE_TOWARDZERO; }
+int scalanative_fe_upward() { return FE_UPWARD; }
---
+++ unit-tests/native/src/test/scala/scala/scalanative/libc/FenvTest.scala
@@ -0,0 +1,106 @@
+package scala.scalanative.libc
+import scala.scalanative.unsafe._
+import scala.scalanative.libc._
+import scala.scalanative.meta.LinktimeInfo
+import org.junit.Test
+import org.junit.Assert._
+class FEnvTest {
+
+  @Test def exceptionFlagsAreUnique(): Unit = {
+    assertEquals(
+      6,
+      Set(
+        fenv.FE_DIVBYZERO,
+        fenv.FE_INEXACT,
+        fenv.FE_INVALID,
+        fenv.FE_OVERFLOW,
+        fenv.FE_UNDERFLOW,
+        fenv.FE_ALL_EXCEPT
+      ).size
+    )
+  }
+
+  @Test def raiseCheckClearException(): Unit = {
+    if (LinktimeInfo.isWindows) {
+      // Avoid link time error --`fe_raiseexcept` does not exists-- because it is not supported on Windows.
+    } else {
+
+      def assertContainsFlags(expected: CInt, actual: CInt): Unit = {
+        assertTrue(
+          s"""expected: $expected
+        |actual: $actual
+        |FE_DIVBYZERO: ${fenv.FE_DIVBYZERO}
+        |FE_INEXACT: ${fenv.FE_INEXACT}
+        |FE_INVALID: ${fenv.FE_INVALID}
+        |FE_OVERFLOW: ${fenv.FE_OVERFLOW}
+        |FE_UNDERFLOW: ${fenv.FE_UNDERFLOW}
+        |""".stripMargin,
+          (actual & expected) == expected
+        )
+      }
+      val exs = List(
+        fenv.FE_DIVBYZERO,
+        fenv.FE_INEXACT,
+        fenv.FE_INVALID,
+        fenv.FE_OVERFLOW,
+        fenv.FE_UNDERFLOW
+      )
+      for {
+        n <- 1 to exs.length
+        comb <- exs.combinations(n)
+      } yield {
+        val flag = comb.foldLeft(0) { case (acc, f) => acc | f }
+        assertEquals(
+          0,
+          fenv.feclearexcept(fenv.FE_ALL_EXCEPT)
+        )
+        assertEquals(
+          0,
+          fenv.feraiseexcept(flag)
+        )
+        assertContainsFlags(
+          flag,
+          fenv.fetestexcept(fenv.FE_ALL_EXCEPT)
+        )
+        assertEquals(
+          0,
+          fenv.feclearexcept(fenv.FE_ALL_EXCEPT)
+        )
+      }
+    }
+  }
+
+  @Test def roundingFlagsAreUnique(): Unit = {
+    assertEquals(
+      4,
+      Set(
+        fenv.FE_DOWNWARD,
+        fenv.FE_TONEAREST,
+        fenv.FE_TOWARDZERO,
+        fenv.FE_UPWARD
+      ).size
+    )
+  }
+  @Test def setAndGetRoundingFlag(): Unit = {
+    val original = fenv.fegetround()
+    for {
+      flag <- List(
+        fenv.FE_DOWNWARD,
+        fenv.FE_TONEAREST,
+        fenv.FE_TOWARDZERO,
+        fenv.FE_UPWARD
+      )
+    } yield {
+      assertEquals(
+        0,
+        fenv.fesetround(flag)
+      )
+      assertEquals(
+        flag,
+        fenv.fegetround()
+      )
+    }
+    // restore fenv
+    fenv.fesetround(original)
+  }
+}
