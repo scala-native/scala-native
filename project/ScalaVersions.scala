1ff781e5661200c0e67556109f86380b0f3f0943
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-04-07 17:03:22 +0200

    Add 3.1.2 to cross versions

--- project/ScalaVersions.scala
+++ project/ScalaVersions.scala
@@ -4,7 +4,7 @@ object ScalaVersions {
   val crossScala211 = Seq("2.11.12")
   val crossScala212 = Seq("2.12.13", "2.12.14", "2.12.15")
   val crossScala213 = Seq("2.13.4", "2.13.5", "2.13.6", "2.13.7", "2.13.8")
-  val crossScala3 = Seq("3.1.0", "3.1.1")
+  val crossScala3 = Seq("3.1.0", "3.1.1", "3.1.2")
 
   val scala211: String = crossScala211.last
   val scala212: String = crossScala212.last
