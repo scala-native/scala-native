d78c60ca2b7ec71bdb09d32ee42d9cd4bd3202aa
ekrich <ekrichardson@gmail.com>
2022-07-01 08:32:54 -0700

    Fix added scaladoc links

--- nativelib/src/main/scala/scala/scalanative/runtime/time.scala
+++ nativelib/src/main/scala/scala/scalanative/runtime/time.scala
@@ -7,14 +7,15 @@ import scala.scalanative.unsafe.{CLongLong, extern}
 object time {
 
   /** Monotonically increasing time for use in timers and for
-   *  [[java.lang.System.nanoTime()]] in the Java library.
+   *  [[java.lang.System$.nanoTime()* System.nanoTime()]] in the Java library.
    *  @return
    *    increasing time hopefully at better than millisecond resolution
    */
   def scalanative_nano_time(): CLongLong = extern
 
   /** Milliseconds from the UNIX epoch to implement
-   *  [[java.lang.System.currentTimeMillis()]] in the Java library.
+   *  [[java.lang.System$.currentTimeMillis()* System.currentTimeMillis()]] in
+   *  the Java library.
    *  @return
    *    time in millis (UTC)
    */
