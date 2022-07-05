242b3b7e25880a28df935e1fb31c813e2168f019
ekrich <ekrichardson@gmail.com>
2022-07-05 09:04:45 -0700

    Add Windows impl to nanotime

--- nativelib/src/main/resources/scala-native/time_nano.c
+++ nativelib/src/main/resources/scala-native/time_nano.c
@@ -1,9 +1,9 @@
-#ifdef _WIN32
+#if defined(_WIN32)
 #define WIN32_LEAN_AND_MEAN
-#include <profileapi.h>
+#include <Windows.h>
 #else
 #include <time.h>
-#ifdef __APPLE__
+#if defined(__APPLE__)
 #if (__MAC_OS_X_VERSION_MIN_REQUIRED <= __MAC_10_12)
 #error "macOS version must be greater than 10.12"
 #endif
@@ -12,12 +12,20 @@
 
 long long scalanative_nano_time() {
     long long nano_time;
-
     const long long NANOSECONDS_PER_SECOND = 1000000000LL;
 
+#if defined(_WIN32)
+    LARGE_INTEGER count;
+    LARGE_INTEGER freq;
+    QueryPerformanceCounter(&count);
+    QueryPerformanceFrequency(&freq);
+    int nanosPerCount = NANOSECONDS_PER_SECOND / freq.QuadPart;
+    nano_time = count.QuadPart * nanosPerCount;
+#else
     struct timespec ts;
     clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
     nano_time = ts.tv_sec * NANOSECONDS_PER_SECOND + ts.tv_nsec;
+#endif
 
     return nano_time;
 }
