553b13bb0eb020859ef7f21f27a399a5a1c4f1e0
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2022-07-15 14:54:22 +0200

    Make junit async.await async as in Scala.js

--- junit-async/native/src/main/scala/scala/scalanative/junit/async/package.scala
+++ junit-async/native/src/main/scala/scala/scalanative/junit/async/package.scala
@@ -1,6 +1,7 @@
 package scala.scalanative.junit
 
 import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
 import scala.scalanative.meta.LinktimeInfo.isMultithreadingEnabled
 
@@ -10,8 +11,7 @@ package object async {
     if (isMultithreadingEnabled)
       Await.result(future, Duration.Inf)
     else {
-      scala.scalanative.runtime.loop()
-      future.value.get.get
+      future.map(_ => Success(()))
     }
   }
 }
