d7104bfd113c6ffae2879fa0202ababf48547951
Eric K Richardson <ekrichardson@gmail.com>
2022-07-18 11:03:38 -0700

    Fixes: #1915: Consider clock_gettime for macos and linux (#2704)
    * Change nanotime to C
    
    * Fix added scaladoc links
    
    * Add Windows impl to nanotime
    
    * Change to use clock_gettime of posix platforms
    
    * Remove hardcode frequency, use macro substitution
    
    * Add windows frequency shared value
    
    * Address review comments
    
    * Missed changing a constant in the Windows code
    
    * Add missing newline at end of file
    
    * Address documentation concerns
    
    * Fix grammer
    
    * Add message to SystemTest systemNanoTimeIsMonotonicallyIncreasing
    
    * Define constants as long long
    
    * Handle return values
    
    * GetSystemTimeAsFileTime returns void
    
    * Fix Windows error handling, update comments

---
+++ nativelib/src/main/resources/scala-native/win_freq.c
@@ -0,0 +1,25 @@
+#if defined(_WIN32)
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include "win_freq.h"
+
+static int winFreqQuadPartValue = 0;
+
+int winFreqQuadPart(int *quad) {
+    int retval = 1; // assume ok for caching
+    // check if cache is set
+    if (winFreqQuadPartValue == 0) {
+        LARGE_INTEGER freq;
+        retval = QueryPerformanceFrequency(&freq);
+        if (retval != 0) {
+            // set cache value
+            winFreqQuadPartValue = freq.QuadPart;
+        }
+    }
+    // assign cache value or default 0 on failure
+    *quad = winFreqQuadPartValue;
+
+    return retval;
+}
+
+#endif
