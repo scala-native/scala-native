660e118a989f50565f2d55d7f494d141e6a1b029
Lee Tibbert <lee.tibbert@gmail.com>
2022-07-19 20:54:47 -0400

    Fix #2738: Shorten posixlib uio codepaths & add UioTest

--- unit-tests/native/src/test/scala/scala/scalanative/posix/sys/Udp6SocketTest.scala
+++ unit-tests/native/src/test/scala/scala/scalanative/posix/sys/Udp6SocketTest.scala
@@ -4,12 +4,10 @@ package sys
 import scalanative.unsafe._
 import scalanative.unsigned._
 
-import scalanative.libc.errno
-import scalanative.libc.string.{memcmp, strerror}
+import scalanative.libc.string.memcmp
 
 import scalanative.posix.arpa.inet._
-import scalanative.posix.fcntl
-import scalanative.posix.fcntl.{F_SETFL, O_NONBLOCK}
+import scalanative.posix.errno
 import scalanative.posix.netinet.in._
 import scalanative.posix.netinet.inOps._
 import scalanative.posix.sys.socket._
@@ -65,59 +63,6 @@ class Udp6SocketTest {
     }
   }
 
-  // For some unknown reason inlining content of this method leads to failures
-  // on Unix, probably due to bug in linktime conditions.
-  private def setSocketBlocking(socket: CInt): Unit = {
-    if (isWindows) {
-      val mode = stackalloc[CInt]()
-      !mode = 1
-      assertNotEquals(
-        "iotctl setBLocking",
-        -1,
-        ioctlSocket(socket.toPtr[Byte], FIONBIO, mode)
-      )
-    } else {
-      assertNotEquals(
-        s"fcntl set blocking",
-        -1,
-        fcntl.fcntl(socket, F_SETFL, O_NONBLOCK)
-      )
-    }
-  }
-
-  private def createAndCheckUdpSocket(): CInt = {
-    if (isWindows) {
-      val socket = WSASocketW(
-        addressFamily = AF_INET6,
-        socketType = SOCK_DGRAM,
-        protocol = IPPROTO_UDP,
-        protocolInfo = null,
-        group = 0.toUInt,
-        flags = WSA_FLAG_OVERLAPPED
-      )
-      assertNotEquals("socket create", InvalidSocket, socket)
-      socket.toInt
-    } else {
-      val sock = sys.socket.socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP)
-      assertNotEquals("socket create", -1, sock)
-      sock
-    }
-  }
-
-  private def closeSocket(socket: CInt): Unit = {
-    if (isWindows) WinSocketApi.closeSocket(socket.toPtr[Byte])
-    else unistd.close(socket)
-  }
-
-  private def checkRecvfromResult(v: CSSize, label: String): Unit = {
-    if (v.toInt < 0) {
-      val reason =
-        if (isWindows) ErrorHandlingApiOps.errorMessage(GetLastError())
-        else fromCString(strerror(errno.errno))
-      fail(s"$label failed - $reason")
-    }
-  }
-
   private def formatIn6addr(addr: in6_addr): String = Zone { implicit z =>
     val dstSize = INET6_ADDRSTRLEN + 1
     val dst = alloc[Byte](dstSize.toUSize)
@@ -139,69 +84,11 @@ class Udp6SocketTest {
       WinSocketApiOps.init()
     }
 
-    val in6SockAddr = alloc[sockaddr_in6]()
-    in6SockAddr.sin6_family = AF_INET6.toUShort
-
-    /* Scala Native currently implements neither inaddr_loopback
-     * nor IN6ADDR_LOOPBACK_INIT. When they become available,
-     * this code can be simplified by using the former instead
-     * of the inet_pton(code below). All things in due time.
-     *
-     * in6SockAddr.sin6_addr = in6addr_loopback
-     */
-
-    // sin6_port is already the desired 0; "find a free port".
-    // all other fields already 0.
-
-    val localhost = c"::1"
-    val ptonStatus = inet_pton(
-      AF_INET6,
-      localhost,
-      in6SockAddr.sin6_addr.at1.at(0).asInstanceOf[Ptr[Byte]]
-    )
-
-    assertEquals(s"inet_pton failed errno: ${errno.errno}", ptonStatus, 1)
-
-    val inSocket: CInt = createAndCheckUdpSocket()
+    val (inSocket, outSocket, out6Addr) = getUdpLoopbackSockets(AF_INET6)
 
     try {
-      setSocketBlocking(inSocket)
-
-      // Get port for sendto() to use.
-      val bindStatus = bind(
-        inSocket,
-        in6SockAddr
-          .asInstanceOf[Ptr[sockaddr]],
-        sizeof[sockaddr_in6].toUInt
-      )
-
-      assertNotEquals(s"bind failed,  errno: ${errno.errno}", -1, bindStatus)
-
-      val inAddrInfo = alloc[sockaddr_in6]()
-      val gsnAddrLen = alloc[socklen_t]()
-      !gsnAddrLen = sizeof[sockaddr_in6].toUInt
-
-      val gsnStatus = getsockname(
-        inSocket,
-        inAddrInfo.asInstanceOf[Ptr[sockaddr]],
-        gsnAddrLen
-      )
-
-      assertNotEquals("getsockname failed errno: ${errno.errno}", -1, gsnStatus)
-
-      // Now use port.
-      val outSocket = createAndCheckUdpSocket()
-
-      try {
-        val out6Addr = alloc[sockaddr_in6]()
-        out6Addr.sin6_family = AF_INET6.toUShort
-        out6Addr.sin6_port = inAddrInfo.sin6_port
-        out6Addr.sin6_addr = in6SockAddr.sin6_addr
-
-        // all other fields in out6Addr have been cleared by alloc[].
-
-        val outData =
-          """
+      val outData =
+        """
           |"She moved through the fair" lyrics, Traditional, no copyright
           |   I dreamed it last night
           |   That my true love came in
@@ -213,94 +100,89 @@ class Udp6SocketTest {
           |   Till our wedding day."
           """.stripMargin
 
-        val nBytesSent = sendto(
-          outSocket,
-          toCString(outData),
-          outData.length.toUSize,
-          0,
-          out6Addr.asInstanceOf[Ptr[sockaddr]],
-          sizeof[sockaddr_in6].toUInt
+      val nBytesSent = sendto(
+        outSocket,
+        toCString(outData),
+        outData.length.toUSize,
+        0,
+        out6Addr.asInstanceOf[Ptr[sockaddr]],
+        sizeof[sockaddr_in6].toUInt
+      )
+
+      assertTrue(s"sendto failed errno: ${errno.errno}\n", (nBytesSent >= 0))
+      assertEquals("sendto length", outData.size, nBytesSent.toInt)
+
+      // If inSocket did not get data by timeout, it probably never will.
+      pollReadyToRecv(inSocket, 30 * 1000) // assert fail on error or timeout
+
+      /// Two tests using one inbound packet, save test duplication.
+
+      // Provide extra room to allow detecting extra junk being sent.
+      val maxInData = 2 * outData.length
+      val inData: Ptr[Byte] = alloc[Byte](maxInData.toUSize)
+
+      // Test not fetching remote address. Exercise last two arguments.
+      val nBytesPeekedAt =
+        recvfrom(
+          inSocket,
+          inData,
+          maxInData.toUInt,
+          MSG_PEEK,
+          null.asInstanceOf[Ptr[sockaddr]],
+          null.asInstanceOf[Ptr[socklen_t]]
         )
 
-        assertTrue(s"sendto failed errno: ${errno.errno}\n", (nBytesSent >= 0))
-        assertEquals("sendto length", outData.size, nBytesSent.toInt)
+      checkIoResult(nBytesPeekedAt, "recvfrom_1")
 
-        // If inSocket did not get data by timeout, it probably never will.
-        pollReadyToRecv(inSocket, 30 * 1000) // assert fail on error or timeout
+      // When sending a small UDP datagram, data will be sent in one shot.
+      assertEquals("recvfrom_1 length", nBytesSent, nBytesPeekedAt)
 
-        /// Two tests using one inbound packet, save test duplication.
+      // Test retrieving remote address.
+      val srcAddr = alloc[sockaddr_in6]()
+      val srcAddrLen = alloc[socklen_t]()
+      !srcAddrLen = sizeof[sockaddr_in6].toUInt
 
-        // Provide extra room to allow detecting extra junk being sent.
-        val maxInData = 2 * outData.length
-        val inData: Ptr[Byte] = alloc[Byte](maxInData.toUSize)
+      val nBytesRecvd =
+        recvfrom(
+          inSocket,
+          inData,
+          maxInData.toUInt,
+          0,
+          srcAddr.asInstanceOf[Ptr[sockaddr]],
+          srcAddrLen
+        )
 
-        // Test not fetching remote address. Exercise last two arguments.
-        val nBytesPeekedAt =
-          recvfrom(
-            inSocket,
-            inData,
-            maxInData.toUInt,
-            MSG_PEEK,
-            null.asInstanceOf[Ptr[sockaddr]],
-            null.asInstanceOf[Ptr[socklen_t]]
-          )
+      checkIoResult(nBytesRecvd, "recvfrom_2")
+      assertEquals("recvfrom_2 length", nBytesSent, nBytesRecvd)
 
-        checkRecvfromResult(nBytesPeekedAt, "recvfrom_1")
+      // Did packet came from where we expected, and not from Mars?
 
-        /* Friendlier code here and after the next recvfrom() would loop
-         *  on partial reads rather than fail.
-         *  Punt to a future generation. Since this is loopback and
-         *  writes are small, if any bytes are ready, all should be.
-         */
-        assertEquals("recvfrom_1 length", nBytesSent, nBytesPeekedAt)
+      val expectedAddr = out6Addr.asInstanceOf[Ptr[sockaddr_in6]]
 
-        // Test retrieving remote address.
-        val srcAddr = alloc[sockaddr_in6]()
-        val srcAddrLen = alloc[socklen_t]()
-        !srcAddrLen = sizeof[sockaddr_in6].toUInt
-
-        val nBytesRecvd =
-          recvfrom(
-            inSocket,
-            inData,
-            maxInData.toUInt,
-            0,
-            srcAddr.asInstanceOf[Ptr[sockaddr]],
-            srcAddrLen
-          )
-
-        checkRecvfromResult(nBytesRecvd, "recvfrom_2")
-        assertEquals("recvfrom_2 length", nBytesSent, nBytesRecvd)
-
-        // Did packet came from where we expected, and not from Mars?
-
-        val addrsMatch = {
-          0 == memcmp(
-            in6SockAddr.sin6_addr.at1.at(0).asInstanceOf[Ptr[Byte]],
-            srcAddr.sin6_addr.at1.at(0).asInstanceOf[Ptr[Byte]],
-            sizeof[in6_addr]
-          )
-        }
-
-        if (!addrsMatch) {
-          val expectedNtop = formatIn6addr(in6SockAddr.sin6_addr)
-          val gotNtop = formatIn6addr(srcAddr.sin6_addr)
-
-          val msg =
-            s"expected remote address: '${expectedNtop}' got: '${gotNtop}'"
-          fail(msg)
-        }
-
-        assertEquals("inData is not NUL terminated", 0, inData(nBytesRecvd))
-
-        // Are received contents good?
-        assertEquals("recvfrom content", outData, fromCString(inData))
-
-      } finally {
-        closeSocket(outSocket)
+      val addrsMatch = {
+        0 == memcmp(
+          expectedAddr.sin6_addr.at1.at(0).asInstanceOf[Ptr[Byte]],
+          srcAddr.sin6_addr.at1.at(0).asInstanceOf[Ptr[Byte]],
+          sizeof[in6_addr]
+        )
       }
+
+      if (!addrsMatch) {
+        val expectedNtop = formatIn6addr(expectedAddr.sin6_addr)
+        val gotNtop = formatIn6addr(srcAddr.sin6_addr)
+
+        val msg =
+          s"expected remote address: '${expectedNtop}' got: '${gotNtop}'"
+        fail(msg)
+      }
+
+      assertEquals("inData is not NUL terminated", 0, inData(nBytesRecvd))
+
+      // Are received contents good?
+      assertEquals("recvfrom content", outData, fromCString(inData))
     } finally {
-      closeSocket(inSocket)
+      SocketTestHelpers.closeSocket(inSocket)
+      SocketTestHelpers.closeSocket(outSocket)
     }
   }
 }
--- unit-tests/native/src/test/scala/scala/scalanative/posix/sys/UdpSocketTest.scala
+++ unit-tests/native/src/test/scala/scala/scalanative/posix/sys/UdpSocketTest.scala
@@ -4,18 +4,11 @@ package sys
 import scalanative.unsafe._
 import scalanative.unsigned._
 
-import scalanative.libc.errno
-import scalanative.libc.string.strerror
-
-import scalanative.posix.arpa.inet.inet_addr
-import scalanative.posix.fcntl.{F_SETFL, O_NONBLOCK}
+import scalanative.posix.errno
 import scalanative.posix.netinet.in._
 import scalanative.posix.netinet.inOps._
-import scalanative.posix.poll._
-import scalanative.posix.pollOps._
 import scalanative.posix.sys.socket._
 import scalanative.posix.sys.SocketTestHelpers._
-import scalanative.posix.unistd
 
 import scalanative.meta.LinktimeInfo.isWindows
 
@@ -31,147 +24,22 @@ import org.junit.Assume._
 import org.junit.Before
 
 class UdpSocketTest {
-
-  val isIPv4Available = hasLoopbackAddress(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
-
   @Before
   def before(): Unit = {
+    val isIPv4Available = hasLoopbackAddress(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
     assumeTrue("IPv4 UDP loopback is not available", isIPv4Available)
-  }
-
-  // For some unknown reason inlining content of this method leads to failures
-  // on Unix, probably due to bug in linktime conditions.
-  private def setSocketBlocking(socket: CInt): Unit = {
-    if (isWindows) {
-      val mode = stackalloc[CInt]()
-      !mode = 1
-      assertNotEquals(
-        "iotctl setBLocking",
-        -1,
-        ioctlSocket(socket.toPtr[Byte], FIONBIO, mode)
-      )
-    } else {
-      assertNotEquals(
-        s"fcntl set blocking",
-        -1,
-        fcntl.fcntl(socket, F_SETFL, O_NONBLOCK)
-      )
-    }
-  }
-
-  private def createAndCheckUdpSocket(): CInt = {
-    if (isWindows) {
-      val socket = WSASocketW(
-        addressFamily = AF_INET,
-        socketType = SOCK_DGRAM,
-        protocol = IPPROTO_UDP,
-        protocolInfo = null,
-        group = 0.toUInt,
-        flags = WSA_FLAG_OVERLAPPED
-      )
-      assertNotEquals("socket create", InvalidSocket, socket)
-      socket.toInt
-    } else {
-      val sock = sys.socket.socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
-      assertNotEquals("socket create", -1, sock)
-      sock
-    }
-  }
-
-  private def closeSocket(socket: CInt): Unit = {
-    if (isWindows) WinSocketApi.closeSocket(socket.toPtr[Byte])
-    else unistd.close(socket)
-  }
-
-  private def checkRecvfromResult(v: CSSize, label: String): Unit = {
-    if (v.toInt < 0) {
-      val reason =
-        if (isWindows) ErrorHandlingApiOps.errorMessage(GetLastError())
-        else fromCString(strerror(errno.errno))
-      fail(s"$label failed - $reason")
-    }
-  }
-
-  // Make available to Udp6SocketTest.scala
-  private[sys] def pollReadyToRecv(fd: CInt, timeout: CInt) = {
-    // timeout is in milliseconds
-
-    if (isWindows) {
-      val fds = stackalloc[WSAPollFd](1.toUSize)
-      fds.socket = fd.toPtr[Byte]
-      fds.events = WinSocketApiExt.POLLIN
-
-      val ret = WSAPoll(fds, 1.toUInt, timeout)
-
-      if (ret == 0) {
-        fail(s"poll timed out after ${timeout} milliseconds")
-      } else if (ret < 0) {
-        val reason = ErrorHandlingApiOps.errorMessage(GetLastError())
-        fail(s"poll for input failed - $reason")
-      }
-    } else {
-      val fds = stackalloc[struct_pollfd](1.toUSize)
-      (fds + 0).fd = fd
-      (fds + 0).events = pollEvents.POLLIN | pollEvents.POLLRDNORM
-
-      errno.errno = 0
-      // poll() sounds like a nasty busy wait loop, but is event driven in kernel
-
-      val ret = poll(fds, 1.toUInt, timeout)
-
-      if (ret == 0) {
-        fail(s"poll timed out after ${timeout} milliseconds")
-      } else if (ret < 0) {
-        val reason = fromCString(strerror(errno.errno))
-        fail(s"poll for input failed - $reason")
-      }
-      // else good to go
-    }
   }
 
   @Test def sendtoRecvfrom(): Unit = Zone { implicit z =>
     if (isWindows) {
       WinSocketApiOps.init()
     }
-    val localhost = c"127.0.0.1"
-    val localhostInetAddr = inet_addr(localhost)
 
-    val inSocket: CInt = createAndCheckUdpSocket()
+    val (inSocket, outSocket, outAddr) = getUdpLoopbackSockets(AF_INET)
 
     try {
-      val inAddr = alloc[sockaddr]()
-      val inAddrInPtr = inAddr.asInstanceOf[Ptr[sockaddr_in]]
-
-      inAddrInPtr.sin_family = AF_INET.toUShort
-      inAddrInPtr.sin_addr.s_addr = localhostInetAddr
-      // inAddrInPtr.sin_port is already the desired 0; "find a free port".
-
-      setSocketBlocking(inSocket)
-
-      // Get port for sendto() to use.
-      val bindStatus = bind(inSocket, inAddr, sizeof[sockaddr].toUInt)
-      assertNotEquals(s"bind failed,  errno: ${errno.errno}", -1, bindStatus)
-
-      val inAddrInfo = alloc[sockaddr]()
-      val gsnAddrLen = alloc[socklen_t]()
-      !gsnAddrLen = sizeof[sockaddr].toUInt
-
-      val gsnStatus = getsockname(inSocket, inAddrInfo, gsnAddrLen)
-      assertNotEquals("getsockname", -1, gsnStatus)
-
-      // Now use port.
-      val outSocket = createAndCheckUdpSocket()
-
-      try {
-        val outAddr = alloc[sockaddr]()
-        val outAddrInPtr = outAddr.asInstanceOf[Ptr[sockaddr_in]]
-        outAddrInPtr.sin_family = AF_INET.toUShort
-        outAddrInPtr.sin_addr.s_addr = localhostInetAddr
-        outAddrInPtr.sin_port =
-          inAddrInfo.asInstanceOf[Ptr[sockaddr_in]].sin_port
-
-        val outData =
-          """
+      val outData =
+        """
           |Four Freedoms -
           |   Freedom of speech
           |   Freedom of worship
@@ -179,72 +47,68 @@ class UdpSocketTest {
           |   Freedom from fear
           """.stripMargin
 
-        val nBytesSent = sendto(
-          outSocket,
-          toCString(outData),
-          outData.length.toUSize,
-          0,
-          outAddr,
-          sizeof[sockaddr].toUInt
+      val nBytesSent = sendto(
+        outSocket,
+        toCString(outData),
+        outData.length.toUSize,
+        0,
+        outAddr,
+        sizeof[sockaddr].toUInt
+      )
+
+      assertTrue(s"sendto failed errno: ${errno.errno}\n", (nBytesSent >= 0))
+      assertEquals("sendto", outData.size, nBytesSent.toInt)
+
+      // If inSocket did not get data by timeout, it probably never will.
+      pollReadyToRecv(inSocket, 30 * 1000) // assert fail on error or timeout
+
+      /// Two tests using one inbound packet, save test duplication.
+
+      // Provide extra room to allow detecting extra junk being sent.
+      val maxInData = 2 * outData.length
+      val inData: Ptr[Byte] = alloc[Byte](maxInData.toUSize)
+
+      // Test not fetching remote address. Exercise last two args as nulls.
+      val nBytesPeekedAt =
+        recvfrom(
+          inSocket,
+          inData,
+          maxInData.toUInt,
+          MSG_PEEK,
+          null.asInstanceOf[Ptr[sockaddr]],
+          null.asInstanceOf[Ptr[socklen_t]]
         )
 
-        assertTrue(s"sendto failed errno: ${errno.errno}\n", (nBytesSent >= 0))
-        assertEquals("sendto", outData.size, nBytesSent.toInt)
+      checkIoResult(nBytesPeekedAt, "recvfrom_1")
 
-        // If inSocket did not get data by timeout, it probably never will.
-        pollReadyToRecv(inSocket, 30 * 1000) // assert fail on error or timeout
+      // When sending a small UDP datagram, data will be sent in one shot.
+      assertEquals("recvfrom_1 length", nBytesSent, nBytesPeekedAt)
 
-        /// Two tests using one inbound packet, save test duplication.
+      // Test retrieving remote address.
+      val srcAddr = alloc[sockaddr]()
+      val srcAddrLen = alloc[socklen_t]()
+      !srcAddrLen = sizeof[sockaddr].toUInt
+      val nBytesRecvd =
+        recvfrom(inSocket, inData, maxInData.toUInt, 0, srcAddr, srcAddrLen)
 
-        // Provide extra room to allow detecting extra junk being sent.
-        val maxInData = 2 * outData.length
-        val inData: Ptr[Byte] = alloc[Byte](maxInData.toUSize)
+      checkIoResult(nBytesRecvd, "recvfrom_2")
+      assertEquals("recvfrom_2 length", nBytesSent, nBytesRecvd)
 
-        // Test not fetching remote address. Exercise last two args as nulls.
-        val nBytesPeekedAt =
-          recvfrom(
-            inSocket,
-            inData,
-            maxInData.toUInt,
-            MSG_PEEK,
-            null.asInstanceOf[Ptr[sockaddr]],
-            null.asInstanceOf[Ptr[socklen_t]]
-          )
-        checkRecvfromResult(nBytesPeekedAt, "recvfrom_1")
+      // Packet came from where we expected, and not Mars.
+      assertEquals(
+        "unexpected remote address",
+        outAddr.asInstanceOf[Ptr[sockaddr_in]].sin_addr.s_addr,
+        srcAddr.asInstanceOf[Ptr[sockaddr_in]].sin_addr.s_addr
+      )
 
-        // Friendlier code here and after the next recvfrom() would loop
-        // on partial reads rather than fail.
-        // Punt to a future generation. Since this is loopback and
-        // writes are small, if any bytes are ready, all should be.
-        assertEquals("recvfrom_1 length", nBytesSent, nBytesPeekedAt)
+      assertEquals("inData NUL termination", 0, inData(nBytesRecvd.toUSize))
 
-        // Test retrieving remote address.
-        val srcAddr = alloc[sockaddr]()
-        val srcAddrLen = alloc[socklen_t]()
-        !srcAddrLen = sizeof[sockaddr].toUInt
-        val nBytesRecvd =
-          recvfrom(inSocket, inData, maxInData.toUInt, 0, srcAddr, srcAddrLen)
-
-        checkRecvfromResult(nBytesRecvd, "recvfrom_2")
-        assertEquals("recvfrom_2 length", nBytesSent, nBytesRecvd)
-
-        // Packet came from where we expected, and not Mars.
-        assertEquals(
-          "unexpected remote address",
-          localhostInetAddr,
-          srcAddr.asInstanceOf[Ptr[sockaddr_in]].sin_addr.s_addr
-        )
-
-        assertEquals("inData NUL termination", 0, inData(nBytesRecvd.toUSize))
-
-        // Contents are good.
-        assertEquals("recvfrom content", outData, fromCString(inData))
-      } finally {
-        closeSocket(outSocket)
-      }
+      assertEquals("recvfrom content", outData, fromCString(inData))
+      // Contents are good.
 
     } finally {
-      closeSocket(inSocket)
+      SocketTestHelpers.closeSocket(inSocket)
+      SocketTestHelpers.closeSocket(outSocket)
     }
   }
 }
