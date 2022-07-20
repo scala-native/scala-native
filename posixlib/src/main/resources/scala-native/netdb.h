f198e86ee43cf8a941dd916ed1ce3c0b97b17c71
Lee Tibbert <lee.tibbert@gmail.com>
2022-07-20 10:59:33 -0400

    Windows needs socklen_t

--- posixlib/src/main/resources/scala-native/netdb.h
+++ posixlib/src/main/resources/scala-native/netdb.h
@@ -1,17 +1,18 @@
 #ifdef _WIN32
 #include <WinSock2.h>
+#include "sys/socket.h" // socklen_t
 #define strdup(arg1) _strdup(arg1);
 #else
 #include <netdb.h>
 #endif
 
 struct scalanative_addrinfo {
-    int ai_flags;                         /* Input flags.  */
-    int ai_family;                        /* Protocol family for socket.  */
-    int ai_socktype;                      /* Socket type.  */
-    int ai_protocol;                      /* Protocol for socket.  */
-    socklen_t ai_addrlen;                 /* Length of socket address.  */
-    struct scalanative_sockaddr *ai_addr; /* Socket address for socket.  */
-    char *ai_canonname; /* Canonical name for service location.  */
-    void *ai_next;      /* Pointer to next in list.  */
+    int ai_flags;         /* Input flags.  */
+    int ai_family;        /* Protocol family for socket.  */
+    int ai_socktype;      /* Socket type.  */
+    int ai_protocol;      /* Protocol for socket.  */
+    socklen_t ai_addrlen; /* Length of socket address.  */
+    void *ai_addr;        /* Socket address for socket.  */
+    char *ai_canonname;   /* Canonical name for service location.  */
+    void *ai_next;        /* Pointer to next in list.  */
 };
