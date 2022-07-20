2a288f5c6ee4f52c8c8e3cf7271bf3df227820bb
Lee Tibbert <lee.tibbert@gmail.com>
2022-07-20 19:11:10 -0400

    Simplify & shorten posixlib netdb code paths

--- posixlib/src/main/scala/scala/scalanative/posix/netdb.scala
+++ posixlib/src/main/scala/scala/scalanative/posix/netdb.scala
@@ -1,21 +1,49 @@
-package scala.scalanative.posix
+<package scala.scalanative.posix
 
 import scalanative.unsafe._
+
 import scalanative.posix.sys.socket
 import scalanative.posix.netinet.in
 
+import scalanative.runtime.Platform
+
+/** netdb.h for Scala
+ *  @see
+ *    [[https://scala-native.readthedocs.io/en/latest/lib/posixlib.html]]
+ */
 @extern
 object netdb {
-  type addrinfo = CStruct8[CInt, CInt, CInt, CInt, socket.socklen_t, Ptr[
-    socket.sockaddr
-  ], Ptr[CChar], Ptr[Byte]]
+  /* This is the Linux layout. FreeBSD & macOS have the same size but
+   * swap ai_addr and ai_cannonname. FreeBSD documents this, macOS tells
+   * whoppers: it documents the Linux order in man pages and implements
+   * the FreeBSD layout.
+   *
+   * Handled in netdbOps below.
+   */
 
-  @name("scalanative_freeaddrinfo")
+  /* _Static_assert code in netdb.c checks that Scala Native and operating
+   * system structute definitions match "close enough". If you change
+   * something here, please make the corresponding changes there.
+   */
+
+  type addrinfo = CStruct8[
+    CInt, // ai_flags
+    CInt, // ai_family
+    CInt, // ai_socktype
+    CInt, // ai_protocol
+    socket.socklen_t, // ai_addrlen
+    Ptr[socket.sockaddr], // ai_addr
+    Ptr[CChar], // ai_canonname
+    Ptr[Byte] // ai_next
+  ]
+
   def freeaddrinfo(addr: Ptr[addrinfo]): Unit = extern
 
-  // direct call to C
   def gai_strerror(errcode: CInt): CString = extern
 
+  /* To comply with POSIX, GAI needs scalaNative C help with null hints arg.
+   * One can not have executable code in an 'extern' object.
+   */
   @name("scalanative_getaddrinfo")
   def getaddrinfo(
       name: CString,
@@ -24,7 +52,6 @@ object netdb {
       res: Ptr[Ptr[addrinfo]]
   ): CInt = extern
 
-  // direct call to C
   def getnameinfo(
       addr: Ptr[socket.sockaddr],
       addrlen: socket.socklen_t,
@@ -89,14 +116,23 @@ object netdb {
 object netdbOps {
   import netdb._
 
+  final val useBsdAddrinfo = (Platform.isMac() || Platform.isFreeBSD())
+
   implicit class addrinfoOps(val ptr: Ptr[addrinfo]) extends AnyVal {
     def ai_flags: CInt = ptr._1
     def ai_family: CInt = ptr._2
     def ai_socktype: CInt = ptr._3
     def ai_protocol: CInt = ptr._4
     def ai_addrlen: socket.socklen_t = ptr._5
-    def ai_addr: Ptr[socket.sockaddr] = ptr._6
-    def ai_canonname: Ptr[CChar] = ptr._7
+
+    def ai_addr: Ptr[socket.sockaddr] =
+      if (!useBsdAddrinfo) ptr._6
+      else ptr._7.asInstanceOf[Ptr[socket.sockaddr]]
+
+    def ai_canonname: Ptr[CChar] =
+      if (!useBsdAddrinfo) ptr._7
+      else ptr._6.asInstanceOf[Ptr[CChar]]
+
     def ai_next: Ptr[Byte] = ptr._8
 
     def ai_flags_=(v: CInt): Unit = ptr._1 = v
@@ -104,8 +140,15 @@ object netdbOps {
     def ai_socktype_=(v: CInt): Unit = ptr._3 = v
     def ai_protocol_=(v: CInt): Unit = ptr._4 = v
     def ai_addrlen_=(v: socket.socklen_t): Unit = ptr._5 = v
-    def ai_addr_=(v: Ptr[socket.sockaddr]): Unit = ptr._6 = v
-    def ai_canonname_=(v: Ptr[CChar]): Unit = ptr._7 = v
+
+    def ai_addr_=(v: Ptr[socket.sockaddr]): Unit =
+      if (!useBsdAddrinfo) ptr._6 = v
+      else ptr._7 = v.asInstanceOf[Ptr[CChar]]
+
+    def ai_canonname_=(v: Ptr[CChar]): Unit =
+      if (!useBsdAddrinfo) ptr._7 = v
+      else ptr._6 = v.asInstanceOf[Ptr[socket.sockaddr]]
+
     def ai_next_=(v: Ptr[Byte]): Unit = ptr._8 = v
   }
 }
