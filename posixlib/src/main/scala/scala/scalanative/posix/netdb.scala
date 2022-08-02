6300840d866cf41ef8c22175a2e064c0930be2f7
LeeTibbert <LeeTibbert@users.noreply.github.com>
2022-08-02 18:02:19 -0400

    Simplify & shorten posixlib netdb code paths (#2743)
    * Simplify & shorten posixlib netdb code paths
    * Windows uses BSD style layout for struct addrinfo
    * Add Scaladoc giving URL to Open Group year & spec

--- posixlib/src/main/scala/scala/scalanative/posix/netdb.scala
+++ posixlib/src/main/scala/scala/scalanative/posix/netdb.scala
@@ -1,21 +1,49 @@
 package scala.scalanative.posix
 
 import scalanative.unsafe._
+
 import scalanative.posix.sys.socket
 import scalanative.posix.netinet.in
 
+import scalanative.runtime.Platform
+
+/** netdb.h for Scala
+ *  @see
+ *    [[https://scala-native.readthedocs.io/en/latest/lib/posixlib.html]]
+ */
 @extern
 object netdb {
-  type addrinfo = CStruct8[CInt, CInt, CInt, CInt, socket.socklen_t, Ptr[
-    socket.sockaddr
-  ], Ptr[CChar], Ptr[Byte]]
+  /* This is the Linux layout. FreeBSD, macOS, and Windows have the same
+   * size but swap ai_addr and ai_canonname. FreeBSD & Windows document this.
+   * macOS tells whoppers: it documents the Linux order in man pages and
+   * implements the FreeBSD layout.
+   *
+   * Access to the proper field for the OS is handled in netdbOps below.
+   */
 
-  @name("scalanative_freeaddrinfo")
+  /* _Static_assert code in netdb.c checks that Scala Native and operating
+   * system structure definitions match "close enough". If you change
+   * something here, please make the corresponding changes there.
+   */
+
+  type addrinfo = CStruct8[
+    CInt, // ai_flags
+    CInt, // ai_family
+    CInt, // ai_socktype
+    CInt, // ai_protocol
+    socket.socklen_t, // ai_addrlen
+    Ptr[socket.sockaddr], // ai_addr
+    Ptr[CChar], // ai_canonname
+    Ptr[Byte] // ai_next
+  ]
+
   def freeaddrinfo(addr: Ptr[addrinfo]): Unit = extern
 
-  // direct call to C
   def gai_strerror(errcode: CInt): CString = extern
 
+  /* To comply with POSIX, GAI needs scalaNative C help with null hints arg.
+   * One can not have executable code in an 'extern' object.
+   */
   @name("scalanative_getaddrinfo")
   def getaddrinfo(
       name: CString,
@@ -24,7 +52,6 @@ object netdb {
       res: Ptr[Ptr[addrinfo]]
   ): CInt = extern
 
-  // direct call to C
   def getnameinfo(
       addr: Ptr[socket.sockaddr],
       addrlen: socket.socklen_t,
@@ -86,17 +113,30 @@ object netdb {
   def EAI_OVERFLOW: CInt = extern
 }
 
+/** Allow using C names to access 'addrinfo' structure fields.
+ */
 object netdbOps {
   import netdb._
 
-  implicit class addrinfoOps(val ptr: Ptr[addrinfo]) extends AnyVal {
+  final val useBsdAddrinfo = (Platform.isMac() ||
+    Platform.isFreeBSD() ||
+    Platform.isWindows())
+
+  implicit class addrinfoOps(private val ptr: Ptr[addrinfo]) extends AnyVal {
     def ai_flags: CInt = ptr._1
     def ai_family: CInt = ptr._2
     def ai_socktype: CInt = ptr._3
     def ai_protocol: CInt = ptr._4
     def ai_addrlen: socket.socklen_t = ptr._5
-    def ai_addr: Ptr[socket.sockaddr] = ptr._6
-    def ai_canonname: Ptr[CChar] = ptr._7
+
+    def ai_addr: Ptr[socket.sockaddr] =
+      if (!useBsdAddrinfo) ptr._6
+      else ptr._7.asInstanceOf[Ptr[socket.sockaddr]]
+
+    def ai_canonname: Ptr[CChar] =
+      if (!useBsdAddrinfo) ptr._7
+      else ptr._6.asInstanceOf[Ptr[CChar]]
+
     def ai_next: Ptr[Byte] = ptr._8
 
     def ai_flags_=(v: CInt): Unit = ptr._1 = v
@@ -104,8 +144,15 @@ object netdbOps {
     def ai_socktype_=(v: CInt): Unit = ptr._3 = v
     def ai_protocol_=(v: CInt): Unit = ptr._4 = v
     def ai_addrlen_=(v: socket.socklen_t): Unit = ptr._5 = v
-    def ai_addr_=(v: Ptr[socket.sockaddr]): Unit = ptr._6 = v
-    def ai_canonname_=(v: Ptr[CChar]): Unit = ptr._7 = v
+
+    def ai_addr_=(v: Ptr[socket.sockaddr]): Unit =
+      if (!useBsdAddrinfo) ptr._6 = v
+      else ptr._7 = v.asInstanceOf[Ptr[CChar]]
+
+    def ai_canonname_=(v: Ptr[CChar]): Unit =
+      if (!useBsdAddrinfo) ptr._7 = v
+      else ptr._6 = v.asInstanceOf[Ptr[socket.sockaddr]]
+
     def ai_next_=(v: Ptr[Byte]): Unit = ptr._8 = v
   }
 }
