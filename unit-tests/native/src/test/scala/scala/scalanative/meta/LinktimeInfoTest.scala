504e97293ccd4b1c8941d3490581c923ff366a0e
Arman Bilge <armanbilge@gmail.com>
2022-08-29 13:43:43 +0000

    Add ()

--- unit-tests/native/src/test/scala/scala/scalanative/meta/LinktimeInfoTest.scala
+++ unit-tests/native/src/test/scala/scala/scalanative/meta/LinktimeInfoTest.scala
@@ -8,10 +8,10 @@ import org.junit.Assert._
 class LinktimeInfoTest {
 
   @Test def testOS(): Unit = {
-    assertEquals(Platform.isFreeBSD, LinktimeInfo.isFreeBSD)
-    assertEquals(Platform.isLinux, LinktimeInfo.isLinux)
-    assertEquals(Platform.isMac, LinktimeInfo.isMac)
-    assertEquals(Platform.isWindows, LinktimeInfo.isWindows)
+    assertEquals(Platform.isFreeBSD(), LinktimeInfo.isFreeBSD)
+    assertEquals(Platform.isLinux(), LinktimeInfo.isLinux)
+    assertEquals(Platform.isMac(), LinktimeInfo.isMac)
+    assertEquals(Platform.isWindows(), LinktimeInfo.isWindows)
   }
 
 }
