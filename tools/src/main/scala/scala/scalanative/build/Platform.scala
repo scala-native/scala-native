210eedd3958fc1e1ac9bd354d8534d5b826b6219
Arman Bilge <armanbilge@gmail.com>
2022-08-29 17:40:57 +0200

    Add `LinktimeInfo.is{FreeBSD,Mac,Linux}` (#2809)
    * Add `LinktimeInfo.is{FreeBSD,Mac,Linux}`

--- tools/src/main/scala/scala/scalanative/build/Platform.scala
+++ tools/src/main/scala/scala/scalanative/build/Platform.scala
@@ -7,6 +7,8 @@ object Platform {
     System.getProperty("os.name", "unknown").toLowerCase(Locale.ROOT)
 
   lazy val isWindows: Boolean = osUsed.startsWith("windows")
-  lazy val isUnix: Boolean = osUsed.contains("linux") || osUsed.contains("unix")
+  lazy val isLinux: Boolean = osUsed.contains("linux")
+  lazy val isUnix: Boolean = isLinux || osUsed.contains("unix")
   lazy val isMac: Boolean = osUsed.contains("mac")
+  lazy val isFreeBSD: Boolean = osUsed.contains("freebsd")
 }
