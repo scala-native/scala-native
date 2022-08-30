a46228c836a826dadc6dcdb46907a774572fae7d
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-08-30 23:10:05 +0200

    Update scalafmt config

--- unit-tests/shared/src/test/scala-3.2/scala/Scala3_2_StdLibTest.scala
+++ unit-tests/shared/src/test/scala-3.2/scala/Scala3_2_StdLibTest.scala
@@ -5,14 +5,14 @@ import org.junit.Assert._
 import scala.scalanative.junit.utils.AssertThrows.assertThrows
 
 class Scala3_2_StdLibTest:
-  // Usage of methods added in Scala 3.2 
+  // Usage of methods added in Scala 3.2
   // to make sure scalalib was compiled with correct sources
   @Test def canLinkScalaVersionSpecificMethods(): Unit = {
     assertThrows(
-      classOf[NullPointerException], 
+      classOf[NullPointerException],
       scala.runtime.Scala3RunTime.nnFail()
     )
 
     val mirror = new scala.runtime.TupleMirror(2)
-    assertNotNull(mirror.fromProduct((3,2)))
+    assertNotNull(mirror.fromProduct((3, 2)))
   }
