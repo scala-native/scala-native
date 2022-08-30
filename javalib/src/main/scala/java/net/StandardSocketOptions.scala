46270f34e0f133e97cab3a0706601063e3663d95
Lee Tibbert <lee.tibbert@gmail.com>
2022-08-29 23:54:29 -0400

    Fix #2814: 0.4.x: Port and extend StandardSocketOptions

---
+++ javalib/src/main/scala/java/net/StandardSocketOptions.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2022 Arman Bilge
+ *
+ * Original code is from the armanbilge/epollcat project at
+ * https://github.com/armanbilge/epollcat/
+ *
+ * For full original license notice , see:
+ *     https://github.com/scala-native/scala-native/blob/main/LICENSE.md
+ *
+ * Additional code provided by the Scala Native project carries the
+ * Scala Native license, described in the same LICENSE.md.
+ */
+
+package java.net
+
+object StandardSocketOptions {
+
+  val IP_MULTICAST_IF: SocketOption[java.net.NetworkInterface] =
+    new StdSocketOption("IP_MULTICAST_IF", classOf)
+
+  val IP_MULTICAST_LOOP: SocketOption[java.lang.Boolean] =
+    new StdSocketOption("IP_MULTICAST_LOOP", classOf)
+
+  val IP_MULTICAST_TTL: SocketOption[java.lang.Integer] =
+    new StdSocketOption("IP_MULTICAST_TTL", classOf)
+
+  val IP_TOS: SocketOption[java.lang.Integer] =
+    new StdSocketOption("IP_TOS", classOf)
+
+  val SO_BROADCAST: SocketOption[java.lang.Boolean] =
+    new StdSocketOption("SO_BROADCAST", classOf)
+
+  val SO_KEEPALIVE: SocketOption[java.lang.Boolean] =
+    new StdSocketOption("SO_KEEPALIVE", classOf)
+
+  val SO_LINGER: SocketOption[java.lang.Integer] =
+    new StdSocketOption("SO_LINGER", classOf)
+
+  val SO_RCVBUF: SocketOption[java.lang.Integer] =
+    new StdSocketOption("SO_RCVBUF", classOf)
+
+  val SO_REUSEADDR: SocketOption[java.lang.Boolean] =
+    new StdSocketOption("SO_REUSEADDR", classOf)
+
+  val SO_REUSEPORT: SocketOption[java.lang.Boolean] =
+    new StdSocketOption("SO_REUSEPORT", classOf)
+
+  val SO_SNDBUF: SocketOption[java.lang.Integer] =
+    new StdSocketOption("SO_SNDBUF", classOf)
+
+  val TCP_NODELAY: SocketOption[java.lang.Boolean] =
+    new StdSocketOption("TCP_NODELAY", classOf)
+
+  private final class StdSocketOption[T](val name: String, val `type`: Class[T])
+      extends SocketOption[T] {
+    override def toString = name
+  }
+}
