ddb96f1c9246751d7c902a5f8919df3e0624c9a6
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-08-31 14:14:43 +0200

    Update version to 3.2.0

--- project/ScalaVersions.scala
+++ project/ScalaVersions.scala
@@ -5,7 +5,7 @@ object ScalaVersions {
   val crossScala211 = Seq("2.11.12")
   val crossScala212 = Seq("2.12.13", "2.12.14", "2.12.15", "2.12.16")
   val crossScala213 = Seq("2.13.4", "2.13.5", "2.13.6", "2.13.7", "2.13.8")
-  val crossScala3 = Seq("3.1.0", "3.1.1", "3.1.2", "3.1.3", "3.2.0-RC4")
+  val crossScala3 = Seq("3.1.0", "3.1.1", "3.1.2", "3.1.3", "3.2.0")
 
   // Version of Scala 3 standard library sources used for publishing
   // Workaround allowing to produce NIR for Scala 3.2.x+ and allowing to consume existing libraries using 3.1.x
