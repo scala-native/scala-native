b0d2fccbdef24cc891942c457e632779cf272e77
Anton Sviridov <keynmol@gmail.com>
2022-09-19 20:22:59 +0100

    Publish local command for easier local development

--- project/Commands.scala
+++ project/Commands.scala
@@ -13,7 +13,8 @@ object Commands {
     "test-tools" ::
       "test-mima" ::
       "test-runtime" ::
-      "test-scripted" :: _
+      "test-scripted" ::
+      "publish-local-dev" :: _
   }
 
   lazy val testRuntime = projectVersionCommand("test-runtime") {
@@ -114,6 +115,65 @@ object Commands {
           )
 
         fn(version, state)
+    }
+  }
+
+  private def projectFullVersionCommand(
+      name: String
+  )(fn: (String, State) => State): Command = {
+    Command.args(name, "<args>") {
+      case (state, args) =>
+        val version = args.headOption
+          .getOrElse(
+            "Used command needs explicit full Scala version as an argument"
+          )
+
+        fn(version, state)
+    }
+  }
+
+  lazy val publishLocalDev = {
+    projectFullVersionCommand("publish-local-dev") {
+      case (version, state) =>
+        val binaryVersion = CrossVersion.binaryScalaVersion(version)
+
+        val sbtPluginModules = List(
+          Build.util,
+          nir,
+          tools,
+          testRunner,
+          testInterface,
+          testInterfaceSbtDefs,
+          junitRuntime,
+          nativelib,
+          clib,
+          posixlib,
+          windowslib,
+          auxlib,
+          javalib,
+          scalalib
+        ).map(_.forBinaryVersion("2.12").id).map(id => s"$id/publishLocal")
+
+        val compilerPlugin = nscPlugin.forBinaryVersion(binaryVersion).id
+
+        val runtimeModules = List(
+          nativelib,
+          clib,
+          posixlib,
+          windowslib,
+          auxlib,
+          javalib,
+          scalalib,
+          testInterface,
+          junitRuntime,
+          testInterfaceSbtDefs
+        ).map(_.forBinaryVersion(binaryVersion).id)
+          .map(id => s"$id/publishLocal")
+
+        sbtPluginModules :::
+          List(s"++${version} $compilerPlugin/publishLocal") :::
+          runtimeModules :::
+          state
     }
   }
 
