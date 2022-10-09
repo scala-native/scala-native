b0c9fefba9ed6ca975eaaae9750b1553947180bb
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-10-09 23:20:49 +0200

    Add Scala 2.12.17 and Scala 2.13.10

--- project/ScalaVersions.scala
+++ project/ScalaVersions.scala
@@ -3,8 +3,8 @@ package build
 object ScalaVersions {
   // Versions of Scala used for publishing compiler plugins
   val crossScala211 = Seq("2.11.12")
-  val crossScala212 = Seq("2.12.13", "2.12.14", "2.12.15", "2.12.16")
-  val crossScala213 = Seq("2.13.4", "2.13.5", "2.13.6", "2.13.7", "2.13.8")
+  val crossScala212 = Seq("2.12.13", "2.12.14", "2.12.15", "2.12.16", "2.12.17")
+  val crossScala213 = Seq("2.13.4", "2.13.5", "2.13.6", "2.13.7", "2.13.8", "2.13.9", "2.13.10")
   val crossScala3 = Seq("3.1.0", "3.1.1", "3.1.2", "3.1.3", "3.2.0")
 
   // Version of Scala 3 standard library sources used for publishing
