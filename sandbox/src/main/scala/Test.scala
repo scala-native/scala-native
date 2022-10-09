86552221ac7f48938984eef536c3eee3bddd2975
Anton Sviridov <keynmol@gmail.com>
2022-10-09 17:00:39 +0100

    Return sandbox to the way it was

--- sandbox/src/main/scala/Test.scala
+++ sandbox/src/main/scala/Test.scala
@@ -1,17 +1,5 @@
-package sandbox.test
-
-object SANDBOX_TEST {
-  def test() = throw new Exception("what")
-  def hello() = 25
-  def foo() = {
-    println("yo")
-    hello()
-
-    test()
-  }
+object Test {
   def main(args: Array[String]): Unit = {
     println("Hello, World!")
-
-    foo()
   }
 }
