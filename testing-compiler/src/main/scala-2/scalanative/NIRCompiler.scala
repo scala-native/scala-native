c058c46590954e0430ae69242f48192b0d53ab8d
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-10-10 12:20:33 +0200

    Fix source incompatibilites for CommandLineParser

--- testing-compiler/src/main/scala-2/scalanative/NIRCompiler.scala
+++ testing-compiler/src/main/scala-2/scalanative/NIRCompiler.scala
@@ -2,7 +2,7 @@ package scala.scalanative
 
 import scala.reflect.internal.util.{BatchSourceFile, NoFile, SourceFile}
 import scala.reflect.internal.util.Position
-import scala.tools.cmd.CommandLineParser
+import scala.scalanative.compat.ParserCompat.parser
 import scala.tools.nsc.{CompilerCommand, Global, Settings}
 import scala.tools.nsc.io.AbstractFile
 import java.nio.file.{Files, Path}
@@ -96,8 +96,7 @@ class NIRCompiler(outputDir: Path) extends api.NIRCompiler {
     // Also, using `command.settings.outputDirs.setSingleOutput` I get strange classpath problems.
     // What's even stranger, is that everything works fine using `-d`!
     val outPath = outputDir.toAbsolutePath
-    val arguments =
-      CommandLineParser.tokenize(s"-d $outPath " + (options mkString " "))
+    val arguments = parser.tokenize(s"-d $outPath " + (options mkString " "))
     val command = new CompilerCommand(arguments.toList, reportError _)
     val reporter = new TestReporter(command.settings)
 
