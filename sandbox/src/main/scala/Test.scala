a2a664af3916ed3b53538ffd61c978e1062d762d
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-10-28 00:37:22 +0200

    Reproduce issue #2858

--- sandbox/src/main/scala/Test.scala
+++ sandbox/src/main/scala/Test.scala
@@ -1,5 +1,13 @@
-object Test {
+//> using scala "2"
+//> using lib "dev.zio::izumi-reflect::2.2.0"
+//> using option "-Xprint:typer"
+
+import izumi.reflect._
+
+object Main {
+  def foo[T: Tag] = ???
   def main(args: Array[String]): Unit = {
-    println("Hello, World!")
+    println(foo)
   }
+
 }
