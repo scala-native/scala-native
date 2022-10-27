7f0f751880d6ec97ed84582f6b13f25e2bbfadab
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-10-28 01:46:46 +0200

    Revert spurious changes

--- sandbox/src/main/scala/Test.scala
+++ sandbox/src/main/scala/Test.scala
@@ -1,13 +1,5 @@
-//> using scala "2"
-//> using lib "dev.zio::izumi-reflect::2.2.0"
-//> using option "-Xprint:typer"
-
-import izumi.reflect._
-
-object Main {
-  def foo[T: Tag] = ???
+object Test {
   def main(args: Array[String]): Unit = {
-    println(foo)
+    println("Hello, World!")
   }
-
 }
