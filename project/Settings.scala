a217363c302c781676a623837d2db4083c30440d
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-10-30 00:49:09 +0200

    Ci/release support (#38)
    * Update ations/cache to v3
    
    * Replaced deprecated set-output commands
    
    * Update versions of zlib and boehm gc for multiarch builds
    
    * Replace ammonite script with scala-cli due to depreacted node.js runner in actions
    
    * Upgrade multiach linux version to 22.04
    
    * Don't use tmp directory in dockerfile
    
    * Move building deps to dedicated script
    
    * Try fix windows setup
    
    * Fix generating hash for docker cache
    
    * try debug reason for failrue
    
    * Revert "try debug reason for failrue"
    
    This reverts commit f56a3e074c106147548ced367b6d6cb7d4c63287.
    
    * try fix
    
    * try debug
    
    * try debug
    
    * Downgrade back to ubuntu 20.04
    
    * try debug
    
    * try debug
    
    * try debug
    
    * fix typo
    
    * try debug
    
    * downgrade boehm to v8.0.6
    
    * try fix
    
    * try debug
    
    * try debug
    
    * Try build without boehm
    
    * try debug
    
    * asd
    
    * Install gcc10
    
    * try fix thin lto
    
    * Disable lto none
    
    * try fix
    
    * ads
    
    * asd
    
    * asd
    
    * asd
    
    * try fix
    
    * asd
    
    * try fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * asd
    
    * Revert "asd"
    
    This reverts commit 94667f9aac839894b599e64cfc898070c4a5458f.
    
    * debug
    
    * try fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * fix build
    
    * try fix
    
    * try fix
    
    * try fix
    
    * cleanup
    
    * try fix
    
    * try fix
    
    * try debug
    
    * try fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * fix typo
    
    * try fix
    
    * fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * try fix
    
    * Cleanup, restore flows
    
    * Move building aggreation to Build, remove helper bash scripts
    
    * Add publish flow
    
    * Use sbt-pgp instead of sbt-ci-release
    
    * try set ci
    
    * fix typo

--- project/Settings.scala
+++ project/Settings.scala
@@ -5,10 +5,13 @@ import sbt.Keys._
 import sbt.nio.Keys.fileTreeView
 import com.typesafe.tools.mima.core._
 import com.typesafe.tools.mima.plugin.MimaPlugin.autoImport._
+import com.jsuereth.sbtpgp.PgpKeys.publishSigned
 import scala.scalanative.sbtplugin.ScalaNativePlugin.autoImport._
 import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
+
 import sbtbuildinfo.BuildInfoPlugin.autoImport._
 import ScriptedPlugin.autoImport._
+import Build.{crossPublishSigned, crossPublishLocal}
 
 import scala.collection.mutable
 import scala.scalanative.build.Platform
@@ -193,6 +196,12 @@ object Settings {
       name = "Denys Shabalin",
       url = url("http://den.sh")
     ),
+    developers += Developer(
+      id = "wojciechmazur",
+      name = "Wojciech Mazur",
+      email = "wmazur@virtuslab.com",
+      url = url("https://github.com/WojciechMazur")
+    ),
     scmInfo := Some(
       ScmInfo(
         browseUrl = url("https://github.com/scala-native/scala-native"),
@@ -222,11 +231,14 @@ object Settings {
     },
     credentials ++= {
       for {
-        realm <- sys.env.get("MAVEN_REALM")
-        domain <- sys.env.get("MAVEN_DOMAIN")
         user <- sys.env.get("MAVEN_USER")
         password <- sys.env.get("MAVEN_PASSWORD")
-      } yield Credentials(realm, domain, user, password)
+      } yield Credentials(
+        realm = "Sonatype Nexus Repository Manager",
+        host = "oss.sonatype.org",
+        userName = user,
+        passwd = password
+      )
     }.toSeq
   )
 
@@ -410,8 +422,42 @@ object Settings {
     crossVersion := CrossVersion.full,
     libraryDependencies ++= Deps.compilerPluginDependencies(scalaVersion.value),
     mavenPublishSettings,
-    exportJars := true
+    exportJars := true,
+    crossPublishSigned := crossPublishCompilerPlugin(publishSigned).value,
+    crossPublishLocal := crossPublishCompilerPlugin(publishLocal).value
   )
+
+  private def crossPublishCompilerPlugin(publishKey: TaskKey[Unit]) = Def.task {
+    val currentVersion = scalaVersion.value
+    val s = state.value
+    val extracted = sbt.Project.extract(s)
+    val id = thisProjectRef.value.project
+    val selfRef = thisProjectRef.value
+    val _ = crossScalaVersions.value.foldLeft(s) {
+      case (state, `currentVersion`) =>
+        println(
+          s"Skip publish $id ${currentVersion} - it should be already published"
+        )
+        state
+      case (state, crossVersion) =>
+        println(s"Try publish $id ${crossVersion}")
+        val (newState, result) = sbt.Project
+          .runTask(
+            selfRef / publishKey,
+            state = extracted.appendWithSession(
+              Seq(
+                selfRef / scalaVersion := crossVersion
+              ),
+              state
+            )
+          )
+          .get
+        result.toEither match {
+          case Left(value) => throw new RuntimeException(value.toString)
+          case Right(_)    => newState
+        }
+    }
+  }
 
   lazy val sbtPluginSettings = Def.settings(
     commonSettings,
