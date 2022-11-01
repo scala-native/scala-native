b9a220a3dc2c8ed7f6f07a71ee5a8bdefcc34767
LeeTibbert <LeeTibbert@users.noreply.github.com>
2022-11-01 17:22:56 -0400

    Fix #2893: Implement posixlib wait.scala (#2969)
    * Fix #2893: Implement posixlib wait.scala
    * Silly Rabbit! Do not run POSIX tests on Windows

---
+++ posixlib/src/main/resources/scala-native/sys/wait.c
@@ -0,0 +1,40 @@
+#if !defined(_WIN32)
+
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+// Symbolic constants, roughly in POSIX declaration order
+
+// idtype_t
+int scalanative_c_p_all() { return P_ALL; }   // POSIX enum: idtype_t
+int scalanative_c_p_pgid() { return P_PGID; } // POSIX enum: idtype_t
+int scalanative_c_p_pid() { return P_PID; }   // POSIX enum: idtype_t
+
+// "constants" for waitpid()
+
+int scalanative_c_wcontinued() { return WCONTINUED; }
+int scalanative_c_wnohang() { return WNOHANG; }
+int scalanative_c_wuntraced() { return WUNTRACED; }
+
+// "constants" for waitid() options
+int scalanative_c_wexited() { return WEXITED; }
+int scalanative_c_wnowait() { return WNOWAIT; }
+int scalanative_c_wstopped() { return WSTOPPED; }
+
+// POSIX "Macros"
+int scalanative_c_wexitstatus(int wstatus) { return WEXITSTATUS(wstatus); }
+
+bool scalanative_c_wifcontinued(int wstatus) { return WIFCONTINUED(wstatus); }
+
+bool scalanative_c_wifexited(int wstatus) { return WIFEXITED(wstatus); }
+
+bool scalanative_c_wifsignaled(int wstatus) { return WIFSIGNALED(wstatus); }
+
+bool scalanative_c_wifstopped(int wstatus) { return WIFSTOPPED(wstatus); }
+
+int scalanative_c_wstopsig(int wstatus) { return WSTOPSIG(wstatus); }
+
+int scalanative_c_wtermsig(int wstatus) { return WTERMSIG(wstatus); }
+
+#endif // !_WIN32
---
+++ unit-tests/native/src/test/scala/scala/scalanative/posix/sys/WaitTest.scala
@@ -0,0 +1,77 @@
+package scala.scalanative.posix
+package sys
+
+import scala.scalanative.unsafe._
+import scala.scalanative.unsigned.UnsignedRichInt
+
+import scala.scalanative.posix.sys.wait._
+
+import scala.scalanative.meta.LinktimeInfo.isWindows
+
+import org.junit.Test
+import org.junit.Assert._
+
+/* Design Note:
+ *     By their definition, these "wait" methods block the current thread.
+ *     They are also defined without a timeout value.
+ *
+ *     The ppoll/epoll/kevent methods needed for the classical
+ *     "block in ppoll/epoll/kevent until either child exits or
+ *     a specified timeout expires, call wait/waitpid/waitid" approach
+ *     is not available. ppoll, epoll, and kevent are not defined in POSIX
+ *     and are operating system specific. They are not implemented in
+ *     Scala Native.
+ *
+ *     These conditions make proper unit-testing difficult.
+ *
+ *     "waitpid()" is/will be well exercise in javalib ProcessTest.
+ *     To keep concerns separate, ProcessTest can not exercise both
+ *     "waitpid()" & "waitid()".
+ *
+ *     Tests for "wait()" & "waitid()" are left as an exercise for the
+ *     reader. Beware that one does not hang the entire Continuous Integration
+ *     build.
+ */
+
+class WaitTest {
+
+  /* The major purpose of this file is the above Design Note.
+   * As long as we are here, might as well do some work.
+   */
+
+  def blackHole(a: Any): Unit = ()
+
+  @Test def waitBindingsShouldCompileAndLink(): Unit = {
+    if (!isWindows) {
+      // zero initialized placeholder
+      val wstatus = stackalloc[CInt](1.toUSize)
+
+      // idtype_t
+      blackHole(P_ALL)
+      blackHole(P_PGID)
+      blackHole(P_PID)
+
+// Symbolic constants, roughly in POSIX declaration order
+
+      // "constants" for waitpid()
+
+      blackHole(WCONTINUED) // XSI
+      blackHole(WNOHANG)
+      blackHole(WUNTRACED)
+
+      // "constants" for waitid() options
+      blackHole(WEXITED)
+      blackHole(WNOWAIT)
+      blackHole(WSTOPPED)
+
+// POSIX "Macros"
+      WEXITSTATUS(!wstatus)
+      WIFCONTINUED(!wstatus) // XSI
+      WIFEXITED(!wstatus)
+      WIFSIGNALED(!wstatus)
+      WIFSTOPPED(!wstatus)
+      WSTOPSIG(!wstatus)
+      WTERMSIG(!wstatus)
+    }
+  }
+}
