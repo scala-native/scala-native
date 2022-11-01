32244ae649590481dc2dfb9fd9391c1837a387aa
Lee Tibbert <lee.tibbert@gmail.com>
2022-11-01 15:03:36 -0400

    Silly Rabbit! Do not run POSIX tests on Windows

--- unit-tests/native/src/test/scala/scala/scalanative/posix/sys/WaitTest.scala
+++ unit-tests/native/src/test/scala/scala/scalanative/posix/sys/WaitTest.scala
@@ -2,9 +2,11 @@ package scala.scalanative.posix
 package sys
 
 import scala.scalanative.unsafe._
-import scalanative.unsigned.UnsignedRichInt
+import scala.scalanative.unsigned.UnsignedRichInt
 
 import scala.scalanative.posix.sys.wait._
+
+import scala.scalanative.meta.LinktimeInfo.isWindows
 
 import org.junit.Test
 import org.junit.Assert._
@@ -40,35 +42,36 @@ class WaitTest {
   def blackHole(a: Any): Unit = ()
 
   @Test def waitBindingsShouldCompileAndLink(): Unit = {
-    // zero initialized placeholder
-    val wstatus = stackalloc[CInt](1.toUSize)
+    if (!isWindows) {
+      // zero initialized placeholder
+      val wstatus = stackalloc[CInt](1.toUSize)
 
-    // idtype_t
-    blackHole(P_ALL)
-    blackHole(P_PGID)
-    blackHole(P_PID)
+      // idtype_t
+      blackHole(P_ALL)
+      blackHole(P_PGID)
+      blackHole(P_PID)
 
 // Symbolic constants, roughly in POSIX declaration order
 
-    // "constants" for waitpid()
+      // "constants" for waitpid()
 
-    blackHole(WCONTINUED) // XSI
-    blackHole(WNOHANG)
-    blackHole(WUNTRACED)
+      blackHole(WCONTINUED) // XSI
+      blackHole(WNOHANG)
+      blackHole(WUNTRACED)
 
-    // "constants" for waitid() options
-    blackHole(WEXITED)
-    blackHole(WNOWAIT)
-    blackHole(WSTOPPED)
+      // "constants" for waitid() options
+      blackHole(WEXITED)
+      blackHole(WNOWAIT)
+      blackHole(WSTOPPED)
 
 // POSIX "Macros"
-    WEXITSTATUS(!wstatus)
-    WIFCONTINUED(!wstatus) // XSI
-    WIFEXITED(!wstatus)
-    WIFSIGNALED(!wstatus)
-    WIFSTOPPED(!wstatus)
-    WSTOPSIG(!wstatus)
-    WTERMSIG(!wstatus)
+      WEXITSTATUS(!wstatus)
+      WIFCONTINUED(!wstatus) // XSI
+      WIFEXITED(!wstatus)
+      WIFSIGNALED(!wstatus)
+      WIFSTOPPED(!wstatus)
+      WSTOPSIG(!wstatus)
+      WTERMSIG(!wstatus)
+    }
   }
-
 }
