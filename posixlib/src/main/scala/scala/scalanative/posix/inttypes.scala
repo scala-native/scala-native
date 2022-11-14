222cac041b807785153a69ae7d956445403d1d59
ekrich <ekrichardson@gmail.com>
2022-11-14 09:09:57 -0800

    Address the inttype comments

--- posixlib/src/main/scala/scala/scalanative/posix/inttypes.scala
+++ posixlib/src/main/scala/scala/scalanative/posix/inttypes.scala
@@ -4,10 +4,23 @@ package posix
 import scala.scalanative.unsafe._
 import scala.scalanative.unsigned.{UByte, UInt, ULong, UShort}
 
+/** Represents the POSIX inttypes.
+ *
+ *  The <inttypes.h> header shall include the <stdint.h> header.
+ */
 @extern object inttypes extends inttypes
 
 @extern trait inttypes extends libc.inttypes {
-  // should be in stdint, optional in C stdint
+  /* These should be in POSIX stdint too. They are optional in C
+   * stdint so they could be inherited from there. They are here
+   * now so leaving them as is.
+   *
+   * There is also the consideration that importing
+   * both inttypes and stdint could create a conflict
+   * so it is unclear the path forward for types in general.
+   * this is also true for any other API that includes
+   * them as a type for convenience to the end user.
+   */
   type uint8_t = UByte
   type uint16_t = UShort
   type uint32_t = UInt
