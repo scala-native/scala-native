9d89270ba4b9b804901cd8ef02ecfae024971c18
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-11-16 11:31:24 +0100

    Remove stale native-code directories after changes to the classpath

--- tools/src/main/scala/scala/scalanative/build/core/NativeLib.scala
+++ tools/src/main/scala/scala/scalanative/build/core/NativeLib.scala
@@ -57,6 +57,7 @@ private[scalanative] object NativeLib {
   def findNativeLibs(config: Config): Seq[NativeLib] = {
     val workdir = config.workdir
     val classpath = config.classPath
+    val nativeCodePrefix = "native-code"
 
     val nativeLibPaths = classpath.flatMap { path =>
       if (isJar(path)) readJar(path)
@@ -72,7 +73,7 @@ private[scalanative] object NativeLib {
             .stripSuffix(jarExt)
         NativeLib(
           src = path,
-          dest = workdir.resolve(s"native-code-$name-$index")
+          dest = workdir.resolve(s"$nativeCodePrefix-$name-$index")
         )
       }
 
@@ -80,8 +81,19 @@ private[scalanative] object NativeLib {
       throw new BuildException(
         s"No Scala Native libraries were found: $classpath"
       )
-    else
+    else {
+      // Fix https://github.com/scala-native/scala-native/pull/2998#discussion_r1023715815
+      // Remove all stale native-code-* directories. These can be created if classpath would change
+      val expectedPaths = extractPaths.map(_.dest.toAbsolutePath()).toSet
+      val nativeCodePattern = raw"$nativeCodePrefix-.*-\d+"
+      Files
+        .list(workdir)
+        .filter(_.getFileName().toString() matches nativeCodePattern)
+        .filter(p => !expectedPaths.contains(p.toAbsolutePath()))
+        .forEach(IO.deleteRecursive(_))
+
       extractPaths
+    }
   }
 
   /** Find the native file paths for this native library
