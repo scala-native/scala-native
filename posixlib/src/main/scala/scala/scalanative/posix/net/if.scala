c95bc79bb8ac5e92d5e138635a3c08ef9cdc7bc4
LeeTibbert <LeeTibbert@users.noreply.github.com>
2022-11-16 11:54:31 -0500

    Implement posixlib net/if.{scala,c} (#3000)
    * Implement posixlib net/if.{scala,c}
    
    * Update the library documentation
    
    * Take a guess as to the include Windows wants/needs
    
    * Try for a better Windows guess
    
    * Another pass as getting Windows to compile

---
+++ posixlib/src/main/scala/scala/scalanative/posix/net/if.scala
@@ -0,0 +1,41 @@
+package scala.scalanative
+package posix
+package net
+
+import scalanative.unsafe._
+
+/** POSIX if.h for Scala
+ *
+ *  The Open Group Base Specifications
+ *  [[https://pubs.opengroup.org/onlinepubs/9699919799 Issue 7, 2018]] edition.
+ */
+@extern object `if` {
+
+  type if_nameindex = CStruct2[
+    CUnsignedInt, // if_index
+    CString // if_name
+  ]
+
+  // Symbolic constants
+
+  @name("scalanative_if_namesize")
+  def IF_NAMESIZE: CInt = extern
+
+  // Methods
+
+  def if_freenameindex(ptr: Ptr[if_nameindex]): Unit = extern
+  def if_indextoname(ifindex: CUnsignedInt, ifname: Ptr[Byte]): CString =
+    extern
+  def if_nameindex(): Ptr[if_nameindex] = extern
+  def if_nametoindex(ifname: CString): CUnsignedInt = extern
+}
+
+object ifOps {
+  import `if`.if_nameindex
+
+  implicit class ifOps(private val ptr: Ptr[if_nameindex]) extends AnyVal {
+    def if_index: CUnsignedInt = ptr._1
+    def if_name: CString = ptr._2
+    // These are used as read-only fields, so no Ops here to set them.
+  }
+}
