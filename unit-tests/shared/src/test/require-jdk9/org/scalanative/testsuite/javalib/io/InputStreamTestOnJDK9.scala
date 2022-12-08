1de4314b99e8b717da4f01d8d96eb5162732cb5d
Lee Tibbert <lee.tibbert@gmail.com>
2022-12-07 20:39:14 -0500

    Fix #2973: Implement some requested j.io.InputStream Java 9 &11 methods

---
+++ unit-tests/shared/src/test/require-jdk9/org/scalanative/testsuite/javalib/io/InputStreamTestOnJDK9.scala
@@ -0,0 +1,102 @@
+package javalib.io
+
+import java.io._
+
+import org.junit.Test
+import org.junit.Assert._
+
+import org.scalanative.testsuite.utils.AssertThrows.assertThrows
+
+class InputStreamTestOnJDK9 {
+
+  @Test def readAllBytes(): Unit = {
+
+    val inputBytes =
+      List(255, 254, 253, 252, 251, 128, 127, 2, 1, 0)
+        .map(_.toByte)
+        .toArray[Byte]
+
+    val streamIn = new ByteArrayInputStream(inputBytes)
+    val result = streamIn.readAllBytes()
+
+    assertEquals("result length", inputBytes.length, result.length)
+  }
+
+  @Test def readNBytesBufferOffLenExceptions(): Unit = {
+    val inputBytes =
+      List(0, 1, 2, 3, 4, 5, 6, 7, 8, 9).map(_.toByte).toArray[Byte]
+
+    val streamIn = new ByteArrayInputStream(inputBytes)
+    val receiver = new Array[Byte](10)
+    val nRead = streamIn.readNBytes(receiver, 0, receiver.length)
+
+    assertThrows(
+      classOf[NullPointerException],
+      streamIn.readNBytes(null, 0, receiver.length)
+    )
+
+    assertThrows(
+      classOf[IndexOutOfBoundsException],
+      streamIn.readNBytes(receiver, -2, receiver.length)
+    )
+
+    assertThrows(
+      classOf[IndexOutOfBoundsException],
+      streamIn.readNBytes(receiver, 0, -3)
+    )
+
+    assertThrows(
+      classOf[IndexOutOfBoundsException],
+      streamIn.readNBytes(receiver, 0, Integer.MAX_VALUE)
+    )
+  }
+
+  @Test def readNBytesBufferOffLen(): Unit = {
+    val inputBytes =
+      List(0, 1, 2, 3, 4, 5, 6, 7, 8, 9).map(_.toByte).toArray[Byte]
+
+    val streamIn = new ByteArrayInputStream(inputBytes)
+    val receiver = new Array[Byte](10)
+    val nRead = streamIn.readNBytes(receiver, 0, receiver.length)
+
+    assertEquals("nRead", receiver.length, nRead)
+
+    val expected = 9
+    assertEquals("expected content", expected, receiver(expected))
+  }
+
+  @Test def transferToNullOutStream(): Unit = {
+    val inputBytes =
+      List(255, 254, 253, 252, 251, 128, 127, 2, 1, 0)
+        .map(_.toByte)
+        .toArray[Byte]
+
+    val streamIn = new ByteArrayInputStream(inputBytes)
+    val streamOut = null.asInstanceOf[ByteArrayOutputStream]
+
+    assertThrows(
+      classOf[NullPointerException],
+      streamIn.transferTo(streamOut)
+    )
+  }
+
+  @Test def transferTo(): Unit = {
+    val inputBytes =
+      List(255, 254, 253, 252, 251, 128, 127, 2, 1, 0)
+        .map(_.toByte)
+        .toArray[Byte]
+
+    val streamIn = new ByteArrayInputStream(inputBytes)
+    val streamOut = new ByteArrayOutputStream()
+
+    val nTransferred = streamIn.transferTo(streamOut).toInt
+
+    assertEquals("nBytes transferred", inputBytes.length, nTransferred)
+    assertEquals("streamOut size", nTransferred, streamOut.size())
+
+    val outputBytes = streamOut.toByteArray()
+    for (j <- 0 until inputBytes.length)
+      assertEquals(s"in(${j}) != out(${j})", inputBytes(j), outputBytes(j))
+  }
+
+}
