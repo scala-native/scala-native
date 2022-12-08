d3b5112fb9a3f4e19d3c2534ea2e2ebc3d9286cb
LeeTibbert <LeeTibbert@users.noreply.github.com>
2022-12-08 05:06:25 -0500

    Fix #2973: Implement some requested j.io.InputStream Java 9 &11 methods (#3031)
    (cherry picked from commit 7012a72e74d66dad2326cdcad44d6a9d0b07bed2)

--- javalib/src/main/scala/java/io/InputStream.scala
+++ javalib/src/main/scala/java/io/InputStream.scala
@@ -1,5 +1,7 @@
 package java.io
 
+import java.{util => ju}
+
 abstract class InputStream extends Closeable {
   def read(): Int
 
@@ -32,6 +34,81 @@ abstract class InputStream extends Closeable {
     }
   }
 
+  // Allow obvious implementation of readAllBytes() to work on both Java 9 & 11
+  def readNBytesImpl(len: Int): Array[Byte] = {
+    if (len < 0)
+      throw new IllegalArgumentException("len < 0")
+
+    def readBytes(len: Int): ByteArrayOutputStream = {
+      val limit = Math.min(len, 1024)
+
+      val storage = new ByteArrayOutputStream(limit) // can grow itself
+      val buffer = new Array[Byte](limit)
+
+      var remaining = len
+
+      while (remaining > 0) {
+        val nRead = read(buffer, 0, limit)
+
+        if (nRead == -1) remaining = 0 // EOF
+        else {
+          storage.write(buffer, 0, nRead)
+          remaining -= nRead
+        }
+      }
+
+      storage
+    }
+
+    /* To stay within the documented 2 * len memory bound for this method,
+     * ensure that the temporary intermediate read buffer is out of scope
+     * and released before calling toByteArray().
+     */
+
+    readBytes(len).toByteArray()
+  }
+
+  /** Java 9
+   */
+  def readAllBytes(): Array[Byte] = readNBytesImpl(Integer.MAX_VALUE)
+
+  /** Java 9
+   */
+  def readNBytes(buffer: Array[Byte], off: Int, len: Int): Int = {
+    ju.Objects.requireNonNull(buffer)
+
+    if ((off < 0) || (len < 0) || (len > buffer.length - off)) {
+      val range = s"Range [${off}, ${off} + ${len})"
+      throw new IndexOutOfBoundsException(
+        s"${range} out of bounds for length ${buffer.length}"
+      )
+    }
+
+    if (len == 0) 0
+    else {
+      var totalBytesRead = 0
+      var remaining = len
+      var offset = off
+
+      while (remaining > 0) {
+        val nRead = read(buffer, offset, remaining)
+
+        if (nRead == -1) remaining = 0 // EOF
+        else {
+          totalBytesRead += nRead
+          remaining -= nRead
+          offset += nRead
+        }
+      }
+
+      totalBytesRead
+    }
+  }
+
+  /** Java 11
+   */
+  def readNBytes(len: Int): Array[Byte] = readNBytesImpl(len)
+
   def skip(n: Long): Long = {
     var skipped = 0
     while (skipped < n && read() != -1) skipped += 1
@@ -48,4 +125,25 @@ abstract class InputStream extends Closeable {
     throw new IOException("Reset not supported")
 
   def markSupported(): Boolean = false
+
+  /** Java 9
+   */
+  def transferTo(out: OutputStream): Long = {
+    val limit = 1024
+    val buffer = new Array[Byte](limit)
+
+    var nTransferred = 0L
+    var done = false
+
+    while (!done) {
+      val nRead = readNBytes(buffer, 0, limit)
+      if (nRead == 0) done = true // EOF
+      else {
+        out.write(buffer, 0, nRead)
+        nTransferred += nRead
+      }
+    }
+
+    nTransferred
+  }
 }
