2775211816aa7ddde17199864fc077eefd5be38b
LeeTibbert <LeeTibbert@users.noreply.github.com>
2022-12-08 05:06:25 -0500

    Fix #2973: Implement some requested j.io.InputStream Java 9 &11 methods (#3031)
    (cherry picked from commit 7012a72e74d66dad2326cdcad44d6a9d0b07bed2)

---
+++ unit-tests/shared/src/test/require-jdk11/org/scalanative/testsuite/javalib/io/InputStreamTestOnJDK11.scala
@@ -0,0 +1,38 @@
+package javalib.io
+
+import java.io._
+
+import org.junit.Test
+import org.junit.Assert._
+
+import org.scalanative.testsuite.utils.AssertThrows.assertThrows
+
+class InputStreamTestOnJDK11 {
+
+  @Test def readNBytesLenNegativeLength(): Unit = {
+    val inputBytes =
+      List(255, 254, 253, 252)
+        .map(_.toByte)
+        .toArray[Byte]
+
+    val streamIn = new ByteArrayInputStream(inputBytes)
+
+    assertThrows(
+      classOf[IllegalArgumentException],
+      streamIn.readNBytes(-3)
+    )
+  }
+
+  @Test def readNBytesLen(): Unit = {
+    val inputBytes =
+      List(255, 254, 253, 252, 251, 128, 127, 2, 1, 0)
+        .map(_.toByte)
+        .toArray[Byte]
+
+    val streamIn = new ByteArrayInputStream(inputBytes)
+    val len = 5
+    val result = streamIn.readNBytes(len)
+
+    assertEquals("result length", len, result.length)
+  }
+}
