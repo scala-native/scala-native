c3fb923fc38c35a2f587f44a8f2ea67e13497c22
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-12-12 20:59:24 +0100

    Define Deps.ScalaCheck as lazy val instead of def

--- project/Deps.scala
+++ project/Deps.scala
@@ -17,8 +17,7 @@ object Deps {
   }.headOption.getOrElse(throw new RuntimeException("Unknown Scala versions"))
   def ScalaReflect(version: String) = "org.scala-lang" % "scala-reflect" % version
 
-  def ScalaCheck(scalaVersion: String) = "org.scalacheck" %% "scalacheck" % "1.15.4"
-
+  lazy val ScalaCheck          = "org.scalacheck"         %% "scalacheck"                 % "1.15.4"
   lazy val ScalaTest           = "org.scalatest"          %% "scalatest"                  % "3.2.9"
   lazy val ScalaParCollections = "org.scala-lang.modules" %% "scala-parallel-collections" % "1.0.3"
   lazy val SbtPlatformDeps     = "org.portable-scala"      % "sbt-platform-deps"          % "1.0.1"
@@ -27,7 +26,7 @@ object Deps {
   lazy val JUnit               = "junit"                   % "junit"                      % "4.13.2"
 
   def Tools(scalaVersion: String) = {
-    List(ScalaCheck(scalaVersion) % "test", ScalaTest % "test") ++
+    List(ScalaCheck % "test", ScalaTest % "test") ++
       scalaVersionsDependendent(scalaVersion) {
         case (2, 12) => Nil
         case _       => ScalaParCollections :: Nil
