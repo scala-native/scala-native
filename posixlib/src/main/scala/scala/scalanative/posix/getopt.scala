3f9fc40b0ca62fda0a177431001d28f2b8c32636
LeeTibbert <LeeTibbert@users.noreply.github.com>
2023-01-10 05:25:27 -0500

    Fix #3608: Remove deprecation warnings in ExternTest (#3075)
    (cherry picked from commit 02a1a07dee98761edca271bbcf8835aa37b0ab92)

--- posixlib/src/main/scala/scala/scalanative/posix/getopt.scala
+++ posixlib/src/main/scala/scala/scalanative/posix/getopt.scala
@@ -4,7 +4,8 @@ package posix
 import scalanative.unsafe._
 
 @deprecated(
-  "getopt is no longer part of POSIX 2018 and will be removed. Use unistd instead"
+  "getopt is no longer part of POSIX 2018 and will be removed. Use unistd instead.",
+  "0.5.0"
 )
 @extern
 object getopt {
