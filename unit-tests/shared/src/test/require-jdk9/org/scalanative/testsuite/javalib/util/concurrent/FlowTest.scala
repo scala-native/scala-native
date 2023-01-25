e078a23f25b9024c976848c39cba942e54a90028
Arman Bilge <armanbilge@gmail.com>
2023-01-25 07:09:24 -0800

    Add `java.util.concurrent.Flow` (#3099)
    * Add `j.u.c.Flow`
    * Add `defaultBufferSize()`
    * Ported from Scala.js
    
    (cherry picked from commit a2cd0e2a756b40bd6c48623e6091dd53d855fdbd)

---
+++ unit-tests/shared/src/test/require-jdk9/org/scalanative/testsuite/javalib/util/concurrent/FlowTest.scala
@@ -0,0 +1,80 @@
+// Ported from Scala.js commit: fb20d6f dated: 2023-01-20
+
+package org.scalanative.testsuite.javalib.util.concurrent
+
+import java.util.concurrent.Flow
+
+import org.junit.Test
+import org.junit.Assert._
+
+class FlowTest {
+  import FlowTest._
+
+  @Test def testDefaultBufferSize(): Unit =
+    assertEquals(256, Flow.defaultBufferSize())
+
+  @Test def testProcessor(): Unit = {
+    val processor = makeProcessor[Int, String]()
+    processor.subscribe(makeSubscriber[String]())
+    processor.onSubscribe(makeSubscription())
+    processor.onNext(42)
+    processor.onError(new Exception)
+    processor.onComplete()
+  }
+
+  @Test def testPublisher(): Unit = {
+    val publisher = makePublisher[Int]()
+    publisher.subscribe(makeSubscriber[Int]())
+  }
+
+  @Test def testSubscriber(): Unit = {
+    val subscriber = makeSubscriber[Int]()
+    subscriber.onSubscribe(makeSubscription())
+    subscriber.onNext(42)
+    subscriber.onError(new Exception)
+    subscriber.onComplete()
+  }
+
+  @Test def testSubscription(): Unit = {
+    val subscription = makeSubscription()
+    subscription.request(42)
+    subscription.cancel()
+  }
+
+}
+
+object FlowTest {
+
+  def makeProcessor[T, R](): Flow.Processor[T, R] = {
+    new Flow.Processor[T, R] {
+      def subscribe(subscriber: Flow.Subscriber[_ >: R]): Unit = ()
+      def onSubscribe(subscription: Flow.Subscription): Unit = ()
+      def onNext(item: T): Unit = ()
+      def onError(throwable: Throwable): Unit = ()
+      def onComplete(): Unit = ()
+    }
+  }
+
+  def makePublisher[T](): Flow.Publisher[T] = {
+    new Flow.Publisher[T] {
+      def subscribe(subscriber: Flow.Subscriber[_ >: T]): Unit = ()
+    }
+  }
+
+  def makeSubscriber[T](): Flow.Subscriber[T] = {
+    new Flow.Subscriber[T] {
+      def onSubscribe(subscription: Flow.Subscription): Unit = ()
+      def onNext(item: T): Unit = ()
+      def onError(throwable: Throwable): Unit = ()
+      def onComplete(): Unit = ()
+    }
+  }
+
+  def makeSubscription(): Flow.Subscription = {
+    new Flow.Subscription {
+      def request(n: Long): Unit = ()
+      def cancel(): Unit = ()
+    }
+  }
+
+}
