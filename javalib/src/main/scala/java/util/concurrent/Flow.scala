5c86919f90f0be01399795fdf54a8f1d3fb4f389
Arman Bilge <armanbilge@gmail.com>
2023-01-25 07:09:24 -0800

    Add `java.util.concurrent.Flow` (#3099)
    * Add `j.u.c.Flow`
    * Add `defaultBufferSize()`
    * Ported from Scala.js
    
    (cherry picked from commit a2cd0e2a756b40bd6c48623e6091dd53d855fdbd)

---
+++ javalib/src/main/scala/java/util/concurrent/Flow.scala
@@ -0,0 +1,30 @@
+// Ported from Scala.js commit: fb20d6f dated: 2023-01-20
+
+package java.util.concurrent
+
+import scalanative.annotation.alwaysinline
+
+object Flow {
+
+  @alwaysinline def defaultBufferSize(): Int = 256
+
+  trait Processor[T, R] extends Subscriber[T] with Publisher[R]
+
+  @FunctionalInterface
+  trait Publisher[T] {
+    def subscribe(subscriber: Subscriber[_ >: T]): Unit
+  }
+
+  trait Subscriber[T] {
+    def onSubscribe(subscription: Subscription): Unit
+    def onNext(item: T): Unit
+    def onError(throwable: Throwable): Unit
+    def onComplete(): Unit
+  }
+
+  trait Subscription {
+    def request(n: Long): Unit
+    def cancel(): Unit
+  }
+
+}
