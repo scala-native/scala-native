2347037c16efbfa18d4807dc919b29da0b295674
Wojciech Mazur <wojciech.mazur95@gmail.com>
2023-01-29 13:47:27 +0100

    Update scripts/gyb_all.sh script

--- scripts/gyb_all.sh
+++ scripts/gyb_all.sh
@@ -6,10 +6,12 @@ scalanativeUnsafe=nativelib/src/main/scala/scala/scalanative/unsafe
 scalanativeUnsigned=nativelib/src/main/scala/scala/scalanative/unsigned
 scalanativeRuntime=nativelib/src/main/scala/scala/scalanative/runtime
 
-function gyb {
+function gyb() {
   file=$1
   if [ ${file: -4} == ".gyb" ]; then
-    scripts/gyb.py --line-directive '' -o "${file%.gyb}" "$file"
+    outputFile="${file%.gyb}"
+    echo "Generate $outputFile"
+    scripts/gyb.py --line-directive '' -o "$outputFile" "$file"
   else 
     echo "$file is not a .gyb file"
     exit 1
@@ -21,9 +23,11 @@ gyb $scalanativeUnsafe/Nat.scala.gyb
 gyb $scalanativeUnsafe/CStruct.scala.gyb
 gyb $scalanativeUnsafe/CFuncPtr.scala.gyb
 gyb $scalanativeUnsafe/Size.scala.gyb
-
 gyb $scalanativeUnsigned/USize.scala.gyb
 
 gyb $scalanativeRuntime/Arrays.scala.gyb
 gyb $scalanativeRuntime/Boxes.scala.gyb
 gyb $scalanativeRuntime/Primitives.scala.gyb
+
+gyb clib/src/main/scala/scala/scalanative/libc/atomic.scala.gyb
+gyb clib/src/main/resources/scala-native/atomic.c.gyb
