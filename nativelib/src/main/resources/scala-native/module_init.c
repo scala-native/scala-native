e130aec53d4e920e92b05328388c294f69fb5014
Wojciech Mazur <wojciech.mazur95@gmail.com>
2023-02-01 10:51:27 +0100

    Add sleep if spining for long time

--- nativelib/src/main/resources/scala-native/module_init.c
+++ nativelib/src/main/resources/scala-native/module_init.c
@@ -11,6 +11,30 @@
 #define YieldThread() sched_yield()
 #endif
 
+#ifdef WIN32
+#include <windows.h>
+#elif _POSIX_C_SOURCE >= 199309L
+#include <time.h> // for nanosleep
+#else
+#include <unistd.h> // for usleep
+#endif
+
+// cross-platform sleep function
+static void sleep_ms(int milliseconds) { 
+#ifdef WIN32
+    Sleep(milliseconds);
+#elif _POSIX_C_SOURCE >= 199309L
+    struct timespec ts;
+    ts.tv_sec = milliseconds / 1000;
+    ts.tv_nsec = (milliseconds % 1000) * 1000000;
+    nanosleep(&ts, NULL);
+#else
+    if (milliseconds >= 1000)
+        sleep(milliseconds / 1000);
+    usleep((milliseconds % 1000) * 1000);
+#endif
+}
+
 typedef _Atomic(void **) ModuleRef;
 typedef ModuleRef *ModuleSlot;
 typedef void (*ModuleCtor)(ModuleRef);
@@ -37,16 +61,22 @@ ModuleRef __scalanative_loadModule(ModuleSlot slot, void *classInfo,
     }
 
     // Wait in loop until initialization is finished
-    while (*module != classInfo) {
-        InitializationContext *ctx = (InitializationContext *)module;
-        // Usage of module in it's constructor, return unitializied instance
-        // thread=null can happen only in the main thread initialization
-        if (ctx->thread == currentNativeThread || ctx->thread == NULL) {
-            return ctx->instance;
+    if (*module != classInfo) {
+        int spin = 0;
+        while (*module != classInfo) {
+            InitializationContext *ctx = (InitializationContext *)module;
+            // Usage of module in it's constructor, return unitializied instance
+            // thread=null can happen only in the main thread initialization
+            if (ctx->thread == currentNativeThread || ctx->thread == NULL) {
+                return ctx->instance;
+            }
+            if (spin++ < 32)
+                YieldThread();
+            else
+                sleep_ms(1);
+            scalanative_gc_safepoint_poll();
+            module = atomic_load_explicit(slot, memory_order_acquire);
         }
-        YieldThread();
-        scalanative_gcYield();
-        module = atomic_load_explicit(slot, memory_order_acquire);
     }
     return module;
 }
