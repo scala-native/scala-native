e102b747e0d0cc536955d9acecedb8f787a60c22
Wojciech Mazur <wojciech.mazur95@gmail.com>
2023-02-03 23:07:49 +0100

    Implement subset of static `java.lang.invoke.VarHandle` methods

---
+++ javalib/src/main/scala/java/lang/invoke/VarHandle.scala
@@ -0,0 +1,45 @@
+package java.lang.invoke
+
+import scala.scalanative.libc.atomic._
+import scala.scalanative.libc.atomic.memory_order._
+import scala.scalanative.annotation._
+
+class VarHandle {}
+
+object VarHandle {
+  @alwaysinline
+  private def loadFence(): Unit = atomic_thread_fence(memory_order_acquire)
+
+  @alwaysinline
+  private def storeFence(): Unit = atomic_thread_fence(memory_order_release)
+
+  /** Ensures that loads before the fence will not be reordered with loads and
+   *  stores after the fence.
+   */
+  @alwaysinline
+  def acquireFence(): Unit = loadFence()
+
+  /** Ensures that loads and stores before the fence will not be reordered with
+   *  stores after the fence.
+   */
+  @alwaysinline
+  def releaseFence(): Unit = storeFence()
+
+  /** Ensures that loads and stores before the fence will not be reordered with
+   *  loads and stores after the fence.
+   */
+  @alwaysinline
+  def fullFence(): Unit = atomic_thread_fence(memory_order_seq_cst)
+
+  /** Ensures that loads before the fence will not be reordered with loads after
+   *  the fence.
+   */
+  @alwaysinline
+  def loadLoadFence(): Unit = loadFence()
+
+  /** Ensures that stores before the fence will not be reordered with stores
+   *  after the fence.
+   */
+  @alwaysinline
+  def storeStoreFence(): Unit = storeFence()
+}
