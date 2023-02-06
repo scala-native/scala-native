79d9138e0be274e683eeaed3ca49220e767f8f54
Wojciech Mazur <wojciech.mazur95@gmail.com>
2023-02-06 15:45:02 +0100

    Rename `scalanative.runtime.ExecutionContext.global` to `singlethreaded`

--- nativelib/src/main/scala/scala/scalanative/runtime/ExecutionContext.scala
+++ nativelib/src/main/scala/scala/scalanative/runtime/ExecutionContext.scala
@@ -3,26 +3,32 @@ package runtime
 
 import scala.collection.mutable.ListBuffer
 import scala.concurrent.ExecutionContextExecutor
+import scala.scalanative.meta.LinktimeInfo.isMultithreadingEnabled
 
 object ExecutionContext {
-  def global: ExecutionContextExecutor = QueueExecutionContext
+  @deprecated(
+    "Use `singleThreaded` instead, since `global` may infer that it supports concurrent execution.",
+    since = "0.5.0"
+  )
+  def global: ExecutionContextExecutor = singleThreaded
+  def singleThreaded: ExecutionContextExecutor = QueueExecutionContext
 
   private object QueueExecutionContext extends ExecutionContextExecutor {
+    private val queue: ListBuffer[Runnable] = new ListBuffer
     def execute(runnable: Runnable): Unit = queue += runnable
     def reportFailure(t: Throwable): Unit = t.printStackTrace()
-  }
 
-  private val queue: ListBuffer[Runnable] = new ListBuffer
-
-  private[runtime] def loop(): Unit = {
-    while (queue.nonEmpty) {
-      val runnable = queue.remove(0)
-      try {
-        runnable.run()
-      } catch {
-        case t: Throwable =>
-          QueueExecutionContext.reportFailure(t)
+    def loop(): Unit = {
+      while (queue.nonEmpty) {
+        val runnable = queue.remove(0)
+        try runnable.run()
+        catch {
+          case t: Throwable =>
+            QueueExecutionContext.reportFailure(t)
+        }
       }
     }
   }
+
+  private[runtime] def loop(): Unit = QueueExecutionContext.loop()
 }
