7fcab332c56d1a60114c20c4ae22267e73f1411f
Wojciech Mazur <wojciech.mazur95@gmail.com>
2023-02-07 08:45:27 +0100

    Add missing junit-async adaption for using Await.result

--- junit-async/native/src/main/scala/scala/scalanative/junit/async/package.scala
+++ junit-async/native/src/main/scala/scala/scalanative/junit/async/package.scala
@@ -1,11 +1,17 @@
 package scala.scalanative.junit
 
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration.Duration
+import scala.scalanative.meta.LinktimeInfo.isMultithreadingEnabled
 
 package object async {
   type AsyncResult = Unit
   def await(future: Future[_]): AsyncResult = {
-    scala.scalanative.runtime.loop()
-    future.value.get.get
+    if (isMultithreadingEnabled)
+      Await.result(future, Duration.Inf)
+    else {
+      scala.scalanative.runtime.loop()
+      future.value.get.get
+    }
   }
 }
