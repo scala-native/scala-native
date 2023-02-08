850a6d2b5382525be1607f099cddb1e23dea223c
Wojciech Mazur <wojciech.mazur95@gmail.com>
2023-02-08 21:58:11 +0100

    Fix compilation of Scala 2.12 scalalib with JDK8

--- scalalib/overrides-2.12/scala/collection/immutable/VM.scala
+++ scalalib/overrides-2.12/scala/collection/immutable/VM.scala
@@ -1,9 +1,10 @@
 package scala.collection.immutable
 
-import java.lang.invoke.VarHandle
+import scala.scalanative.libc.atomic._
+import scala.scalanative.libc.atomic.memory_order._
 
 // Backport from scala.runtime moved into s.c.immutable and made package
 // private to avoid the need for MiMa whitelisting.
 /* private[immutable] */ object VM {
-  def releaseFence(): Unit = VarHandle.releaseFence()
+  def releaseFence(): Unit = atomic_thread_fence(memory_order_release)
 }
