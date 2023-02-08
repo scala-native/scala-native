06b1f412f31c02fb63b2476dc90775fc6db616f0
Wojciech Mazur <wojciech.mazur95@gmail.com>
2023-02-08 19:21:06 +0100

    Fix scalalib scala.collection.immutable.VM implementation for Scala 2.12

--- scalalib/overrides-2.12/scala/collection/immutable/VM.scala
+++ scalalib/overrides-2.12/scala/collection/immutable/VM.scala
@@ -1,7 +1,9 @@
 package scala.collection.immutable
 
+import java.lang.invoke.VarHandle
+
 // Backport from scala.runtime moved into s.c.immutable and made package
 // private to avoid the need for MiMa whitelisting.
 /* private[immutable] */ object VM {
-  def releaseFence(): Unit = ()
+  def releaseFence(): Unit = VarHandle.releaseFence()
 }
