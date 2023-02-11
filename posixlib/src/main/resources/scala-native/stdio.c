d60ebd3b17b2c2aec16239f50a823b7f31289a93
Lee Tibbert <lee.tibbert@gmail.com>
2023-02-11 15:35:29 -0500

    Pay obeisance to Windows; also correct typo elsewhere

--- posixlib/src/main/resources/scala-native/stdio.c
+++ posixlib/src/main/resources/scala-native/stdio.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+#if !defined(L_ctermid)
+#if defined(_WIN32)
+// Windows MAX_PATH is 260, plus 1 for terminating NUL/NULL/"\u0000".
+#define L_ctermid 260 + 1
+#else
+#error "L_ctermid is not defined in stdio.h"
+#endif
+#endif
+
 // This file contains functions that wrap posixlib
 // built-in macros. We need this because Scala Native
 // can not expand C macros, and that's the easiest way to
