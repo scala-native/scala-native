a54825ac7c8646405256120add75558f301be795
Lee Tibbert <lee.tibbert@gmail.com>
2023-02-20 23:55:47 -0500

    Fix #3176: The setting multithreadingSupport from .sbt files is now respected

--- project/MyScalaNativePlugin.scala
+++ project/MyScalaNativePlugin.scala
@@ -9,6 +9,28 @@ import scala.scalanative.sbtplugin.ScalaNativePlugin.autoImport._
 object MyScalaNativePlugin extends AutoPlugin {
   override def requires: Plugins = ScalaNativePlugin
 
+  private def multithreadingEnabledBySbtSysProps(): Option[Boolean] = {
+    /* Started as: sbt -Dscala.scalanative.multithreading.enable=true
+     * That idiom is used by Windows Continuous Integration (CI).
+     *
+     * BEWARE the root project Quirk!
+     * This feature is not meant for general use. Anybody using it
+     * should understand how it works.
+     *
+     * Setting multithreading on the command line __will_ override any
+     * such setting in a .sbt file in all projects __except_ the root
+     * project. "show nativeConfig" will show the value from .sbt files
+     * "show sandbox3/nativeConfig" will show the effective value for
+     * non-root projects.
+     *
+     * Someday this quirk will get fixed.
+     */
+    sys.props.get("scala.scalanative.multithreading.enable") match {
+      case Some(v) => Some(java.lang.Boolean.parseBoolean(v))
+      case None    => None
+    }
+  }
+
   override def projectSettings: Seq[Setting[_]] = Def.settings(
     /* Remove libraryDependencies on ourselves; we use .dependsOn() instead
      * inside this build.
@@ -16,12 +38,13 @@ object MyScalaNativePlugin extends AutoPlugin {
     libraryDependencies ~= { libDeps =>
       libDeps.filterNot(_.organization == "org.scala-native")
     },
-    nativeConfig ~= {
-      _.withCheck(true)
+    nativeConfig ~= { nc =>
+      nc.withCheck(true)
         .withCheckFatalWarnings(true)
         .withDump(true)
         .withMultithreadingSupport(
-          sys.props.contains("scala.scalanative.multithreading.enable")
+          multithreadingEnabledBySbtSysProps()
+            .getOrElse(nc.multithreadingSupport)
         )
     },
     scalacOptions ++= {
