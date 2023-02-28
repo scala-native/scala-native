e1d5bced93dcd7443eb319e581857dfbb6646ddb
Wojciech Mazur <wojciech.mazur95@gmail.com>
2023-02-28 21:01:32 +0100

    Adapt CodeGen to emit opaque pointers whenever possible

--- tools/src/main/scala/scala/scalanative/codegen/PlatformInfo.scala
+++ tools/src/main/scala/scala/scalanative/codegen/PlatformInfo.scala
@@ -1,12 +1,13 @@
 package scala.scalanative.codegen
 
-import scala.scalanative.build.Config
+import scala.scalanative.build.{Config, Discover}
 
 private[scalanative] case class PlatformInfo(
     targetTriple: Option[String],
     targetsWindows: Boolean,
     is32Bit: Boolean,
-    isMultithreadingEnabled: Boolean
+    isMultithreadingEnabled: Boolean,
+    useOpaquePointers: Boolean
 ) {
   val sizeOfPtr = if (is32Bit) 4 else 8
   val sizeOfPtrBits = sizeOfPtr * 8
@@ -16,6 +17,8 @@ object PlatformInfo {
     targetTriple = config.compilerConfig.targetTriple,
     targetsWindows = config.targetsWindows,
     is32Bit = config.compilerConfig.is32BitPlatform,
-    isMultithreadingEnabled = config.compilerConfig.multithreadingSupport
+    isMultithreadingEnabled = config.compilerConfig.multithreadingSupport,
+    useOpaquePointers =
+      Discover.features.opaquePointers(config.compilerConfig).isAvailable
   )
 }
