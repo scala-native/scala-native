0fabc7a00a81cecb32dd3c5693c2fb1fcc9008fe
Wojciech Mazur <wojciech.mazur95@gmail.com>
2023-03-08 12:03:36 +0100

    Return BoxedUnit in Lower only when it it required by checking definition result type

--- tools/src/main/scala/scala/scalanative/codegen/Lower.scala
+++ tools/src/main/scala/scala/scalanative/codegen/Lower.scala
@@ -47,6 +47,10 @@ object Lower {
     private val fresh = new util.ScopedVar[Fresh]
     private val unwindHandler = new util.ScopedVar[Option[Local]]
     private val currentDefn = new util.ScopedVar[Defn.Define]
+    private def currentDefnRetType = {
+      val Type.Function(_, ret) = currentDefn.get.ty
+      ret
+    }
 
     private val unreachableSlowPath = mutable.Map.empty[Option[Local], Local]
     private val nullPointerSlowPath = mutable.Map.empty[Option[Local], Local]
@@ -103,6 +107,17 @@ object Lower {
           Next.Label(name, args.map(genVal(buf, _)))
         case n => n
       }
+    }
+
+    private def optionallyBoxedUnit(v: nir.Val)(implicit
+        pos: nir.Position
+    ): nir.Val = {
+      require(
+        v.ty == Type.Unit,
+        s"Definition is expected to return Unit type, found ${v.ty}"
+      )
+      if (currentDefnRetType == Type.Unit) Val.Unit
+      else unit
     }
 
     override def onInsts(insts: Seq[Inst]): Seq[Inst] = {
@@ -169,10 +184,9 @@ object Lower {
         case inst @ Inst.Ret(v) =>
           implicit val pos: Position = inst.pos
           genGCSafepoint(buf)
-          val retVal = v.ty match {
-            case Type.Unit => Val.Unit
-            case _         => genVal(buf, v)
-          }
+          val retVal =
+            if (v.ty == Type.Unit) optionallyBoxedUnit(v)
+            else genVal(buf, v)
           buf += Inst.Ret(retVal)
 
         case inst @ Inst.Jump(next) =>
@@ -215,9 +229,11 @@ object Lower {
     }
 
     override def onInst(inst: Inst): Inst = {
+      implicit def pos: nir.Position = inst.pos
       inst match {
-        case Inst.Ret(v) if v.ty == Type.Unit => inst
-        case _                                => super.onInst(inst)
+        case Inst.Ret(v) if v.ty == Type.Unit =>
+          Inst.Ret(optionallyBoxedUnit(v))
+        case _ => super.onInst(inst)
       }
     }
 
