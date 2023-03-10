e6fdf3ef3ce0763f0d60082438f6fe1af86e0872
Wojciech Mazur <wmazur@virtuslab.com>
2023-03-10 17:28:41 +0100

    Fix issues found in extended CI runs (#3213)
    * Don't use Array[RawPtr] in MemoryPoolZone
    
    * Free chunks of `defaultMemoryPool` when using ASAN at exit
    
    * Fix Windows CI
    
    * Fix compilation warning in StackTrace.c on MacOS
    
    * Fix macos test-runtime-ext, use LLVM clang instead of Apple clang

--- nativelib/src/main/scala/scala/scalanative/runtime/libc.scala
+++ nativelib/src/main/scala/scala/scalanative/runtime/libc.scala
@@ -11,6 +11,7 @@ object libc {
   def malloc(size: CSize): RawPtr = extern
   def realloc(ptr: RawPtr, size: CSize): RawPtr = extern
   def free(ptr: RawPtr): Unit = extern
+  def free(ptr: Ptr[_]): Unit = extern
   def strlen(str: CString): CSize = extern
   def wcslen(str: CWideString): CSize = extern
   def strcpy(dest: CString, src: CString): CString = extern
@@ -22,6 +23,7 @@ object libc {
   def memset(dest: Ptr[_], ch: CInt, count: CSize): RawPtr = extern
   def memmove(dest: RawPtr, src: RawPtr, count: CSize): RawPtr = extern
   def remove(fname: CString): CInt = extern
+  def atexit(func: CFuncPtr0[Unit]): CInt = extern
 
   // Glue layer defined in libc
   @name("scalanative_atomic_compare_exchange_strong_byte")
