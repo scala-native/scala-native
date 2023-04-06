e42bbfa8406ef572b8efd312009472423d5ede13
Wojciech Mazur <wojciech.mazur95@gmail.com>
2023-04-06 21:20:26 +0000

    Adapt `BeforeAndAfterClassTest` for potentially parallel execution

--- junit-test/shared/src/test/scala/scala/scalanative/junit/BeforeAndAfterClassTest.scala
+++ junit-test/shared/src/test/scala/scala/scalanative/junit/BeforeAndAfterClassTest.scala
@@ -4,13 +4,29 @@ import org.junit._
 import org.junit.Assert._
 import org.junit.Assume._
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+import java.util.concurrent.atomic.AtomicBoolean
+
 import scala.scalanative.junit.utils.JUnitTest
+import scala.scalanative.junit.async.await
 
 object TraitParent {
   @BeforeClass def beforeTrait(): Unit =
     assumeFalse("before should be ignored in trait", true)
   @AfterClass def afterTrait(): Unit =
     assumeFalse("after should be ignored in trait", true)
+}
+
+object State {
+  // Becouse we modify shared global state make sure only 1 test is being executed at a time
+  def acquire() = while (!locked.compareAndSet(false, true)) {
+    if (Thread.interrupted()) throw new InterruptedException()
+    await { Future("waiting") }
+  }
+  def release() = locked.set(false)
+  private val locked = new AtomicBoolean(false)
 }
 
 trait TraitParent {
@@ -20,11 +36,14 @@ trait TraitParent {
 object ClassParent {
   var wasReached = false
   @BeforeClass def beforeParentClass(): Unit = {
+    // It's the first handler that would be called, ensure its state would not be interfered
+    State.acquire()
     assertFalse("beforeParent class should not be yet reached", wasReached)
     wasReached = true
   }
   @AfterClass def afterParentClass(): Unit = {
     assertFalse("afterParent should be reset in child", wasReached)
+    State.release() // last handler
   }
 }
 abstract class ClassParent extends TraitParent()
