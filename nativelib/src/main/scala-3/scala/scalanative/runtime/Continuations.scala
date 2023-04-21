25f1f499756be255b3dd6c10e6ccb24e4e593eeb
Natsu Kagami <nki@nkagami.me>
2023-04-21 17:28:36 +0200

    Simplify some more

--- nativelib/src/main/scala-3/scala/scalanative/runtime/Continuations.scala
+++ nativelib/src/main/scala-3/scala/scalanative/runtime/Continuations.scala
@@ -9,49 +9,51 @@ import scala.collection.mutable
 object Continuations:
   import ContImpl.*
 
-  type ContFnT = ContImpl.BoundaryLabel => Ptr[Byte]
-
-  private inline def pNull[T]: Ptr[T] = fromRawPtr[T](castIntToRawPtr(0))
-
-  private inline def pObj[T](o: T) = fromRawPtr[Byte](
-    castObjectToRawPtr(o.asInstanceOf[Object])
-  )
-
-  private inline def objP[T](p: Ptr[Byte]) =
-    castRawPtrToObject(p.rawptr).asInstanceOf[T]
-
-  inline def boundary[T](inline f: BoundaryLabel[T] ?=> T)(using Tag[T]): T =
+  inline def boundary[T](inline f: BoundaryLabel[T] ?=> T): T =
     val call: ContFnT = (x: ContImpl.BoundaryLabel) => pObj(f(using x))
     val resP = ContImpl.boundary(contFn, pObj(call))
     objP[T](resP)
 
   opaque type BoundaryLabel[-T] = ContImpl.BoundaryLabel
 
+  inline def break(using BoundaryLabel[Unit]): Nothing =
+    suspend[Nothing, Unit](_ => ())
+  inline def break[T](value: T)(using BoundaryLabel[T]): Nothing =
+    suspend[Nothing, T](_ => value)
+
   inline def suspend[R, T](
       inline f: (R => T) => T
-  )(using label: BoundaryLabel[T])(using
-      Tag[T],
-      Tag[R]
-  ): R =
+  )(using label: BoundaryLabel[T]): R =
     suspendCont[R, T](cont => f(r => cont(r)))
 
   def suspend[T](
       f: (() => T) => T
-  )(using label: BoundaryLabel[T])(using Tag[T]): Unit =
+  )(using label: BoundaryLabel[T]): Unit =
     suspendCont[Unit, T](cont => f(() => cont(())))
 
   inline def suspendCont[R, T](
       inline f: Continuation[R, T] => T
-  )(using label: BoundaryLabel[T])(using
-      Tag[T],
-      Tag[R]
-  ): R =
+  )(using label: BoundaryLabel[T]): R =
     val cont = Continuation()
     val call = (c: ContImpl.Continuation) =>
       cont.cont = c
       pObj(f(cont))
     val resP = ContImpl.suspend(label, suspendFn, pObj(call), pObj(cont))
     objP[R](resP)
+
+  class Continuation[-R, +T] extends (R => T):
+    private[Continuations] var cont: ContImpl.Continuation = pNull
+    private val allocs = mutable.ArrayBuffer[ObjectArray]()
+
+    def apply(x: R): T = objP[T](resume(cont, pObj(x)))
+
+    private[Continuations] def alloc(size: CUnsignedLong): Ptr[Byte] =
+      val obj = ObjectArray.alloc(size.toInt) // round up the blob size
+      allocs += obj
+      obj.at(0).asInstanceOf[Ptr[Byte]]
+  end Continuation
+
+  // STATIC FUNCTIONS THAT CALL PASSED-IN FUNCTION OBJECTS
 
   private val suspendFn: SuspendFn = CFuncPtr2.fromScalaFunction((cont, f) =>
     val fn = objP[ContImpl.Continuation => Ptr[Byte]](f)
@@ -68,17 +70,18 @@ object Continuations:
 
   ContImpl.setAlloc(CFuncPtr2.fromScalaFunction(allocateBlob))
 
-  class Continuation[-R, +T] extends (R => T):
-    private[Continuations] var cont: ContImpl.Continuation = pNull
-    private val allocs = mutable.ArrayBuffer[ObjectArray]()
+  // FOR WORKING WITH POINTERS
 
-    def apply(x: R): T = objP[T](resume(cont, pObj(x)))
+  type ContFnT = ContImpl.BoundaryLabel => Ptr[Byte]
 
-    private[Continuations] def alloc(size: CUnsignedLong): Ptr[Byte] =
-      val obj = ObjectArray.alloc(size.toInt) // round up the blob size
-      allocs += obj
-      obj.at(0).asInstanceOf[Ptr[Byte]]
-  end Continuation
+  private inline def pNull[T]: Ptr[T] = fromRawPtr[T](castIntToRawPtr(0))
+
+  private inline def pObj[T](o: T) = fromRawPtr[Byte](
+    castObjectToRawPtr(o.asInstanceOf[Object])
+  )
+
+  private inline def objP[T](p: Ptr[Byte]) =
+    castRawPtrToObject(p.rawptr).asInstanceOf[T]
 
 end Continuations
 
