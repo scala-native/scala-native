c040cc51205484c103257ecb2e523851b47619e8
Natsu Kagami <nki@nkagami.me>
2023-04-21 15:33:20 +0200

    Avoid allocating more objects in scala native

--- nativelib/src/main/scala-3/scala/scalanative/runtime/Continuations.scala
+++ nativelib/src/main/scala-3/scala/scalanative/runtime/Continuations.scala
@@ -3,24 +3,26 @@ package scala.scalanative.runtime
 import scala.scalanative.unsafe.*
 import scala.scalanative.runtime.libc.{malloc, free}
 import scala.scalanative.unsafe.Size.intToSize
-import scala.scalanative.runtime.Intrinsics.castIntToRawPtr
+import scala.scalanative.runtime.Intrinsics.*
 
 object Continuations:
   import ContImpl.*
 
-  private def allocObj[T](using Tag[T]) =
-    val arr = ObjectArray.alloc(1)
-    arr.at(0).asInstanceOf[Ptr[T]]
+  type ContFnT = ContImpl.BoundaryLabel => Ptr[Byte]
+
+  private inline def pNull[T]: Ptr[T] = fromRawPtr[T](castIntToRawPtr(0))
+
+  private inline def pObj[T](o: T) = fromRawPtr[Byte](
+    castObjectToRawPtr(o.asInstanceOf[Object])
+  )
+
+  private inline def objP[T](p: Ptr[Byte]) =
+    castRawPtrToObject(p.rawptr).asInstanceOf[T]
 
   inline def boundary[T](inline f: BoundaryLabel[T] ?=> T)(using Tag[T]): T =
-    val fPtr = allocObj[ContImpl.BoundaryLabel ?=> Ptr[Byte]]
-    !fPtr = (x: ContImpl.BoundaryLabel) ?=>
-      val r = f
-      val resPtr = allocObj[T]
-      !resPtr = r
-      resPtr.asInstanceOf[Ptr[Byte]]
-    val resP = ContImpl.boundary(contFn, fPtr.asInstanceOf[Ptr[Byte]])
-    !resP.asInstanceOf[Ptr[T]]
+    val call: ContFnT = (x: ContImpl.BoundaryLabel) => pObj(f(using x))
+    val resP = ContImpl.boundary(contFn, pObj(call))
+    objP[T](resP)
 
   opaque type BoundaryLabel[-T] = ContImpl.BoundaryLabel
 
@@ -37,13 +39,8 @@ object Continuations:
   def suspend[T](
       f: (() => T) => T
   )(using label: BoundaryLabel[T])(using Tag[T]): Unit =
-    val fPtr = allocObj[ContImpl.Continuation => Ptr[Byte]]
-    !fPtr = (cont: ContImpl.Continuation) =>
-      val r = f(() => resumeCont(cont))
-      val resPtr = allocObj[T]
-      !resPtr = r
-      resPtr.asInstanceOf[Ptr[Byte]]
-    ContImpl.suspend(label, suspendFn, fPtr.asInstanceOf[Ptr[Byte]])
+    val call = (cont: ContImpl.Continuation) => pObj(f(() => resumeCont(cont)))
+    ContImpl.suspend(label, suspendFn, pObj(call))
 
   inline def suspendCont[R, T](
       inline f: Continuation[R, T] => T
@@ -51,51 +48,34 @@ object Continuations:
       Tag[T],
       Tag[R]
   ): R =
-    val fPtr = allocObj[ContImpl.Continuation => Ptr[Byte]]
-    !fPtr = (cont: ContImpl.Continuation) =>
-      val r = f(cont)
-      val resPtr = allocObj[T]
-      !resPtr = r
-      resPtr.asInstanceOf[Ptr[Byte]]
-    val resP = ContImpl.suspend(label, suspendFn, fPtr.asInstanceOf[Ptr[Byte]])
-    !resP.asInstanceOf[Ptr[R]]
+    val call = (cont: ContImpl.Continuation) => pObj(f(cont))
+    val resP = ContImpl.suspend(label, suspendFn, pObj(call))
+    objP[R](resP)
 
   inline def resumeCont[R, T](cont: Continuation[R, T], input: R)(using
       Tag[T],
       Tag[R]
   ): T =
-    val rPtr = allocObj[R]
-    !rPtr = input
-    val resP =
-      ContImpl.resume(cont, rPtr.asInstanceOf[Ptr[Byte]]).asInstanceOf[Ptr[T]]
-    val res = !resP
-    res
+    objP[T](ContImpl.resume(cont, pObj(input)))
 
   // special case for R = Unit
   inline private def resumeCont[T](cont: Continuation[Unit, T])(using
       Tag[T]
   ): T =
-    val resP =
-      ContImpl
-        .resume(cont, fromRawPtr[Byte](castIntToRawPtr(0)))
-        .asInstanceOf[Ptr[T]]
-    val res = !resP
-    res
+    objP[T](ContImpl.resume(cont, pNull))
 
   private val suspendFn: SuspendFn = CFuncPtr2.fromScalaFunction((cont, f) =>
-    val fp = f.asInstanceOf[Ptr[ContImpl.Continuation => Ptr[Byte]]]
-    val fn = !fp
+    val fn = objP[ContImpl.Continuation => Ptr[Byte]](f)
     fn(cont)
   )
 
   private val contFn: ContFn = CFuncPtr2.fromScalaFunction((label, f) =>
-    given ContImpl.BoundaryLabel = label
-    val fp = f.asInstanceOf[Ptr[ContImpl.BoundaryLabel ?=> Ptr[Byte]]]
-    !fp
+    val fp = objP[ContFnT](f)
+    fp(label)
   )
 
   private def allocateBlob(size: CUnsignedLong): Ptr[Byte] =
-    val obj = ObjectArray.alloc((size.toInt + 7) / 8) // round up the blob size
+    val obj = ObjectArray.alloc(size.toInt) // round up the blob size
     obj.at(0).asInstanceOf[Ptr[Byte]]
 
   ContImpl.setAlloc(CFuncPtr1.fromScalaFunction(allocateBlob))
