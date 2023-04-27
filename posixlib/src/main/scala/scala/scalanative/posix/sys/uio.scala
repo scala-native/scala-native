0542c8f99130d4f7515eef9f483b8389f4b38f71
Arman Bilge <armanbilge@gmail.com>
2023-04-27 04:32:13 -0700

    Backport `uioOps` to 0.4.x (#3259)

--- posixlib/src/main/scala/scala/scalanative/posix/sys/uio.scala
+++ posixlib/src/main/scala/scala/scalanative/posix/sys/uio.scala
@@ -18,3 +18,21 @@ object uio {
   def writev(fildes: CInt, iov: Ptr[iovec], iovcnt: CInt): CSSize = extern
 
 }
+
+object uioOps {
+  import uio.iovec
+
+  implicit class iovecOps(val ptr: Ptr[iovec]) extends AnyVal {
+    def iov_base: Ptr[Byte] = ptr._1
+    def iov_len: CSize = ptr._2
+    def iov_base_=(v: Ptr[Byte]): Unit = ptr._1 = v
+    def iov_len_=(v: CSize): Unit = ptr._2 = v
+  }
+
+  implicit class iovecValOps(val vec: iovec) extends AnyVal {
+    def iov_base: Ptr[Byte] = vec._1
+    def iov_len: CSize = vec._2
+    def iov_base_=(v: Ptr[Byte]): Unit = vec._1 = v
+    def iov_len_=(v: CSize): Unit = vec._2 = v
+  }
+}
