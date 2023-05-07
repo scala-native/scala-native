4076ea04d64ea91d09b14b14c5a2a4430c8500b0
Abdullahsab3 <abdallah-sab3@hotmail.com>
2023-05-07 18:17:03 +0200

    fixed scalafmt :)

--- unit-tests/native/src/test/scala/scala/scalanative/runtime/HeapSizeTest.scala.scala
+++ unit-tests/native/src/test/scala/scala/scalanative/runtime/HeapSizeTest.scala.scala
@@ -7,22 +7,25 @@ import scalanative.unsigned.{ULong, UnsignedRichInt}
 
 class HeapSizeTest {
 
-    @Before
-    val conversionFactor = (1024 * 1024 * 1024).toULong
-    val lowerBound : ULong = 0.toULong
-    val higherBound : ULong = 32.toULong * conversionFactor
+  @Before
+  val conversionFactor = (1024 * 1024 * 1024).toULong
+  val lowerBound: ULong = 0.toULong
+  val higherBound: ULong = 32.toULong * conversionFactor
 
-    @Test
-    def checkInitHeapSize(): Unit = {
-        val initHeapSz = GC.getInitHeapSize()
-        assertTrue(s"0 <= ${initHeapSz / conversionFactor}GB < 32GB", initHeapSz >= lowerBound && initHeapSz < higherBound)
-    }
+  @Test def checkInitHeapSize(): Unit = {
+    val initHeapSz = GC.getInitHeapSize()
+    assertTrue(
+      s"0 <= ${initHeapSz / conversionFactor}GB < 32GB",
+      initHeapSz >= lowerBound && initHeapSz < higherBound
+    )
+  }
 
-    @Test 
-    def checkMaxHeapSize(): Unit = {
-        val maxHeapSize = GC.getMaxHeapSize()
-        assertTrue(s"0 < ${maxHeapSize / conversionFactor}GB <= 32GB", maxHeapSize > lowerBound && maxHeapSize <= higherBound)
-    }
-    
-  
+  @Test def checkMaxHeapSize(): Unit = {
+    val maxHeapSize = GC.getMaxHeapSize()
+    assertTrue(
+      s"0 < ${maxHeapSize / conversionFactor}GB <= 32GB",
+      maxHeapSize > lowerBound && maxHeapSize <= higherBound
+    )
+  }
+
 }
