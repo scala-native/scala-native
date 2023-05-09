23612200e84e6adc482b6171a1d189d47d9e421c
Lee Tibbert <lee.tibbert@gmail.com>
2023-05-08 22:33:37 -0400

    clangfmt

--- posixlib/src/main/resources/scala-native/sys/uname.c
+++ posixlib/src/main/resources/scala-native/sys/uname.c
@@ -1,4 +1,4 @@
-#if defined(__unix__) || defined(__unix) || defined(unix) || \
+#if defined(__unix__) || defined(__unix) || defined(unix) ||                   \
     (defined(__APPLE__) && defined(__MACH__))
 #include <sys/utsname.h>
 #include <stddef.h>
