d6fd8866b19e4d12b24a63657ee1fad52941a8e1
Lee Tibbert <lee.tibbert@gmail.com>
2023-05-08 22:27:26 -0400

    Add robustness and explain origin of magic 256

--- posixlib/src/main/scala/scala/scalanative/posix/sys/utsname.scala
+++ posixlib/src/main/scala/scala/scalanative/posix/sys/utsname.scala
@@ -5,6 +5,20 @@ import scala.scalanative.unsafe.Nat._
 
 @extern
 object utsname {
+  /* Design notes:
+   * 1) The 256 "magic"" number appears to be the macOS macro _SYS_NAMELEN.
+   *    Linux uses a much smaller number (65).
+   *    _Static_assert() guard code exists in uname.c to ensure that
+   *    the size used by the operating system is less than or equal to this.
+   *    That prevents new or changed operating systems from writing to
+   *    memory where it should: i.e. spraying memory.
+   *
+   * 2) The allocation of the entire array inside the structure follows
+   *    the Open Group 2018 POSIX description. That is, the fields are
+   *    actual arrays (CArray) and not the pointers to the beginning of an
+   *    array (Ptr[Byte]) one might expect.
+   */
+
   type _256 = Digit3[_2, _5, _6]
   private type str = CArray[Byte, _256]
   type utsname = CStruct5[str, str, str, str, str]
