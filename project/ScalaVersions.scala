1a76f50c67e98eb9b3c8b64bbbf2b4920deb98cd
Wojciech Mazur <wmazur@virtuslab.com>
2023-06-01 14:32:49 +0200

    Support Scala 2.12.18 (#3302)
    * Add Scala 2.12.18 to crossScalaVersions
    
    * Use Scala 2.12.18 in the CI
    
    * Update partests for Scala 2.12.18

--- project/ScalaVersions.scala
+++ project/ScalaVersions.scala
@@ -18,7 +18,7 @@ package build
 
 object ScalaVersions {
   // Versions of Scala used for publishing compiler plugins
-  val crossScala212 = (13 to 17).map(v => s"2.12.$v")
+  val crossScala212 = (13 to 18).map(v => s"2.12.$v")
   val crossScala213 = (4 to 10).map(v => s"2.13.$v")
   val crossScala3 = List(
     (0 to 3).map(v => s"3.1.$v"),
