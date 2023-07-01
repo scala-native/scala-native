7f424a70215f0ab612e2addda695809f5e564d92
Wojciech Mazur <wmazur@virtuslab.com>
2023-07-01 02:02:41 +0200

    Implement custom memory layouts based on existance of nir.Attr.Align

--- tools/src/main/scala/scala/scalanative/codegen/MemoryLayout.scala
+++ tools/src/main/scala/scala/scalanative/codegen/MemoryLayout.scala
@@ -8,6 +8,10 @@ import scalanative.util.unsupported
 import scalanative.codegen.MemoryLayout.PositionedType
 import scala.scalanative.build.Config
 import scala.scalanative.linker.ClassRef
+import scala.scalanative.linker.Field
+import scala.scalanative.nir.Attr.Alignment
+import scala.scalanative.build.BuildException
+import scala.annotation.tailrec
 
 final case class MemoryLayout(
     size: Long,
@@ -65,17 +69,108 @@ object MemoryLayout {
     val pos = mutable.UnrolledBuffer.empty[PositionedType]
     var offset = 0L
 
-    tys.foreach { ty =>
-      offset = align(offset, alignmentOf(ty))
+    val maxAlign = tys.foldLeft(1L) {
+      case (maxAlign, ty) =>
+        val align = alignmentOf(ty)
+
+        offset = this.align(offset, align)
+        pos += PositionedType(ty, offset)
+        offset += sizeOf(ty)
+
+        align.max(maxAlign)
+    }
+
+    MemoryLayout(align(offset, maxAlign), pos.toSeq)
+  }
+
+  def ofAlignedFields(
+      fields: Seq[Field]
+  )(implicit platform: PlatformInfo, meta: Metadata): MemoryLayout = {
+    import meta.layouts.ObjectHeader
+
+    val pos = mutable.UnrolledBuffer.empty[PositionedType]
+    var offset = 0L
+    var maxAlign = 1L
+
+    def addPadding(alignment: Int): Unit = {
+      val remainingPadding = align(offset, alignment) - offset
+      if (remainingPadding > 0) {
+        val last = pos.last
+        // Update last postion to set correct padding by replaceing the type with struct{ty, array[byte]}
+        pos.update(
+          pos.indexOf(last),
+          last.copy(ty =
+            Type.StructValue(
+              Seq(last.ty, Type.ArrayValue(Type.Byte, remainingPadding.toInt))
+            )
+          )
+        )
+        offset += remainingPadding
+      }
+    }
+
+    def addField(ty: Type, fieldAlignment: Option[Int] = None): Unit = {
+      val alignment = fieldAlignment.map(_.toLong).getOrElse(alignmentOf(ty))
+      maxAlign = maxAlign.max(alignment)
+
+      offset = align(offset, alignment)
+      println(offset)
       pos += PositionedType(ty, offset)
       offset += sizeOf(ty)
     }
 
-    val alignment = {
-      if (tys.isEmpty) 1
-      else tys.map(alignmentOf(_)).max
+    lazy val dynamicAlignmentWidth = {
+      val propName =
+        "scala.scalanative.meta.linktimeinfo.contendedPaddingWidth"
+      meta.linked.resolvedVals
+        .get(propName)
+        .collectFirst { case Val.Int(value) => value }
+        .getOrElse(
+          throw new BuildException(
+            s"Unable to resolve size of dynamic field alignment, linktime property not found: $propName"
+          )
+        )
+    }
+    def resolveAlignWidth(align: Alignment): Int = align.size match {
+      case nir.Attr.Alignment.linktimeResolved => dynamicAlignmentWidth
+      case fixedWidth                          => fixedWidth
     }
 
-    MemoryLayout(align(offset, alignment), pos.toSeq)
+    def isGroupAligned(field: Field) =
+      field.attrs.align.flatMap(_.group).isDefined
+
+    // fields should be already ordered by group names
+    @tailrec def loop(fields: List[Field]): Unit = fields match {
+      case Nil => ()
+      case field :: tail =>
+        val alignInfo = field.attrs.align
+        val groupName = alignInfo.flatMap(_.group)
+        val groupTail =
+          if (isGroupAligned(field))
+            tail.takeWhile(_.attrs.align.flatMap(_.group) == groupName)
+          else Nil
+        val headAlignSize = alignInfo.map(resolveAlignWidth)
+        // Align size is equal to maximal alignment of all fields in the group
+        val alignSize = headAlignSize.map {
+          groupTail.foldLeft(_) {
+            case (maxSize, field) =>
+              field.attrs.align
+                .map(resolveAlignWidth)
+                .map(_.max(maxSize))
+                .getOrElse(maxSize)
+          }
+        }
+
+        alignSize.foreach(addPadding)
+        addField(field.ty, alignSize)
+        groupTail.foreach(field => addField(field.ty))
+        alignSize.foreach(addPadding)
+
+        loop(tail.drop(groupTail.size))
+    }
+
+    addField(ObjectHeader.layout)
+    loop(fields.toList)
+    MemoryLayout(align(offset, maxAlign), pos.toSeq)
   }
 }
