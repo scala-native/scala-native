a313e675cbe6da96d5de467d5d7e5c7f73519bbc
Wojciech Mazur <wmazur@virtuslab.com>
2023-07-02 01:19:49 +0200

    Seperate Scala 2/3 unsigned package. Optimize Scala 3 by implementing toU conversions as inline defs

---
+++ nativelib/src/main/scala-2/scala/scalanative/unsigned/package.scala
@@ -0,0 +1,40 @@
+package scala.scalanative
+
+import scala.scalanative.runtime.Intrinsics._
+
+package object unsigned {
+
+  /** Scala Native unsigned extensions to the standard Byte. */
+  implicit class UnsignedRichByte(val value: Byte) extends AnyVal {
+    @inline def toUByte: UByte = unsignedOf(value)
+    @inline def toUShort: UShort = unsignedOf(value.toShort)
+    @inline def toUInt: UInt = unsignedOf(byteToUInt(value))
+    @inline def toULong: ULong = unsignedOf(byteToULong(value))
+  }
+
+  /** Scala Native unsigned extensions to the standard Short. */
+  implicit class UnsignedRichShort(val value: Short) extends AnyVal {
+    @inline def toUByte: UByte = unsignedOf(value.toByte)
+    @inline def toUShort: UShort = unsignedOf(value)
+    @inline def toUInt: UInt = unsignedOf(shortToUInt(value))
+    @inline def toULong: ULong = unsignedOf(shortToULong(value))
+  }
+
+  /** Scala Native unsigned extensions to the standard Int. */
+  implicit class UnsignedRichInt(val value: Int) extends AnyVal {
+    @inline def toUByte: UByte = unsignedOf(value.toByte)
+    @inline def toUShort: UShort = unsignedOf(value.toShort)
+    @inline def toUInt: UInt = unsignedOf(value)
+    @inline def toULong: ULong = unsignedOf(intToULong(value))
+    @inline def toUSize: USize = unsignedOf(castIntToRawSizeUnsigned(value))
+  }
+
+  /** Scala Native unsigned extensions to the standard Long. */
+  implicit class UnsignedRichLong(val value: Long) extends AnyVal {
+    @inline def toUByte: UByte = unsignedOf(value.toByte)
+    @inline def toUShort: UShort = unsignedOf(value.toShort)
+    @inline def toUInt: UInt = unsignedOf(value.toInt)
+    @inline def toULong: ULong = unsignedOf(value)
+    @inline def toUSize: USize = unsignedOf(castLongToRawSize(value))
+  }
+}
