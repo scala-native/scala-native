daf5d6c3285720fa89480fbdcedc6c56e1b01568
Wojciech Mazur <wmazur@virtuslab.com>
2023-07-19 09:19:20 +0200

    Adjust Publish and JDK compliance CI jobs (#3398)
    * Simplify publish CI job, just compile all projects to ensure correctness
    * Fix compilation of auxlib with older Scala versions
    * Test Jdk compliance with Scala 2
    * Fix compilation of StreamTest under Scala 2
    
    (cherry picked from commit 61f4230a0bb7fe6c0fc58496ffddf4fd319debf0)

--- unit-tests/shared/src/test/scala/org/scalanative/testsuite/javalib/util/stream/StreamTest.scala
+++ unit-tests/shared/src/test/scala/org/scalanative/testsuite/javalib/util/stream/StreamTest.scala
@@ -725,7 +725,10 @@ class StreamTest {
 
     val spliter =
       Stream
-        .iterate[jl.Integer](0, (n: jl.Integer) => n + 1)
+        .iterate[jl.Integer](
+          0,
+          ((n: jl.Integer) => n + 1): UnaryOperator[jl.Integer]
+        )
         .limit(srcSize)
         .spliterator()
 
@@ -832,7 +835,10 @@ class StreamTest {
 
     val unsizedSpliter =
       Stream
-        .iterate[jl.Integer](0, (n: jl.Integer) => n + 1)
+        .iterate[jl.Integer](
+          0,
+          ((n: jl.Integer) => n + 1): UnaryOperator[jl.Integer]
+        )
         .limit(srcSize)
         .spliterator()
 
