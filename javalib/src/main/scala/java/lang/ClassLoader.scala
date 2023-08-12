126007f2ecb5e099a30b74b269c62cde19b1a31d
Rikito Taniguchi <rikiriki1238@gmail.com>
2023-08-12 15:00:53 +0900

    Remove ClassLoader

--- javalib/src/main/scala/java/lang/ClassLoader.scala
+++
@@ -1,9 +0,0 @@
-package java.lang
-
-class ClassLoader protected (parent: ClassLoader) {
-  def this() = this(null)
-  // def loadClass(name: String): Class[_] = ???
-  // def getParent(): ClassLoader = ???
-  // def getResourceAsStream(name: String): java.io.InputStream = ???
-  // def getResources(name: String): java.util.Enumeration[_] = ???
-}
