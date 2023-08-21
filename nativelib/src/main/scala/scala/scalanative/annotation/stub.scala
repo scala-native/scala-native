dc37007ca96d608ad3cc875fb0937f3b478415e9
Rikito Taniguchi <rikiriki1238@gmail.com>
2023-08-21 17:49:12 +0900

    Revert scaladoc change in stub.scala

--- nativelib/src/main/scala/scala/scalanative/annotation/stub.scala
+++ nativelib/src/main/scala/scala/scalanative/annotation/stub.scala
@@ -3,6 +3,6 @@ package annotation
 
 /** An annotation that is used to indicate that a given method is provided as a
  *  stub, but is not currently supported. These methods are not discovered by
- *  the linker.
+ *  the linker by default, but will be discovered only if a special flag is
  */
 final class stub extends scala.annotation.StaticAnnotation
