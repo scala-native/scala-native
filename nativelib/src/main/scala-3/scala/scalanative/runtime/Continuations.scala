6489924abdeb1556b01b19919828d9b587d0c6ef
Natsu Kagami <nki@nkagami.me>
2023-09-01 17:01:09 +0200

    Add comments to the Continuations scala implementation

--- nativelib/src/main/scala-3/scala/scalanative/runtime/Continuations.scala
+++ nativelib/src/main/scala-3/scala/scalanative/runtime/Continuations.scala
@@ -10,45 +10,77 @@ import scala.util.Try
 object Continuations:
   import ContImpl.*
 
-  ContImpl.init(CFuncPtr2.fromScalaFunction(allocateBlob))
-
-  inline def boundary[T](inline f: BoundaryLabel[T] ?=> T): T =
-    val call: ContFnT = (x: ContImpl.BoundaryLabel) => pObj(Try(f(using x)))
-    val resP = ContImpl.boundary(contFn, pObj(call))
-    objP[Try[T]](resP).get
-
   opaque type BoundaryLabel[-T] = ContImpl.BoundaryLabel
 
-  inline def break(using BoundaryLabel[Unit]): Nothing =
-    suspend[Nothing, Unit](_ => ())
-  inline def break[T](value: T)(using BoundaryLabel[T]): Nothing =
-    suspend[Nothing, T](_ => value)
+  ContImpl.init(CFuncPtr2.fromScalaFunction(allocateBlob))
 
+  /** Marks the given body as suspendable with a `BoundaryLabel` that `suspend`
+   *  can refer to. Forwards the return value of `body`, or the return value of
+   *  the `suspend` call, if it happens during the execution of `f`.
+   *
+   *  __Safety__: the passed-in `BoundaryLabel` cannot be used outside of the
+   *  scope of the body. Suspending to a `BoundaryLabel` not created by a
+   *  `boundary` call higher on the same call stack is undefined behaviour.
+   */
+  inline def boundary[T](inline body: BoundaryLabel[T] ?=> T): T =
+    val call: ContFnT = (x: ContImpl.BoundaryLabel) =>
+      objToPtr(Try(body(using x)))
+    val resP = ContImpl.boundary(contFn, objToPtr(call))
+    objFromPtr(resP).asInstanceOf[Try[T]].get
+
+  /** Suspends the current running stack up to the corresponding `boundary` into
+   *  a continuation `cont: R => T` and passes it into `f`. The return value of
+   *  `f(cont)` is returned to `boundary`.
+   *
+   *  Same as `suspendCont`, but hides the fact that the passed in function is a
+   *  continuation.
+   */
   inline def suspend[R, T](
       inline f: (R => T) => T
   )(using label: BoundaryLabel[T]): R =
-    suspendCont[R, T](cont => f(r => cont(r)))
+    suspendCont[R, T](f)
 
+  /** Same as `suspend` where the continuation expects no parameters. */
   def suspend[T](
       f: (() => T) => T
   )(using label: BoundaryLabel[T]): Unit =
     suspendCont[Unit, T](cont => f(() => cont(())))
 
+  /** Immediately return to the `boundary`. */
+  inline def break(using BoundaryLabel[Unit]): Nothing =
+    suspend[Nothing, Unit](_ => ())
+
+  /** Immediately return to the `boundary`, returning the given value. */
+  inline def break[T](value: T)(using BoundaryLabel[T]): Nothing =
+    suspend[Nothing, T](_ => value)
+
   inline def suspendCont[R, T](
       inline f: Continuation[R, T] => T
   )(using label: BoundaryLabel[T]): R =
     val cont = Continuation()
+    // We want to reify the post-suspension body here,
+    // while creating the full continuation object.
     val call = (c: ContImpl.Continuation) =>
       cont.cont = c
-      pObj(Try(f(cont)))
-    val resP = ContImpl.suspend(label, suspendFn, pObj(call), pObj(cont))
-    objP[R](resP)
+      objToPtr(Try(f(cont)))
+    val resP =
+      ContImpl.suspend(label, suspendFn, objToPtr(call), objToPtr(cont))
+    objFromPtr(resP).asInstanceOf[R]
 
+  /** A `Continuation` holds the C implementation continuation pointer,
+   *  alongside a list of `ObjectArray`s, used for storing suspended fragments
+   *  of the stack.
+   *
+   *  These fragments need to be treated as possibly containing pointers into
+   *  the GC heap, and so needs to be scanned by the GC. We store them in an
+   *  `ObjectArray` to simulate just that.
+   */
   class Continuation[-R, +T] extends (R => T):
-    private[Continuations] var cont: ContImpl.Continuation = pNull
+    private[Continuations] var cont: ContImpl.Continuation = nullPtr
     private val allocs = mutable.ArrayBuffer[ObjectArray]()
 
-    def apply(x: R): T = objP[Try[T]](resume(cont, pObj(x))).get
+    def apply(x: R): T =
+      objFromPtr(resume(cont, objToPtr(x))).asInstanceOf[Try[T]].get
 
     private[Continuations] def alloc(size: CUnsignedLong): Ptr[Byte] =
       val obj = ObjectArray.alloc(size.toInt) // round up the blob size
@@ -58,31 +90,46 @@ object Continuations:
 
   // STATIC FUNCTIONS THAT CALL PASSED-IN FUNCTION OBJECTS
 
-  private val suspendFn: SuspendFn = CFuncPtr2.fromScalaFunction((cont, f) =>
-    val fn = objP[ContImpl.Continuation => Ptr[Byte]](f)
+  /** Transformed version of the suspend lambda, to be passed to cont_suspend.
+   *  Takes:
+   *    - `cont`: the reified continuation
+   *    - `arg`: The suspend lambda as Continuation => Ptr[Byte] (the returned
+   *      object, cast to a pointer)
+   *
+   *  Returns Ptr[Byte] / void*.
+   */
+  private val suspendFn: SuspendFn = CFuncPtr2.fromScalaFunction((cont, arg) =>
+    val fn = objFromPtr(arg).asInstanceOf[ContImpl.Continuation => Ptr[Byte]]
     fn(cont)
   )
 
-  private val contFn: ContFn = CFuncPtr2.fromScalaFunction((label, f) =>
-    val fp = objP[ContFnT](f)
+  /** Transformed version of the boundary body, to be passed to cont_boundary.
+   *  Takes:
+   *    - `label`: the boundary label
+   *    - `arg`: The boundary body as BoundaryLabel ?=> Ptr[Byte] (the returned
+   *      object, cast to a pointer)
+   *
+   *  Returns Ptr[Byte] / void*.
+   */
+  private val contFn: ContFn = CFuncPtr2.fromScalaFunction((label, arg) =>
+    val fp = objFromPtr(arg).asInstanceOf[ContFnT]
     fp(label)
   )
 
   private def allocateBlob(size: CUnsignedLong, cont: Ptr[Byte]): Ptr[Byte] =
-    objP[Continuation[_, _]](cont).alloc(size)
+    objFromPtr(cont).asInstanceOf[Continuation[_, _]].alloc(size)
 
   // FOR WORKING WITH POINTERS
 
   type ContFnT = ContImpl.BoundaryLabel => Ptr[Byte]
 
-  private inline def pNull[T]: Ptr[T] = fromRawPtr[T](castIntToRawPtr(0))
+  private inline def nullPtr[T]: Ptr[T] = fromRawPtr[T](castIntToRawPtr(0))
 
-  private inline def pObj[T](o: T) = fromRawPtr[Byte](
-    castObjectToRawPtr(o.asInstanceOf[Object])
-  )
-
-  private inline def objP[T](p: Ptr[Byte]) =
-    castRawPtrToObject(p.rawptr).asInstanceOf[T]
+  // Cast object to/from pointer.
+  private inline def objToPtr(o: Any) =
+    fromRawPtr[Byte](castObjectToRawPtr(o.asInstanceOf[AnyRef]))
+  private inline def objFromPtr(p: Ptr[Byte]): AnyRef =
+    castRawPtrToObject(p.rawptr)
 
 end Continuations
 
