118c17ae7f185ab35cfccb1d8b23074c4f56f1fb
Natsu Kagami <nki@nkagami.me>
2023-09-04 15:58:20 +0200

    Implement delimcc for x86-linux
    The implementation mostly just adds the Assembly part. Due to x86 passing most things
    through stack we have to be more careful with cleaning up after ourselves with the parameters.
    Furthermore, it is not possible to keep the "stack pointer offset = 8" claim without
    more complicated Assembly code, so I just opted to make it special for x86.

--- nativelib/src/main/resources/scala-native/delimcc.c
+++ nativelib/src/main/resources/scala-native/delimcc.c
@@ -18,9 +18,21 @@
     (defined(__linux__) || defined(__APPLE__)) // x86-64 linux and macOS
 #define ASM_JMPBUF_SIZE 72
 #define JMPBUF_STACK_POINTER_OFFSET (16 / 8)
+#elif defined(__i386__) &&                                                     \
+    (defined(__linux__) || defined(__APPLE__)) // x86 linux and macOS
+#define ASM_JMPBUF_SIZE 32
+#define JMPBUF_STACK_POINTER_OFFSET (16 / 4)
 #elif defined(__x86_64__) && defined(_WIN64) // x86-64 Windows
 #define ASM_JMPBUF_SIZE 256
 #define JMPBUF_STACK_POINTER_OFFSET (16 / 8)
+#endif
+
+// The return address is always stored in stack_btm - BOUNDARY_LR_OFFSET.
+// See `_lh_boundary_entry`.
+#if defined(__i386__)
+#define BOUNDARY_LR_OFFSET 12
+#else
+#define BOUNDARY_LR_OFFSET 8
 #endif
 
 // Apple platforms mangle the names of some symbols in assembly. We override the
@@ -36,10 +48,6 @@
 #define __cont_resume_impl _cont_resume_impl
 #endif
 
-// The return address is always stored in stack_btm - BOUNDARY_LR_OFFSET.
-// See `_lh_boundary_entry`.
-#define BOUNDARY_LR_OFFSET 8
-
 #define __externc extern
 #define __noreturn __attribute__((noreturn))
 #define __returnstwice __attribute__((returns_twice))
@@ -51,7 +59,8 @@ __externc __returnstwice int _lh_setjmp(lh_jmp_buf buf);
 // Jumps to the given setjmp'd buffer, returning arg as the value.
 // arg must be non-zero.
 __externc void *_lh_longjmp(lh_jmp_buf buf, int arg);
-// Stores the return address in sp+8, then calls __cont_boundary_impl.
+// Stores the return address in sp+BOUNDARY_LR_OFFSET, then calls
+// __cont_boundary_impl.
 __externc __returnstwice void *_lh_boundary_entry(ContFn *f, void *arg);
 // Allocate enough stack for the resumption, and then call __cont_resume_impl.
 __externc void *_lh_resume_entry(ptrdiff_t cont_size, Continuation *c,
@@ -286,7 +295,9 @@ void __cont_resume_impl(void *tail, Continuation *cont, void *out,
     // set up stuff
     memcpy(return_buf, cont->buf, ASM_JMPBUF_SIZE);
 
+#if !defined(__i386__) // 32 bit platforms don't have an alignment restriction?
     assert((diff & 15) == 0);
+#endif
     fprintf(stderr,
             "diff is %ld, stack (size = %ld) goes %p~%p -> %p~%p | original "
             "cont = %p [%p]\n",
