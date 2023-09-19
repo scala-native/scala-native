3d72c1db2b15debf00b36319280abfe4ece00d75
LeeTibbert <LeeTibbert@users.noreply.github.com>
2023-09-19 11:23:36 -0400

    Implement `java.nio.file.{AccessMode,ReadOnlyFileSystemException}` (#3479)

---
+++ javalib/src/main/scala/java/nio/file/AccessMode.scala
@@ -0,0 +1,21 @@
+package java.nio.file
+
+sealed class AccessMode(name: String, ordinal: Int)
+    extends _Enum[AccessMode](name, ordinal) {
+  override def toString() = this.name
+}
+
+object AccessMode {
+  final val EXECUTE = new AccessMode("EXECUTE", 0)
+  final val READ = new AccessMode("READ", 1)
+  final val WRITE = new AccessMode("WRITE", 1)
+
+  private[this] val cachedValues =
+    Array(EXECUTE, READ, WRITE)
+  def values(): Array[AccessMode] = cachedValues.clone()
+  def valueOf(name: String): AccessMode = {
+    cachedValues.find(_.name() == name).getOrElse {
+      throw new IllegalArgumentException("No enum const AccessMode." + name)
+    }
+  }
+}
