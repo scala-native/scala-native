5a047d33c9c2f48980b00b208715b8134c88a6e3
LeeTibbert <leetibbert@users.noreply.github.com>
2023-09-28 10:19:44 +0200

    Fix #3511: Scripted tests with better Java 21 support (#3512)
    (cherry picked from commit 1fa5e5c414fcfa707004a771e1c8bb88aac33a7c)

--- project/ScalaVersions.scala
+++ project/ScalaVersions.scala
@@ -35,8 +35,14 @@ object ScalaVersions {
   val scala213: String = crossScala213.last
   val scala3: String = "3.1.3"
 
-  // minimum version - 1.5 is required for Scala 3 and 1.5.8 has log4j vulnerability fixed
-  val sbt10Version: String = "1.5.8"
+  // minimum version rationale:
+  //   1.5 is required for Scala 3 and
+  //   1.5.8 has log4j vulnerability fixed
+  //   1.9.0 is required in order to use Java >= 21
+  //   1.9.4 fixes (Common Vulnerabilities and Exposures) CVE-2022-46751
+  //   1.9.6 is current
+
+  val sbt10Version: String = "1.9.6"
   val sbt10ScalaVersion: String = scala212
 
   val libCrossScalaVersions: Seq[String] =
