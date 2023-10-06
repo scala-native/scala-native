1cd5a5da58974e33ce6f426d3052425829bb7224
Wojciech Mazur <wmazur@virtuslab.com>
2023-10-06 12:54:41 +0200

    Validate well known issue in provided or discovered NativeConfig

--- tools/src/main/scala/scala/scalanative/build/Validator.scala
+++ tools/src/main/scala/scala/scalanative/build/Validator.scala
@@ -14,8 +14,8 @@ object Validator {
    */
   def validate(config: Config): Config =
     (validateMainClass _)
-      .andThen(validateBasename)
       .andThen(validateClasspath)
+      .andThen(validateCompileConfig)
       .apply(config)
 
   // throws if Application with no mainClass
@@ -33,18 +33,36 @@ object Validator {
     config
   }
 
-  // throws if moduleName or baseName is not set
-  private def validateBasename(config: Config): Config =
-    if (config.baseName.trim.isEmpty) { // trim for non default error
-      throw new BuildException(
-        "Config defaultBasename or NativeConfig baseName must be set."
-      )
-    } else config
-
   // filter so classpath only has jars or directories
   private def validateClasspath(config: Config): Config = {
     val fclasspath = NativeLib.filterClasspath(config.classPath)
     config.withClassPath(fclasspath)
+  }
+
+  private def validateCompileConfig(config: Config): Config = {
+    val c = config.compilerConfig
+    val issues = List.newBuilder[String]
+
+    if (c.multithreadingSupport) c.gc match {
+      case GC.Commix =>
+        issues += "CommixGC does not support multithreading yet, use other GC implementation (Immix, Boehm)"
+      case _ => ()
+    }
+    if (!Files.exists(c.clang))
+      issues += s"Provided clang path '${c.clang.toAbsolutePath()}' does not exist, specify a valid path to LLVM Toolchain distribution using config or LLVM_BIN environment variable"
+    if (!Files.exists(c.clangPP))
+      issues += s"Provided clang++ path '${c.clangPP.toAbsolutePath()}' does not exist, specify a valid path to LLVM Toolchain distribution using config or LLVM_BIN environment variable"
+    if (c.baseName.trim().isEmpty())
+      issues += s"Provided baseName is blank, provide a name of target artifact without extensions to allow for determinstic builds"
+
+    issues.result() match {
+      case Nil => config
+      case issues =>
+        throw new BuildException(
+          (s"Found ${issues.size} issue within provided confguration: " :: issues)
+            .mkString("\n  - ")
+        )
+    }
   }
 
 }
