2c0e7dfeb9497e0411133bbe091732e1d54bcfff
Dimi Racordon <dimitri.racordon@gmail.com>
2023-11-01 11:33:45 +0100

    Add code documentation for `nir.Defn` (#3590)
    * Add code documentation
    * Apply Wojciech's comments

--- nir/src/main/scala/scala/scalanative/nir/Defns.scala
+++ nir/src/main/scala/scala/scalanative/nir/Defns.scala
@@ -2,33 +2,89 @@ package scala.scalanative
 package nir
 import scala.scalanative.nir.Defn.Define
 
+/** A definition in NIR.
+ *
+ *  Programs in NIR are represented as a sequence of definitions denoting types,
+ *  methods and fields. Definitions fall into two categories:
+ *
+ *    - Top-level definitions: these represent classes, modules, traits, or
+ *      global variables and constants.
+ *    - Member definitions: these represent fields or methods.
+ *
+ *  Classes and modules inherit from a single parent with the exception of
+ *  `java.lang.Object`, which sits at the top of the hierarchy. They may
+ *  additionally implement traits.
+ *
+ *  Definitions may also carry attributes providing further information about
+ *  their semantics (e.g., whether a method may be inlined). Attributes are also
+ *  used to mark special-purpose definitions, such as stubs, proxies and FFIs.
+ */
 sealed abstract class Defn {
+
+  /** Returns the name of the definition. */
   def name: Global
+
+  /** Returns the attributes of the definition. */
   def attrs: Attrs
+
+  /** Returns the site in the program sources corresponding to the definition.
+   */
   def pos: Position
-  final def show: String = nir.Show(this)
+
+  /** Returns a textual representation of `this`. */
+  final def show: String =
+    nir.Show(this)
+
+  /** Returns `true` iff `this` is considered as an entry point by reachability
+   *  analysis.
+   */
   final def isEntryPoint = this match {
     case Define(attrs, Global.Member(_, sig), _, _, _) =>
       sig.isClinit || attrs.isExtern
     case _ => false
   }
+
 }
 
 object Defn {
-  // low-level
-  final case class Var(attrs: Attrs, name: Global.Member, ty: Type, rhs: Val)(
-      implicit val pos: Position
-  ) extends Defn
-  final case class Const(attrs: Attrs, name: Global.Member, ty: Type, rhs: Val)(
-      implicit val pos: Position
-  ) extends Defn
+
+  /** A variable definition corresponding to either a field in class or module,
+   *  or to a top-level global variable.
+   */
+  final case class Var(
+      attrs: Attrs,
+      name: Global.Member,
+      ty: Type,
+      rhs: Val
+  )(implicit val pos: Position)
+      extends Defn
+
+  /** A unique, read-only instance of some type.
+   *
+   *  A constant definition is distinct from a constant literal, which would be
+   *  represented by a `Val`.
+   */
+  final case class Const(
+      attrs: Attrs,
+      name: Global.Member,
+      ty: Type,
+      rhs: Val
+  )(implicit val pos: Position)
+      extends Defn
+
+  /** A method declaration.
+   *
+   *  Methods of abstract classes and traits can be declared without a
+   *  definition and are resolved at runtime through dynamic dispatch.
+   */
   final case class Declare(
       attrs: Attrs,
       name: Global.Member,
       ty: Type.Function
-  )(implicit
-      val pos: Position
-  ) extends Defn
+  )(implicit val pos: Position)
+      extends Defn
+
+  /** A method definition. */
   final case class Define(
       attrs: Attrs,
       name: Global.Member,
@@ -37,48 +93,75 @@ object Defn {
       debugInfo: Define.DebugInfo = Define.DebugInfo.empty
   )(implicit val pos: Position)
       extends Defn
+
   object Define {
+
+    /** A set of metadata about a definition for debugging purposes. */
     case class DebugInfo(
         localNames: LocalNames,
         lexicalScopes: Seq[DebugInfo.LexicalScope]
     ) {
+
+      /** A map from scope ID to its value. */
       lazy val lexicalScopeOf: Map[ScopeId, DebugInfo.LexicalScope] =
         lexicalScopes.map {
           case scope @ DebugInfo.LexicalScope(id, _, _) => (id, scope)
         }.toMap
+
     }
+
     object DebugInfo {
+
+      /** An empty set of debug metadata. */
       val empty: DebugInfo = DebugInfo(
         localNames = Map.empty,
         lexicalScopes = Seq(LexicalScope.AnyTopLevel)
       )
 
+      /** A lexical scope in the program sources. */
       case class LexicalScope(
           id: ScopeId,
           parent: ScopeId,
           srcPosition: Position
       ) {
-        def isTopLevel: Boolean = id.isTopLevel
+
+        /** Returns `true` iff `this` is the top-level scope. */
+        def isTopLevel: Boolean =
+          id.isTopLevel
+
       }
+
       object LexicalScope {
-        final val AnyTopLevel =
-          LexicalScope(ScopeId.TopLevel, ScopeId.TopLevel, Position.NoPosition)
+
+        /** Returns a top-level scope covering the given site in the source
+         *  program.
+         */
         def TopLevel(defnPosition: Position) =
-          AnyTopLevel.copy(srcPosition = defnPosition)
-        implicit val ordering: Ordering[LexicalScope] = Ordering.by(_.id.id)
+          LexicalScope(ScopeId.TopLevel, ScopeId.TopLevel, defnPosition)
+
+        /** An abstract top-level scope. */
+        final val AnyTopLevel =
+          TopLevel(Position.NoPosition)
+
+        /** The order between lexical scopes. */
+        implicit val ordering: Ordering[LexicalScope] =
+          Ordering.by(_.id.id)
+
       }
+
     }
 
   }
 
-  // high-level
+  /** The NIR representation of a Scala trait. */
   final case class Trait(
       attrs: Attrs,
       name: Global.Top,
       traits: Seq[Global.Top]
-  )(implicit
-      val pos: Position
-  ) extends Defn
+  )(implicit val pos: Position)
+      extends Defn
+
+  /** The NIR representation of a Scala class. */
   final case class Class(
       attrs: Attrs,
       name: Global.Top,
@@ -86,6 +169,8 @@ object Defn {
       traits: Seq[Global.Top]
   )(implicit val pos: Position)
       extends Defn
+
+  /** The NIR representation of a Scala module. */
   final case class Module(
       attrs: Attrs,
       name: Global.Top,
@@ -93,4 +178,5 @@ object Defn {
       traits: Seq[Global.Top]
   )(implicit val pos: Position)
       extends Defn
+
 }
--- nir/src/main/scala/scala/scalanative/nir/package.scala
+++ nir/src/main/scala/scala/scalanative/nir/package.scala
@@ -1,13 +1,35 @@
 package scala.scalanative
 
 package object nir {
+
+  /** The name of a variable in program sources. */
   type LocalName = String
+
+  /** A map from SSA identifier to its name in program sources.
+   *
+   *  Local variables get lowered to an static assignment that is assigned to a
+   *  unique identifier in the context of its definition. Instances of this type
+   *  are used to maintain the correspondance between an SSA ID and its
+   *  corresponding name in program sources.
+   */
   type LocalNames = Map[Local, LocalName]
+
+  /** The identifier of a lexical scope. */
   case class ScopeId(id: Int) extends AnyVal {
+
+    /** Returns `true` iff `this` is the top-level scope. */
     def isTopLevel: Boolean = this.id == ScopeId.TopLevel.id
+
   }
+
   object ScopeId {
+
+    /** Returns the innermost scope containing `id`. */
     def of(id: Local): ScopeId = ScopeId(id.id.toInt)
+
+    /** The top-level scope. */
     val TopLevel = ScopeId(0)
+
   }
+
 }
