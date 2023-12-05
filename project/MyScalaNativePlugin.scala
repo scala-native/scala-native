04cf143e0fff6e020dbd039e438e26ab5ca881e4
Rikito Taniguchi <rikiriki1238@gmail.com>
2023-12-05 22:00:52 +0900

    Integrate with AsyncProfiler (#3602)
    * Integrate with AsyncProfiler
    * Define nativeLinkProfiling in MyScalaNativePlugin
    * Configure output format
    * Enable to configure async-profiler from task arguments

--- project/MyScalaNativePlugin.scala
+++ project/MyScalaNativePlugin.scala
@@ -3,12 +3,21 @@ package build
 import sbt._
 import sbt.Keys._
 
+import scala.scalanative.sbtplugin.Utilities._
 import scala.scalanative.sbtplugin.ScalaNativePlugin
 import scala.scalanative.sbtplugin.ScalaNativePlugin.autoImport._
 import scala.sys.env
+import complete.DefaultParsers._
+
+import one.profiler.AsyncProfilerLoader
+import one.profiler.AsyncProfiler
+import build.OutputType._
 
 object MyScalaNativePlugin extends AutoPlugin {
   override def requires: Plugins = ScalaNativePlugin
+
+  lazy val nativeLinkProfiling =
+    inputKey[File]("Running nativeLink with AsyncProfiler.")
 
   // see: https://github.com/scalameta/metals/blob/0176a491cd209a09852ab33f99fd7de639e8e2dd/metals/src/main/scala/scala/meta/internal/builds/BloopInstall.scala#L81
   final val isGeneratingForIDE =
@@ -55,6 +64,80 @@ object MyScalaNativePlugin extends AutoPlugin {
     }
   }
 
+  private def nativeLinkProfilingImpl = Def.inputTaskDyn {
+    val sbtLogger = streams.value.log
+    val logger = sbtLogger.toLogger
+
+    val args = spaceDelimited("<arg>").parsed
+
+    val commands = args.headOption.getOrElse(
+      throw new IllegalArgumentException(
+        "usage: nativeLinkProfiling <commands> <output-type>\n" +
+          "<commands>: `,` delimited arguments for async-profiler. refer https://github.com/async-profiler/async-profiler/blob/49d08fd068f81f1c952320c4bd082d991e09db97/src/arguments.cpp#L65-L113 \n" +
+          "<output-type>: text|collapsed|flamegraph|tree (default: flamegraph) \n" +
+          "e.g. `nativeLinkProfiling events=cpu,interval=10000000,threads"
+      )
+    )
+
+    val outputType = (for {
+      input <- args.tail.headOption.toRight(
+        new IllegalArgumentException("Missing output type")
+      )
+      typ <- OutputType.fromString(input)
+    } yield typ) match {
+      case Left(ex) =>
+        logger.warn(
+          s"${ex.getMessage()}, using default output type: `flamegraph`"
+        )
+        OutputType.Flamegraph
+      case Right(value) => value
+    }
+
+    val profilerOpt: Option[AsyncProfiler] =
+      if (AsyncProfilerLoader.isSupported())
+        try {
+          Some(AsyncProfilerLoader.load())
+        } catch {
+          case ex: IllegalStateException => {
+            logger.warn(
+              s"Couldn't load async-profiler, restart sbt to workaround the problem. " +
+                "This is usually caused because the previous sbt's classloader loaded the async-profiler DLL. \n" +
+                ex.getMessage()
+            )
+            throw ex
+          }
+          case e: Throwable => throw e
+        }
+      else {
+        logger.warn(
+          "Couldn't load async-profiler for the current OS, architecture or glibc is unavailable. " +
+            "Profiling will not be available." +
+            "See the supported platforms https://github.com/jvm-profiling-tools/ap-loader#supported-platforms"
+        )
+        None
+      }
+
+      val module = moduleName.value
+      val out =
+        (crossTarget.value / s"$module-profile.${outputType.extension}").toString
+      profilerOpt match {
+        case Some(profiler) =>
+          Def.task {
+            logger.info(
+              s"[async-profiler] starting profiler with commands: start,$commands"
+            )
+            profiler.execute(s"start,$commands")
+            nativeLink.value
+          } andFinally {
+            logger.info(s"[async-profiler] stop profiler, output to ${out}")
+            profiler.execute("stop")
+            profiler.execute(s"${outputType.name},file=${out}")
+          }
+        case None =>
+          nativeLink
+      }
+  }
+
   override def projectSettings: Seq[Setting[_]] = Def.settings(
     /* Remove libraryDependencies on ourselves; we use .dependsOn() instead
      * inside this build.
@@ -81,6 +164,35 @@ object MyScalaNativePlugin extends AutoPlugin {
           (LocalProject("scala-native") / baseDirectory).value,
           s"https://raw.githubusercontent.com/scala-native/scala-native/v$nativeVersion/"
         )
+    },
+    inConfig(Compile) {
+      nativeLinkProfiling := nativeLinkProfilingImpl
+        .tag(NativeTags.Link)
+        .evaluated,
     }
   )
+}
+
+sealed abstract class OutputType(val name: String) {
+  def extension: String = this match {
+    case Text       => "txt"
+    case Collapsed  => "csv"
+    case Flamegraph => "html"
+    case Tree       => "html"
+  }
+}
+
+object OutputType {
+  case object Text extends OutputType("text")
+  case object Collapsed extends OutputType("collapsed")
+  case object Flamegraph extends OutputType("flamegraph")
+  case object Tree extends OutputType("tree")
+  def fromString(s: String): Either[IllegalArgumentException, OutputType] =
+    s match {
+      case Text.name       => Right(Text)
+      case Collapsed.name  => Right(Collapsed)
+      case Flamegraph.name => Right(Flamegraph)
+      case Tree.name       => Right(Tree)
+      case _ => Left(new IllegalArgumentException(s"Unknown output type: $s"))
+    }
 }
