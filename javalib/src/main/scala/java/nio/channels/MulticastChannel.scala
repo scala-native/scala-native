59c6020c0f2b7af44e9a0542f22f9e6f162dff40
Michel Davit <michel@davit.fr>
2023-12-05 09:29:10 +0100

    Remove channel leftover

--- javalib/src/main/scala/java/nio/channels/MulticastChannel.scala
+++
@@ -1,9 +0,0 @@
-package java.nio.channels
-
-import java.net.{InetAddress, NetworkInterface}
-
-trait MulticastChannel extends NetworkChannel {
-
-  def join(group: InetAddress, interf: NetworkInterface): MembershipKey
-
-}
