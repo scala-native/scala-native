eb9a1713c0e1bbfd7b1e324fbf519687b94e3a2e
Wojciech Mazur <wmazur@virtuslab.com>
2023-12-14 00:33:05 +0100

    Use load_acquire and store_plain for final fields

--- tools/src/main/scala/scala/scalanative/codegen/Lower.scala
+++ tools/src/main/scala/scala/scalanative/codegen/Lower.scala
@@ -593,7 +593,7 @@ object Lower {
 
       val memoryOrder =
         if (field.attrs.isVolatile) nir.MemoryOrder.SeqCst
-        else if (field.attrs.isFinal) nir.MemoryOrder.Monotonic
+        else if (field.attrs.isFinal) nir.MemoryOrder.Acquire
         else nir.MemoryOrder.Unordered
       val elem = genFieldElemOp(buf, genVal(buf, obj), name)
       genLoadOp(buf, n, nir.Op.Load(ty, elem, Some(memoryOrder)))
@@ -611,9 +611,10 @@ object Lower {
           throw new LinkingException(s"Metadata for field '$name' not found")
       }
 
+      // No explicit memory order for store of final field, 
+      // all final fields are published with release fence when existing the constructor
       val memoryOrder =
         if (field.attrs.isVolatile) nir.MemoryOrder.SeqCst
-        else if (field.attrs.isFinal) nir.MemoryOrder.Monotonic
         else nir.MemoryOrder.Unordered
       val elem = genFieldElemOp(buf, genVal(buf, obj), name)
       genStoreOp(buf, n, nir.Op.Store(ty, elem, value, Some(memoryOrder)))
