d09749157d4a6906245da4b29d4a718e387fa04f
Alex Dupre <ale@bitgold.com>
2023-12-21 18:48:08 +0100

    Reword the comment and remove unused vals.

--- test-interface/src/main/scala/scala/scalanative/testinterface/TestMain.scala
+++ test-interface/src/main/scala/scala/scalanative/testinterface/TestMain.scala
@@ -28,27 +28,23 @@ object TestMain {
       |""".stripMargin
   }
 
-  final val iPv4Loopback = "127.0.0.1"
-  final val iPv6Loopback = "::1"
-
   private def setFreeBSDWorkaround(): Unit = {
     /* Standard out-of-the-box FreeBSD differs from Linux & macOS in
-     * not allowing IPv4 mapped IPv6 addresses, such as :FFFF:127.0.0.1
-     * or ::ffff:7f00:1.  These can be enabled at runtime by running
-     *
-     *   sysctl net.inet6.ip6.v6only=0
-     *
-     * To make it persistent on reboot it is possible to set the line
-     * ipv6_ipv4mapping="YES" in /etc/rc.conf.
+     * not allowing IPv4-mapped IPv6 addresses, such as :FFFF:127.0.0.1
+     * or ::ffff:7f00:1.
      *
      * Another difference is that Java versions >= 11 on FreeBSD set
      * java.net.preferIPv4Stack=true by default, so the sbt server
      * listens only on a tcp4 socket.
      *
-     * The easiest way to make TestMain to work on most FreeBSD machines
-     * with different Java versions without changing system defaults is
-     * to set java.net.preferIPv4Stack=true in Scala Native in order to
-     * always use AF_INET IPv4 socket.
+     * Even if IPv4-mapped IPv6 addresses can be enabled (via the
+     * net.inet6.ip6.v6only=0 sysctl and/or via the ipv6_ipv4mapping="YES"
+     * rc.conf variable) and sbt can be instructed to listen on an IPv6
+     * socket (via the java.net.preferIPv4Stack=false system property),
+     * the easiest way to make TestMain to work on most FreeBSD machines,
+     * with different Java versions, is to set
+     * java.net.preferIPv4Stack=true in Scala Native, before the first
+     * Java network call, in order to always use an AF_INET IPv4 socket.
      */
 
     System.setProperty("java.net.preferIPv4Stack", "true")
@@ -75,9 +71,8 @@ object TestMain {
     }
 
     if (LinktimeInfo.isFreeBSD) setFreeBSDWorkaround()
-    val serverAddr = iPv4Loopback
     val serverPort = args(0).toInt
-    val clientSocket = new Socket(serverAddr, serverPort)
+    val clientSocket = new Socket("127.0.0.1", serverPort)
     val nativeRPC = new NativeRPC(clientSocket)(ExecutionContext.global)
     val bridge = new TestAdapterBridge(nativeRPC)
 
