3bbc63377ba8a6f2654d1be1c6ce32ddd0d482f6
Wojciech Mazur <wmazur@virtuslab.com>
2024-01-08 12:57:44 +0100

    Fix singlethreaded compilation

--- nativelib/src/main/resources/scala-native/gc/commix/MutatorThread.c
+++ nativelib/src/main/resources/scala-native/gc/commix/MutatorThread.c
@@ -39,8 +39,10 @@ void MutatorThread_init(Field_t *stackbottom) {
     // Following init operations might trigger GC, needs to be executed after
     // acknownleding the new thread in MutatorThreads_add
     Allocator_InitCursors(&self->allocator);
+#ifdef SCALANATIVE_MULTITHREADING_ENABLED
     // Stop if there is ongoing GC_collection
     scalanative_GC_yield();
+#endif
 }
 
 void MutatorThread_delete(MutatorThread *self) {
