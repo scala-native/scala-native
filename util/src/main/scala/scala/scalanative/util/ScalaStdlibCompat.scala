3ccbaee752a7034bb66ae8c0c46ff25f7a2bc420
Wojciech Mazur <wmazur@virtuslab.com>
2024-01-13 12:18:26 +0100

    Use ArraySeq in BinaryDeserializer

---
+++ util/src/main/scala/scala/scalanative/util/ScalaStdlibCompat.scala
@@ -0,0 +1,20 @@
+package scala.scalanative.util
+
+object ScalaStdlibCompat {
+  private[scalanative] object ArraySeqCompatDef {
+    val ArraySeq = scala.collection.immutable.Vector
+    type ArraySeq[T] = scala.collection.immutable.Vector[T]
+  }
+
+  private[scalanative] object ArraySeqCompatSelect {
+    import ArraySeqCompatDef._
+    object Inner {
+      import scala.collection.immutable._
+      val ArraySeqAlias = ArraySeq
+      type ArraySeqAlias[T] = ArraySeq[T]
+    }
+  }
+  // Vector in Scala 2.12, ArraySeq otherwise
+  val ArraySeqCompat = ArraySeqCompatSelect.Inner.ArraySeqAlias
+  type ArraySeqCompat[T] = ArraySeqCompatSelect.Inner.ArraySeqAlias[T]
+}
