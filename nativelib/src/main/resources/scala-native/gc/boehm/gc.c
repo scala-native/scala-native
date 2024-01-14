4304ed436574cd91703568924d5795c807f4f1c9
Wojciech Mazur <wmazur@virtuslab.com>
2024-01-14 01:41:41 +0100

    fix boehm

--- nativelib/src/main/resources/scala-native/gc/boehm/gc.c
+++ nativelib/src/main/resources/scala-native/gc/boehm/gc.c
@@ -7,7 +7,6 @@
 #include <gc/gc.h>
 #include "shared/ScalaNativeGC.h"
 #include <stdlib.h>
-#include <errno.h>
 #include <string.h>
 #include "shared/Parsing.h"
 
@@ -58,6 +57,7 @@ void scalanative_GC_collect() { GC_gcollect(); }
 
 void scalanative_GC_register_weak_reference_handler(void *handler) {}
 
+#ifdef SCALANATIVE_MULTITHREADING_ENABLED
 #ifdef _WIN32
 HANDLE scalanative_GC_CreateThread(LPSECURITY_ATTRIBUTES threadAttributes,
                                    SIZE_T stackSize, ThreadStartRoutine routine,
@@ -73,6 +73,7 @@ int scalanative_GC_pthread_create(pthread_t *thread, pthread_attr_t *attr,
     return GC_pthread_create(thread, attr, routine, args);
 }
 #endif
+#endif // SCALANATIVE_MULTITHREADING_ENABLED
 
 // ScalaNativeGC interface stubs. Boehm GC relies on STW using signal handlers
 void scalanative_GC_set_mutator_thread_state(GC_MutatorThreadState unused){};
