ee9b1aafc6e54117ffe13f5da9614b8035de914f
Wojciech Mazur <wmazur@virtuslab.com>
2024-01-15 11:23:45 +0100

    fix: Suppress warnings in ScopedVar on Scala 3.4.x

--- util/src/main/scala/scala/scalanative/util/ScopedVar.scala
+++ util/src/main/scala/scala/scalanative/util/ScopedVar.scala
@@ -47,7 +47,9 @@ object ScopedVar {
     try body
     finally stack.reverse.foreach(_.pop())
   }
-  @nowarn("msg=`_` is deprecated for wildcard arguments of types")
+  // @nowarn("msg=The syntax `x: _\\*` is no longer supported for vararg splices")
+  // @nowarn("msg=`_` is deprecated for wildcard arguments of types")
+  @nowarn() // Cannot define multiple @nowarn annottations in Scala 2.12
   def scopedPushIf[T](
       shouldPushAssignments: Boolean
   )(lazyAssignments: => Seq[Assignment[_]])(body: => T): T = {
