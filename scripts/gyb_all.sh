d38a32d307abe6cdc256b4d69f357d36a48c80bc
Wojciech Mazur <wmazur@virtuslab.com>
2024-01-16 11:20:20 +0100

    Use template to define base java nio buffer types

--- scripts/gyb_all.sh
+++ scripts/gyb_all.sh
@@ -39,6 +39,7 @@ gyb_files $nativelib $scala $runtime Arrays Boxes Primitives
 gyb clib/src/main/scala/scala/scalanative/libc/stdatomic.scala.gyb
 gyb clib/src/main/resources/scala-native/stdatomic.c.gyb
 
+gyb $javaNIO/Buffers.scala.gyb
 gyb $javaNIO/HeapBuffers.scala.gyb
 gyb $javaNIO/HeapByteBufferViews.scala.gyb
 gyb $javaNIO/MappedByteBufferViews.scala.gyb
