217dc224cab39514af004bb29af9d4870c50869c
Wojciech Mazur <wmazur@virtuslab.com>
2024-02-14 17:01:24 +0100

    Keep using Scala 3.3.x as default until new release of Scala 2.13

--- project/ScalaVersions.scala
+++ project/ScalaVersions.scala
@@ -21,12 +21,12 @@ object ScalaVersions {
   val crossScala212 = (14 to 18).map(v => s"2.12.$v")
   val crossScala213 = (8 to 12).map(v => s"2.13.$v")
   val crossScala3 = List(
-    // Bug in compiler leads to errors in tests, don't use it as default until RC2
+    // Move it to last entry (default binary version) when Scala 2.13 supports Scala 3.4 TASTy
+    (0 to 0).map(v => s"3.4.$v"),
     // windowslib fails to compile with 3.1.{0-1}
     (2 to 3).map(v => s"3.1.$v"),
     (0 to 2).map(v => s"3.2.$v"),
-    (0 to 2).map(v => s"3.3.$v"),
-    (0 to 0).map(v => s"3.4.$v")
+    (0 to 2).map(v => s"3.3.$v")
   ).flatten
 
   // Scala versions used for publishing libraries
