a9d3e5e67facde68926b2c3de6b884aa1e61020e
Wojciech Mazur <wmazur@virtuslab.com>
2024-02-15 16:57:40 +0100

    Make sure Marker_markRange uses alligned pointers

--- nativelib/src/main/resources/scala-native/gc/immix/Marker.c
+++ nativelib/src/main/resources/scala-native/gc/immix/Marker.c
@@ -128,7 +128,12 @@ NO_SANITIZE static void Marker_markRange(Heap *heap, Stack *stack,
                                          word_t **from, word_t **to) {
     assert(from != NULL);
     assert(to != NULL);
-    for (word_t **current = from; current <= to; current += 1) {
+    // Align start address
+    const intptr_t alignmentMask = ~(sizeof(word_t) - 1);
+    word_t **alignedFrom = (word_t **)((intptr_t)from & alignmentMask);
+    // Align end address to be optionally 1 higher when unalligned
+    word_t **alignedTo = (word_t **)((intptr_t)(to + 1) & alignmentMask);
+    for (word_t **current = alignedFrom; current <= alignedTo; current += 1) {
         word_t *addr = *current;
         if (Heap_IsWordInHeap(heap, addr)) {
             Marker_markConservative(heap, stack, addr);
