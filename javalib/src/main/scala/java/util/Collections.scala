58743d05d6bf65007da7673318e232608a53e3f5
Wojciech Mazur <wmazur@virtuslab.com>
2024-03-05 10:19:37 +0100

    Fix signatures of Array method, taking a generic param with type bounds on Comperable: Java defined types are erased to Object, Scala defined ones are preserved in the generic type

--- javalib/src/main/scala/java/util/Collections.scala
+++ javalib/src/main/scala/java/util/Collections.scala
@@ -67,7 +67,7 @@ object Collections {
   }
 
   // Differs from original type definition, original: [T <: jl.Comparable[_ >: T]]
-  def sort[T <: jl.Comparable[T]](list: List[T]): Unit =
+  def sort[T <: jl._Comparable[T]](list: List[T]): Unit =
     list.sort(null)
 
   def sort[T](list: List[T], c: Comparator[_ >: T]): Unit =
@@ -274,14 +274,14 @@ object Collections {
   }
 
   // Differs from original type definition, original: [T <: jl.Comparable[_ >: T]], returning T
-  def min[T <: AnyRef with jl.Comparable[T]](coll: Collection[_ <: T]): AnyRef =
+  def min[T <: AnyRef with jl._Comparable[T]](coll: Collection[_ <: T]): AnyRef =
     min(coll, Comparator.naturalOrder[T]())
 
   def min[T](coll: Collection[_ <: T], comp: Comparator[_ >: T]): T =
     coll.scalaOps.reduceLeft((a, b) => if (comp.compare(a, b) <= 0) a else b)
 
-  // Differs from original type definition, original: [T <: jl.Comparable[_ >: T]], returning T
-  def max[T <: AnyRef with jl.Comparable[T]](coll: Collection[_ <: T]): AnyRef =
+  // Differs from original type definition, original: [T <: jl.Comparable[_ >: T]], returning 
+  def max[T <: AnyRef with jl._Comparable[T]](coll: Collection[_ <: T]): AnyRef =
     max(coll, Comparator.naturalOrder[T]())
 
   def max[T](coll: Collection[_ <: T], comp: Comparator[_ >: T]): T =
