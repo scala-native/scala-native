5bd3b4706b8294b3703d3c66c32c6007541b8fde
Kirill A. Korinsky <kirill@korins.ky>
2024-03-11 16:48:03 +0100

    feature [posix]: Add bindings to scheduler policy (#3831)

---
+++ posixlib/src/main/resources/scala-native/sched.c
@@ -0,0 +1,44 @@
+#if defined(__unix__) || defined(__unix) || defined(unix) ||                   \
+    (defined(__APPLE__) && defined(__MACH__))
+
+#include <sched.h>
+
+int scalanative_sched_other() { return SCHED_OTHER; }
+
+int scalanative_sched_fifo() { return SCHED_FIFO; }
+
+int scalanative_sched_rr() { return SCHED_RR; }
+
+int scalanative_sched_sporadic() {
+#ifdef SCHED_SPORADIC
+    return SCHED_SPORADIC;
+#else
+    return SCHED_OTHER;
+#endif
+}
+
+int scalanative_sched_batch() {
+#ifdef SCHED_BATCH
+    return SCHED_BATCH;
+#else
+    return SCHED_OTHER;
+#endif
+}
+
+int scalanative_sched_idle() {
+#ifdef SCHED_IDLE
+    return SCHED_IDLE;
+#else
+    return SCHED_OTHER;
+#endif
+}
+
+int scalanative_sched_deadline() {
+#ifdef SCHED_DEADLINE
+    return SCHED_DEADLINE;
+#else
+    return SCHED_OTHER;
+#endif
+}
+
+#endif // Unix or Mac OS
