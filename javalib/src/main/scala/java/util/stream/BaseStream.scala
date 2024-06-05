3dc9334a8835cde881c4f0a8ace591e5d9e5942f
Lee Tibbert <lee.tibbert@gmail.com>
2024-06-04 22:54:04 -0400

    Remove extraneous & distracting comment

--- javalib/src/main/scala/java/util/stream/BaseStream.scala
+++ javalib/src/main/scala/java/util/stream/BaseStream.scala
@@ -3,7 +3,6 @@ package java.util.stream
 import java.util.Iterator
 import java.util.Spliterator
 
-// trait BaseStream[+T, +S <: BaseStream[T, S]] extends AutoCloseable {
 trait BaseStream[T, S <: BaseStream[T, S]] extends AutoCloseable {
   def close(): Unit
   def isParallel(): Boolean
