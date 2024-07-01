8606dcc8c7462dfda62521402feeb1cdd96160ed
Lee Tibbert <lee.tibbert@gmail.com>
2024-07-01 09:40:30 -0400

    Can I reproduce previous failing case

--- unit-tests/shared/src/test/require-jdk21/org/scalanative/testsuite/javalib/lang/NegativeZeroTestOnJDK21.scala
+++ unit-tests/shared/src/test/require-jdk21/org/scalanative/testsuite/javalib/lang/NegativeZeroTestOnJDK21.scala
@@ -10,12 +10,6 @@ import java.lang._
 
 class NegativeZeroTestOnJDK21 {
 
-  // tolerances for IEEE 754 floating point number comparisons.
-  final val epsilonD = 0.00000000d
-  final val epsilonF = 0.0f
-
-  // clamp() Double
-
   @Test def mathMinWithNegativeZero(): Unit = {
 
     val min_A = Math.min(jl.Double.valueOf(-0.0d), 0.0d)
@@ -25,15 +19,15 @@ class NegativeZeroTestOnJDK21 {
       1.0 / min_A == Double.NEGATIVE_INFINITY
     )
 
-    /* Fails on CI, passes Clang 18 & JVM
+    //    /* Fails on CI, passes Clang 18 & JVM */
     val min_B = Math.min(0.0d, jl.Double.valueOf(-0.0d))
 
     assertTrue(
       "wrong min_B, min(0.0D, -0.0D)",
       1.0 / min_B == Double.NEGATIVE_INFINITY
     )
-     */
 
+    // Seems to pass on CI Clang 14, as well as Windows Clang 17
     val min_C = Math.min(jl.Double.valueOf(-0.0d), jl.Double.valueOf(-0.0d))
 
     assertTrue(
