e22c00a7af8fa211d69e6e8935fb95bb51d7beb7
Wojciech Mazur <wmazur@virtuslab.com>
2024-11-14 11:35:29 +0100

    fix[nscplugin] Correct generation of closure with type lambda parameters. (#4083)
    * Fix generation of clouser with type lambda parameters. Also improve closuer codegen by usage of narrowed parameter types (previously any ref type was interpreted as j.l.Object)
    * Require Scala 3.4+ to test issue 4082. Fix generation of "require-*" dirs

---
+++ unit-tests/shared/src/test/require-scala3.4/org/scalanative/testsuite/scala/scalanative/IssuesTestScala3_4.scala
@@ -0,0 +1,12 @@
+package org.scalanatvie.testsuite.scala.scalanative
+
+import org.junit.Test
+import org.junit.Assert._
+
+class IssuesTestScala3_4 {
+  @Test def i4082(): Unit = {
+    // Ensure links
+    def test[I[_], O[_]](f: [C] => (I[C], I[C] => O[C]) => O[C]): Unit = ()
+    assertEquals((), test[List, List]([C] => (input, cont) => List.empty))
+  }
+}
