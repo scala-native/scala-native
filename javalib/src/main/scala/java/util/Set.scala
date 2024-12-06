908ab06974ca6147aa18de0e5bcdaf866060133b
Dimi Racordon <dimitri.racordon@gmail.com>
2024-12-06 13:39:51 +0100

    Reorder method definitions according to the JDK documentation

--- javalib/src/main/scala/java/util/Set.scala
+++ javalib/src/main/scala/java/util/Set.scala
@@ -1,16 +1,18 @@
 package java.util
 
 trait Set[E] extends Collection[E] {
-  def size(): scala.Int
+
   def add(obj: E): scala.Boolean
+
+  def clear(): Unit
   def contains(obj: Any): scala.Boolean
   def containsAll(c: Collection[_]): Boolean
   def isEmpty(): scala.Boolean
   def iterator(): Iterator[E]
-  def clear(): Unit
   def remove(obj: Any): scala.Boolean
   def removeAll(c: Collection[_]): Boolean
   def retainAll(c: Collection[_]): Boolean
+  def size(): scala.Int
 
   // TODO:
   // def addAll(coll: Collection[_ <: E]): scala.Boolean
@@ -28,6 +30,7 @@ trait Set[E] extends Collection[E] {
         Spliterator.DISTINCT
     )
   }
+
 }
 
 object Set {
