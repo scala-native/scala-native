f7adea16d9f1120a40133f76af8f5e53bf63ab38
Wojciech Mazur <wmazur@virtuslab.com>
2024-12-07 13:10:12 +0100

    Fix CLA check

--- scripts/check-cla.sh
+++ scripts/check-cla.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 set -eux
 
-AUTHOR="$1"
+_AUTHOR="$1"
+AUTHOR=$(jq -rn --arg x "${_AUTHOR}" '$x|@uri')
 echo "Pull request submitted by $AUTHOR";
-URL_AUTHOR=$(jq -rn --arg x "$AUTHOR" '$x|@uri')
-signed=$(curl -s "https://www.lightbend.com/contribute/cla/scala/check/$URL_AUTHOR" | jq -r ".signed");
+signed=$(curl -L -s "https://contribute.akka.io/contribute/cla/scala/check/$AUTHOR" | jq -r ".signed");
 if [ "$signed" = "true" ] ; then
   echo "CLA check for $AUTHOR successful";
 else
   echo "CLA check for $AUTHOR failed";
-  echo "Please sign the Scala CLA to contribute to Scala Native";
-  echo "Go to https://www.lightbend.com/contribute/cla/scala and then";
+  echo "Please sign the Scala CLA to contribute to Scala Native.";
+  echo "Go to https://contribute.akka.io/contribute/cla/scala and then";
   echo "comment on the pull request to ask for a new check.";
   echo "";
-  echo "Check if CLA is signed: https://www.lightbend.com/contribute/cla/scala/check/$AUTHOR";
+  echo "Check if CLA is signed: https://contribute.akka.io/contribute/cla/scala/check/$AUTHOR";
   exit 1;
 fi;
