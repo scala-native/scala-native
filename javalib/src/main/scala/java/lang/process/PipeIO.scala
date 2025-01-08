82d23ad6ae86327498b526109b8e77e4bae51d8f
Lee Tibbert <lee.tibbert@gmail.com>
2025-01-07 23:47:01 -0500

    Add overload in attempt to please Windows; may have test runner source corruption

--- javalib/src/main/scala/java/lang/process/PipeIO.scala
+++ javalib/src/main/scala/java/lang/process/PipeIO.scala
@@ -1,18 +1,25 @@
 package java.lang.process
 
 import java.io._
-import scala.annotation.tailrec
+
 import scala.scalanative.annotation.stub
-import scala.scalanative.unsafe._
 import scala.scalanative.posix.sys.ioctl._
 import scala.scalanative.meta.LinktimeInfo.isWindows
+
+import scala.scalanative.unsafe._
+import scala.scalanative.unsigned._
+
 import scala.scalanative.windows.DWord
 import scala.scalanative.windows.NamedPipeApi.PeekNamedPipe
-import scala.scalanative.unsigned._
-/*
- * Used to set up input/output streams for a child process spawned by
+
+/* Used to set up input/output streams for a child process spawned by
  * a ProcessBuilder using the Redirect info for the stream.
  */
+
+/* Design Note:
+ *   TBS
+ */
+
 private[lang] final class PipeIO[T](
     val nullStream: T,
     val fdStream: (GenericProcess, FileDescriptor) => T
@@ -31,60 +38,94 @@ private[lang] object PipeIO {
         ioStream.nullStream
     }
   }
+
   trait Stream extends InputStream {
     def process: GenericProcess
     def drain(): Unit = {}
   }
+
   class StreamImpl(val process: GenericProcess, is: FileInputStream)
-      extends BufferedInputStream(is)
-      with Stream {
-    override def available() = try {
-      super.available() match {
-        // Check the FileInputStream in case the BufferedInputStream hasn't been filled yet.
-        case 0 =>
-          in.available() match {
-            case 0 if !drained => availableFD()
-            case a             => a
+      extends Stream {
+
+    private var src: InputStream = is
+
+    // By convention, caller is synchronized on 'src'.
+    private def availableUnSync() = {
+      src match {
+        case fis: FileInputStream => availableFD()
+        case _                    => src.available()
+      }
+    }
+
+    override def available(): Int = src.synchronized {
+      try
+        availableUnSync()
+      finally process.checkResult()
+    }
+
+    override def read(): Int = src.synchronized {
+      try
+        src.read()
+      finally process.checkResult()
+    }
+
+    override def read(b: Array[Byte]): Int = this.read(b, 0, b.length)
+
+    override def read(buf: Array[scala.Byte], offset: Int, len: Int): Int =
+      src.synchronized {
+        /* The read(buf, offset, len) within this method will always check
+         * arguments, so save a few cycles and do not do check here.
+         */
+
+        try {
+          val avail = availableUnSync()
+
+          if (avail > 0) {
+            src.read(buf, offset, avail)
+          } else {
+            src match {
+              case fis: FileInputStream =>
+                val nRead = src.read(buf, offset, 1)
+
+                if (nRead == -1) -1
+                else {
+                  val a = availableUnSync()
+
+                  if (a == 0) nRead
+                  else {
+                    val newOffset = offset + 1
+                    val availableBuffer = buf.length - newOffset
+                    if (availableBuffer <= 0) nRead
+                    else {
+                      val nToRead = Math.min(availableBuffer, a)
+                      val nr =
+                        if (nToRead == 0) 0
+                        else src.read(buf, newOffset, nToRead)
+                      if (nr <= 0) nRead else nRead + nr
+                    }
+                  }
+                }
+
+              case _ => -1 // EOF
+
+            }
           }
-        case a => a
-      }
-    } finally process.checkResult()
-
-    override def read(): Int = synchronized {
-      // Retry read if failed due to termination of the process (file descriptor would become invalidated)
-      // In such case output would be drained and underlying input stream would be replaced with fully read content
-      try super.read()
-      catch { case _: IOException if drained => super.read() }
-      finally process.checkResult()
-    }
-    override def read(buf: Array[scala.Byte], offset: Int, len: Int) =
-      try super.read(buf, offset, len)
-      catch { case _: IOException if drained => super.read(buf, offset, len) }
-      finally process.checkResult()
-
-    override def drain() = synchronized {
-      @tailrec
-      def loop(readBuf: Array[scala.Byte]): Array[Byte] = {
-        if (drained) readBuf
-        else {
-          val toRead = availableFD()
-          if (toRead > 0) {
-            val size = readBuf.size
-            val newBuf = java.util.Arrays.copyOf(readBuf, size + toRead)
-            is.read(newBuf, size, toRead)
-            loop(newBuf)
-          } else readBuf
-        }
+        } finally process.checkResult()
       }
 
-      val readBuf = loop(Array.emptyByteArray)
-      drained = true
-      is.close()
-      this.in = new ByteArrayInputStream(readBuf)
+    /* Switch horses, or at least InputStreams in media res.
+     * See Design Note at top of file.
+     */
+    override def drain(): Unit = src.synchronized {
+
+      val newSrc = new ByteArrayInputStream(src.readNBytes(availableUnSync()))
+
+      src.close() // release JVM FileDescriptor and, especially, its OS fd.
+
+      src = newSrc
     }
 
-    @volatile private var drained = false
-    private def availableFD() = {
+    private def availableFD(): Int = {
       if (isWindows) {
         val availableTotal = stackalloc[DWord]()
         val hasPeaked = PeekNamedPipe(
@@ -127,6 +168,7 @@ private[lang] object PipeIO {
     override def read(): Int = 0
     override def read(buf: Array[scala.Byte], offset: Int, len: Int) = -1
   }
+
   private object NullOutput extends OutputStream {
     override def close(): Unit = {}
     override def write(b: Int): Unit = {}
