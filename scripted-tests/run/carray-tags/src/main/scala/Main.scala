97cf50d29ae689eb16eade3f14bc78f157c11a61
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2025-01-09 15:14:27 +0100

    Fix Scala 3 syntax in run/carray-tags

--- scripted-tests/run/carray-tags/src/main/scala/Main.scala
+++ scripted-tests/run/carray-tags/src/main/scala/Main.scala
@@ -2,7 +2,7 @@ import scala.scalanative.unsafe._
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val carr = stackalloc[CArray[Int, Nat.Digit2[Nat._1, Nat._2]]]
+    val carr = stackalloc[CArray[Int, Nat.Digit2[Nat._1, Nat._2]]]()
     val obtained = carr.length
     val expected = 12
     assert(obtained == expected, s"expected $expected, got $obtained")
