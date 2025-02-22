4e7312acfa3cd1c7201815bfb5fbdba53ed285c5
Lee Tibbert <lee.tibbert@gmail.com>
2025-02-22 08:28:52 -0500

    Port java.io CharArrayReader & CharArrayWriter from Scala.js

---
+++ unit-tests/shared/src/test/scala/org/scalanative/testsuite/javalib/io/CharArrayReaderTest.scala
@@ -0,0 +1,182 @@
+// Ported from Scala.js, commit: c473689, dated 2021-05-06
+
+package org.scalanative.testsuite.javalib.io
+
+import java.io._
+import org.junit.Assert._
+import org.junit.Test
+
+import org.scalanative.testsuite.utils.AssertThrows.assertThrows
+
+class CharArrayReaderTest {
+  private val hw: Array[Char] =
+    Array('H', 'e', 'l', 'l', 'o', 'W', 'o', 'r', 'l', 'd')
+
+  private def withClose[T <: AutoCloseable](
+      closeable: T
+  )(fn: T => Unit): Unit = {
+    fn(closeable)
+
+    if (closeable != null)
+      closeable.close()
+  }
+
+  @Test def ctorCharArray(): Unit = {
+    withClose(new CharArrayReader(hw)) { cr =>
+      assertTrue("Failed to create reader", cr.ready())
+    }
+  }
+
+  @Test def ctorCharArrayWithOffsetAndLength(): Unit = {
+    // CharArrayReader for "Worl"
+    withClose(new CharArrayReader(hw, 5, 4)) { cr =>
+      assertTrue("Failed to create reader", cr.ready())
+      val c = new Array[Char](10)
+      // Doesn't read past length & starts at offset
+      val bytesRead: Int = cr.read(c, 0, 10)
+      assertEquals(4, bytesRead)
+      assertArrayEquals(
+        "Worl".toCharArray,
+        java.util.Arrays.copyOf(c, bytesRead)
+      )
+      assertEquals(-1, cr.read())
+      assertFalse(cr.ready())
+    }
+  }
+
+  @Test def ctorCharArrayWithOffsetAndLargerLength(): Unit = {
+    // CharArrayReader for "World"
+    withClose(new CharArrayReader(hw, 5, 100)) { cr =>
+      assertTrue("Failed to create reader", cr.ready())
+      val c = new Array[Char](100)
+      // Doesn't read past buffer length
+      val bytesRead: Int = cr.read(c, 0, 100)
+      assertEquals(5, bytesRead)
+      assertArrayEquals(
+        "World".toCharArray,
+        java.util.Arrays.copyOf(c, bytesRead)
+      )
+      assertEquals(-1, cr.read())
+      assertFalse(cr.ready())
+    }
+  }
+
+  @Test def readThrowsIOExceptionAfterClose(): Unit = {
+    withClose(new CharArrayReader(hw)) { cr =>
+      cr.close()
+      assertThrows(classOf[IOException], cr.read())
+    }
+  }
+
+  @Test def markSupported(): Unit = {
+    withClose(new CharArrayReader(hw)) { cr =>
+      assertTrue("markSupported returned false", cr.markSupported)
+    }
+  }
+
+  @Test def readChar(): Unit = {
+    withClose(new CharArrayReader(hw)) { cr =>
+      assertEquals("Read returned incorrect char", 'H', cr.read())
+    }
+
+    withClose(new CharArrayReader(Array[Char]('\u8765'))) { cr =>
+      assertEquals("Incorrect double byte char", '\u8765', cr.read())
+    }
+  }
+
+  @Test def readCharArray(): Unit = {
+    withClose(new CharArrayReader(hw)) { cr =>
+      val c = new Array[Char](11)
+      cr.read(c, 1, 10)
+      assertArrayEquals(Array(0.toChar) ++ hw, c)
+    }
+  }
+
+  @Test def readyThrowsIOException(): Unit = {
+    withClose(new CharArrayReader(hw)) { cr =>
+      assertTrue("ready returned false", cr.ready())
+      cr.skip(1000L)
+      assertTrue("ready returned true", !cr.ready())
+      cr.close()
+
+      assertThrows(classOf[IOException], cr.ready())
+    }
+
+    withClose(new CharArrayReader(hw)) { cr =>
+      cr.close()
+      assertThrows(classOf[IOException], cr.ready())
+    }
+  }
+
+  @Test def resetAfterMark(): Unit = {
+    withClose(new CharArrayReader(hw)) { cr =>
+      cr.skip(5L)
+      // Mark current position
+      cr.mark(100)
+      assertEquals("Reset failed to return to marker position", 'W', cr.read())
+      // Reset back  to 'W'
+      cr.reset()
+      assertEquals("Reset failed to return to marker position", 'W', cr.read())
+    }
+  }
+
+  @Test def markLimitMinusOneIgnored(): Unit = {
+    withClose(new CharArrayReader(hw)) { cr =>
+      cr.skip(5L)
+      // Mark current position
+      cr.mark(-1)
+      val c = new Array[Char](5)
+      cr.read(c, 0, 5)
+      assertArrayEquals(
+        "Mark read limit should be ignored",
+        "World".toCharArray,
+        c
+      )
+
+      // Reset back  to 'W'
+      cr.reset()
+      assertEquals("Reset failed to return to marker position", 'W', cr.read())
+    }
+  }
+
+  @Test def skipNegative(): Unit = {
+    withClose(new CharArrayReader(hw)) { cr =>
+      assertEquals("Negative skip values should return zero", 0, cr.skip(-1L))
+    }
+  }
+
+  @Test def skipZero(): Unit = {
+    withClose(new CharArrayReader(hw)) { cr =>
+      assertEquals("Zero skip value should return zero", 0, cr.skip(0L))
+    }
+  }
+
+  @Test def skipReturnsArraySizeWhenGreater(): Unit = {
+    withClose(new CharArrayReader(hw)) { cr =>
+      assertEquals("Skip didn't return array size", 10L, cr.skip(1000L))
+    }
+  }
+
+  @Test def resetFromOffsetReader(): Unit = {
+    val data = "offsetHello world!".toCharArray
+    val offsetLength = 6
+    val length = data.length - offsetLength
+
+    withClose(new CharArrayReader(data, offsetLength, length)) { cr =>
+      cr.reset()
+      for (i <- 0 until length) {
+        assertEquals(data(offsetLength + i), cr.read().toChar)
+      }
+      // Verify EOF
+      assertEquals(-1, cr.read())
+    }
+  }
+
+  @Test def skip(): Unit = {
+    withClose(new CharArrayReader(hw)) { cr =>
+      val skipped = cr.skip(5L)
+      assertEquals("Failed to skip correct number of chars", 5L, skipped)
+      assertEquals("Skip skipped wrong chars", 'W', cr.read())
+    }
+  }
+}
---
+++ unit-tests/shared/src/test/scala/org/scalanative/testsuite/javalib/io/CharArrayWriterTest.scala
@@ -0,0 +1,158 @@
+// Ported from Scala.js, commit: c473689, dated 2021-05-06
+
+package org.scalanative.testsuite.javalib.io
+
+import java.io.{CharArrayWriter, StringWriter}
+import org.junit.Assert._
+import org.junit.Test
+
+import org.scalanative.testsuite.utils.AssertThrows.assertThrows
+
+class CharArrayWriterTest {
+  private val hw: Array[Char] =
+    Array('H', 'e', 'l', 'l', 'o', 'W', 'o', 'r', 'l', 'd')
+
+  private def withClose[T <: AutoCloseable](
+      closeable: T
+  )(fn: T => Unit): Unit = {
+    fn(closeable)
+
+    if (closeable != null)
+      closeable.close()
+  }
+
+  @Test def ctorSize: Unit = {
+    withClose(new CharArrayWriter(90)) { cw =>
+      assertEquals(0, cw.size)
+    }
+  }
+
+  @Test def ctorSizeNegativeThrows(): Unit = {
+    assertThrows(classOf[IllegalArgumentException], new CharArrayWriter(-1))
+  }
+
+  @Test def ctorSizeDefault(): Unit = {
+    withClose(new CharArrayWriter) { cw =>
+      assertEquals(0, cw.size)
+    }
+  }
+
+  @Test def close(): Unit = {
+    withClose(new CharArrayWriter) { cw =>
+      cw.close()
+      cw.close() // no-op
+      assertEquals(0, cw.size)
+    }
+  }
+
+  @Test def flush(): Unit = {
+    withClose(new CharArrayWriter) { cw =>
+      cw.flush()
+    }
+  }
+
+  @Test def reset(): Unit = {
+    withClose(new CharArrayWriter) { cw =>
+      cw.write("HelloWorld", 5, 5)
+      cw.reset()
+      cw.write("HelloWorld", 0, 5)
+
+      assertArrayEquals("Hello".toCharArray, cw.toCharArray)
+    }
+  }
+
+  @Test def size(): Unit = {
+    withClose(new CharArrayWriter) { cw =>
+      assertEquals(0, cw.size)
+      cw.write(hw, 5, 5)
+      assertEquals(5, cw.size)
+    }
+  }
+
+  @Test def toCharArray(): Unit = {
+    withClose(new CharArrayWriter) { cw =>
+      cw.write("HelloWorld", 0, 10)
+      assertArrayEquals("HelloWorld".toCharArray, cw.toCharArray)
+    }
+  }
+
+  @Test def testToString(): Unit = {
+    withClose(new CharArrayWriter) { cw =>
+      cw.write("HelloWorld", 5, 5)
+      assertEquals("World", cw.toString)
+    }
+  }
+
+  @Test def writeSubArrayToCharArray(): Unit = {
+    withClose(new CharArrayWriter) { cw =>
+      cw.write(hw, 5, 5)
+      assertEquals("World", cw.toString)
+      assertArrayEquals("World".toCharArray, cw.toCharArray)
+    }
+  }
+
+  @Test def throwsIndexOutOfBoundsException(): Unit = {
+    withClose(new CharArrayWriter) { obj =>
+      assertThrows(
+        classOf[IndexOutOfBoundsException],
+        obj.write(Array[Char]('0'), 0, -1)
+      )
+    }
+  }
+
+  @Test def writeChar(): Unit = {
+    withClose(new CharArrayWriter) { cw =>
+      cw.write('T')
+      assertEquals("T", cw.toString)
+      assertArrayEquals(Array('T'), cw.toCharArray)
+    }
+  }
+
+  @Test def writeString(): Unit = {
+    withClose(new CharArrayWriter) { cw =>
+      cw.write("HelloWorld", 5, 5)
+      assertEquals("World", cw.toString)
+      assertArrayEquals("World".toCharArray, cw.toCharArray)
+    }
+  }
+
+  @Test def writeToStringWriter(): Unit = {
+    withClose(new CharArrayWriter) { cw =>
+      cw.write("HelloWorld", 0, 10)
+      withClose(new StringWriter) { sw =>
+        cw.writeTo(sw)
+        assertEquals("HelloWorld", sw.toString)
+      }
+    }
+  }
+
+  @Test def appendChar(): Unit = {
+    withClose(new CharArrayWriter(10)) { cw =>
+      val testChar = ' '
+      cw.append(testChar)
+      cw.flush()
+      assertEquals(String.valueOf(testChar), cw.toString)
+      assertArrayEquals(Array(testChar), cw.toCharArray)
+    }
+  }
+
+  @Test def appendCharSequence(): Unit = {
+    withClose(new CharArrayWriter(10)) { cw =>
+      val testString: CharSequence = "My Test String"
+      cw.append(testString)
+      cw.flush()
+      assertEquals("My Test String", cw.toString)
+      assertArrayEquals("My Test String".toCharArray, cw.toCharArray)
+    }
+  }
+
+  @Test def appendCharSequenceWithOffset(): Unit = {
+    withClose(new CharArrayWriter(10)) { cw =>
+      val testString: String = "My Test String"
+      cw.append(testString, 1, 3)
+      cw.flush()
+      assertEquals(testString.substring(1, 3), cw.toString)
+      assertArrayEquals(testString.substring(1, 3).toCharArray, cw.toCharArray)
+    }
+  }
+}
