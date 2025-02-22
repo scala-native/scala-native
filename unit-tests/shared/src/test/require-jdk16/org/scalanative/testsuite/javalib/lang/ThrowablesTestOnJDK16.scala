7f1e47e00a13cd03f4e893e2bea85f22d5858c9b
LeeTibbert <LeeTibbert@users.noreply.github.com>
2025-02-22 11:11:18 -0500

    Add two JDK 9 & 16 java.lang IndexOutOfBoundsExceptions (#4239)

---
+++ unit-tests/shared/src/test/require-jdk16/org/scalanative/testsuite/javalib/lang/ThrowablesTestOnJDK16.scala
@@ -0,0 +1,24 @@
+package org.scalanative.testsuite.javalib.lang
+
+import java.{lang => jl}
+import java.util.Objects
+
+import org.junit.Test
+import org.junit.Assert._
+
+import org.scalanative.testsuite.utils.AssertThrows.assertThrows
+
+class ThrowablesTestOnJDK16 {
+
+  @Test def indexOutOfBoundsException_Long(): Unit = {
+    val ioobExc = new IndexOutOfBoundsException(jl.Long.MAX_VALUE)
+    Objects.requireNonNull(ioobExc, "Long index")
+
+    // Check Exception message visually, since can rightly vary across JVMs.
+    assertThrows(
+      "IndexOutOfBounds Long",
+      classOf[java.lang.IndexOutOfBoundsException],
+      throw ioobExc
+    )
+  }
+}
