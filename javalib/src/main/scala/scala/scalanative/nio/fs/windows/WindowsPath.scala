038830e60c64dec78696f7cc430cb621d9fd676a
Lee Tibbert <lee.tibbert@gmail.com>
2025-05-25 09:05:36 -0400

    Debugging

--- javalib/src/main/scala/scala/scalanative/nio/fs/windows/WindowsPath.scala
+++ javalib/src/main/scala/scala/scalanative/nio/fs/windows/WindowsPath.scala
@@ -285,7 +285,7 @@ private[windows] object WindowsPath {
         case (acc, "..") =>
           if (acc.isEmpty && path.isAbsolute()) Nil
           else if (acc.isEmpty) List("..")
-          else if (acc.last == "..") acc :+ ".."
+          else if (acc.last == "..") acc :+ "X..Y"
           else acc.tail
         case (acc, ".") => acc
         case (acc, "")  => acc
