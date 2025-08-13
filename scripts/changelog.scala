ff050999fcde22b3a670e1719ee41e7654a4c1ed
Scala Steward <scala_steward@virtuslab.com>
2025-08-13 11:23:47 +0000

    Update github-api to 1.329

--- scripts/changelog.scala
+++ scripts/changelog.scala
@@ -1,5 +1,5 @@
 // Based on Ammonite script created by Tomasz Godzik in scalameta/metals https://github.com/scalameta/metals/commits/main/bin/merged_prs.sc
-//> using dep org.kohsuke:github-api:1.327
+//> using dep org.kohsuke:github-api:1.329
 //> using toolkit latest
 
 import scala.collection.mutable.ListBuffer
