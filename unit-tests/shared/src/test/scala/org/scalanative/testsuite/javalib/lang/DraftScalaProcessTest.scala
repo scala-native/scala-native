05ebf4ad894eac62bb5319154b084e291bf0582f
Lee Tibbert <lee.tibbert@gmail.com>
2025-09-09 17:43:40 -0400

    Try using lineStream

--- unit-tests/shared/src/test/scala/org/scalanative/testsuite/javalib/lang/DraftScalaProcessTest.scala
+++ unit-tests/shared/src/test/scala/org/scalanative/testsuite/javalib/lang/DraftScalaProcessTest.scala
@@ -169,7 +169,7 @@ class DraftScalaProcessTest {
       }
   }
 
-  // @Ignore  // Coal face
+  @Ignore // Appears to pass on Windows Scala 3 & 2.13
   @Test def testScalaString_0_D(): Unit = {
     /* This is testScalaString_0_B modified to use
      * a Process class overload which is closer to the Issue reproducer.
@@ -209,6 +209,48 @@ class DraftScalaProcessTest {
       if (Platform.isWindows) {
         // Sometimes Success is best revealed by Failure.
         fail("Expected case 0_D: Make it evident that Windows process exited")
+      }
+  }
+
+  // @Ignore  // Coal face
+  @Test def testScalaString_0_E(): Unit = {
+    /* This is testScalaString_0_E modified to use
+     * a deprecated lineStream() rather than lazyLines().
+     * The !! method appears to use a lineStream.
+     * Do things still work?
+     */
+
+    val argv = Seq("git", "init", "-b", "main")
+    val cwd: Option[File] = None
+
+    val proc = sys.process.Process(argv, None)
+
+    // Lee: Careful lazyLines is Scala 2.13 & 3 only.
+    //      Will fail to compile in CI for Scala 2.12, but the other
+    //      cases are may tell me something.
+
+    // Will throw Exception if process exits with error code.
+    val response = proc.lineStream
+
+    response
+      .take(1)
+      .foreach(x => {
+        assertTrue(
+          s"foreach: '${x}'",
+          (x.startsWith("Initialized empty Git repository in") ||
+            x.startsWith("Reinitialized existing Git repository in"))
+        )
+      })
+
+    /* Contorted DEBUG logic ahead. Focus attention on Windows SN case.
+     * If the process is exiting correctly, it should always get to the
+     * fail(), that is overall Success. If process hangs, then a
+     * successful Windows CI run is really failure.
+     */
+    if (!Platform.executingInJVM)
+      if (Platform.isWindows) {
+        // Sometimes Success is best revealed by Failure.
+        fail("Expected case 0_E: Make it evident that Windows process exited")
       }
   }
 
