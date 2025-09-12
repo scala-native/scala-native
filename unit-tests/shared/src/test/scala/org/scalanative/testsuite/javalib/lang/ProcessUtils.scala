1bdccc993ba4478f851e181837bee539f89ffc28
Lee Tibbert <lee.tibbert@gmail.com>
2025-09-12 07:52:37 -0400

    For debug, increase Windows 'echo' exit timeout

--- unit-tests/shared/src/test/scala/org/scalanative/testsuite/javalib/lang/ProcessUtils.scala
+++ unit-tests/shared/src/test/scala/org/scalanative/testsuite/javalib/lang/ProcessUtils.scala
@@ -70,7 +70,9 @@ object ProcessUtils {
     // completion time.  If a process expected to exit in milliseconds
     // takes that three orders of magnitude longer, it must be reported.
 
-    val tmo = 10
+    // Debug
+    val tmo = if (isWindows) 60
+              else 10
     val tmUnit = TimeUnit.SECONDS
 
     assertTrue(
