fb99ae3581b4faa08f2989489ff65bbb91b7fc9f
Wojciech Mazur <wmazur@virtuslab.com>
2025-09-15 19:17:48 +0200

    Add scala.package to interflow/Allowlist

--- tools/src/main/scala/scala/scalanative/interflow/Allowlist.scala
+++ tools/src/main/scala/scala/scalanative/interflow/Allowlist.scala
@@ -54,6 +54,7 @@ private[scalanative] object Allowlist {
   val pure = {
     val out = mutable.Set.empty[nir.Global]
     out += nir.Global.Top("scala.Predef$")
+    out += nir.Global.Top("scala.package$")
     out += nir.Global.Top("scala.runtime.BoxesRunTime$")
     out += nir.Global.Top("scala.scalanative.runtime.Boxes$")
     out += nir.Global.Top("scala.scalanative.runtime.package$")
