f9767cb42eca00e50d1cce44da09c3eba3369a4f
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2025-10-04 08:18:16 -0700

    scalafmt: remove excluded paths

--- scalalib/overrides-2.12/scala/collection/immutable/VM.scala
+++ scalalib/overrides-2.12/scala/collection/immutable/VM.scala
@@ -5,6 +5,7 @@ import scala.scalanative.libc.stdatomic.memory_order._
 
 // Backport from scala.runtime moved into s.c.immutable and made package
 // private to avoid the need for MiMa allowlisting.
-/* private[immutable] */ object VM {
+/* private[immutable] */
+object VM {
   def releaseFence(): Unit = atomic_thread_fence(memory_order_release)
 }
