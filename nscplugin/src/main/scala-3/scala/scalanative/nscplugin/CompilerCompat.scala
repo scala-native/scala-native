89579035eac361ad03c9f6a52cff55b06b13b074
Wojciech Mazur <wmazur@virtuslab.com>
2025-10-19 02:53:37 +0200

    Adjust AdaptLazyVals for usage of VarHandles in Scala 3.8

--- nscplugin/src/main/scala-3/scala/scalanative/nscplugin/CompilerCompat.scala
+++ nscplugin/src/main/scala-3/scala/scalanative/nscplugin/CompilerCompat.scala
@@ -1,15 +1,30 @@
 package scala.scalanative.nscplugin
 
+import dotty.tools.dotc.core.NameKinds
+
 object CompilerCompat {
 
   private object SymUtilsCompatDef:
     val SymUtils = dotty.tools.dotc.core.Symbols
 
   private object SymUtilsCompatSelect:
-    import SymUtilsCompatDef._
+    import SymUtilsCompatDef.*
     object Inner {
-      import dotty.tools.dotc.transform._
+      import dotty.tools.dotc.transform.*
       val SymUtilsAlias = SymUtils
     }
   val SymUtilsCompat = SymUtilsCompatSelect.Inner.SymUtilsAlias
+
+  // NamesKinds.LazyVarHandleName, introduced in 3.8.0
+  private object LazyValHandleNameCompatDef:
+    val LazyVarHandleName = NameKinds.SuffixNameKind(-1, "", "")
+  private object LazyValHandleNameCompatSelect:
+    import LazyValHandleNameCompatDef.*
+    object Inner {
+      // provides access to NameKind.LazyVarHandleName in 3.8.0 or uses dummy definition otherwise
+      import NameKinds.*
+      val LazyValHandleNameCompat = LazyVarHandleName
+    }
+  val LazyValHandleNameCompat =
+    LazyValHandleNameCompatSelect.Inner.LazyValHandleNameCompat
 }
