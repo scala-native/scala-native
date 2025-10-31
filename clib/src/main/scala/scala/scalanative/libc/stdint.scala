ee2a610ccf0e50699f2e2f1cf59dbda54292bc77
Lee Tibbert <lee.tibbert@gmail.com>
2025-10-30 22:45:57 -0400

    Implement and use two libc stdint fields

--- clib/src/main/scala/scala/scalanative/libc/stdint.scala
+++ clib/src/main/scala/scala/scalanative/libc/stdint.scala
@@ -4,10 +4,14 @@ import scala.scalanative.unsafe._
 
 @extern object stdint extends stdint
 
+// A very partial implementation. May it grow to completeness with time.
+
 @extern private[scalanative] trait stdint {
   // intmax_t and uintmax_t are not always equivalent to `long long`,
   // but they are usually `long long` in common data models.
   type intmax_t = CLongLong
   type uintmax_t = CUnsignedLongLong
 
+  type intptr_t = CLongLong
+  type uintptr_t = CUnsignedLongLong
 }
