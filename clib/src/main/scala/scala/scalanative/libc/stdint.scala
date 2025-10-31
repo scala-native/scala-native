d79a328554816414df7f475c58cf42b88c7b9e2b
Lee Tibbert <lee.tibbert@gmail.com>
2025-10-31 11:17:19 -0400

    Implement and use two libc stdint fields

--- clib/src/main/scala/scala/scalanative/libc/stdint.scala
+++ clib/src/main/scala/scala/scalanative/libc/stdint.scala
@@ -4,10 +4,26 @@ import scala.scalanative.unsafe._
 
 @extern object stdint extends stdint
 
+// A very partial implementation. May it grow to completeness with time.
+
 @extern private[scalanative] trait stdint {
   // intmax_t and uintmax_t are not always equivalent to `long long`,
   // but they are usually `long long` in common data models.
   type intmax_t = CLongLong
   type uintmax_t = CUnsignedLongLong
 
+  /* Assume & implement a data model where the size of a C/C++ pointer is the
+   * same as the maximum size of a C/C++ data object.
+   * 
+   * C and C++ do not __require__ this match but it is empirically true
+   * for the most common data models. This is similar to the intmax_t/uintmax_t
+   * assumption above.
+   *
+   * Reference:
+   *   https://en.wikipedia.org/wiki/64-bit_computing#64-bit_data_models
+   *   Retrieved 2025-10-31
+   */
+
+  type intptr_t = CSSize
+  type uintptr_t = CSize
 }
