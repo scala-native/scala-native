5556616feb18653c393ca1994f3a8141304fedba
Lee Tibbert <lee.tibbert@gmail.com>
2025-11-09 14:17:48 -0500

    remove non-POSIX pollEvents blivet

--- posixlib/src/main/scala/scala/scalanative/posix/poll.scala
+++ posixlib/src/main/scala/scala/scalanative/posix/poll.scala
@@ -149,49 +149,6 @@ object poll {
   def POLLNVAL: CInt = extern // Invalid polling request
 }
 
-/* pollEvents is not POSIX but is scaffolding until UnixProcessGen2
- * becomes available for change.
- */
-@extern
-@define("__SCALANATIVE_POSIX_POLL")
-object pollEvents {
-
-  @name("scalanative_pollin")
-  def POLLIN: CInt = extern // Data ready to be read
-
-  @name("scalanative_pollpri")
-  def POLLPRI: CInt = extern // Urgent data ready to be read
-
-  @name("scalanative_pollout")
-  def POLLOUT: CInt = extern // Writing now will not block
-
-  // XOpen events
-
-  @name("scalanative_pollrdnorm")
-  def POLLRDNORM: CInt = extern // Normal data may be read
-
-  @name("scalanative_pollrdband")
-  def POLLRDBAND: CInt = extern // Priority data may be read
-
-  @name("scalanative_pollwrnorm")
-  def POLLWRNORM: CInt = extern // Writing now will not block
-
-  @name("scalanative_pollwrband")
-  def POLLWRBAND: CInt = extern // Priority data may be written
-
-  // Always checked in revents
-
-  @name("scalanative_pollerr")
-  def POLLERR: CInt = extern // Error condition
-
-  @name("scalanative_pollhup")
-  def POLLHUP: CInt = extern // Hung up
-
-  @name("scalanative_pollnval")
-  def POLLNVAL: CInt = extern // Invalid polling request
-
-}
-
 object pollOps {
   import poll._
 
