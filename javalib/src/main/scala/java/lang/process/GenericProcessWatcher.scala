8bd28cb0d6b58494ccd911885af373ee2a61acff
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2025-11-10 15:46:14 -0800

    GenericProcessWatcher: move most to nested object
    Move processes field, watcher thread and some methods to a nested object
    so that we both encapsulate this functionality and also delay thread
    initialization without using `lazy`.
    
    This will allow us to add another "lazy" field to the Processes object
    later on.

--- javalib/src/main/scala/java/lang/process/GenericProcessWatcher.scala
+++ javalib/src/main/scala/java/lang/process/GenericProcessWatcher.scala
@@ -11,59 +11,76 @@ private[process] object GenericProcessWatcher {
 
   import ju.concurrent._
 
-  private val processes = new ConcurrentHashMap[jl.Long, GenericProcessHandle]
-
-  private val lock = new locks.ReentrantLock()
-  private val hasProcessesToWatch = lock.newCondition()
-
-  def watchForTermination(handle: GenericProcessHandle): Unit = {
-    processes.put(handle.pid(), handle)
-    assert(
-      watcherThread.isAlive(),
-      "GenericProcessWatcher watch thread is terminated"
-    )
-    // If we cannot lock it means that watcher thread is already executing, no need to wake it up
-    if (lock.tryLock()) {
-      try hasProcessesToWatch.signal()
-      finally lock.unlock()
-    }
-  }
-
-  private lazy val watcherThread: Thread = Thread
-    .ofPlatform()
-    .daemon(true)
-    .group(ThreadGroup.System)
-    .name("ScalaNative-GenericProcessWatcher")
-    .startInternal { () =>
-      lock.lock()
-      try {
-        while (true) Try {
-          removeProcessesIf(_.hasExited)
-          while (processes.isEmpty())
-            hasProcessesToWatch.await()
-          if (!reapSomeProcesses()) Thread.sleep(100) // ms
-        }
-      } finally lock.unlock()
-    }
-
-  // return true if something has been reaped
-  private val reapSomeProcesses: () => Boolean =
-    if (LinktimeInfo.isWindows) claimAllCompleted
-    else if (UnixProcess.useGen2) claimAllCompleted
-    else UnixProcessGen1.waitpidAny
+  @alwaysinline
+  def watchForTermination(handle: GenericProcessHandle): Unit =
+    Processes.add(handle)
 
   @alwaysinline
-  def claimAllCompleted(): Boolean =
-    removeProcessesIf(_.checkIfExited())
+  def completeWith(pid: Long)(ec: => Int): Boolean =
+    Processes.complete(pid, ec)
 
-  def completeWith(pid: Long)(ec: => Int): Boolean = {
-    val ref = processes.remove(pid)
-    (ref ne null) && ref.setCachedExitCode(ec)
+  private object Processes {
+    private val processes = new ConcurrentHashMap[jl.Long, GenericProcessHandle]
+
+    private val lock = new locks.ReentrantLock()
+    private val todo: locks.Condition = lock.newCondition()
+
+    def add(handle: GenericProcessHandle): Unit = {
+      processes.put(handle.pid(), handle)
+      signal()
+    }
+
+    protected final def signal(): Unit = {
+      assert(
+        watcherThread.isAlive(),
+        "GenericProcessWatcher watch thread is terminated"
+      )
+      // If we cannot lock it means that watcher thread is already executing, no need to wake it up
+      if (lock.tryLock())
+        try todo.signal()
+        finally lock.unlock()
+    }
+
+    private final val watcherThread: Thread = Thread
+      .ofPlatform()
+      .daemon(true)
+      .group(ThreadGroup.System)
+      .name("ScalaNative-GenericProcessWatcher")
+      .startInternal(Task)
+
+    private object Task extends Runnable {
+      override def run(): Unit = {
+        lock.lock()
+        try {
+          while (true) Try {
+            removeProcessesIf(_.hasExited)
+            while (processes.isEmpty()) todo.await()
+            if (!reapSomeProcesses()) Thread.sleep(100) // ms
+          }
+        } finally
+          lock.unlock()
+      }
+    }
+
+    // return true if something has been reaped
+    private val reapSomeProcesses: () => Boolean =
+      if (LinktimeInfo.isWindows) claimAllCompleted
+      else if (UnixProcess.useGen2) claimAllCompleted
+      else UnixProcessGen1.waitpidAny
+
+    @alwaysinline
+    def claimAllCompleted(): Boolean =
+      removeProcessesIf(_.checkIfExited())
+
+    def complete(pid: Long, ec: => Int): Boolean = {
+      val ref = processes.remove(pid)
+      (ref ne null) && ref.setCachedExitCode(ec)
+    }
+
+    @alwaysinline
+    def removeProcessesIf(f: GenericProcessHandle => Boolean): Boolean =
+      removeIf(processes.values())(f)
   }
-
-  @alwaysinline
-  def removeProcessesIf(f: GenericProcessHandle => Boolean): Boolean =
-    removeIf(processes.values())(f)
 
   private def removeIf[A](coll: ju.Collection[A])(f: A => Boolean): Boolean = {
     var ok = false
