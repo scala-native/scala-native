1c5aaa1e0bc863b25fbceae590cac03d212d8747
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2025-11-11 06:15:10 -0800

    UnixProcessGen2: use atomic to close pidfd
    We must not close pidfd twice since by the time we try it again, that
    file descriptor may have already been re-used and assigned to another,
    unrelated reource.

---
+++ javalib/src/main/scala/java/lang/process/UnixFileDescriptorAtomic.scala
@@ -0,0 +1,32 @@
+package java.lang.process
+
+import java.util.concurrent.atomic.AtomicInteger
+
+import scala.scalanative.annotation.alwaysinline
+import scala.scalanative.posix.unistd
+
+class UnixFileDescriptorAtomic(val value: AtomicInteger) extends AnyVal {
+
+  @alwaysinline def get(): Int = value.get()
+
+  @alwaysinline def release(): Int = value.getAndSet(-1)
+
+  @alwaysinline def close(): Unit = {
+    val prev = release()
+    if (prev != -1) unistd.close(prev)
+  }
+
+}
+
+object UnixFileDescriptorAtomic {
+
+  @alwaysinline
+  def apply(value: AtomicInteger): UnixFileDescriptorAtomic =
+    new UnixFileDescriptorAtomic(value)
+
+  @alwaysinline
+  def apply(value: Int): UnixFileDescriptorAtomic = apply(
+    new AtomicInteger(value)
+  )
+
+}
