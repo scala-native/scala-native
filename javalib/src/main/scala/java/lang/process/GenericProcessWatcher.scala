009655a96a7017327498c5054854877b4ed7dd6e
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2025-11-11 15:22:33 -0800

    GenericProcessWatcher: avoid missing a new process
    Use a custom implementation of AbstractQueuedSynchronizer (the same type
    Semaphore uses), to indicate a binary state (0 stands for empty/closed,
    1 for non-empty/opened).
    
    Unlike with a ReentrantLock, this approach doesn't require "holding" it
    before signalling, thus avoiding a problematic race condition between
    realizing processes.isEmpty and then trying to await a condition which
    may not have been signaled as the lock was unavailable at that moment.

--- javalib/src/main/scala/java/lang/process/GenericProcessWatcher.scala
+++ javalib/src/main/scala/java/lang/process/GenericProcessWatcher.scala
@@ -12,8 +12,36 @@ private[process] object GenericProcessWatcher {
 
   private val processes = new ConcurrentHashMap[jl.Long, GenericProcessHandle]
 
-  private val lock = new locks.ReentrantLock()
-  private val hasProcessesToWatch = lock.newCondition()
+  /* Custom "mutex" to make sure we don't try to reap until there are processes
+   * to wait for.
+   * - release() is called anytime a process is added, and will mark this open
+   * - acquire() is called ONLY if processes was empty:
+   *   - if it's closed, blocks until it's marked open (a process is added)
+   *   - if it's open, close it and check if processes is empty again
+   * Between processes.isEmpty and processesMutex.acquire, several scenarios
+   * could play out:
+   * - processes isn't empty: we proceed to reaping
+   *   - the state of processesMutex is irrelevant here, although it
+   *     is likely open since it's opened anytime a process is added,
+   *     and not closed until processes is empty;
+   * - processes is empty but processesMutex is open:
+   *   - the check is repeated after processesMutex is closed
+   * - processes is empty and processesMutex is closed:
+   *   - acquire() blocks until released
+   *   - it could be released even before acquire() is called, if a
+   *     process is added during the gap between these two calls
+   *   - or it could be released after acquire() is called
+   */
+  private val processesMutex = new locks.AbstractQueuedSynchronizer {
+    // initial state is 0, for "closed"
+    // we don't need the "Shared" versions as we'll have only one waiter
+    override protected final def tryRelease(releases: Int): Boolean =
+      compareAndSetState(0, 1) // if was closed, open it, unblock waiters
+    override protected final def tryAcquire(releases: Int): Boolean =
+      // called only if processes is empty
+      // see more detailed explanation below, where acquire() is called
+      compareAndSetState(1, 0) // if was open, close it, else block
+  }
 
   def watchForTermination(handle: GenericProcessHandle): Unit = {
     processes.put(handle.pid(), handle)
@@ -21,11 +49,7 @@ private[process] object GenericProcessWatcher {
       watcherThread.isAlive(),
       "GenericProcessWatcher watch thread is terminated"
     )
-    // If we cannot lock it means that watcher thread is already executing, no need to wake it up
-    if (lock.tryLock()) {
-      try hasProcessesToWatch.signal()
-      finally lock.unlock()
-    }
+    processesMutex.release(1)
   }
 
   private lazy val watcherThread: Thread = Thread
@@ -34,15 +58,11 @@ private[process] object GenericProcessWatcher {
     .group(ThreadGroup.System)
     .name("ScalaNative-GenericProcessWatcher")
     .startInternal { () =>
-      lock.lock()
-      try {
-        while (true) Try {
-          removeCompleted()
-          while (processes.isEmpty())
-            hasProcessesToWatch.await()
-          if (!reapSomeProcesses()) Thread.sleep(100) // ms
-        }
-      } finally lock.unlock()
+      while (true) Try {
+        removeCompleted()
+        while (processes.isEmpty()) processesMutex.acquire(1)
+        if (!reapSomeProcesses()) Thread.sleep(100) // ms
+      }
     }
 
   // return true if something has been reaped
