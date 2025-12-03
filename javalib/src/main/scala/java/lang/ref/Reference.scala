7b3b3ee06f262f6232eacd0668fff07f266b28db
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2025-12-03 13:50:24 -0800

    [javalib] ReferenceQueue: add dequeue, use in poll, remove (#4693)
    * ReferenceQueue: add dequeue, use in poll, remove
    
    * Reference: rename misleading `dequeue` method

--- javalib/src/main/scala/java/lang/ref/Reference.scala
+++ javalib/src/main/scala/java/lang/ref/Reference.scala
@@ -5,5 +5,5 @@ abstract class Reference[T](private var referent: T) {
   def clear(): Unit = referent = null.asInstanceOf[T]
   def isEnqueued(): Boolean = false
   def enqueue(): Boolean = false
-  private[ref] def dequeue(): Reference[T] = this
+  private[ref] def markDequeued(): Unit = ()
 }
