8165a63cf33b97daa2f463b530f35b648b3d7393
Wojciech Mazur <wmazur@virtuslab.com>
2025-12-08 19:50:19 +0100

    Support Scala 2.12.21 (#4708)
    * Add Scala 2.12.21 to the build
    
    * Add Scala partest configs
    
    * Add missing EOF blank lines

--- project/ScalaVersions.scala
+++ project/ScalaVersions.scala
@@ -18,7 +18,7 @@ package build
 
 object ScalaVersions {
   // Versions of Scala used for publishing compiler plugins
-  val crossScala212 = crossScalaVersions("2.12", 14 to 20)
+  val crossScala212 = crossScalaVersions("2.12", 14 to 21)
   val crossScala213 = crossScalaVersions("2.13", 8 to 18)
   val crossScala3 = List(
     extraCrossScalaVersion("3.").toList,
