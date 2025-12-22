98cb7cd208a143fd309938f619d1ed634a1d08de
Lee Tibbert <lee.tibbert@gmail.com>
2025-12-22 15:00:20 -0500

    Reduce complexity; backout ProcessTestOnJDK9; needs a new PR

--- unit-tests/shared/src/test/require-jdk9/org/scalanative/testsuite/javalib/lang/ProcessTestOnJDK9.scala
+++ unit-tests/shared/src/test/require-jdk9/org/scalanative/testsuite/javalib/lang/ProcessTestOnJDK9.scala
@@ -23,8 +23,9 @@ import Platform._
 
 object ProcessTestOnJDK9 {
   @BeforeClass
-  def checkRuntime(): Unit =
+  def checkRuntime(): Unit = {
     assumeTrue(isMultithreadingEnabled)
+  }
 }
 
 class ProcessTestOnJDK9 {
@@ -151,16 +152,13 @@ class ProcessTestOnJDK9 {
   }
 
   private def runPingWith(redirect: ProcessBuilder.Redirect): String = {
-    // Send one packet to IPv4 localhost and return child output.
-    val countOption = if (Platform.isWindows) "-n" else "-c"
-    val argv = Seq("ping", countOption, "1", "127.0.0.1")
-
+    val argv =
+      if (Platform.isWindows) Seq("ping", "-n", "2", "127.0.0.1")
+      else Seq("ping", "-c", "2", "-i", "10", "127.0.0.1")
     val proc: Process =
       processForCommand(argv: _*).redirectOutput(redirect).start()
-
     val stdout =
       new String(proc.getInputStream.readAllBytes(), StandardCharsets.UTF_8)
-
     proc.waitFor()
     stdout
   }
