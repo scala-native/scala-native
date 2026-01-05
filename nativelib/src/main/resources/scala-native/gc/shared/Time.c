a71826db1f6700fc2cc3483669943f0ce514c59c
Wojciech Mazur <wmazur@virtuslab.com>
2026-01-05 11:52:36 +0100

    Fix compilation on Windows - win_freq.h is a javalib header

--- nativelib/src/main/resources/scala-native/gc/shared/Time.c
+++ nativelib/src/main/resources/scala-native/gc/shared/Time.c
@@ -9,6 +9,24 @@
 #if defined(_WIN32)
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+
+// Cached frequency value for QueryPerformanceCounter
+static int winFreqQuadPart(int *quad) {
+    static int winFreqQuadPartValue = 0;
+    int retval = 1; // assume ok for caching
+    // check if cache is set
+    if (winFreqQuadPartValue == 0) {
+        LARGE_INTEGER freq;
+        retval = QueryPerformanceFrequency(&freq);
+        if (retval != 0) {
+            // set cache value
+            winFreqQuadPartValue = freq.QuadPart;
+        }
+    }
+    // assign cache value or default 0 on failure
+    *quad = winFreqQuadPartValue;
+    return retval;
+}
 #endif
 
 long long Time_current_millis(void) {
