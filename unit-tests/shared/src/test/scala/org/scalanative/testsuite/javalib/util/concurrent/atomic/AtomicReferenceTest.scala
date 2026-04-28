67e5eec6619864b6b8a05907f84a90be88f38d7a
虎鸣 <hepin.p@alibaba-inc.com>
2026-04-29 05:17:40 +0800

    Complete AtomicReference JSR166 TCK

--- unit-tests/shared/src/test/scala/org/scalanative/testsuite/javalib/util/concurrent/atomic/AtomicReferenceTest.scala
+++ unit-tests/shared/src/test/scala/org/scalanative/testsuite/javalib/util/concurrent/atomic/AtomicReferenceTest.scala
@@ -9,8 +9,10 @@ import java.util.function.UnaryOperator
 import org.junit.Assert._
 import org.junit.{Ignore, Test}
 
-@Ignore
-class AtomicReferenceTest {
+import org.scalanative.testsuite.javalib.util.concurrent.JSR166Test
+
+class AtomicReferenceTest extends JSR166Test {
+  import JSR166Test._
 
   // This test suite is INCOMPLETE (obviously!).
   //
@@ -79,6 +81,88 @@ class AtomicReferenceTest {
       s"result after function: ${result} != expected: ${expected}",
       result == expected
     )
+  }
+
+  @Test def testConstructor(): Unit = {
+    val ai = new AtomicReference[Integer](one)
+    assertSame(one, ai.get())
+  }
+
+  @Test def testConstructor2(): Unit = {
+    val ai = new AtomicReference[Integer]()
+    assertNull(ai.get())
+  }
+
+  @Test def testGetSet(): Unit = {
+    val ai = new AtomicReference[Integer](one)
+    assertSame(one, ai.get())
+    ai.set(two)
+    assertSame(two, ai.get())
+    ai.set(m3)
+    assertSame(m3, ai.get())
+  }
+
+  @Test def testGetLazySet(): Unit = {
+    val ai = new AtomicReference[Integer](one)
+    assertSame(one, ai.get())
+    ai.lazySet(two)
+    assertSame(two, ai.get())
+    ai.lazySet(m3)
+    assertSame(m3, ai.get())
+  }
+
+  @Test def testCompareAndSet(): Unit = {
+    val ai = new AtomicReference[Integer](one)
+    assertTrue(ai.compareAndSet(one, two))
+    assertTrue(ai.compareAndSet(two, m4))
+    assertSame(m4, ai.get())
+    assertFalse(ai.compareAndSet(m5, seven))
+    assertSame(m4, ai.get())
+    assertTrue(ai.compareAndSet(m4, seven))
+    assertSame(seven, ai.get())
+  }
+
+  @throws[Exception]
+  @Test def testCompareAndSetInMultipleThreads(): Unit = {
+    val ai = new AtomicReference[Integer](one)
+    val t = new Thread(new CheckedRunnable() {
+      override def realRun(): Unit = {
+        while (!ai.compareAndSet(two, three)) Thread.`yield`()
+      }
+    })
+
+    t.start()
+    assertTrue(ai.compareAndSet(one, two))
+    t.join(LONG_DELAY_MS)
+    assertFalse(t.isAlive())
+    assertSame(three, ai.get())
+  }
+
+  @deprecated
+  @Test def testWeakCompareAndSet(): Unit = {
+    val ai = new AtomicReference[Integer](one)
+    while (!ai.weakCompareAndSet(one, two)) ()
+    while (!ai.weakCompareAndSet(two, m4)) ()
+    assertSame(m4, ai.get())
+    while (!ai.weakCompareAndSet(m4, seven)) ()
+    assertSame(seven, ai.get())
+  }
+
+  @Test def testGetAndSet(): Unit = {
+    val ai = new AtomicReference[Integer](one)
+    assertSame(one, ai.getAndSet(zero))
+    assertSame(zero, ai.getAndSet(m10))
+    assertSame(m10, ai.getAndSet(one))
+  }
+
+  @Ignore("No ObjectInputStreams in Scala Native")
+  @Test def testSerialization(): Unit = ()
+
+  @Test def testToString(): Unit = {
+    val ai = new AtomicReference[Integer](one)
+    assertEquals(one.toString(), ai.toString())
+    ai.set(two)
+    assertEquals(two.toString(), ai.toString())
   }
 
 }
