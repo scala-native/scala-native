5979771fdceb29dbe94bff40ba5b9efcd44493cf
虎鸣 <hepin.p@alibaba-inc.com>
2026-04-29 12:33:39 +0800

    Run JSR166 Map TCK for Hashtable

--- unit-tests/shared/src/test/scala/org/scalanative/testsuite/javalib/util/HashtableTest.scala
+++ unit-tests/shared/src/test/scala/org/scalanative/testsuite/javalib/util/HashtableTest.scala
@@ -1,16 +1,37 @@
 package org.scalanative.testsuite.javalib.util
 
 import java.util._
+import java.{util => ju}
+
+import scala.reflect.ClassTag
 
 import org.junit.Test
 
 import org.scalanative.testsuite.utils.AssertThrows.assertThrows
 
-class HashtableTest {
+class HashtableTest extends MapTest {
+  // Runs the public-domain JSR166 generic Map TCK for Hashtable.
+  def factory: HashtableFactory = new HashtableFactory
 
   @Test def putOnNullKeyOrValue(): Unit = {
     val t = new Hashtable[AnyRef, AnyRef]()
     assertThrows(classOf[NullPointerException], t.put(null, "value"))
     assertThrows(classOf[NullPointerException], t.put("key", null))
   }
+}
+
+class HashtableFactory extends MapFactory {
+  override def implementationName: String =
+    "java.util.Hashtable"
+
+  override def empty[K: ClassTag, V: ClassTag]: ju.Hashtable[K, V] =
+    new ju.Hashtable[K, V]
+
+  override def allowsNullKeys: Boolean = false
+  override def allowsNullValues: Boolean = false
+
+  override def allowsNullKeysQueries: Boolean = false
+  override def allowsNullValuesQueries: Boolean = false
+
+  override def isConcurrent: Boolean = true
 }
