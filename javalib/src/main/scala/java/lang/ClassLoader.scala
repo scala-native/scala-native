c1ddcaa4034a34e0a6389c45269972c2109b7468
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2026-05-10 12:13:04 +0200

    Fix `ClassLoader#getResourceAsStream` JVM compatibility

--- javalib/src/main/scala/java/lang/ClassLoader.scala
+++ javalib/src/main/scala/java/lang/ClassLoader.scala
@@ -3,7 +3,8 @@ package java.lang
 class ClassLoader protected (parent: ClassLoader) {
   def this() = this(null)
   def getResourceAsStream(name: String): java.io.InputStream =
-    this.getClass().getResourceAsStream(name)
+    if (name.startsWith("/")) null
+    else this.getClass().getResourceAsStream("/" + name)
   def getUnnamedModule(): Module = new Module(null, this)
 }
 
