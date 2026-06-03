7db51b9fac62885d18d276c458f5b747b8e42d3c
Wojciech Mazur <wmazur@virtuslab.com>
2026-06-04 01:29:00 +0200

    [sbt-plugin] Fix publishing libraries under sbt 2.0 (#4941)
    * Fix publishing libraries under sbt 2.0
    
    * Fix failing tests and do cleanups
    
    * scalafmt

--- sbt-scala-native/src/main/scala-sbt-2/scala/scalanative/sbtplugin/PluginCompat.scala
+++ sbt-scala-native/src/main/scala-sbt-2/scala/scalanative/sbtplugin/PluginCompat.scala
@@ -79,7 +79,14 @@ private[scalanative] object PluginCompat:
     toNioPaths(cp).map(_.toFile())
 
   val sbtVersionBaseSettings = Seq[Setting[?]](
-    Keys.platform := ScalaNativePlatform.current
+    Keys.platform := ScalaNativePlatform.current,
+    // On sbt 2, `platform` encodes the Native suffix; use Scala `CrossVersion.binary`
+    // only (not ScalaNativeCrossVersion.binary) to avoid doubling native0.5 in ids.
+    // Redefines with sbt default implementation
+    Keys.crossVersion := {
+      if Keys.crossPaths.value then CrossVersion.binary
+      else CrossVersion.disabled
+    }
   )
 
   val incrementalTestSettings: Seq[Setting[?]] =
