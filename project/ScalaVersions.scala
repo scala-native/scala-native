9f8100bfe1712e28a7253d13674ac6dd55b77235
Wojciech Mazur <wmazur@virtuslab.com>
2026-06-09 14:55:06 +0200

    Add Scala 3.9.0-RC1 to the build

--- project/ScalaVersions.scala
+++ project/ScalaVersions.scala
@@ -38,7 +38,7 @@ object ScalaVersions {
 
   // Tested in scheduled nightly CI to check compiler plugins
   // List maintains only upcoming releases, removed from the list after reaching stable status
-  lazy val scala3RCVersions = List("3.3.8-RC2")
+  lazy val scala3RCVersions = List("3.3.8-RC2", "3.9.0-RC1")
 
   // List of nightly versions can be found here: https://repo.scala-lang.org/ui/native/maven-nightlies/org/scala-lang/scala3-compiler_3
   // or check outputs of `scala -O --version -S 3.nightly`
