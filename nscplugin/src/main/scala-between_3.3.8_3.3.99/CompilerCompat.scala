aea46cf1a6d42d94d7b42196ba7dcf365a69d42d
Wojciech Mazur <wmazur@virtuslab.com>
2026-06-11 18:39:50 +0200

    Support Scala 3.3.8 LTS

---
+++ nscplugin/src/main/scala-between_3.3.8_3.3.99/CompilerCompat.scala
@@ -0,0 +1,20 @@
+package scala.scalanative.nscplugin
+
+import dotty.tools.dotc.ast.tpd.Tree
+import dotty.tools.dotc.core.Contexts.Context
+import dotty.tools.dotc.core.Symbols.Symbol
+import dotty.tools.dotc.util.ReadOnlyMap
+object CompilerCompat {
+  val SymUtils = dotty.tools.dotc.core.Symbols
+  val SymbolExtensions =
+    dotty.tools.backend.jvm.DottyBackendInterface.symExtensions
+  abstract class ScalaPrimitives(ctx: Context)
+      extends dotty.tools.backend.jvm.DottyPrimitives(ctx) {
+    private given Context = ctx
+    protected def nirPrimitives: ReadOnlyMap[Symbol, Int]
+
+    override def isPrimitive(tree: Tree): Boolean =
+      nirPrimitives.contains(tree.symbol) || super.isPrimitive(tree)
+  }
+  val LazyValHandleName = Some(dotty.tools.dotc.core.NameKinds.LazyVarHandleName)
+}
