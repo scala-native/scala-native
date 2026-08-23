83d25d460b33c29960e6f78d11fe7c4ee3e8e985
Scala Steward <scala_steward@virtuslab.com>
2026-08-23 23:27:01 +0000

    Update scala3-library to 3.9.0-RC6

--- project/ScalaVersions.scala
+++ project/ScalaVersions.scala
@@ -38,7 +38,7 @@ object ScalaVersions {
 
   // Tested in scheduled nightly CI to check compiler plugins
   // List maintains only upcoming releases, removed from the list after reaching stable status
-  lazy val scala3RCVersions = List("3.9.0-RC3")
+  lazy val scala3RCVersions = List("3.9.0-RC6")
 
   // List of nightly versions can be found here: https://repo.scala-lang.org/ui/native/maven-nightlies/org/scala-lang/scala3-compiler_3
   // or check outputs of `scala -O --version -S 3.nightly`
