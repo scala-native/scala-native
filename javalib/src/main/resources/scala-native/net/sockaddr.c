306998ad5317a9623a6a986e6ef031d4e36c6fbf
Wojciech Mazur <wmazur@virtuslab.com>
2026-09-11 21:38:23 +0200

    Fix setting port in riscv server - caused by invalid handling on unsigned integer

---
+++ javalib/src/main/resources/scala-native/net/sockaddr.c
@@ -0,0 +1,19 @@
+#if defined(SCALANATIVE_COMPILE_ALWAYS) ||                                     \
+    defined(__SCALANATIVE_JAVALIB_SOCKET_HELPERS)
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <winsock2.h>
+#else
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#endif
+#include <stdint.h>
+
+void scalanative_sockaddr_in_set_port(void *addr, uint32_t port) {
+    ((struct sockaddr_in *)addr)->sin_port = htons((uint16_t)port);
+}
+
+void scalanative_sockaddr_in6_set_port(void *addr, uint32_t port) {
+    ((struct sockaddr_in6 *)addr)->sin6_port = htons((uint16_t)port);
+}
+#endif
