9e7ae43d973c16529fc5aaa5b60358a315b7e746
Wojciech Mazur <wmazur@virtuslab.com>
2026-09-24 20:54:52 +0200

    [runtime] Give string constants C linkage so C++ can reference them
    MSVC mangles global variables, so eh.cpp looked for
    ?snFatalErrorPrefix@@3QEBDEB while string_constants.c defines the plain
    C symbol. Itanium ABI targets do not mangle globals, which is why this
    only broke on Windows.

--- nativelib/src/main/resources/scala-native/string_constants.h
+++ nativelib/src/main/resources/scala-native/string_constants.h
@@ -4,7 +4,15 @@
 #define SN_ERROR_MSG(msg) "ScalaNative: " msg
 #define SN_FATAL_ERROR_MSG(msg) "ScalaNative Fatal Error: " msg
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern const char *const snErrorPrefix;
 extern const char *const snFatalErrorPrefix;
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // CONSTANTS_H
